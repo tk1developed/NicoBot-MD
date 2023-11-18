b53658c284ae57319fa3921d54e01490c3527753
Daxmax123 <145521972+Daxmax123@users.noreply.github.com>
2023-11-18 17:23:23 -0500

    Update owner-actualizacion.js

--- plugins/owner-actualizacion.js
+++ plugins/owner-actualizacion.js
@@ -4,8 +4,8 @@ import axios from 'axios';
 let previousCommitSHA = '';
 let previousUpdatedAt = '';
 let previousCommitUser = ''; 
-const owner = 'BrunoSobrino';
-const repo = 'TheMystic-Bot-MD';
+const owner = 'Daxmax123';
+const repo = 'shinonome-bot';
 const handler = async (m, {conn, text, usedPrefix, command}) => {
  conn.sendMessage(m.chat, {text: `*[❗] Comando activado con éxito, te notificaremos cuando haya algun cambio en el repositorio oficial.*`}, {quoted: m});  
 try {
