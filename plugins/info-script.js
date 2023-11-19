e27355af650b457429a0bf0bf5190de631522cde
Daxmax123 <145521972+Daxmax123@users.noreply.github.com>
2023-11-19 15:57:09 -0500

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -1,7 +1,7 @@
 import moment from 'moment-timezone';
 import fetch from 'node-fetch';
 const handler = async (m, { conn, args }) => {
-   const res = await fetch('https://api.github.com/repos/BrunoSobrino/TheMystic-Bot-MD');
+   const res = await fetch('https://api.github.com/repos/Daxmax123/shinonome-bot');
    const json = await res.json();
    let txt = `           *乂  B O T  -  S C R I P T  乂*\n\n`;
       txt += `◦  *Nombre:* ${json?.name || '❥ᰰຼ ⃟ᬽ៸𝕊𝕙𝕚𝕟𝕠𝕟𝕠𝕞𝕖-𝔹𝕠𝕥༒★»'}\n`;
