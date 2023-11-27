5f043e712dfb521b2e2200be14c6beb850a22f03
Daxmax123 <145521972+Daxmax123@users.noreply.github.com>
2023-11-27 09:13:29 -0500

    Update info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -3,7 +3,7 @@ const handler = async (m, {conn, usedPrefix}) => {
   const document = doc[Math.floor(Math.random() * doc.length)];
   const text = `*𝙷𝙾𝙻𝙰 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 👋🏻, 𝚃𝙴 𝙸𝙽𝚅𝙸𝚃𝙾 𝙰 𝚄𝙽𝙸𝚁𝚃𝙴 𝙰 𝙻𝙾𝚂 𝙶𝚁𝚄𝙿𝙾𝚂 𝙾𝙵𝙸𝙲𝙸𝙰𝙻𝙴𝚂 𝙳𝙴 ◤✞ 𝖄𝖊𝕵𝖔𝖙𝖆 𝕸𝕯 ✞◥ 𝙿𝙰𝚁𝙰 𝙲𝙾𝙽𝚅𝙸𝚅𝙸𝚁 𝙲𝙾𝙽 𝙻𝙰 𝙲𝙾𝙼𝚄𝙽𝙸𝙳𝙰𝙳 :D*
 
-*➤ ꧁༺ 𝓖𝓻𝓾𝓹𝓸𝓼 𝓭𝓮𝓵 𝓫𝓸𝓽 ༻꧂:*
+*➤ ꧁༺ Grupos del Bot ༻꧂:*
 *1.-* https://chat.whatsapp.com/BEUrPhS6Dzk7fKJJU08S5y
 
 *2.-* https://chat.whatsapp.com/KXjS9CBRF680GBf8fiXDeu`.trim();
