37dcd444ed5fc07fca4ca73358a2e1402e6fa46c
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2023-12-21 16:32:50 -0500

    Actualizar owner-restart.js

--- plugins/owner-restart.js
+++ plugins/owner-restart.js
@@ -1,7 +1,7 @@
 const handler = async (m, {conn, isROwner, text}) => {
   if (!process.send) throw 'Dont: node main.js\nDo: node index.js';
     // conn.readMessages([m.key])
-    await m.reply('*[ ⚠ ] Reiniciando el Bot...*\n\n*—◉ Espere un momento para volver a usar el Bot, puede tomar unos minutos.*');
+    await m.reply('*[ ⚠ ] Reiniciando el Bot...*\n\n*—◉ Espera Que El Bot Se Reinicie Para Poderlo Usar Correctamente.*');
     process.send('reset');
 };
 handler.help = ['restart'];
