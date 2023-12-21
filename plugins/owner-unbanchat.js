f7396dea44589a2d024d6fe7faef4b47b5d0e700
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2023-12-21 16:31:20 -0500

    Actualizar owner-unbanchat.js

--- plugins/owner-unbanchat.js
+++ plugins/owner-unbanchat.js
@@ -1,6 +1,6 @@
 const handler = async (m) => {
   global.db.data.chats[m.chat].isBanned = false;
-  m.reply('*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚂𝚃𝙴 𝙲𝙷𝙰𝚃 𝙵𝚄𝙴 𝙳𝙴𝚂𝙱𝙰𝙽𝙴𝙰𝙳𝙾 𝙲𝙾𝙽 𝙴𝚇𝙸𝚃𝙾*');
+  m.reply('*👊Esᴛᴇ Cʜᴀᴛ Yᴀ Esᴛᴀ Dᴇsʙᴀɴᴇᴀᴅᴏ, Pᴜᴇᴅᴇɴ Usᴀʀᴍᴇ*');
 };
 handler.help = ['unbanchat'];
 handler.tags = ['owner'];
