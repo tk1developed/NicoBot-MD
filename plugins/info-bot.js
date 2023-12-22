3f002572212e92bd378c953c538f28696ab26dde
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2023-12-21 19:50:30 -0500

    Actualizar info-bot.js

--- plugins/info-bot.js
+++ plugins/info-bot.js
@@ -8,7 +8,7 @@ handler.all = async function(m) {
   // const estiloaudio = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: {"audioMessage": { "mimetype":"audio/ogg; codecs=opus", "seconds": "99569", "ptt": "true"}}}
   if (/^bot$/i.test(m.text) && !chat.isBanned) {
     conn.sendPresenceUpdate('recording', m.chat);
-    await m.reply(`*(=◍․̫◍=)/ ♥ Hola, en que lo puedo ayudar (=◍․̫◍=)?*`);
+    await m.reply(`*🦁Hola Bebé Soy SakuraBot, Creado Por Diego, En Que Puedo Ayudarte?*`);
     m.conn.sendMessage(m.chat, {audio: {url: vn}, fileName: 'error.mp3', mimetype: 'audio/mpeg', ptt: true}, {quoted: m});
     // conn.sendFile(m.chat, vn, 'bot.mp3', null, m, true, { type: 'audioMessage', seconds: '4556', ptt: true, sendEphemeral: true, quoted: m })
   }
