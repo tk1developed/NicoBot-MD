e7e6947a4618c885a0455bda6c4e854581ac241e
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2023-12-22 19:56:03 -0500

    Actualizar menu-menu.js

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -21,8 +21,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
     const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
     const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
     const document = doc[Math.floor(Math.random() * doc.length)];
-    const str = `╭꒱≡≡≡≡≡╡.｡❅*⋆⍋*∞*｡❅*⋆⍋*∞*｡╞≡≡≡≡♡
-┊    ❥ᰰຼ ⃟ᬽ៸𝕊𝔸𝕂𝕌ℝ𝔸𝔹𝕆𝕋-𝕄𝔻༒★»
+    const str = `╭꒱≡≡≡≡≡╡ ⃟ᬽ៸𝕊𝔸𝕂𝕌ℝ𝔸𝔹𝕆𝕋-𝕄𝔻༒★»╞≡≡≡≡♡
 ┊-----------➫┇◦✦◦✦◦✦◦✦◦┋:̖́-
 ┊🦁 *𝗛ola, ${taguser}*
 ┊➪𖤍❐𖤍❐𖤍❐𖤍❐𖤍❐𖤍✎
