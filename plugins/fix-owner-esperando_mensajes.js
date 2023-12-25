8cc3af0cf752c2a7fc99feeb455e3ad59694af25
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2023-12-25 10:54:43 -0500

    Actualizar fix-owner-esperando_mensajes.js

--- plugins/fix-owner-esperando_mensajes.js
+++ plugins/fix-owner-esperando_mensajes.js
@@ -7,11 +7,11 @@ const handler = async (m, { conn, usedPrefix }) => {
   if (global.conn.user.jid !== conn.user.jid) {
     return conn.sendMessage(m.chat, {text: '*😆Tɪᴇɴᴇs Qᴜᴇ Usᴀʀʟᴏ Eɴ Eʟ Nᴜᴍᴇʀᴏ Pʀɪɴᴄɪᴘᴀʟ Dᴇʟ Bᴏᴛ.*'}, {quoted: m});
   }
-  await conn.sendMessage(m.chat, {text: '*[❗] Iniciando proceso de eliminación de todos los archivos de sesión, excepto el archivo creds.json...*'}, {quoted: m});
+  await conn.sendMessage(m.chat, {text: '*🌿 Iɴɪᴄɪᴀɴᴅᴏ Eʟ Pʀᴏᴄᴇsᴏ Dᴇ Eʟɪᴍɪɴᴀᴄɪᴏ́ɴ Dᴇ Tᴏᴅᴏs Lᴏs Aʀᴄʜɪᴠᴏs Dᴇ Sᴇssɪᴏɴ, Exᴇᴄᴛᴏ Eʟ Aʀᴄʜɪᴠᴏ creds.json...*'}, {quoted: m});
   const sessionPath = './ShinoSession/';
   try {
     if (!existsSync(sessionPath)) {
-      return await conn.sendMessage(m.chat, {text: '*[❗] La carpeta SakuraBotSession no existe o está vacía.*'}, {quoted: m});
+      return await conn.sendMessage(m.chat, {text: '*👊 Lᴀ Cᴀʀᴘᴇᴛᴀ SakuraBotSession no existe o está vacía.*'}, {quoted: m});
     }
     const files = await fs.readdir(sessionPath);
     let filesDeleted = 0;
@@ -22,15 +22,15 @@ const handler = async (m, { conn, usedPrefix }) => {
       }
     }
     if (filesDeleted === 0) {
-      await conn.sendMessage(m.chat, {text: '*[❗] No se encontró ningún archivo para eliminar en la carpeta SakuraBotSession.*'}, {quoted: m});
+      await conn.sendMessage(m.chat, {text: '*🍁 Nᴏ Sᴇ Eɴᴄᴏɴᴛʀᴏ Eʟ Cᴀʀᴄʜɪᴠᴏ Pᴀʀᴀ Eʟɪᴍɪɴᴀʀ Lᴀ Cᴀʀᴘᴇᴛᴀ SakuraBotSession.*'}, {quoted: m});
     } else {
-      await conn.sendMessage(m.chat, {text: `*[❗] Se eliminaron ${filesDeleted} archivos de sesión, excepto el archivo creds.json.*`}, {quoted: m});
+      await conn.sendMessage(m.chat, {text: `*🌸 Sᴇ Eʟɪᴍɪɴᴀʀᴏɴ ${filesDeleted} Aʀᴄʜɪᴠᴏᴢ Dᴇ Sᴇssɪᴏɴ, Esxᴇᴘᴛᴏ Eʟ Aʀᴄʜɪᴠᴏ creds.json.*`}, {quoted: m});
     }
   } catch (err) {
     console.error('Error al leer la carpeta o los archivos de sesión:', err);
-    await conn.sendMessage(m.chat, {text: '*[❗] Ocurrió un error al eliminar los archivos de sesión.*'}, {quoted: m});
+    await conn.sendMessage(m.chat, {text: '*🔰 Oᴄᴜʀʀɪᴏ Uɴ Pʀᴏʙʟᴇᴍᴀ Aʟ Eʟɪᴍɪɴᴀʀ Lᴏs Aʀᴄʜɪᴠᴏᴢ Dᴇ Sᴇssɪᴏɴ.*'}, {quoted: m});
   }
-  await conn.sendMessage(m.chat, {text: `*👋 ¡Hola! Ahora me ves?*\n\n*[❗] Si el Bot no le responde a sus comandos por favor haga un pequeño spam*\n\n*—◉ Ejemplo:*\n${usedPrefix}s\n${usedPrefix}s\n${usedPrefix}s`}, {quoted: m});
+  await conn.sendMessage(m.chat, {text: `*👋 ¡Hola! Ahora me ves?*\n\n*🔔 Si el Bot no le responde a sus comandos por favor haga un pequeño spam*\n\n*—◉ Ejemplo:*\n${usedPrefix}s\n${usedPrefix}s\n${usedPrefix}s`}, {quoted: m});
 };
 handler.help = ['del_reg_in_session_owner'];
 handler.tags = ['owner'];
