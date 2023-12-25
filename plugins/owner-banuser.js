2b4834edf8841f7c3c7f4f6094bb28b3e1ba2ced
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2023-12-25 11:43:30 -0500

    Actualizar owner-banuser.js

--- plugins/owner-banuser.js
+++ plugins/owner-banuser.js
@@ -6,7 +6,7 @@ const handler = async (m, {conn, participants, usedPrefix, command}) => {
   else who = m.chat;
   const users = global.db.data.users;
   users[who].banned = true;
-    m.reply('*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝙵𝚄𝙴 𝙱𝙰𝙽𝙴𝙰𝙳𝙾 𝙲𝙾𝙽 𝙴𝚇𝙸𝚃𝙾*\n*—◉ 𝙴𝙻 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝙽𝙾 𝙿𝙾𝙳𝚁𝙰 𝚄𝚂𝙰𝚁 𝙴𝙻 𝙱𝙾𝚃 𝙷𝙰𝚂𝚃𝙰 𝚀𝚄𝙴 𝚂𝙴𝙰 𝙳𝙴𝚂𝙱𝙰𝙽𝙴𝙰𝙳𝙾*');
+    m.reply('*😆👊 Exɪᴛᴏ Yᴀ Esᴛᴀ Bᴀɴᴇᴀᴅᴏ Eʟ Usᴜᴀʀɪᴏ*\n*—◉ Eʟ Usᴜᴀʀɪᴏ Nᴏ Pᴜᴇᴅᴇ Usᴀʀᴍᴇ*');
 };
 handler.command = /^banuser$/i;
 handler.rowner = true;
