f57b0737937036744bb9278c2f502c5b2a8a5a41
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2023-12-26 22:00:55 -0500

    Actualizar marker-lolice.js

--- plugins/marker-lolice.js
+++ plugins/marker-lolice.js
@@ -2,7 +2,7 @@ const handler = async (m, {conn}) => {
   const who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
   conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/lolice', {
     avatar: await conn.profilePictureUrl(who, 'image').catch((_) => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
-  }), 'error.png', '*🚔🚨 𝐋𝐎𝐋𝐈𝐂𝐎𝐍𝐒 𝐂𝐎𝐌𝐎 𝐓𝐔 𝐒𝐎𝐋𝐎 𝐏𝐄𝐑𝐓𝐄𝐍𝐄𝐂𝐄𝐍 𝐀 𝐋𝐀 𝐂𝐀𝐑𝐂𝐄𝐋 🚨🚔*', m);
+  }), 'error.png', '*🚔🚨 𝘚𝘈𝘒𝘜𝘓𝘖𝘓𝘐𝘊𝘌 𝘊𝘖𝘔𝘖 𝘛𝘜 𝘚𝘖𝘓𝘖 𝘗𝘌𝘙𝘛𝘌𝘕𝘌𝘊𝘌𝘕 𝘈 𝘓𝘈 𝘊𝘈𝘙𝘊𝘌𝘓 🚨🚔*', m);
 };
 handler.help = ['lolice'];
 handler.tags = ['maker'];
