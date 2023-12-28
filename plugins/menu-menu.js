fba3d07537d502c6ead05bb92d2ddb82361ec7a4
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2023-12-28 18:58:06 -0500

    Actualizar menu-menu.js

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -26,7 +26,7 @@ const str = `➪ *𝗛ola, ${taguser}*
 *╭꒱🦁⸽⃕SᴀᴋᴜʀᴀBᴏᴛ-MD🍁⃨፝⃕✰*
 ┊----------➫┇◦✦◦✦◦✦◦✦◦┋:̖́-
 ┊👑 *Creador:* Diego 
-┊✨️ *Numero:* 573013114854
+┊✨️ *Numero Del Creador:* 573013114854
 ┊🌵 *Bot Oficial:* +51935499065
 ┊🕑 *Tiempo activo:* ${uptime}
 ┊📂 *Usuarios regs:* ${rtotalreg}
