2e0e7f563c23ef4f393c8af86298309305942420
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2023-12-28 12:20:04 -0500

    Actualizar _autobio.js

--- plugins/_autobio.js
+++ plugins/_autobio.js
@@ -8,7 +8,7 @@ if (process.send) { process.send('uptime')
 _muptime = await new Promise(resolve => { process.once('message', resolve) 
 setTimeout(resolve, 2000) }) * 1000}
 let uptime = clockString(_uptime)
-let bio = `By:Diego✨💖 - SakuraBot-MD ⏳️Tiempo Activo: ${uptime}` 
+let bio = `𝘉𝘺:𝘋𝘪𝘦𝘨𝘰✨💖 - ${global.packname} ⏳️Tiempo Activo: ${uptime}` 
 await this.updateProfileStatus(bio).catch(_ => _)
 setting.status = new Date() * 1
 } 
