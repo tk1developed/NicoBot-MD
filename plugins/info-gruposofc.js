232f40da947fec802fefa1387266474cd7942726
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2023-12-29 13:08:02 -0500

    Actualizar info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -2,7 +2,7 @@ import fs from "fs"
 let handler = async (m, { conn, command }) => {
 let estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: {orderMessage: { itemCount : 2023, status: 1, surface : 1, message: 'By Azami ©', orderTitle: 'Bang', thumbnail: fs.readFileSync('Menu2.jpg'), sellerJid: '0@s.whatsapp.net'}}}
 let foto = './storage/imagenes/grupos.jpg'
-let texto = `*${nn}*`
+let texto = `*https://chat.whatsapp.com/G7AuDxFPn7e2vGjio7XTIO*`
 
 
 await conn.sendFile(m.chat, foto, 'Curiosity.jpg', texto, estilo)}
