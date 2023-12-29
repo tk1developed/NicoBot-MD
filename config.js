c78eff9b094473b0f21a9a0e1237c44d1ae0e3a2
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2023-12-29 16:46:39 -0500

    Actualizar config.js

--- config.js
+++ config.js
@@ -1,36 +1,92 @@
-import { watchFile, unwatchFile } from 'fs'
-import chalk from 'chalk'
-import { fileURLToPath, pathToFileURL } from 'url'
+import {watchFile, unwatchFile} from 'fs';
+import chalk from 'chalk';
+import {fileURLToPath} from 'url';
+import fs from 'fs'; 
+import cheerio from 'cheerio';
+import fetch from 'node-fetch';
+import axios from 'axios';
+import moment from 'moment-timezone';
 
-global.owner = [['573013114854', 'SakuraBot-MD ♨️', true], ['573207200376'], ['573106909511'], ['573218138672'], ['51935499065'], ['51944167935'], ['573217297116'], ['573235215844'], ['50258115623']]
+global.botnumber = ""
 
-//BETA: Si quiere evitar escribir el número que será bot en la consola, agregué desde aquí entonces:
-//Sólo aplica para opción 2 (ser bot con código de texto de 8 digitos)
-global.botNumberCode = '' //Ejemplo: +59309090909
-global.confirmCode = ''
+global.owner = [
+  ['573013114854', '👑 Sakura - Creador 👑', true],
+  ['573218138672', '💫 Sakura - Collaborator 1 💫', true],
+  ['573207200376', '💫 Sakura - Collaborator 2 💫', true],
+  ['573106909511', '💫 Sakura - Collaborator 3 💫', true],
+  ['51935499065', '💫 Sakura - Collaborator 4 💫', true],
+  ['573246822621', '💫 Sakura - Collaborator 5 💫', true],
+  ['+51 944 167 935', '💫 Sakura - Collaborator 6 💫', true]
+];
 
-global.animxscans = ['573013114854']
-global.suittag = ['573013114854']
-global.mods = []
-global.prems = []
+global.suittag = ['573013114854'];
+global.prems = ['573218138672'];
 
-global.packname = '𝙎𝙖𝙠𝙪𝙧𝙖𝘽𝙤𝙩-𝙈𝘿'
-global.author = '𝘋𝘪𝘦𝘨𝘰 𝘖𝘧𝘪𝘤𝘪𝘢𝘭'
-global.wm = 'Ⓢ︎ 𝖲𝖺𝗄𝗎𝗋𝖺𝖡𝗈𝗍-𝖬𝖣'
-global.wm2 = '𝗦akura : 𝗕𝗈𝗍'
-global.diego = 'Diego'
-global.cb = 'SakuraBot-MD'
+global.packname = '𝙎𝙖𝙠𝙪𝙧𝙖𝘽𝙤𝙩-𝙈𝘿';
+global.author = '𝘿𝙞𝙚𝙜𝙤 𝙊𝙛𝙞𝙘𝙞𝙖𝙡';
+global.wm = '𝘚𝘢𝘬𝘶𝘳𝘪𝘵𝘢-𝘔𝘋';
+global.titulowm = '𝘚𝘢𝘬𝘶𝘳𝘪𝘵𝘢-𝘔𝘋';
+global.titulowm2 = `𝘋𝘪𝘦𝘨𝘰 𝘖𝘧𝘪𝘤𝘪𝘢𝘭`
+global.igfg = '𝘋𝘪𝘦𝘨𝘰 𝘖𝘧𝘪𝘤𝘪𝘢𝘭';
+global.wait = '*⏳️ Cargando Espere 1 Momento...*';
 
-global.vs = 'V2 • 1.0.5'
-global.library = 'Baileys'
-global.baileys = '@whiskeysockets/baileys'
-global.lenguaje = 'Español'
-global.menudi = ['⛶','❏','⫹⫺']
-global.dev = '© Diego ~ Zam'
-global.devnum = '+57 301 3114854'
+global.imagen1 = fs.readFileSync('./Menu2.jpg');
+global.imagen2 = fs.readFileSync('./src/nuevobot.jpg');
+global.imagen3 = fs.readFileSync('./src/Pre Bot Publi.png');
+global.imagen4 = fs.readFileSync('./Menu.png');
+global.imagen5 = fs.readFileSync('./src/+18.jpg');
+global.imagen6 = fs.readFileSync('./Menu3.png');
 
-let file = fileURLToPath(import.meta.url)
-watchFile(file, () => { unwatchFile(file)
-console.log(chalk.yellow('Se actualizo el archivo config.js'))
-import(`${file}?update=${Date.now()}`)
-})
+global.mods = [];
+
+//* *******Tiempo***************
+global.d = new Date(new Date + 3600000);
+global.locale = 'es';
+global.dia = d.toLocaleDateString(locale, {weekday: 'long'});
+global.fecha = d.toLocaleDateString('es', {day: 'numeric', month: 'numeric', year: 'numeric'});
+global.mes = d.toLocaleDateString('es', {month: 'long'});
+global.año = d.toLocaleDateString('es', {year: 'numeric'});
+global.tiempo = d.toLocaleString('en-US', {hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true});
+//* ****************************
+global.wm2 = `${dia} ${fecha}\nSakuritaBot`;
+global.gt = 'SakuraBot';
+global.mysticbot = 'SakuraBot';
+global.md = 'https://github.com/diegojadibot/SakuraBot-MD';
+global.mysticbot = 'https://github.com/diegojadibot/SakuraBot-MD';
+global.waitt = '*⏳ Cargando Espere Un Momento...*';
+global.waittt = '*⏳ Cargando Espere Un Momento...*';
+global.waitttt = '*⏳ Cargando Espere Un Momento...*';
+global.nomorown = '573013114854';
+global.pdoc = ['application/vnd.openxmlformats-officedocument.presentationml.presentation', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/vnd.ms-excel', 'application/msword', 'application/pdf', 'text/rtf'];
+global.cmenut = '❖––––––『';
+global.cmenub = '┊✦ ';
+global.cmenuf = '╰━═┅═━––––––๑\n';
+global.cmenua = '\n⌕ ❙❘❙❙❘❙❚❙❘❙❙❚❙❘❙❘❙❚❙❘❙❙❚❙❘❙❙❘❙❚❙❘ ⌕\n     ';
+global.dmenut = '*❖─┅──┅〈*';
+global.dmenub = '*┊»*';
+global.dmenub2 = '*┊*';
+global.dmenuf = '*╰┅────────┅✦*';
+global.htjava = '⫹⫺';
+global.htki = '*⭑•̩̩͙⊱•••• ☪*';
+global.htka = '*☪ ••••̩̩͙⊰•⭑*';
+global.comienzo = '• • ◕◕════';
+global.fin = '════◕◕ • •';
+global.botdate = `*[ 📅 ] Fecha:*  ${moment.tz('America/Mexico_City').format('DD/MM/YY')}`;
+global.bottime = `*[ ⏳ ] Hora:* ${moment.tz('America/Mexico_City').format('HH:mm:ss')}`;
+global.fgif = {key: {participant: '0@s.whatsapp.net'}, message: {'videoMessage': {'title': wm, 'h': `Hmm`, 'seconds': '999999999', 'gifPlayback': 'true', 'caption': bottime, 'jpegThumbnail': fs.readFileSync('./Menu.png')}}};
+global.multiplier = 99;
+global.flaaa = [
+  'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=water-logo&script=water-logo&fontsize=90&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextColor=%23000&shadowGlowColor=%23000&backgroundColor=%23000&text=',
+  'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=crafts-logo&fontsize=90&doScale=true&scaleWidth=800&scaleHeight=500&text=',
+  'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=amped-logo&doScale=true&scaleWidth=800&scaleHeight=500&text=',
+  'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text=',
+  'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&fillColor1Color=%23f2aa4c&fillColor2Color=%23f2aa4c&fillColor3Color=%23f2aa4c&fillColor4Color=%23f2aa4c&fillColor5Color=%23f2aa4c&fillColor6Color=%23f2aa4c&fillColor7Color=%23f2aa4c&fillColor8Color=%23f2aa4c&fillColor9Color=%23f2aa4c&fillColor10Color=%23f2aa4c&fillOutlineColor=%23f2aa4c&fillOutline2Color=%23f2aa4c&backgroundColor=%23101820&text=',
+];
+//* ************************
+
+const file = fileURLToPath(import.meta.url);
+watchFile(file, () => {
+  unwatchFile(file);
+  console.log(chalk.redBright('Update \'config.js\''));
+  import(`${file}?update=${Date.now()}`);
+});
