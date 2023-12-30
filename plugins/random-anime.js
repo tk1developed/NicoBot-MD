bef1d42ecee6db702a08318f6d7d702bc7fa49a1
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2023-12-29 21:03:32 -0500

    Actualizar random-anime.js

--- plugins/random-anime.js
+++ plugins/random-anime.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const handler = async (m, {command, conn, usedPrefix}) => {
-  const res = (await axios.get(`https://raw.githubusercontent.com/Daxmax123/shinonome-bot/master/src/JSON/anime-${command}.json`)).data;
+  const res = (await axios.get(`https://raw.githubusercontent.com/diegojadibot/sakurabot/master/src/JSON/anime-${command}.json`)).data;
   const haha = await res[Math.floor(res.length * Math.random())];
   conn.sendFile(m.chat, haha, 'error.jpg', `_${command}_`, m);
 // conn.sendButton(m.chat, `_${command}_`.trim(), author, haha, [['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', `${usedPrefix + command}`]], m)
