a4554ea28f450e9c49a34c540f9f0e9fb5c06f01
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2023-12-30 00:50:47 -0500

    Actualizar owner-leavegc.js

--- plugins/owner-leavegc.js
+++ plugins/owner-leavegc.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, text, command }) => {
 let id = text ? text : m.chat  
 let chat = global.db.data.chats[m.chat]
 chat.welcome = false
-await conn.reply(id, `*${wm} 𝘼𝘽𝘼𝙉𝘿𝙊𝙉𝘼 𝙀𝙇 𝙂𝙍𝙐𝙋𝙊, 𝙁𝙐𝙀 𝙂𝙀𝙉𝙄𝘼𝙇 𝙀𝙎𝙏𝘼𝙍 𝘼𝙌𝙐𝙄 👋*`) 
+await conn.reply(id, `${packname} *ABANDONA EL GRUPO, FUE GENIAL ESTAR AQUÍ 🌵*`) 
 await conn.groupLeave(id)
 try {  
 chat.welcome = true
