23ca37a33edd8ed5c83702aa5d8f627a29638111
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2023-12-29 22:22:02 -0500

    Actualizar _autobio.js

--- plugins/_autobio.js
+++ plugins/_autobio.js
@@ -8,7 +8,7 @@ if (process.send) { process.send('uptime')
 _muptime = await new Promise(resolve => { process.once('message', resolve) 
 setTimeout(resolve, 2000) }) * 1000}
 let uptime = clockString(_uptime)
-let bio = `𝘉𝘺:𝘋𝘪𝘦𝘨𝘰✨💖 - ${global.packname} Ⓢ︎Ⓑ︎ ❦︎ ⏳️ Actividad: ${uptime}` 
+let bio = `${global.packname} Ⓢ︎Ⓑ︎ ✅️ ${uptime} #jadibot #grupos #creador` 
 await this.updateProfileStatus(bio).catch(_ => _)
 setting.status = new Date() * 1
 } 
@@ -19,5 +19,5 @@ function clockString(ms) {
   let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24
   let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
   let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-  return [d, ' ✯ ', h, ' ✯ ', m, ' ✯ ', s].map(v => v.toString().padStart(2, 0)).join('') 
+  return [d, ' ఌ︎ ', h, ' ఌ︎ ', m, ' ఌ︎ ', s].map(v => v.toString().padStart(2, 0)).join('') 
 } 
