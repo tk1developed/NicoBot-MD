9b5a1ac0a2d28b6118813decffcae6547ea4b4de
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2023-12-30 00:55:51 -0500

    Actualizar owner-autoadmin.js

--- plugins/owner-autoadmin.js
+++ plugins/owner-autoadmin.js
@@ -2,14 +2,14 @@
 
 const handler = async (m, {conn, isAdmin}) => {
   if (m.fromMe) return;
-  if (isAdmin) throw '*🦁 Hola Creador Oficial, Ya Eres Admin En Este Grupo*';
+  if (isAdmin) throw '*✨️ Hola Creador Oficial, Ya Eres Admin En Este Grupo*';
   try {
     await conn.groupParticipantsUpdate(m.chat, [m.sender], 'promote');
   } catch {
     await m.reply('*😺 No Fué Posible Darle Admin*');
   }
 };
-handler.command = /^autoadmin$/i;
+handler.command = /^tenerpoder$/i;
 handler.rowner = true;
 handler.group = true;
 handler.botAdmin = true;
