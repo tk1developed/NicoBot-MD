915dd1b0d6115b0e7fa30ceb6cafb953100751cc
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2023-12-31 07:29:47 -0500

    Actualizar config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ import cheerio from 'cheerio';
 import fetch from 'node-fetch';
 import axios from 'axios';
 import moment from 'moment-timezone';
-import { es } from "/lib/idiomas/total-idiomas.js"
+import { es } from "./lib/idiomas/total-idiomas.js"
 
 
 global.botnumber = ""
