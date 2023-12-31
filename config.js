d715b7dc617a3c60523a4093b1ea0e80abd17a97
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2023-12-31 02:36:40 -0600

    Uwu

--- config.js
+++ config.js
@@ -6,6 +6,8 @@ import cheerio from 'cheerio';
 import fetch from 'node-fetch';
 import axios from 'axios';
 import moment from 'moment-timezone';
+import { es } from "./lib/idiomas/total-idiomas.js"
+
 
 global.botnumber = ""
 
