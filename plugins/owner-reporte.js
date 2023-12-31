5166592cc5131d965fbdc4dd80f7382e66b8be0c
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2023-12-30 19:03:44 -0500

    Actualizar owner-reporte.js

--- plugins/owner-reporte.js
+++ plugins/owner-reporte.js
@@ -3,7 +3,7 @@ const handler = async (m, {conn, text, usedPrefix, command}) => {
   if (text.length < 10) throw `*⚠️ Eʟ Rᴇᴘᴏʀᴛᴇ Dᴇᴠᴇ Sᴇʀ Mᴀxɪᴍᴏ 10 Cᴀʀᴀᴄᴛᴇʀᴇs!*`;
   if (text.length > 1000) throw `*⚠️ Eʟ Rᴇᴘᴏʀᴛᴇ Dᴇᴠᴇ Sᴇʀ Mᴀxɪᴍᴏ Dᴇ 100 Cᴀʀᴀᴄᴛᴇʀᴇs!*`;
     const teks = `*╭━━[ 𝙍𝙀𝙋𝙊𝙍𝙏𝙀 ]━━━⬣*\n*┃*\n*┃* ➦☬⃝✨𝑵𝒖𝒎𝒆𝒓𝒐 *╰❖────『✙』────❖* \n┃ ✦ Wa.me/${m.sender.split`@`[0]}\n*┃*\n*┃* ➦☬⃝✨𝑴𝒆𝒏𝒔𝒂𝒋𝒆\n*┃* ✦ ${text}\n*┃*\n*╰❖────『𝑬𝑵𝑽𝑰𝑨𝑫𝑶 』────❖* *╰═══════ ♢.✰.♢ ════════☐*`
-  conn.reply('51935499065@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, null, {contextInfo: {mentionedJid: [m.sender]}});
+  conn.reply('573013114854@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, null, {contextInfo: {mentionedJid: [m.sender]}});
   m.reply(`*[⚠️] Exito el reporte ha sido enviado a mí Creador. Tendrá una respuesta pronto. Si se detecta que el reporte es Falso será Ignorado el reporte.*`);
 };
 handler.help = ['reporte', 'request'].map((v) => v + ' <teks>');
