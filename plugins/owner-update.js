032958983ed5c498ae7801ad6ce56117e757a894
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2023-12-31 19:31:44 -0500

    Actualizar owner-update.js

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -16,7 +16,7 @@ const handler = async (m, { conn, text }) => {
           .split('\n')
           .filter(line => line.trim() !== '')
           .map(line => {
-            if (line.includes('.npm/') || line.includes('.cache/') || line.includes('tmp/') || line.includes('ShinoSession/') || line.includes('npm-debug.log')) {
+            if (line.includes('.npm/') || line.includes('.cache/') || line.includes('tmp/') || line.includes('SakuraBotSession/') || line.includes('npm-debug.log')) {
               return null;
             }
             return '*◉ ' + line.slice(3) + '*';
