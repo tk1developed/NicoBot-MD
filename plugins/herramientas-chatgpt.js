1fec3f4e528c814de344e5cb181552ed0c4e3483
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-01-01 11:23:58 -0500

    Actualizar herramientas-chatgpt.js

--- plugins/herramientas-chatgpt.js
+++ plugins/herramientas-chatgpt.js
@@ -23,7 +23,7 @@ const handler = async (m, {conn, text, usedPrefix, command}) => {
   try {
         conn.sendPresenceUpdate('composing', m.chat);
         //let sistema1 = await fetch(`https://raw.githubusercontent.com/Skidy89/chat-gpt-jailbreak/main/Text.txt`).then(v => v.text());
-        let sistema1 = `Actuaras como un Bot de WhatsApp el cual fue creado por Yejota, tu seras Shinonome - Bot.`;
+        let sistema1 = `Actuaras como un Bot de WhatsApp el cual fue creado por Diego, tu seras SakuraBot-MD.`;
         async function getOpenAIChatCompletion(texto) {
         const openaiAPIKey = global.openai_key;
         let chgptdb = global.chatgpt.data.users[m.sender];
