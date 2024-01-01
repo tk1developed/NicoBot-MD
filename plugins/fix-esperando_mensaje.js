cfe2ff579c6cdd08e4277ec2a8c41510d2f27daf
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-01-01 11:23:24 -0500

    Actualizar fix-esperando_mensaje.js

--- plugins/fix-esperando_mensaje.js
+++ plugins/fix-esperando_mensaje.js
@@ -5,7 +5,7 @@ import path from 'path';
 
 const handler = async (m, { conn, usedPrefix }) => {
   if (global.conn.user.jid !== conn.user.jid) {
-    return conn.sendMessage(m.chat, {text: '*[❗] Utiliza este comando directamente en el número principal del Bot*'}, {quoted: m});
+    return conn.sendMessage(m.chat, {text: '*[🌵] Utiliza este comando directamente en el número principal del Bot*'}, {quoted: m});
   }
   const chatId = m.isGroup ? [m.chat, m.sender] : [m.sender];
   const sessionPath = './SakuraBotSession/';
