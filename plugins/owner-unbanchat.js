7e3767fde275997e1cc3885dab94f746ac9fd663
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-01-02 11:08:23 -0500

    Actualizar owner-unbanchat.js

--- plugins/owner-unbanchat.js
+++ plugins/owner-unbanchat.js
@@ -1,9 +1,9 @@
 const handler = async (m) => {
   global.db.data.chats[m.chat].isBanned = false;
-  m.reply('*👊Esᴛᴇ Cʜᴀᴛ Yᴀ Esᴛᴀ Dᴇsʙᴀɴᴇᴀᴅᴏ, Pᴜᴇᴅᴇɴ Usᴀʀᴍᴇ*');
+  m.reply('[💖]𝗖𝗵𝗮𝘁 𝗗𝗲𝘀𝗯𝗮𝗻𝗲𝗮𝗱𝗼 𝗖𝗼𝗻 𝗘𝘅𝗶𝘁𝗼 , 𝗬𝗮 𝗣𝘂𝗲𝗱𝗲𝗻 𝗨𝘀𝗮𝗿 𝗦𝗮𝗸𝘂𝗿𝗮𝗕𝗼𝘁-𝗠𝗗🌹');
 };
 handler.help = ['unbanchat'];
 handler.tags = ['owner'];
-handler.command = /^unbanchat$/i;
+handler.command = /^desprohibirchat$/i;
 handler.rowner = true;
 export default handler;
