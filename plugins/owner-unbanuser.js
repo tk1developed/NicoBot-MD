20651fe7b1cdbec1c812db95603c8269a77aeef3
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-01-02 11:09:43 -0500

    Actualizar owner-unbanuser.js

--- plugins/owner-unbanuser.js
+++ plugins/owner-unbanuser.js
@@ -1,15 +1,15 @@
 const handler = async (m, {conn, text}) => {
-  if (!text) throw '*ᴇsᴄʀɪʙᴀ ᴇʟ @𝚝𝚊𝚐 ᴅᴇʟ ᴜsᴜᴀʀɪᴏ ʙᴀɴᴇᴀᴅᴏ*';
+  if (!text) throw '[⛔] 𝐎𝐰𝐧𝐞𝐫, 𝐞𝐭𝐢𝐪𝐮𝐞𝐭𝐞 𝐚𝐥 𝐮𝐬𝐮𝐚𝐫𝐢𝐨 𝐩𝐚𝐫𝐚 𝐝𝐞𝐬𝐛𝐚𝐧𝐞𝐚𝐫.';
   let who;
   if (m.isGroup) who = m.mentionedJid[0];
   else who = m.chat;
-  if (!who) throw '*ᴇsᴄʀɪʙᴀ ᴇʟ @𝚝𝚊𝚐 ᴅᴇʟ ᴜsᴜᴀʀɪᴏ ʙᴀɴᴇᴀᴅᴏ*';
+  if (!who) throw '[⛔] 𝐎𝐰𝐧𝐞𝐫, 𝐞𝐭𝐢𝐪𝐮𝐞𝐭𝐞 𝐚𝐥 𝐮𝐬𝐮𝐚𝐫𝐢𝐨 𝐩𝐚𝐫𝐚 𝐝𝐞𝐬𝐛𝐚𝐧𝐞𝐚𝐫.';
   const users = global.db.data.users;
   users[who].banned = false;
-  conn.reply(m.chat, `*🦁 Lɪsᴛᴏ Aᴍᴏ*\n*—◉ Eʟ Usᴜᴀʀɪᴏ Yᴀ Pᴜᴇᴅᴇ Usᴀʀ Mɪs Cᴏᴍᴀɴᴅᴏs*`, m);
+  conn.reply(m.chat, `[💖] 𝐏𝐞𝐫𝐟𝐞𝐜𝐭𝐨, 𝐞𝐥 𝐮𝐬𝐮𝐚𝐫𝐢𝐨 𝐟𝐮𝐞 𝐝𝐞𝐬𝐛𝐚𝐧𝐞𝐚𝐝𝐨 𝐜𝐨𝐧 𝐞𝐱𝐢𝐭𝐨, 𝐞𝐥 𝐮𝐬𝐮𝐚𝐫𝐢𝐨 𝐲𝐚 𝐩𝐮𝐞𝐝𝐞 𝐮𝐬𝐚𝐫 𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭-𝐌𝐃🌹.`, m);
 };
 handler.help = ['unbanuser'];
 handler.tags = ['owner'];
-handler.command = /^unbanuser$/i;
+handler.command = /^desprohibiruser$/i;
 handler.rowner = true;
 export default handler;
