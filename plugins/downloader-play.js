2268e88fa608e938c6c6f9804c26a88a5d1ebfc9
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-01-04 15:29:40 -0500

    Actualizar downloader-play.js

--- plugins/downloader-play.js
+++ plugins/downloader-play.js
@@ -23,14 +23,14 @@ const handler = async (m, {conn, command, args, text, usedPrefix}) => {
     const texto1 = `*◉—⌈🔊 MUSIC PLAY 🔊⌋—◉*\n
 ✰📌 *Titulo:* ${yt_play[0].title}
 ✰📆 *Publicado:* ${yt_play[0].ago}
- ⌚ *Duracion:* ${secondString(yt_play[0].duration.seconds)}
- 👀 *Vistas:* ${`${MilesNumber(yt_play[0].views)}`}
- ✰👤 *Autor:* ${yt_play[0].author.name}
- ✰🍧 *Canal:* ${yt_play[0].author.url}
- ✰🆔 *ID:* ${yt_play[0].videoId}
- ✰🌤 *Tipo:* ${yt_play[0].type}
- ✰🔗 *Link:* ${yt_play[0].url}\n
- *_Enviando ${additionalText}, Aguarde Un Momento. . . ．．．_*`.trim();
+✰⌚ *Duracion:* ${secondString(yt_play[0].duration.seconds)}
+✰👀 *Vistas:* ${`${MilesNumber(yt_play[0].views)}`}
+✰🏝 *Autor:* ${yt_play[0].author.name}
+✰🍧 *Canal:* ${yt_play[0].author.url}
+✰🏕 *ID:* ${yt_play[0].videoId}
+✰🌤 *Tipo:* ${yt_play[0].type}
+✰🔗 *Link:* ${yt_play[0].url}\n
+*_Enviando ${additionalText}, Aguarde Un Momento. . . ．．．_*`.trim();
     conn.sendMessage(m.chat, {image: {url: yt_play[0].thumbnail}, caption: texto1}, {quoted: m});
     if (command == 'play') {
     try {    
