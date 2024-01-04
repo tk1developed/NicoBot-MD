0b8ade04c58fe75adb919a8bc72124be42854f59
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-01-04 15:12:28 -0500

    Actualizar downloader-play.js

--- plugins/downloader-play.js
+++ plugins/downloader-play.js
@@ -21,16 +21,16 @@ const handler = async (m, {conn, command, args, text, usedPrefix}) => {
       additionalText = 'video 🎥';
     }
     const texto1 = `*◉——⌈🔊 YOUTUBE PLAY 🔊⌋——◉*\n
-❏ 📌 *Titulo:* ${yt_play[0].title}
-❏ 📆 *Publicado:* ${yt_play[0].ago}
-❏ ⌚ *Duracion:* ${secondString(yt_play[0].duration.seconds)}
-❏ 👀 *Vistas:* ${`${MilesNumber(yt_play[0].views)}`}
-❏ 👤 *Autor:* ${yt_play[0].author.name}
-❏ ⏯️ *Canal:* ${yt_play[0].author.url}
-❏ 🆔 *ID:* ${yt_play[0].videoId}
-❏ 🪬 *Tipo:* ${yt_play[0].type}
-❏ 🔗 *Link:* ${yt_play[0].url}\n
-❏ *_Enviando ${additionalText}, aguarde un momento．．．_*`.trim();
+ 📌 *Titulo:* ${yt_play[0].title}
+ 📆 *Publicado:* ${yt_play[0].ago}
+ ⌚ *Duracion:* ${secondString(yt_play[0].duration.seconds)}
+ 👀 *Vistas:* ${`${MilesNumber(yt_play[0].views)}`}
+ 👤 *Autor:* ${yt_play[0].author.name}
+ ⏯️ *Canal:* ${yt_play[0].author.url}
+ 🆔 *ID:* ${yt_play[0].videoId}
+ 🪬 *Tipo:* ${yt_play[0].type}
+ 🔗 *Link:* ${yt_play[0].url}\n
+ *_Enviando ${additionalText}, aguarde un momento．．．_*`.trim();
     conn.sendMessage(m.chat, {image: {url: yt_play[0].thumbnail}, caption: texto1}, {quoted: m});
     if (command == 'play') {
     try {    
