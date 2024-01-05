1ec2af514d68874a5cc312cee5b0e0252b819eec
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-01-05 16:24:31 -0500

    Actualizar owner-update.js

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -1,13 +1,13 @@
 import {execSync} from "child_process"
 let handler = async (m, {conn, text}) => {
-  await m.reply(`*🚀 C A R G A N D O*`)
+  await m.reply(`*🚀 E S P E R A*`)
   try {
     if (global.conn.user.jid == conn.user.jid) {
       let stdout = execSync("git pull" + (m.fromMe && text ? " " + text : ""))
       await await await conn.reply(m.chat, stdout.toString(), m)
     }
   } catch {
-    var update = execSync("git remote set-url origin https://github.com/AzamiJs/CuriosityBot-MD.git && git pull")
+    var update = execSync("git remote set-url origin https://github.com/diegojadibot/SakuraBot-MD.git && git pull")
     await await await m.reply(update.toString())
   }
 };
