cd42a7ade9a20e44c32f5ce36444f265ff08b556
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-01-11 12:25:10 -0500

    Actualizar config-enable.js

--- plugins/config-enable.js
+++ plugins/config-enable.js
@@ -77,13 +77,6 @@ const handler = async (m, {conn, usedPrefix, command, args, isOwner, isAdmin, is
 
 --------------------------------
 
-*Opción:* 🤖 | MODEJADIBOT
-*Comando:* ${usedPrefix + command} modejadibot
-*Descripción:* Activa o desactiva el uso del comando para sub bots (${usedPrefix}serbot / ${usedPrefix}jadibot). 
-*Nota:* Este comando solo podrá ser usado por owners del Bot.
-
---------------------------------
-
 *Opción:* 🏢 | GCONLY
 *Comando:* ${usedPrefix + command} gconly
 *Descripción:* El Bot solo respondera a los comandos si es un grupo. 
