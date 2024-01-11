eaff66e6888e211d94c39bc1553ecf17f4b6c8ac
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-01-11 11:53:42 -0500

    Actualizar mipilot-stopbot.js

--- plugins/mipilot-stopbot.js
+++ plugins/mipilot-stopbot.js
@@ -1,6 +1,6 @@
 import fs from "fs"
 async function handler(m, {conn, usedPrefix}) {
-   if (conn.user.jid == global.conn.user.jid) return m.reply(`*[❗] No puedes apagar el Bot principal.*`)
+   if (conn.user.jid == global.conn.user.jid) return m.reply(`*🙀 𝘏𝘦𝘺 𝘌𝘴𝘱𝘦𝘳𝘢, 𝘌𝘭 𝘉𝘰𝘵 𝘖𝘳𝘪𝘯𝘤𝘪𝘱𝘢𝘭 𝘕𝘰 𝘚𝘦 𝘗𝘶𝘦𝘥𝘦 𝘈𝘱𝘢𝘨𝘢𝘳.*`)
    m.reply(`*[❗] Dejarás de ser un Sub Bot en 5 segundos...*`)
    conn.fstop = true
    conn.ws.close()
