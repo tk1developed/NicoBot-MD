d37d270c6cdf142edf8c7aaccfb0b254c2719e06
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-01-11 21:41:51 -0500

    Actualizar plugins

---
+++ plugins/_autolevelup.js
@@ -0,0 +1,22 @@
+import db from '../lib/database.js'
+import { canLevelUp } from '../lib/levelling.js'
+
+export function before(m) {
+    let user = db.data.users[m.sender]
+    if (!user.autolevelup)
+        return !0
+    let before = user.level * 1
+    while (canLevelUp(user.level, user.exp, global.multiplier))
+        user.level++
+
+    if (before !== user.level) {
+        m.reply(`
+⤴ Bot Tiburón🦈 | ⚠️ *「 LEVEL-UP v2 」*
+
+Felicitaciones, has subido de nivel!
+*${before}* ↣ *${user.level}*
+usa *.profile* para revisar
+        `.trim())
+    }
+}
+export const disabled = true
