723e6d4766bc6b26151431a14e662418cde73d58
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-01-12 19:56:10 -0500

    Actualizar rpg-myns.js

--- plugins/rpg-myns.js
+++ plugins/rpg-myns.js
@@ -1,7 +1,7 @@
 import {createHash} from 'crypto';
 const handler = async function(m, {conn, text, usedPrefix}) {
   const sn = createHash('md5').update(m.sender).digest('hex');
-  m.reply(`${sn}`.trim());
+  m.reply(`❣️𝘌𝘴𝘵𝘦 𝘌𝘴 𝘛𝘶 𝘕𝘶𝘮𝘦𝘳𝘰 𝘋𝘦 𝘚𝘦𝘳𝘪𝘦: ${sn}`.trim());
 };
 handler.help = ['myns'];
 handler.tags = ['xp'];
