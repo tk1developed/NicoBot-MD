cd86699ad092dc35b8ef8434a4d482806f703d4d
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-01-14 17:03:47 -0500

    Actualizar maker-gay.js

--- plugins/maker-gay.js
+++ plugins/maker-gay.js
@@ -3,7 +3,7 @@ const handler = async (m, {conn}) => {
   const who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
   await conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/gay', {
     avatar: await conn.profilePictureUrl(who, 'image').catch((_) => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
-  }), 'error.png', '*🏳️‍🌈 𝐐𝐮𝐢𝐞𝐧 𝐐𝐮𝐢𝐞𝐫𝐞 𝐕𝐢𝐨𝐥𝐚𝐫 𝐀 𝐄𝐝𝐭𝐞 𝐆𝐚𝐲 𝐄𝐬 100% 𝐆𝐚𝐲 𝐕𝐢𝐨𝐥𝐞𝐧𝐥𝐨 🏳️‍🌈*', m);
+  }), 'error.png', '*🏳️‍🌈 𝐐𝐮𝐢𝐞𝐧 𝐐𝐮𝐢𝐞𝐫𝐞 𝐕𝐢𝐨𝐥𝐚𝐫 𝐀 𝐄𝐬𝐭𝐞 𝐆𝐚𝐲 𝐄𝐬 100% 𝐆𝐚𝐲 𝐕𝐢𝐨𝐥𝐞𝐧𝐥𝐨 🏳️‍🌈*', m);
   await conn.sendMessage(m.chat, {audio: {url: vn}, fileName: `error.mp3`, mimetype: 'audio/mpeg', ptt: true}, {quoted: m});
 };
 handler.help = ['gay'];
