3d51df857ad52a64ddad5493753a73fb0c40741d
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-01-16 16:45:20 -0500

    Actualizar info-infobot.js

--- plugins/info-infobot.js
+++ plugins/info-infobot.js
@@ -1,29 +1,26 @@
-import { generateWAMessageFromContent } from "@whiskeysockets/baileys";
-import os from "os";
-import util from "util";
-import sizeFormatter from "human-readable";
-import MessageType from "@whiskeysockets/baileys";
-import fs from "fs";
-import { performance } from "perf_hooks";
-const handler = async (m, { conn, usedPrefix }) => {
-  const _uptime = process.uptime() * 1000;
-  const uptime = clockString(_uptime);
-  const totalreg = Object.keys(global.db.data.users).length;
-  const chats = Object.entries(conn.chats).filter(
-    ([id, data]) => id && data.isChats,
-  );
-  const groupsIn = chats.filter(([id]) => id.endsWith("@g.us"));
-  const groups = chats.filter(([id]) => id.endsWith("@g.us"));
-  const used = process.memoryUsage();
-  const { restrict, antiCall, antiprivado, modejadibot } =
-    global.db.data.settings[conn.user.jid] || {};
-  const { autoread, gconly, pconly, self } = global.opts || {};
-  const old = performance.now();
-  const neww = performance.now();
-  const speed = neww - old;
-  const info = `
+import { totalmem, freemem } from 'os'
+import os from 'os'
+import util from 'util'
+import osu from 'node-os-utils'
+import { performance } from 'perf_hooks'
+import { sizeFormatter } from 'human-readable'
+import speed from 'performance-now'
+import { spawn, exec, execSync } from 'child_process'
+const format = sizeFormatter({ std: 'JEDEC', decimalPlaces: 2, keepTrailingZeroes: false, render: (literal, symbol) => `${literal} ${symbol}B` })
 
-*🚀Velocidad*
+var handler = async (m, { conn }) => {
+
+let timestamp = speed()
+let latensi = speed() - timestamp
+
+let _muptime = process.uptime() * 1000
+let muptime = clockString(_muptime)
+
+let chats = Object.entries(conn.chats).filter(([id, data]) => id && data.isChats)
+let groups = Object.entries(conn.chats).filter(([jid, chat]) => jid.endsWith('@g.us') && chat.isChats && !chat.metadata?.read_only && !chat.metadata?.announce).map(v => v[0])
+
+
+let texto = `*🚀Velocidad*
 *${speed} ms*
 
 *⏰️Actividad*
@@ -34,50 +31,21 @@ const handler = async (m, { conn, usedPrefix }) => {
 𝘊𝘩𝘢𝘵𝘴 𝘋𝘦 𝘎𝘳𝘶𝘱𝘰𝘴: *${groups.length}* 
 𝘊𝘩𝘢𝘵𝘴 𝘛𝘰𝘵𝘢𝘭𝘦𝘴: *${chats.length}* 
 
- ⃟ᬽ៸𝙎𝙖𝙠𝙪𝙧𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿༒★»
-`.trim();
-  const doc = [
-    "pdf",
-    "zip",
-    "vnd.openxmlformats-officedocument.presentationml.presentation",
-    "vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-    "vnd.openxmlformats-officedocument.wordprocessingml.document",
-  ];
-  const document = doc[Math.floor(Math.random() * doc.length)];
-  const Message = {
-    document: { url: `https://github.com/BrunoSobrino/TheMystic-Bot-MD` },
-    mimetype: `application/${document}`,
-    fileName: `「 𝘚𝘢𝘬𝘶𝘳𝘪𝘵𝘢𝘉𝘰𝘵🦁 」`,
-    fileLength: 99999999999999,
-    pageCount: 200,
-    contextInfo: {
-      forwardingScore: 200,
-      isForwarded: true,
-      externalAdReply: {
-        mediaUrl: "https://github.com/BrunoSobrino/TheMystic-Bot-MD",
-        mediaType: 2,
-        previewType: "pdf",
-        title: "𝘛𝘶 𝘔𝘦𝘫𝘰𝘳 𝘉𝘰𝘵",
-        body: wm,
-        thumbnail: imagen1,
-        sourceUrl: "https://atom.bio/sakuralite",
-      },
-    },
-    caption: info,
-    footer: wm,
-    headerType: 6,
-  };
-  conn.sendMessage(m.chat, Message, { quoted: m });
-};
-handler.help = ["infobot", "speed"];
-handler.tags = ["info", "tools"];
-handler.command = /^(ping|speed|infobot)$/i;
-export default handler;
+ ⃟ᬽ៸𝙎𝙖𝙠𝙪𝙧𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿༒★»`.trim()
+
+conn.sendMessage(m.chat, { text: texto, contextInfo: { externalAdReply: { title: '', body: 'SAKURA - BOT - MD', thumbnailUrl: 'https://telegra.ph/file/3a33821beb8cad1286824.jpg', sourceUrl: '', mediaType: 1, renderLargerThumbnail: true }}})
+
+}
+handler.help = ['ping']
+handler.tags = ['bot']
+handler.command = ['ping', 'speed']
+
+handler.register = true
+
+export default handler
 
 function clockString(ms) {
-  const h = Math.floor(ms / 3600000);
-  const m = Math.floor(ms / 60000) % 60;
-  const s = Math.floor(ms / 1000) % 60;
-  console.log({ ms, h, m, s });
-  return [h, m, s].map((v) => v.toString().padStart(2, 0)).join(":");
-}
+let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')}
