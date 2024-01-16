8a1da169451c1331519fee49973fd21fc3765f6c
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-01-15 20:03:34 -0500

    Actualizar menu-menu.js

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -3,13 +3,9 @@ let handler = async (m, { conn, command }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 let str = `Hola👋 Buenas Como Estas?
 
-🌥Soy _*SakuraBotLite-MD*_ Un Bot Con Simples Comandos Pero Divertido🪁
+✨️El Menu Cambió Ahora Puedes Escribir #allmenu Para Ver El Menu Completo Del Bot🧭
 
-✨️El Menu Cambió Ahora Puedes Escribir .allmenu Para Ver El Menu Completo Del Bot🧭
-
-😺Puedes Contactar A Mi Creador Si Es Necesario @573013114854 🌻
-
-🌥Usa El Bot Y No Agas Spam A Los Comandos, Eso Causa Que El Numero Del Bot Se Valla Al Soporte🧑‍💻
+😺Puedes Contactar A Mi Creador Si Es Necesario 573013114854 🌻
 
 💻 No Olvides Registrarte, Deves Estar En Mi Base De Datos, Ejemplo #verificar Diego.20 🗃`
 await conn.sendFile(m.chat, media, 'gata.mp4', str, fkontak)}
