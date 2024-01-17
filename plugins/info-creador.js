6c41a5156ba460761545dce18298886241abdea3
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-01-17 10:13:43 -0500

    Actualizar info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,9 +1,14 @@
 const handler = async (m, {conn, usedPrefix}) => {
   const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
   const document = doc[Math.floor(Math.random() * doc.length)];
-  const text = `*♡︎ 𝘕𝘶𝘮𝘦𝘳𝘰 𝘋𝘦 𝘔𝘪 𝘊𝘳𝘦𝘢𝘥𝘰𝘳 Wa.me/573013114854*
-
-*❦︎ 𝘕𝘶𝘮𝘦𝘳𝘰 𝘋𝘦𝘭 𝘊𝘰𝘭𝘢𝘣𝘰𝘳𝘢𝘥𝘰𝘳: Wa.me/51935499065*`.trim();
+  const text = `┏━━━━━━━━━━━━━━━━⋄
+┃⍣ *♡︎ 𝐂𝐫𝐞𝐚𝐝𝐨𝐫 𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭𝐋𝐢𝐭𝐞-𝐌𝐃
+┃⍣ Wa.me/573013114854*
+┃
+┃
+┃⍣ *❦︎ 𝐂𝐨𝐥𝐚𝐛𝐨𝐫𝐚𝐝𝐨𝐫 𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭𝐋𝐢𝐭𝐞-𝐌𝐃:
+┃⍣ Wa.me/51935499065*
+╰━━━━━━━━━━━━━━━━⋄`.trim();
   const buttonMessage= {
     'document': {url: `https://github.com/BrunoSobrino/TheMystic-Bot-MD`},
     'mimetype': `application/${document}`,
