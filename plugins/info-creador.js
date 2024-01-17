bb40b347422d34d776381a830bc33433354fefb8
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-01-17 10:15:14 -0500

    Actualizar info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -2,11 +2,11 @@ const handler = async (m, {conn, usedPrefix}) => {
   const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
   const document = doc[Math.floor(Math.random() * doc.length)];
   const text = `┏━━━━━━━━━━━━━━━━⋄
-┃⍣ ♡︎ 𝐂𝐫𝐞𝐚𝐝𝐨𝐫 𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭𝐋𝐢𝐭𝐞-𝐌𝐃
+┃⍣🌻🪁 𝐂𝐑𝐄𝐀𝐃𝐎𝐑: 
 ┃⍣ Wa.me/573013114854
 ┃
 ┃
-┃⍣ ❦︎ 𝐂𝐨𝐥𝐚𝐛𝐨𝐫𝐚𝐝𝐨𝐫 𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭𝐋𝐢𝐭𝐞-𝐌𝐃:
+┃⍣✨🌴 𝐂𝐎𝐋𝐀𝐁𝐎𝐑𝐀𝐃𝐎𝐑:
 ┃⍣ Wa.me/51935499065
 ╰━━━━━━━━━━━━━━━━⋄`.trim();
   const buttonMessage= {
