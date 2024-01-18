df55d3ab1a948661cece76bbed4ca048b65c38c2
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-01-18 11:37:48 -0500

    Actualizar info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -1,54 +1,9 @@
 let media = './Menu7.png'
 let handler = async (m, { conn, command }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
-let str = `😻𝘏𝘰𝘭𝘢, 𝘛𝘦 𝘐𝘯𝘷𝘪𝘵𝘰 𝘈 𝘜𝘯𝘪𝘳𝘵𝘦 𝘌𝘯 𝘕𝘶𝘦𝘴𝘵𝘳𝘰𝘴 𝘎𝘳𝘶𝘱𝘰𝘴🫶, 𝘛𝘢𝘮𝘣𝘪𝘦𝘯 𝘗𝘢𝘳𝘢 𝘘𝘶𝘦 𝘕𝘰𝘴 𝘚𝘪𝘨𝘢𝘴 𝘌𝘯 𝘕𝘶𝘦𝘴𝘵𝘳𝘰𝘴 𝘊𝘢𝘯𝘢𝘭𝘦𝘴❣️, 𝘠 𝘗𝘢𝘳𝘢 𝘘𝘶𝘦 𝘝𝘪𝘷𝘢𝘴 𝘊𝘰𝘯 𝘕𝘶𝘦𝘴𝘵𝘳𝘢𝘴 𝘊𝘰𝘮𝘶𝘯𝘪𝘥𝘢𝘥𝘦𝘴, 𝘎𝘳𝘶𝘱𝘰𝘴 𝘠 𝘊𝘢𝘯𝘢𝘭𝘦𝘴❤️🫶, 𝘛𝘦 𝘋𝘦𝘫𝘰 𝘓𝘰𝘴 𝘓𝘪𝘯𝘬 𝘋𝘦 𝘓𝘰𝘴 𝘎𝘳𝘶𝘱𝘰𝘴 𝘠 𝘊𝘢𝘯𝘢𝘭𝘦𝘴ꨄ︎ :𝘋
+let str = `*📁 Según los datos de la database, tengo ${rtotalreg} usuarios registrados*
 
-1 𝘚𝘢𝘬𝘶𝘳𝘢𝘉𝘰𝘵𝘓𝘪𝘵𝘦 | 𝘖𝘍𝘊
-▢ 
-https://chat.whatsapp.com/J6AJyZjMIiH6T7zHor6ycV
-
-2 𝘚𝘢𝘬𝘶𝘳𝘢𝘉𝘰𝘵𝘓𝘪𝘵𝘦 | 𝘖𝘍𝘊
-▢ https://chat.whatsapp.com/L69Ok8LTIJNCTNsJydkXaE
-
-3 𝘚𝘢𝘬𝘶𝘳𝘢𝘉𝘰𝘵𝘓𝘪𝘵𝘦 | 𝘖𝘍𝘊
-▢ https://chat.whatsapp.com/IdajR1RodSm1g2NtcRhU3F
-
-4 𝘚𝘢𝘬𝘶𝘳𝘢𝘉𝘰𝘵𝘓𝘪𝘵𝘦 | 𝘖𝘍𝘊
-▢ https://chat.whatsapp.com/Ke0iZ2RS3cLLMtNmktQHZ0
-
-5 𝘚𝘢𝘬𝘶𝘳𝘢𝘉𝘰𝘵𝘓𝘪𝘵𝘦 | 𝘖𝘍𝘊
-▢ https://chat.whatsapp.com/H3ySZrOIJFHKwSqKp9T8z3
-
-6 —★ 🎟]「 𝘌𝘯𝘭𝘢𝘤𝘦 𝘚𝘢𝘬𝘶𝘳𝘢𝘉𝘰𝘵𝘓𝘪𝘵𝘦 | 𝘖𝘍𝘊
-▢ https://chat.whatsapp.com/B9b5jAI4hqv95dH1QmAuF6
-
-7 😻 𝑺𝒐𝒑𝒐𝒓𝒕𝒆 𝑺𝒂𝒌𝒖𝒓𝒂𝑩𝒐𝒕 / 𝑵𝒐 𝑩𝒐𝒕 ♕︎𓅓
-▢ https://chat.whatsapp.com/B5y3UmnpJYQEdeyvSKlLyJ
-
-🧭Grupos En Colaboración:
-
-🌻 𝐍𝐞𝐱𝐮𝐬 𝐁𝐨𝐭 & 𝐒𝐚𝐤𝐮𝐫𝐚 𝐁𝐨𝐭 🪁
-▢ https://chat.whatsapp.com/H6N7chnmJhHJ1jMoF0PRBw
-
-1 ʕ•ᴥ•ʔ 𝑺𝒂𝒌𝒖𝒓𝒂𝑩𝒐𝒕𝑳𝒊𝒕𝒆 & 𝑶𝒏𝒚𝒙𝑩𝒐𝒕 ʕ•ᴥ•ʔ
-▢ https://chat.whatsapp.com/HVpXzDdlNrH738WaJIS21C
-
-▢ ✨ 𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭 & 𝐃𝐢𝐚𝐛𝐥𝐨𝐁𝐨𝐭 🌻
-https://chat.whatsapp.com/DUy6uCyXTQC5ReIyU1iLKm
-
-2 ᰔᩚ♕︎🅜︎🅤︎🅛︎🅣︎🅘︎.🅑︎🅞︎🅣︎🅢︎♕︎ᰔᩚ
-▢ https://chat.whatsapp.com/Cp6LjA7WfbXFQ4de9DFGGZ
-─────────────
-🪁🌥Si Se Restableció El Enlace Del Grupo Entra Aqui En Algunos De Estos Link!! 
-
-Atom.Bio : 
-▢ https://atom.bio/sakuralite
-
-Canal :
-▢  https://whatsapp.com/channel/0029VaHYnwaKQuJIlTFuS22l
-
-Canal 2:
-♧ https://whatsapp.com/channel/0029VaIg4lxInlqUNkN7A53w`
+*🗂️ ${totalreg} no estan registrados*`, fkontak, m)`
 await conn.sendFile(m.chat, media, 'gata.mp4', str, fkontak)}
 /*conn.sendHydrated(m.chat, str, `𝙂𝘼𝙏𝘼 𝘿𝙄𝙊𝙎 - 𝘼𝙎𝙄𝙎𝙏𝙀𝙉𝘾𝙄𝘼\n\n\n` + wm, media, 'https://github.com/diegojadibot/SakuraBotLite-MD', 'SakuraBotLite-MD', null, null, [
 ['𝘾𝙪𝙚𝙣𝙩𝙖𝙨 𝙊𝙛𝙞𝙘𝙞𝙖𝙡𝙚𝙨 | 𝘼𝙘𝙘𝙤𝙪𝙣𝙩𝙨 ✅', '.cuentassb'],
