7d88f070d94e51e69fd5e26c4dfa635cd2c763e4
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-01-18 11:35:19 -0500

    Actualizar _autobio.js

--- plugins/_autobio.js
+++ plugins/_autobio.js
@@ -1,23 +1,15 @@
-let handler = m => m
-handler.all = async function (m) {
-let setting = global.db.data.settings[this.user.jid]
+var handler = async (m) => {
 
-let _uptime = process.uptime() * 1000
-let _muptime
-if (process.send) { process.send('uptime')
-_muptime = await new Promise(resolve => { process.once('message', resolve) 
-setTimeout(resolve, 2000) }) * 1000}
-let uptime = clockString(_uptime)
-let bio = `${global.packname} ✅️ ${uptime} 🌵 Ⓢ︎Ⓑ︎ Creador: Diego🚀`
-await this.updateProfileStatus(bio).catch(_ => _)
-setting.status = new Date() * 1
-} 
+let totalreg = Object.keys(global.db.data.users).length
+let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length
+
+conn.reply(m.chat, `*📁 Según los datos de la database, tengo ${rtotalreg} usuarios registrados*
+
+*🗂️ ${totalreg} no estan registrados*`, fkontak, m)
+
+}
+handler.help = ['database', 'user']
+handler.tags = ['info']
+handler.command = /^(database|jumlahdatabase|user)$/i
+
 export default handler
-
-function clockString(ms) {
-  let d = isNaN(ms) ? '--' : Math.floor(ms / 86400000)
-  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24
-  let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
-  let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-  return [d, ' ♫︎ ', h, ' ఌ︎ ', m, ' ♫︎ ', s].map(v => v.toString().padStart(2, 0)).join('') 
-} 
