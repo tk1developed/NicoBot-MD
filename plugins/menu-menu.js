d340cf6811be4acc996b49fe00034832cf5c7379
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-01-20 14:52:59 -0500

    Actualizar menu-menu.js

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -8,7 +8,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
   const {exp, limit, level, role} = global.db.data.users[m.sender];
   const pp = await conn.profilePictureUrl(conn.user.jid).catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png');
   const fkon = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: `status@broadcast` } : {}) }, message: { 'contactMessage': { 'displayName': wm, 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:XL;${wm},;;;\nFN:${wm},\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabell:Ponsel\nEND:VCARD`, 'jpegThumbnail': imagen1, thumbnail: imagen1 ,sendEphemeral: true}}};
-    await conn.reply(m.chat, '𝗘𝗡𝗩𝗜𝗔𝗡𝗗𝗢 𝗘𝗟 𝗠𝗘𝗡𝗨́ 𝗗𝗘𝗟 𝗕𝗢𝗧🚀. . .',m, { contextInfo:{ forwardingScore: 2022, isForwarded: true, externalAdReply: {title: '𝘽𝙪𝙚𝙣𝙖𝙨 𝙐𝙨𝙪𝙖𝙧𝙞𝙤💚!!', body: '⏰Eɴᴠɪᴀɴᴅᴏ EL Mᴇɴᴜ', sourceUrl: global.md, thumbnail: await (await fetch(pp)).buffer() }}})
+    await conn.reply(m.chat, '*𝗣𝗿𝗼𝘅𝗶𝗺𝗮𝗺𝗲𝗻𝘁𝗲 𝗦𝗲 𝗥𝗲𝗺𝗶𝘁𝗶𝗿𝗮 𝗘𝗹 𝗠𝗲𝗻𝘂.*🚀. . .',m, { contextInfo:{ forwardingScore: 2022, isForwarded: true, externalAdReply: {title: '𝘽𝙪𝙚𝙣𝙖𝙨 𝙐𝙨𝙪𝙖𝙧𝙞𝙤💚!!', body: '⏰Eɴᴠɪᴀɴᴅᴏ EL Mᴇɴᴜ', sourceUrl: global.md, thumbnail: await (await fetch(pp)).buffer() }}})
 //m.react('⏰');
     await conn.sendMessage(m.chat, { react: { text: '🌻', key: m.key } })
   let txt =`╭✨️⸽⃕SᴀᴋᴜʀᴀBᴏᴛLɪᴛᴇ-MD🍁⃨፝⃕✰
