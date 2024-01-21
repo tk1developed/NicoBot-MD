e469bfa1d15998164c29f6544865518b98297d95
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-01-20 20:48:37 -0500

    Actualizar downloader-play.js

--- plugins/downloader-play.js
+++ plugins/downloader-play.js
@@ -24,40 +24,40 @@ const handler = async (m, {conn, command, args, text, usedPrefix}) => {
     } else if (command === 'play2') {
       additionalText = 'video 🎥';
     }
-    const texto1 = `┏━━━━━━━━━━━━━━━━⋄
-┃⍣ *◉—⌈🔊 MUSIC PLAY 🔊⌋—◉*
-╰━━━━━━━━━━━━━━━━⋄
-┏━━━━━━━━━━━━━━━━⋄
-┃⍣ 📌 *Titulo:* 
-┃⍣ ${yt_play[0].title}
-┃
-┃⍣ 📆 *Publicado:* 
-┃⍣ ${yt_play[0].ago}
-┃
-┃⍣ ⌚ *Duracion:*
-┃⍣ ${secondString(yt_play[0].duration.seconds)}
-┃
-┃⍣ 👀 *Vistas:* 
-┃⍣ ${`${MilesNumber(yt_play[0].views)}`}
-┃⍣
-┃⍣ 🏝 *Autor:* 
-┃⍣ ${yt_play[0].author.name}
-┃
-┃⍣ 🪁 *Canal:* 
-┃⍣ ${yt_play[0].author.url}
-┃
-┃⍣ 🏕 *ID:* 
-┃⍣ ${yt_play[0].videoId}
-┃
-┃⍣ 🌤 *Tipo:*
-┃⍣ ${yt_play[0].type}
-┃
-┃⍣ 🔗 *Link:* 
-┃⍣ ${yt_play[0].url}
-╰━━━━━━━━━━━━━━━━⋄
-┏━━━━━━━━━━━━━━━━⋄
-┃⍣ *_Enviando ${additionalText}, Aguarde Un Momento. . . ．．．_*
-╰━━━━━━━━━━━━━━━━⋄`.trim();
+    const texto1 = `╔═════  ࿇  ═════╗
+┆⍣ *◉—⌈🔊 MUSIC PLAY 🔊⌋—◉*
+╰──────•◈•──────╯
+╔═════  ࿇  ═════╗
+┆⍣ 📌 *Titulo:* 
+┆⍣ ${yt_play[0].title}
+┆
+┆⍣ 📆 *Publicado:* 
+┆⍣ ${yt_play[0].ago}
+┆
+┆⍣ ⌚ *Duracion:*
+┆⍣ ${secondString(yt_play[0].duration.seconds)}
+┆
+┆⍣ 👀 *Vistas:* 
+┆⍣ ${`${MilesNumber(yt_play[0].views)}`}
+┆⍣
+┆⍣ 🏝 *Autor:* 
+┆⍣ ${yt_play[0].author.name}
+┆
+┆⍣ 🪁 *Canal:* 
+┆⍣ ${yt_play[0].author.url}
+┆
+┆⍣ 🏕 *ID:* 
+┆⍣ ${yt_play[0].videoId}
+┆
+┆⍣ 🌤 *Tipo:*
+┆⍣ ${yt_play[0].type}
+┆
+┆⍣ 🔗 *Link:* 
+┆⍣ ${yt_play[0].url}
+╰──────•◈•──────╯
+╔═════  ࿇  ═════╗
+┆⍣ *_Enviando ${additionalText}, Aguarde Un Momento. . . ．．．_*
+╰──────•◈•──────╯`.trim();
         conn.sendMessage(m.chat, {image: {url: yt_play[0].thumbnail}, caption: texto1}, {quoted: m});
     if (command == 'play') {
     try {    
