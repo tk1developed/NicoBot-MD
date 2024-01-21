1f51aa9e6184639b332945429e1719b588db0061
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-01-20 20:19:11 -0500

    Actualizar bot-.js

--- plugins/bot-.js
+++ plugins/bot-.js
@@ -20,7 +20,7 @@ await conn.sendFile(m.chat, media, 'gata.mp4', str, fkontak)}
 ['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘️', '/menu']
 ], m,)}*/
 
-handler.command = /^linkgc|grupos|grupossakurabot|sakurabotgrupos|gruposdesakurabot|groupofc|grupossb|gruposb|groupsb$/i
+handler.command = /^menus|menu|grupossakurabot|sakurabotgrupos|gruposdesakurabot|groupofc|grupossb|gruposb|groupsb$/i
 handler.exp = 33
 
 export default handler
