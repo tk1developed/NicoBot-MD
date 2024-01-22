d6666ea1ec6e261713c90c45b51f53916de5fb71
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-01-22 15:49:32 -0500

    Actualizar menu-menu.js

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -26,12 +26,6 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
 ┣⚶ *💎Diamantes:* 20
 ┣⚶ *💰SakuCoins:* 15
 ┣⚶ *🪙Tokens:* 2
- ▢ *Nivel:* ${level}
- ▢ *Experiencia:* ${exp}
- ▢ *Rango:* ${role}
- ▢ *Diamantes:* ${limit}
- ▢ *MysticCoins:* ${money}
- ▢ *Tokens:* ${joincount}
 ╰°•°•°•°•°•°•°•°∞°•°•°•°•°•°•°•°
 
 ╔─━━ *SOLUCIONES* ━━─╗
