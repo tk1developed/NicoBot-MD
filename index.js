c360c3f777ed27c5435256c71b0bebeddaa37128
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-01-22 18:09:57 -0500

    Actualizar index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const {name, author} = require(join(__dirname, './package.json'));
 const {say} = cfonts;
 const rl = createInterface(process.stdin, process.stdout);
 
-say('SakuraLite\nWhatsApp Bot MD', {
+say('SakuraLite\nBot MD', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']});
