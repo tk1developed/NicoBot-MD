5c734b6d8543bf387c3de7a857f114d1ba560287
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-01-27 11:06:08 -0500

    Actualizar bot-love.js

--- plugins/bot-love.js
+++ plugins/bot-love.js
@@ -1,13 +1,13 @@
-
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let name = conn.getName(who)
 const pp = './Menu2.jpg'
 
-let love = `\t\t*MEDIDOR DE AMOR*
+let love = `\t\t*𝐸𝑙 𝑀𝑒𝑑𝑖𝑑𝑜𝑟 𝐷𝑒𝑙 𝐴𝑚𝑜𝑟✨*
 
-*El Amor De ${name} Por Ti Es De* *${Math.floor(Math.random() * 100)}%* *De Un 100%*
-*Deberias Pedirle Que Sea Tu  Novia/o ?*`
+𝑬𝒍 𝑨𝒎𝒐𝒓 𝑫𝒆 ${name} 𝑷𝒐𝒓 𝑻𝒊 𝑬𝒔 𝑫𝒆 *${Math.floor(Math.random() * 100)}%* 𝑫𝒆 𝑼𝒏 *100$*
+
+🌻𝑫𝒆𝒃𝒆𝒓𝒊𝒂𝒔 𝑷𝒆𝒅𝒊𝒓𝒍𝒆 𝑴𝒂𝒕𝒓𝒊𝒎𝒐𝒏𝒊𝒐?`
 
 conn.sendMessage(m.chat,{image: {url: pp}, caption: love, mentions: [m.sender]}, {quoted: m}) 
 }
