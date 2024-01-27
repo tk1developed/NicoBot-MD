9f0e62f12c41324bbdded64244f482d93523165a
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-01-26 20:17:18 -0500

    Actualizar propietario-reunion.js

--- plugins/propietario-reunion.js
+++ plugins/propietario-reunion.js
@@ -5,13 +5,14 @@ let handler = async(m, { conn, command, text }) => {
   if (!text) return m.reply(`🚀 ¡𝗖𝘂𝗮𝗹 𝗲𝘀 𝗲𝗹 𝗠𝗼𝘁𝗶𝘃𝗼 𝗽𝗮𝗿𝗮 𝗹𝗮 𝗿𝗲𝘂𝗻𝗶𝗼𝗻 𝗱𝗲𝗹 𝘀𝘁𝗮𝗳𝗳!`)
     if (text.length < 10) return m.reply(`¡𝗠𝗼𝘁𝗶𝘃𝗼 𝗺𝘂𝘆 𝗰𝗼𝗿𝘁𝗼 , 𝗺𝗶𝗻𝗶𝗺𝗼 10 𝗰𝗮𝗿𝗮𝗰𝘁𝗲𝗿𝗲𝘀!`)
 
-    let texto = `💖 @${m.sender.split`@`[0]} 𝐀 𝐒𝐨𝐥𝐢𝐜𝐢𝐭𝐚𝐝𝐨 𝐋𝐚 𝐏𝐫𝐞𝐬𝐞𝐧𝐜𝐢𝐚 𝐃𝐞 𝐋𝐨𝐬 𝐎𝐰𝐧𝐞𝐫 𝐏𝐨𝐫 𝐌𝐨𝐭𝐢𝐯𝐨: ¡${text}
+    let texto = `💖 @${m.sender.split`@`[0]} 𝐀 𝐒𝐨𝐥𝐢𝐜𝐢𝐭𝐚𝐝𝐨 𝐋𝐚 𝐏𝐫𝐞𝐬𝐞𝐧𝐜𝐢𝐚 𝐃𝐞 𝐋𝐨𝐬 𝐎𝐰𝐧𝐞𝐫 𝐏𝐨𝐫 𝐌𝐨𝐭𝐢𝐯𝐨: 
+¡${text}
 𝑺𝒕𝒂𝒇𝒇 𝑺𝒂𝒌𝒖𝒓𝒂𝑳𝒊𝒕𝒆 - 𝑫𝒊𝒆𝒈𝒐 🌻`
 m.reply('🚀 Sᴇ Lᴇ A Eɴᴠɪᴀᴅᴏ Lᴏs Mᴇɴsᴀᴊᴇs A Lᴏs Oᴡɴᴇʀ Dᴇ Sᴀᴋᴜʀᴀ-Bᴏᴛ-Lɪᴛᴇ-MD')
 for (let [jid] of global.owner.filter(([number, _, isDeveloper]) => isDeveloper && number)) {
                         let data = (await conn.onWhatsApp(jid))[0] || {}
                         if (data.exists)
-                        conn.sendPayment(data.jid, '999999999', texto, m)}
+                        conn.sendPayment(data.jid, '99', texto, m)}
   } catch (e) { await conn.reply(m.chat, '𝗖𝗼𝗺𝗮𝗻𝗱𝗼 𝗻𝗼 𝗳𝘂𝗻𝗰𝗶𝗼𝗻𝗮𝗹', m)
                console.log(e)
                } else {
