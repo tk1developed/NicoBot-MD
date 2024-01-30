3dd50cb56c68dd85c972c980a8ec7a4097e3cfc4
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-01-30 09:15:55 -0500

    Actualizar menu-menu.js

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -1,119 +1,61 @@
 import fs from 'fs'
-
 import fetch from 'node-fetch'
-
 import { xpRange } from '../lib/levelling.js'
-
 const { levelling } = '../lib/levelling.js'
-
 import PhoneNumber from 'awesome-phonenumber'
-
 import { promises } from 'fs'
-
 import { join } from 'path'
-
 let handler = async (m, { conn, usedPrefix, usedPrefix: _p, __dirname, text, command }) => {
-
 try {        
-
 let vn = './media/menu.mp3'
-
 let _package = JSON.parse(await promises.readFile(join(__dirname, '../package.json')).catch(_ => ({}))) || {}
-
 let { exp, limit, level, role } = global.db.data.users[m.sender]
-
 let { min, xp, max } = xpRange(level, global.multiplier)
-
 let name = await conn.getName(m.sender)
-
 let d = new Date(new Date + 3600000)
-
 let locale = 'es'
-
 process.once('message', resolve)
-
 setTimeout(resolve, 1000)
-
 }) * 1000
-
 }
-
 let { money, joincount } = global.db.data.users[m.sender]
-
 let user = global.db.data.users[m.sender]
-
 let muptime = clockString(_muptime)
-
 let uptime = clockString(_uptime)
-
 let totalreg = Object.keys(global.db.data.users).length
-
 let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length
-
 let replace = {
-
 '%': '%',
-
 p: _p, uptime, muptime,
-
 me: conn.getName(conn.user.jid),
-
 npmname: _package.name,
-
 npmdesc: _package.description,
 version: _package.version,
-
 exp: exp - min,
-
 maxexp: xp,
-
 totalexp: exp,
-
 xp4levelup: max - exp,
-
 github: _package.homepage ? _package.homepage.url || _package.homepage : '[unknown github url]',
-
 level, limit, name, weton, week, date, dateIslamic, time, totalreg, rtotalreg, role,
-
 readmore: readMore
-
 }
-
 text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
-
 //let user = global.db.data.users[m.sender]
-
 //user.registered = false
-
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-
 let mentionedJid = [who]
-
 let username = conn.getName(who)
-
 let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
-
 //let enlace = { contextInfo: { externalAdReply: {title: wm, body: 'support group' , sourceUrl: nna, thumbnail: await(await fetch(img)).buffer() }}}
-
   let pp = './Menu2.jpg'
-
 //let pp = gataVidMenu.getRandom()
-
 await conn.sendMessage(m.chat, {
 
-        text: `*Hey @${m.sender.split`@`[0]} estamos enviando el menu*
-
-𝘁𝗲𝗻 𝗽𝗮𝗰𝗶𝗲𝗻𝗰𝗶𝗮 𝘆 𝗲𝘃𝗶𝘁𝗮 𝗲𝗹 𝘀𝗽𝗮𝗺 👀
-
-𝐒𝐇𝐀𝐍𝐀 𝐁𝐎𝐓.`,
+        text: `𝐄𝐧𝐯𝐢𝐚𝐧𝐝𝐨 𝐄𝐥 𝐌𝐞𝐧𝐮✨.`,
 contextInfo: { 
-
           mentionedJid: [m.sender],
-
         }
-
       }, { quoted: m })
-
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 
 let menu = `╭✨️⸽⃕SᴀᴋᴜʀᴀBᴏᴛLɪᴛᴇ-MD🍁⃨፝⃕✰
