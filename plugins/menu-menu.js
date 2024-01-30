2ea710b8edbef701087f48e6b249061f65035df6
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-01-30 11:50:48 -0500

    Actualizar menu-menu.js

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -564,8 +564,8 @@ mediaType: 1,
 renderLargerThumbnail: true
 }}}, { quoted: m})
 } catch (e) {
-//await conn.sendButton(m.chat, `\n${wm}`, ['smsMalError3']() + '#report ' + usedPrefix + command, null, [[smsMensError1(), `#reporte ${smsMensError2} *${usedPrefix + command}*`]], m)
-console.log(`❗❗ ${smsMensError2} ${usedPrefix + command} ❗❗`)
+//await conn.sendButton(m.chat, `\n${wm}`, smsMalError3 + '#report ' + usedPrefix + command, null, smsMensError1, `#reporte smsMensError2 *${usedPrefix + command}*`]], m)
+console.log(`❗❗  ${usedPrefix + command} ❗❗`)
 console.log(e)        
 }}
 handler.help = ['menu', 'help', '?']
