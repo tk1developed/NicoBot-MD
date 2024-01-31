a45622ce41f5bdeac0328e57af8bb2cbe1a39000
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-01-30 19:57:27 -0500

    Actualizar menu-menu.js

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -514,8 +514,6 @@ ${readMore}
   }
 };
 handler.command = /^(menu|menú|memu|memú|help|info|comandos|allmenu|2help|menu1.2|ayuda|commands|commandos|cmd)$/i;
-handler.register = true
-export default handler
 handler.exp = 50;
 handler.fail = null;
 export default handler;
