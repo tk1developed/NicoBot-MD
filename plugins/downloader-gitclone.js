ed3ac72c15f9911e7b0bcb0ff15bdd7e6c0ebbf2
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-01-30 19:30:30 -0500

    Actualizar downloader-gitclone.js

--- plugins/downloader-gitclone.js
+++ plugins/downloader-gitclone.js
@@ -1,13 +1,13 @@
 import fetch from 'node-fetch';
 const regex = /(?:https|git)(?::\/\/|@)github\.com[\/:]([^\/:]+)\/(.+)/i;
 const handler = async (m, {args, usedPrefix, command}) => {
-  if (!args[0]) throw `*⏳️El Link?,No Soy Adivino Para Ponerlo. Manda El Link Del GitHub 𝙴𝙹𝙴𝙼𝙿𝙻𝙾: ${usedPrefix + command} https://github.com/diegojadibot/SakuraBot-MD*`;
+  if (!args[0]) throw `*⏳️𝐸𝑛𝑣𝑖𝑎 𝐸𝑙 𝐿𝑖𝑛𝑘 𝐷𝑒𝑙 𝐺𝑖𝑡ℎ𝑢𝑏 𝐷𝑒𝑙 𝐵𝑜𝑡 𝑄𝑢𝑒 𝑄𝑢𝑖𝑒𝑟𝑎𝑠 𝐷𝑒𝑠𝑐𝑎𝑟𝑔𝑎𝑟 𝐿𝑜𝑠 𝐴𝑟𝑐ℎ𝑖𝑣𝑜𝑧 𝐄𝐣𝐞𝐦𝐩𝐥𝐨: ${usedPrefix + command} https://github.com/diegojadibot/SakuraBotLite-MD*`;
   if (!regex.test(args[0])) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙻𝙸𝙽𝙺 𝙸𝙽𝙲𝙾𝚁𝚁𝙴𝙲𝚃𝙾!*';
   let [_, user, repo] = args[0].match(regex) || [];
   repo = repo.replace(/.git$/, '');
   const url = `https://api.github.com/repos/${user}/${repo}/zipball`;
   const filename = (await fetch(url, {method: 'HEAD'})).headers.get('content-disposition').match(/attachment; filename=(.*)/)[1];
-  m.reply(`*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚂𝙿𝙴𝚁𝙴 𝚄𝙽 𝙼𝙾𝙼𝙴𝙽𝚃𝙾 𝙴𝙽 𝙻𝙾 𝚀𝚄𝙴 𝙴𝙽𝚅𝙸𝙾 𝚂𝚄 𝙰𝚁𝙲𝙷𝙸𝚅𝙾, 𝚂𝙸 𝙴𝚂𝚃𝙴 𝙽𝙾 𝙴𝚂 𝙴𝙽𝚅𝙸𝙰𝙳𝙾 𝙿𝚄𝙴𝙳𝙴 𝙳𝙴𝙱𝙴𝚁𝚂𝙴 𝙰 𝚀𝚄𝙴 𝙴𝙻 𝚁𝙴𝙿𝙾𝚂𝙸𝚃𝙾𝚁𝙸𝙾 𝙴𝚂 𝙼𝚄𝚈 𝙿𝙴𝚂𝙰𝙳𝙾*`);
+  m.reply(`*🌼 𝑬𝒔𝒑𝒆𝒓𝒆 𝑬𝒍 𝑨𝒓𝒄𝒉𝒊𝒗𝒐 𝑺𝒆 𝑬𝒔𝒕𝒂 𝑬𝒏𝒗𝒊𝒂𝒏𝒅𝒐....*`);
   conn.sendFile(m.chat, url, filename, null, m);
 };
 handler.help = ['gitclone <url>'];
