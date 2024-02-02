372745fa8f554818229fb8f7343a32265e08e733
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-02-01 22:44:58 -0500

    Actualizar marker-lolice.js

--- plugins/marker-lolice.js
+++ plugins/marker-lolice.js
@@ -1,8 +1,7 @@
 const handler = async (m, {conn}) => {
   const who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
   conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/lolice', {
-    avatar: await conn.profilePictureUrl(who, 'image').catch((_) => '
-https://telegra.ph/file/85a22baa898684f034f52.jpg'),
+    avatar: await conn.profilePictureUrl(who, 'image').catch((_) => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
   }), 'error.png', '*🚔 Hᴇʏ Lʟᴀᴍᴇɴ A Lᴀ Pᴏʟɪᴄɪᴀ Hᴀʏ Uɴ Dᴇʟɪᴄᴜᴇɴᴛᴇ!!🚔*', m);
 };
 handler.help = ['lolice'];
