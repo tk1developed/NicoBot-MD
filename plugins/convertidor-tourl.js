75ab8bcfdd40628953322460e3d55fe2a177da01
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-02-01 22:54:46 -0500

    Actualizar convertidor-tourl.js

--- plugins/convertidor-tourl.js
+++ plugins/convertidor-tourl.js
@@ -3,11 +3,11 @@ import uploadImage from '../lib/uploadImage.js';
 const handler = async (m) => {
   const q = m.quoted ? m.quoted : m;
   const mime = (q.msg || q).mimetype || '';
-  if (!mime) throw '*🌵 Rᴇsᴘᴏɴᴅᴀ A Uɴᴀ Iᴍᴀɢᴇɴ O Uɴ Vɪᴅᴇᴏ Eʟ Cᴜᴀʟ Sᴇʀᴀ́ Cᴏɴᴠᴇʀᴛɪᴅᴏ Eɴ Eɴʟᴀᴄᴇ✨*';
+  if (!mime) throw '*⚓ 𝐻𝑒𝑦 𝑅𝑒𝑠𝑝𝑜𝑛𝑑𝑒 𝐴 𝑈𝑛𝑎 𝐼𝑚𝑎𝑔𝑒𝑛 𝑂 𝑈𝑛 𝑉𝑖𝑑𝑒𝑜 𝐸𝑙 𝐶𝑢𝑎𝑙 𝑄𝑢𝑖𝑒𝑟𝑎𝑠 𝐶𝑜𝑛𝑐𝑒𝑟𝑡𝑖𝑟𝑙𝑜 𝐸𝑛 𝑈𝑛 𝐸𝑛𝑙𝑎𝑐𝑒🌱.*';
   const media = await q.download();
   const isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime);
   const link = await (isTele ? uploadImage : uploadFile)(media);
-  m.reply(`*𝙴𝙽𝙻𝙰𝙲𝙴 𝙰 𝚂𝚄 𝙰𝚁𝙲𝙷𝙸𝚅𝙾:* ${link}`);
+  m.reply(`*⚓ 𝑆𝑢 𝐸𝑛𝑙𝑎𝑐𝑒 𝐷𝑒𝑙 𝐴𝑟𝑐ℎ𝑖𝑣𝑜:* ${link}`);
 };
 handler.help = ['tourl <reply image>'];
 handler.tags = ['sticker'];
