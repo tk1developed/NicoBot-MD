a4a16cfbf1f23ab40c1e5e37a26834bf86529c97
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-02-01 22:55:20 -0500

    Actualizar convertidor-tourl.js

--- plugins/convertidor-tourl.js
+++ plugins/convertidor-tourl.js
@@ -3,7 +3,7 @@ import uploadImage from '../lib/uploadImage.js';
 const handler = async (m) => {
   const q = m.quoted ? m.quoted : m;
   const mime = (q.msg || q).mimetype || '';
-  if (!mime) throw '*⚓ 𝐻𝑒𝑦 𝑅𝑒𝑠𝑝𝑜𝑛𝑑𝑒 𝐴 𝑈𝑛𝑎 𝐼𝑚𝑎𝑔𝑒𝑛 𝑂 𝑈𝑛 𝑉𝑖𝑑𝑒𝑜 𝐸𝑙 𝐶𝑢𝑎𝑙 𝑄𝑢𝑖𝑒𝑟𝑎𝑠 𝐶𝑜𝑛𝑐𝑒𝑟𝑡𝑖𝑟𝑙𝑜 𝐸𝑛 𝑈𝑛 𝐸𝑛𝑙𝑎𝑐𝑒🌱.*';
+  if (!mime) throw '*⚓ 𝐻𝑒𝑦 𝑅𝑒𝑠𝑝𝑜𝑛𝑑𝑒 𝐴 𝑈𝑛𝑎 𝐼𝑚𝑎𝑔𝑒𝑛 𝑂 𝑈𝑛 𝑉𝑖𝑑𝑒𝑜 𝐸𝑙 𝐶𝑢𝑎𝑙 𝑄𝑢𝑖𝑒𝑟𝑎𝑠 𝐶𝑜𝑛𝑣𝑒𝑟𝑡𝑖𝑟𝑙𝑜 𝐸𝑛 𝑈𝑛 𝐸𝑛𝑙𝑎𝑐𝑒🌱.*';
   const media = await q.download();
   const isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime);
   const link = await (isTele ? uploadImage : uploadFile)(media);
