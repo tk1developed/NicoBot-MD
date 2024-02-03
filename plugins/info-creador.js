50d353a70aac31b3573ff0adb77b7d23ee73b3fa
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-02-03 16:24:59 -0500

    Actualizar info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -5,6 +5,4 @@ await conn.sendMessage(m.chat, { contacts: { displayName: '♡᭄ 𝐷 𝐼 𝐸
 handler.help = ['owner']
 handler.tags = ['main']
 handler.command = ['owner', 'diego', 'creador', 'dueño'] 
-
-handler.register = true
 export default handler
