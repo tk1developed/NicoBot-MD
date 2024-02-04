752bcaf031764c148033c12d893f9dbc3c60ac90
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-02-03 19:58:33 -0500

    Actualizar gc-link.js

--- plugins/gc-link.js
+++ plugins/gc-link.js
@@ -3,9 +3,11 @@ const handler = async (m, {conn, args}) => {
   const group = m.chat;
   conn.reply(m.chat, 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group), m, {
     contextInfo: {externalAdReply: {mediaUrl: null, mediaType: 1, description: null,
-      
-      previewType: 0, thumbnail: fs.readFileSync('./Menu2.jpg'),
-      
+       title: '𝐿𝑖𝑛𝑘 𝐺𝑟𝑜𝑢𝑝',
+      body: '𝕾𝖆𝖐𝖚𝖗𝖆-𝕭𝖔𝖙-𝕷𝖎𝖙𝖊-𝕸𝕯🍁🌱',    
+      previewType: 0, thumbnail: fs.readFileSync('./Menu.png'),
+ sourceUrl: `https://atom.bio/sakuralite`}}});
+};     
 handler.help = ['linkgroup'];
 handler.tags = ['group'];
 handler.command = /^link(gro?up)?$/i;
