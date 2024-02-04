b1af20ee56eea8f5812d3df9355f9fc331e3b5a8
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-02-03 19:53:32 -0500

    Actualizar gc-link.js

--- plugins/gc-link.js
+++ plugins/gc-link.js
@@ -1,16 +1,15 @@
-import fs from 'fs';
-const handler = async (m, {conn, args}) => {
-  const group = m.chat;
-  conn.reply(m.chat, 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group), m, {
-    contextInfo: {externalAdReply: {mediaUrl: null, mediaType: 1, description: null,
-      title: '𝙻𝙸𝙽𝙺 𝙳𝙴𝙻 𝙶𝚁𝚄𝙿𝙾',
-      body: '❥ᰰຼ ⃟ᬽ៸𝙎𝙖𝙠𝙪𝙧𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿༒★»',
-      previewType: 0, thumbnail: fs.readFileSync('./Menu2.jpg'),
-      sourceUrl: `https://atom.bio/sakuralite`}}});
-};
-handler.help = ['linkgroup'];
-handler.tags = ['group'];
-handler.command = /^link(gro?up)?$/i;
-handler.group = true;
-handler.botAdmin = true;
-export default handler;
+var handler = async (m, { conn, args }) => {
+
+let group = m.chat
+let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group)
+conn.reply(m.chat, '🚩\v' + link, fkontak, m, { detectLink: true })
+
+}
+handler.help = ['link']
+handler.tags = ['grupo']
+handler.command = /^link(gro?up)?$/i
+
+handler.group = true
+handler.botAdmin = true
+
+export default handler
