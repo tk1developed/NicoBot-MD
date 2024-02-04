e442eec0479e1b1768d9d88d13fefb5b307539fd
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-02-03 19:56:27 -0500

    Actualizar gc-link.js

--- plugins/gc-link.js
+++ plugins/gc-link.js
@@ -1,15 +1,15 @@
-var handler = async (m, { conn, args }) => {
-
-let group = m.chat
-let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group)
-conn.reply(m.chat, '⚓️\v' + link, fkontak, m, { detectLink: true })
-
-}
-handler.help = ['link']
-handler.tags = ['grupo']
-handler.command = /^link(gro?up)?$/i
-
-handler.group = true
-handler.botAdmin = true
-
-export default handler
+import fs from 'fs';
+const handler = async (m, {conn, args}) => {
+  const group = m.chat;
+  conn.reply(m.chat, 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group), m, {
+    contextInfo: {externalAdReply: {mediaUrl: null, mediaType: 1, description: null,
+      
+      previewType: 0, thumbnail: fs.readFileSync('./Menu2.jpg'),
+      sourceUrl: `https://atom.bio/sakuralite`}}});
+};
+handler.help = ['linkgroup'];
+handler.tags = ['group'];
+handler.command = /^link(gro?up)?$/i;
+handler.group = true;
+handler.botAdmin = true;
+export default handler;
