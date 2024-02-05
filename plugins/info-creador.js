816aae5f8ad42920ab866b04f26c4785d30a1956
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-02-05 17:33:44 -0500

    Actualizar info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,9 +1,25 @@
-let handler = async (m, { conn, usedPrefix, isOwner }) => {
-let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;♡᭄ 𝐷 𝐼 𝐸 𝐺 𝑂 ╰‿╯;;\nFN:♡᭄ 𝐷 𝐼 𝐸 𝐺 𝑂 ╰‿╯\nORG:♡᭄ 𝐷 𝐼 𝐸 𝐺 𝑂 ╰‿╯\nTITLE:\nitem1.TEL;waid=573107133321:+57 310 7133321\nitem1.X-ABLabel:ALS⁩\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:♡᭄ 𝐷 𝐼 𝐸 𝐺 𝑂 ╰‿╯\nEND:VCARD`
-await conn.sendMessage(m.chat, { contacts: { displayName: '♡᭄ 𝐷 𝐼 𝐸 𝐺 𝑂 ╰‿╯', contacts: [{ vcard }] }}, {quoted: m})
+import fetch from 'node-fetch'
+
+var handler = async (m, { conn, usedPrefix, text, args, command }) => {
+
+let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+let pp = await conn.profilePictureUrl(who).catch(_ => '')
+let name = await conn.getName(who)
+let biografia = await conn.fetchStatus('5217294888993' +'@s.whatsapp.net').catch(_ => 'Sin Biografía')
+let biografiaBot = await conn.fetchStatus('5214531287294' +'@s.whatsapp.net').catch(_ => 'Sin Biografía')
+let bio = biografia.status?.toString() || 'Sin Biografía'
+let biobot = biografiaBot.status?.toString() || 'Sin Biografía'
+
+await conn.sendContactArray(m.chat, [
+[devnum, `${await conn.getName('573107133321'+'@s.whatsapp.net')}`, `🐢 𝑪𝒓𝒂𝒂𝒅𝒐𝒓`, dev, email, `🇨🇴 Colombia`, `https://www.youtube.com/@SakuraBotLite`, bio],
+[`${conn.user.jid.split('@')[0]}`, `${await conn.getName(conn.user.jid)}`, `🍓 𝐒𝐚𝐤𝐮𝐫𝐚𝐁𝐨𝐭𝐋𝐢𝐭𝐞-𝐌𝐃`, `🦋 Es Un Bot No Agas Spam`, email, `🇲🇽 México`, `https://github.com/diegojadibot/SakuraBotLite-MD`, biobot]
+], m)
+
 }
-handler.help = ['owner']
-handler.tags = ['main']
-handler.command = ['owner', 'diego', 'creador', 'dueño'] 
+handler.help = ['owner', 'contacto', 'creador', 'contactos']
+handler.tags = ['info']
+handler.command = /^(owner|contacto|creador|contactos)/i
+
 handler.register = true
+
 export default handler
