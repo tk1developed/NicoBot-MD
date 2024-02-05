95a3c6c21c29e250fc4d0f685e74bb88a977f02e
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-02-05 10:43:41 -0500

    Actualizar downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -3,7 +3,7 @@ let crime = 500
 let diamante = 10
 const handler = async (m, {conn, usedPrefix, command, groupMetadata, participants, isPrems}) => {
 const date = global.db.data.users[m.sender].crime + 3600000; //3600000 = 1 hs
-if (new Date - global.db.data.users[m.sender].crime < 3600000) return m.reply(`🚓︎ 𝐋𝐀 𝐏𝐨𝐥𝐢𝐜𝐢𝐚 𝐄𝐬𝐭𝐚 𝐕𝐢𝐠𝐢𝐥𝐚𝐧𝐝𝐨 𝐄𝐧 𝐄𝐬𝐭𝐨𝐬 𝐌𝐨𝐧𝐞𝐧𝐭𝐨𝐬. 𝐁𝐮𝐞𝐥𝐯𝐞 𝐄𝐧: ${msToTime(date - new Date())}`)
+if (new Date - global.db.data.users[m.sender].crime < 3600000) return m.reply(`🦋 𝐋𝐚 𝐏𝐨𝐥𝐢𝐜𝐢𝐚 𝐄𝐬𝐭𝐚 𝐕𝐢𝐠𝐢𝐥𝐚𝐧𝐝𝐨 𝐄𝐧 𝐄𝐬𝐭𝐨𝐬 𝐌𝐨𝐧𝐞𝐧𝐭𝐨𝐬. 𝐓𝐞𝐠𝐫𝐞𝐬𝐚 𝐄𝐧: ${msToTime(date - new Date())}`)
 let randow
 if (m.isGroup) randow = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
 else randow = m.chat
@@ -17,8 +17,8 @@ const money = Math.floor(Math.random() * 9000)
 let or = ['text', 'text2', 'text3', 'text4', 'text5']; 
 let media = or[Math.floor(Math.random() * 4)]
 global.db.data.users[m.sender].crime = new Date * 1;
-if (media === 'text') return m.reply(`《💰》${pickRandom(global.robar)} ${exp} XP`).catch(global.db.data.users[m.sender].exp += exp) 
-if (media === 'text2') return m.reply(`《🚓》${pickRandom(global.robmal)} ${exp} XP`).catch(global.db.data.users[m.sender].exp -= crime) 
+if (media === 'text') return m.reply(`🍓 ${pickRandom(global.robar)} ${exp} XP`).catch(global.db.data.users[m.sender].exp += exp) 
+if (media === 'text2') return m.reply(`🚓 ${pickRandom(global.robmal)} ${exp} XP`).catch(global.db.data.users[m.sender].exp -= crime) 
 if (media === 'text3') return m.reply(`💰 *${pickRandom(global.robar)}*\n\n${diamond} 💎𝑫𝒊𝒂𝒎𝒂𝒏𝒕𝒆\n${money} 🐈 𝑺𝒂𝒌𝒖𝑪𝒐𝒊𝒏`).catch(global.db.data.users[m.sender].limit += diamond).catch(global.db.data.users[m.sender].money += money)
 if (media === 'text4') return m.reply(`🚓 ${pickRandom(global.robmal)}\n\n${diamond} 💎𝑫𝒊𝒂𝒎𝒂𝒏𝒕𝒆\n${money} 🐈 𝑺𝒂𝒌𝒖𝑪𝒐𝒊𝒏𝒔`).catch(global.db.data.users[m.sender].limit -= diamante).catch(global.db.data.users[m.sender].money -= crime)
 if (media === 'text5') return conn.reply(m.chat, `🍓 𝐿𝑒 𝐴𝑠 𝑅𝑜𝑏𝑎𝑑𝑜 𝐴 @${randow.split`@`[0]} 𝑈𝑛𝑎 𝐶𝑎𝑛𝑡𝑖𝑑𝑎𝑑 𝐷𝑒: ${exp} XP`, m, {contextInfo: {mentionedJid: [randow]}}).catch(global.db.data.users[m.sender].exp += exp).catch(global.db.data.users[randow].exp -= crime) 
