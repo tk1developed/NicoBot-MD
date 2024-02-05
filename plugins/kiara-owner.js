622402363e41af9eff8b7a523dbe4888e2c8d8a6
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-02-05 17:36:05 -0500

    Actualizar kiara-owner.js

--- plugins/kiara-owner.js
+++ plugins/kiara-owner.js
@@ -1,11 +1,1 @@
-function handler(m) {
-  const data = global.owner.filter(([id, isCreator]) => id && isCreator)
-  this.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
-
-}
-
-handler.help = ['owner']
-handler.tags = ['main']
-handler.command = ['contactos', 'contactanos', 'numeros', 'desarrolladores'] 
-handler.register = true
-export default handler
+//archivo eliminado
