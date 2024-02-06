af0dcef91ff8379f511ad8eeda1bc9138e3df71e
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-02-05 20:51:23 -0500

    Actualizar index.js

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ const {name, author} = require(join(__dirname, './package.json'));
 const {say} = cfonts;
 const rl = createInterface(process.stdin, process.stdout);
 
-say('WhatsApp\nBot MD', {
+say('WhatsApp\nBot Sakura', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']});
-say(`Bot Creado Por Zumy`, {
+say(`Bot Creado Por Zumy @diego`, {
   font: 'console',
   align: 'center',
   gradient: ['red', 'magenta']});
