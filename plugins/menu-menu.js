4b8b282b3256eae473d20a9e4a09da595f85b47d
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-02-05 19:11:27 -0500

    Actualizar menu-menu.js

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -491,6 +491,6 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
 };
 handler.help = ['menu'];
 handler.tags = ['menu'];
-handler.command = /^(menu|menú|COMANDOS|comandos)$/i;
+handler.command = /^(configuraciones)$/i;
 handler.register = true
 export default handler;
