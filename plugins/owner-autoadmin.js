5445cf99005c9ab81dc5406584c577a205f27514
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-02-07 10:17:23 -0500

    Actualizar owner-autoadmin.js

--- plugins/owner-autoadmin.js
+++ plugins/owner-autoadmin.js
@@ -1,15 +1,15 @@
-/* Creditos a https://github.com/unptoadrih15/UPABOT-MD */
+
 
 const handler = async (m, {conn, isAdmin}) => {
   if (m.fromMe) return;
-  if (isAdmin) throw '*✨️ Hola Creador Oficial, Ya Eres Admin En Este Grupo*';
+  if (isAdmin) throw '[🌹] 𝗛𝗢𝗟𝗔,𝗬𝗔 𝗘𝗦 𝗔𝗗𝗠𝗜𝗡 𝗗𝗘 𝗘𝗦𝗧𝗘 𝗚𝗥𝗨𝗣𝗢 𝗠𝗜 𝗢𝗪𝗡𝗘𝗥.';
   try {
     await conn.groupParticipantsUpdate(m.chat, [m.sender], 'promote');
   } catch {
-    await m.reply('*😺 No Fué Posible Darle Admin*');
+    await m.reply('🥀 𝐋𝐎 𝐒𝐈𝐄𝐍𝐓𝐎, 𝐍𝐎 𝐅𝐔𝐈 𝐂𝐀𝐏𝐀𝐙 𝐃𝐄 𝐃𝐀𝐑𝐋𝐄 𝐀𝐃𝐌𝐈𝐍🍂');
   }
 };
-handler.command = /^autoadmin$/i;
+handler.command = /^(tenerpoder|seradmin)$/i;
 handler.rowner = true;
 handler.group = true;
 handler.botAdmin = true;
