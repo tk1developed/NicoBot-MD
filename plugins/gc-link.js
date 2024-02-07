554248f76d922ea4dda809bc4f3ea49e3b9a5ec2
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-02-06 19:08:47 -0500

    Actualizar gc-link.js

--- plugins/gc-link.js
+++ plugins/gc-link.js
@@ -1,15 +1,16 @@
-
-let handler = async (m, { conn, groupMetadata }) => { 
-
- let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(m.chat)
- conn.reply(m.chat, `\n${mssg.linkGp} *${groupMetadata.subject}*\n\n${link}`, m, {detectLink: true})
-//conn.sendHydrated(m.chat, `\n${mssg.linkGp} *${groupMetadata.subject}*\n\n${link}`, packname, null, 'https://www.whatsapp.com/otp/copy/https://chat.whatsapp.com/' + await conn.groupInviteCode(m.chat), 'Copiar', null, null, [ null ], m)
-
-}
-handler.help = ['link']
-handler.tags = ['group']
-handler.command = ['linkgroup', 'link'] 
-handler.group = true
-handler.botAdmin = true
-
-export default handler
+import fs from 'fs';
+const handler = async (m, {conn, args}) => {
+  const group = m.chat;
+  conn.reply(m.chat, 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group), m, {
+    contextInfo: {externalAdReply: {mediaUrl: null, mediaType: 1, description: null,
+       title: '𝐿𝑖𝑛𝑘 𝐺𝑟𝑜𝑢𝑝',
+      body: '𝕾𝖆𝖐𝖚𝖗𝖆-𝕭𝖔𝖙-𝕷𝖎𝖙𝖊-𝕸𝕯🍁🌱',    
+      previewType: 0, thumbnail: fs.readFileSync('./Menu.png'),
+ sourceUrl: `https://atom.bio/sakuralite`}}});
+};     
+handler.help = ['linkgroup'];
+handler.tags = ['group'];
+handler.command = /^link(gro?up)?$/i;
+handler.group = true;
+handler.botAdmin = true;
+export default handler;
