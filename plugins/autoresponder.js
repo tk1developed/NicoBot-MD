460d34eb6d7576fcfa921066644c6c0b23b03153
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-02-06 22:06:51 -0500

    Actualizar autoresponder.js

--- plugins/autoresponder.js
+++ plugins/autoresponder.js
@@ -5,7 +5,7 @@ handler.all = async function (m, {conn}) {
 let chat = global.db.data.chats[m.chat]
 
 if (m.mentionedJid.includes(this.user.jid) && m.isGroup && !chat.isBanned) {
-await this.sendMessage(m.chat, {text: `🍓 𝙌𝙐𝙀 𝙔𝙊 𝙌𝙐𝙀? 🦋`}, {quoted: m, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100})}
+await this.sendMessage(m.chat, {text: `𝙌𝙐𝙀 𝙔𝙊 𝙌𝙐𝙀?`}, {quoted: m, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100})}
 
 
 return !0 }
