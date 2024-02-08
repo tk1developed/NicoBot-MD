e7fdeda3d3c9d3041c27816b9038a918a0c65459
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-02-07 20:15:22 -0500

    Actualizar bot-menu-allmenu-hel.js

--- plugins/bot-menu-allmenu-hel.js
+++ plugins/bot-menu-allmenu-hel.js
@@ -2,7 +2,7 @@ let handler = (m) => m;
 handler.all = async function (m) {
 
 
-  if (/^diego|SK|sk|Diego|zumy$/i.test(m.text)) {
+  if (/^diego|Diego$/i.test(m.text)) {
     var GB = `𝑶𝒘𝒏𝒆𝒓  @${global.suittag} 𝑻𝒆 𝑨𝒏 𝑰𝒏𝒗𝒐𝒄𝒂𝒅𝒐 🍓`;
 
     m.reply(GB, m.chat, { mentions: conn.parseMention(GB)});
