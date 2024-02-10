5386a685079b9ec4520ec48477d4bf0ac46914a5
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-02-10 13:40:38 -0500

    Actualizar bot-.js

--- plugins/bot-.js
+++ plugins/bot-.js
@@ -4,7 +4,7 @@
 };
 handler.command = /^(handerror)$/i;
 handler.owner = true;
-export default handler;*/
+export default handler;
 
 var handler = async (m, { conn }) => {
 
@@ -15,4 +15,4 @@ conn.reply(m.chat, `prueba`)
 
 handler.command = /^(Fr)/i
 
-export default handler
+export default handler*/
