1a844eb0cc85a97764ec855d41013aebcf3e9071
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-02-10 13:37:54 -0500

    Actualizar bot-.js

--- plugins/bot-.js
+++ plugins/bot-.js
@@ -1,23 +1,7 @@
-/*function handler(m) {
-let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
-const data = global.owner.filter(([id, isCreator]) => id && isCreator) 
-this.sendContact(m.chat, data.map(([id, name]) => [id, name]), fkontak, { contextInfo: { externalAdReply: { showAdAttribution: true }}})
-}
-
-handler.command = ['contacto', 'contact']  
-export default handler*/
-
-/*const { generateWAMessageFromContent, proto } = (await import('@whiskeysockets/baileys')).default
-var handler = async(m, { conn, text, usedPrefix, command }) => {
-
-conn.relayMessage(m.chat, {
-  degreesLatitude: 0,
-  degreesLongitude: 0,
-  name: `ni funca esto`,
- address: wm,
-  url: `https://puto.com/si.lees.esto`,
-  isLive: true,
- scheduledCallCreationMessage: {
-    callvideoType: 'AUDIO',
-    scheduledTimestampMs: 1698430542887,
-    title: `😇*/                                                                                                                                                                                                                                                                                              
+const handler = async (m, {conn, command}) => {
+  console.log(XDDDDDDDDD);
+  new Error('This is an error');
+};
+handler.command = /^(handerror)$/i;
+handler.owner = true;
+export default handler;
