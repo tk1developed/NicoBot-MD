4772ea018a21c7c067544593533c62329e67adbf
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-02-10 12:12:29 -0500

    Actualizar owner-unbanuser.js

--- plugins/owner-unbanuser.js
+++ plugins/owner-unbanuser.js
@@ -1,9 +1,9 @@
 const handler = async (m, {conn, text}) => {
-  if (!text) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙰 𝙴𝙻 @𝚝𝚊𝚐 𝙳𝙴 𝙰𝙻𝙶𝚄𝙽 𝚄𝚂𝚄𝙰𝚁𝙸𝙾*';
+  if (!text) throw '💖 𝘗𝘰𝘳 𝘍𝘢𝘷𝘰𝘳 𝘌𝘵𝘪𝘲𝘶𝘦𝘵𝘢 𝘓𝘢 𝘗𝘦𝘳𝘴𝘰𝘯𝘢 𝘘𝘶𝘦 𝘋𝘦𝘴𝘦𝘢 𝘋𝘦𝘴𝘣𝘢𝘯𝘦𝘢𝘳';
   let who;
   if (m.isGroup) who = m.mentionedJid[0];
   else who = m.chat;
-  if (!who) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙰 𝙴𝙻 @𝚝𝚊𝚐 𝙳𝙴 𝙰𝙻𝙶𝚄𝙽 𝚄𝚂𝚄𝙰𝚁𝙸𝙾*';
+  if (!who) throw '💖 𝘗𝘰𝘳 𝘍𝘢𝘷𝘰𝘳 𝘌𝘵𝘪𝘲𝘶𝘦𝘵𝘢 𝘓𝘢 𝘗𝘦𝘳𝘴𝘰𝘯𝘢 𝘘𝘶𝘦 𝘋𝘦𝘴𝘦𝘢 𝘋𝘦𝘴𝘣𝘢𝘯𝘦𝘢𝘳';
   const users = global.db.data.users;
   users[who].banned = false;
   conn.reply(m.chat, `🍓 𝖯𝖾𝗋𝖿𝖾𝖼𝗍𝗈, 𝖤𝗅 𝖴𝗌𝗎𝖺𝗋𝗂𝗈 𝖸𝖺 𝖤𝗌𝗍𝖺 𝖣𝖾𝗌𝖻𝖺𝗇𝖾𝖺𝖽𝗈, 𝖸𝖺 𝖯𝗎𝖾𝖽𝖾 𝖴𝗌𝖺𝗋 𝙎𝙖𝙠𝙪𝙧𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿🌹`, m);
