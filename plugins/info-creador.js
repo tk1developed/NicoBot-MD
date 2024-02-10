40e67b5e972fb41a40fe5c384634529438cc1cdf
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-02-10 13:17:34 -0500

    Actualizar info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,4 +1,13 @@
 function handler(m) {
+let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
+const data = global.owner.filter(([id, isCreator]) => id && isCreator) 
+this.sendContact(m.chat, data.map(([id, name]) => [id, name]), fkontak, { contextInfo: { externalAdReply: { showAdAttribution: true }}})
+}
+
+handler.command = ['contacto', 'contact']  
+export default handler
+
+/*function handler(m) {
   const data = global.owner.filter(([id, isCreator]) => id && isCreator)
   this.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
 
@@ -8,4 +17,4 @@ handler.help = ['owner']
 handler.tags = ['main']
 handler.command = ['owner', 'contactanos', 'crestor', 'creadores'] 
 handler.register = true
-export default handler
+export default handler*/
