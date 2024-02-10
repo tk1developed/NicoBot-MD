e08fdaa2928a7989b5624ef191f2f34bc4f8b9b1
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-02-09 23:01:51 -0500

    Actualizar menu-menu.js

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -7,7 +7,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
   const {money, joincount} = global.db.data.users[m.sender];
   const {exp, limit, level, role} = global.db.data.users[m.sender];
   const pp = await conn.profilePictureUrl(conn.user.jid).catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png');
-  const fkon = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: `status@broadcast` } : {}) }, message: { 'contactMessage': { 'displayName': wm, 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:XL;${wm},;;;\nFN:${wm},\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabell:Ponsel\nEND:VCARD`, 'jpegThumbnail': imagen2, thumbnail: imagen2 ,sendEphemeral: true}}};
+  const fkon = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: `status@broadcast` } : {}) }, message: { 'contactMessage': { 'displayName': wm, 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:XL;${wm},;;;\nFN:${wm},\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabell:Ponsel\nEND:VCARD`, 'jpegThumbnail': imagen1, thumbnail: imagen1, sendEphemeral: true}}};
     await conn.reply(m.chat, '*🦋 𝘗𝘳𝘰𝘯𝘵𝘰 𝘚𝘦 𝘌𝘯𝘷𝘪𝘢𝘳𝘢 𝘌𝘭 𝘔𝘦𝘯𝘶 𝘋𝘦𝘭 𝘉𝘰𝘵...*',m, { contextInfo:{ forwardingScore: 2022, isForwarded: true, externalAdReply: {title: '👋🏻 𝐇𝐨𝐥𝐚!!', body: '🦋 𝖫𝗂𝗇𝖽𝖺 𝖬𝖺𝗇̃𝖺𝗇𝖺!', sourceUrl: global.md, thumbnail: await (await fetch(pp)).buffer() }}})
 //m.react('🍓');
     await conn.sendMessage(m.chat, { react: { text: '🚀', key: m.key } })
