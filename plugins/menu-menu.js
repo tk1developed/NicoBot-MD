f436e9ed62265ee2fa80953af233c7a6c59af6ec
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-02-10 16:57:50 -0500

    Actualizar menu-menu.js

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -16,7 +16,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
 ┋ 🍓𝐒𝐨𝐲 𝐒𝐮𝐛𝐛𝐨𝐭 𝐃𝐞: ${(conn.user.jid == global.conn.user.jid ? '' : `@${global.conn.user.jid.split`@`[0]}`) || 'No Soy Subbot'}
 ┋ 🌿𝐃𝐚𝐭𝐞: ${date} 
 ┋ 🦋𝐕𝐞𝐫𝐬𝐢𝐨𝐧 𝐃𝐞 𝐒𝐚𝐤𝐮𝐫𝐚: ${vs}
-┋ 💖𝐐𝐮𝐞 𝐋𝐢𝐧𝐝𝐨 𝐃𝐢𝐚!
+┋ 💖𝐐𝐮𝐞 𝐋𝐢𝐧𝐝𝐨 𝐃𝐢𝐚 𝐏𝐚𝐫𝐚 𝐓𝐢!
 ╰◆━━━━━━◆❃◆━━━━━━◆
 
 ╭∴━━━ *INFO USER* ━━━∴╮
