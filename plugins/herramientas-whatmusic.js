2dfc60a63f440fc5aae450385a0151080b448500
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-02-11 11:06:43 -0500

    Actualizar herramientas-whatmusic.js

--- plugins/herramientas-whatmusic.js
+++ plugins/herramientas-whatmusic.js
@@ -10,7 +10,7 @@ const handler = async (m) => {
   const q = m.quoted ? m.quoted : m;
   const mime = (q.msg || q).mimetype || '';
   if (/audio|video/.test(mime)) {
-    if ((q.msg || q).seconds > 20) return m.reply('[❗𝐈𝐍𝐅𝐎❗]\n\nEl archivo que carga es demasiado grande, le sugerimos que corte el archivo grande a un archivo más pequeño, 10-20 segundos Los datos de audio son suficientes para identificar');
+    if ((q.msg || q).seconds > 20) return m.reply('🌻 El archivo que carga es demasiado grande, le sugerimos que corte el archivo grande a un archivo más pequeño, 10-20 segundos Los datos de audio son suficientes para identificar');
     const media = await q.download();
     const ext = mime.split('/')[1];
     fs.writeFileSync(`./tmp/${m.sender}.${ext}`, media);
@@ -21,15 +21,15 @@ const handler = async (m) => {
     const txt = `
 𝚁𝙴𝚂𝚄𝙻𝚃𝙰𝙳𝙾𝚂 𝙳𝙴 𝙻𝙰 𝙱𝚄𝚂𝚀𝚄𝙴𝙳𝙰
 
-• 📌 𝚃𝙸𝚃𝚄𝙻𝙾: ${title}
-• 👨‍🎤 𝙰𝚁𝚃𝙸𝚂𝚃𝙰: ${artists !== undefined ? artists.map((v) => v.name).join(', ') : 'No encontrado'}
-• 💾 𝙰𝙻𝙱𝚄𝙼: ${album.name || 'No encontrado'}
-• 🌐 𝙶𝙴𝙽𝙴𝚁𝙾: ${genres !== undefined ? genres.map((v) => v.name).join(', ') : 'No encontrado'}
-• 📆 𝙵𝙴𝙲𝙷𝙰 𝙳𝙴 𝙻𝙰𝙽𝚉𝙰𝙼𝙸𝙴𝙽𝚃𝙾: ${release_date || 'No encontrado'}
+🕊️𝐍𝐨𝐦𝐛𝐫𝐞: ${title}
+🦋𝐂𝐚𝐧𝐭𝐚𝐧𝐭𝐞: ${artists !== undefined ? artists.map((v) => v.name).join(', ') : 'No encontrado'}
+✨𝐀𝐥𝐛𝐮𝐦: ${album.name || 'No encontrado'}
+🦎𝐆𝐞𝐧𝐞𝐫𝐨: ${genres !== undefined ? genres.map((v) => v.name).join(', ') : 'No encontrado'}
+🌻𝐒𝐮𝐛𝐢𝐝𝐨: ${release_date || 'No encontrado'}
 `.trim();
     fs.unlinkSync(`./tmp/${m.sender}.${ext}`);
     m.reply(txt);
-  } else throw '*[❗𝐈𝐍𝐅𝐎❗] 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙰 𝙰 𝚄𝙽 𝙰𝚄𝙳𝙸𝙾*';
+  } else throw '*🦋 𝘙𝘦𝘴𝘱𝘰𝘯𝘥𝘢 𝘈 𝘜𝘯 𝘈𝘶𝘥𝘪𝘰.*';
 };
 handler.command = /^quemusica|quemusicaes|whatmusic$/i;
 export default handler;
