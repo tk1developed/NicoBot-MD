ca284c0b62d04a90ecb602107feb3a948d3e2541
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-02-11 15:56:51 -0500

    Actualizar gc-listanum_kicknum.js

--- plugins/gc-listanum_kicknum.js
+++ plugins/gc-listanum_kicknum.js
@@ -1,5 +1,5 @@
 /*              Codigo Creado Por Bruno Sobrino
-      (https://github.com/BrunoSobrino/TheMystic-Bot-MD)
+      (https://github.com/diegojadibot/SakuraBotLite-MD)
 */
 
 const handler = async (m, {conn, args, groupMetadata, participants, usedPrefix, command, isBotAdmin, isSuperAdmin}) => {
