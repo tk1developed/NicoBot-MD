ed8670b8cc51867571e76dfc097a38ed4f05291e
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-02-12 19:11:28 -0500

    Actualizar owner-setprefix.js

--- plugins/owner-setprefix.js
+++ plugins/owner-setprefix.js
@@ -1,7 +1,7 @@
 const handler = async (m, {conn, text, usedPrefix, command}) => {
-  if (!text) throw `*[❗] No se encontro ningun prefijo, por favor ingrese el prefijo que quiera establecer. Ejemplo:* ${usedPrefix + command} /`;
+  if (!text) throw `*🦋 𝑁𝑜 𝑆𝑒 𝐸𝑛𝑐𝑜𝑛𝑡𝑟𝑜 𝑁𝑖𝑛𝑔𝑢𝑛 𝑃𝑟𝑒𝑓𝑖𝑗𝑜 𝑃𝑜𝑟 𝐹𝑎𝑣𝑜𝑟 𝐸𝑠𝑐𝑟𝑖𝑏𝑎 𝐸𝑙 𝑃𝑟𝑒𝑓𝑖𝑗𝑜. 𝐄𝐣𝐞𝐦𝐩𝐥𝐨:* ${usedPrefix + command} /`;
   global.prefix = new RegExp('^[' + (text || global.opts['prefix'] || '‎xzXZ/i!#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&.\\-').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']');
-  await m.reply(`*[❗] El prefijo actual del Bot se establecio a ${text}*`);
+  await m.reply(`*🦋 𝑷𝒓𝒆𝒇𝒊𝒋𝒐 𝑨𝒄𝒕𝒖𝒂𝒍𝒊𝒛𝒂𝒅𝒐, 𝑷𝒓𝒆𝒇𝒊𝒋𝒐: ${text}*`);
 };
 handler.help = ['setprefix'].map((v) => v + ' [prefix]');
 handler.tags = ['owner'];
