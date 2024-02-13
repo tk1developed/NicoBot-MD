4624bad6f1f8ce8218691e13e863429ce16bb2c2
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-02-13 12:36:34 -0500

    Actualizar instalarbot.js

--- plugins/instalarbot.js
+++ plugins/instalarbot.js
@@ -3,33 +3,45 @@ import fetch from 'node-fetch';
 const handler = async (m, { conn, args }) => {
    const res = await fetch('https://api.github.com/repos/diegojadibot/SakuraBotLite-MD');
    const json = await res.json();
-   let txt = `🌱𝐼𝑛𝑠𝑡𝑎𝑙𝑎𝑐𝑖𝑜𝑛 𝑇𝑒𝑟𝑚𝑢𝑥 𝐴𝑢𝑡𝑜𝑚𝑎𝑡𝑖𝑐𝑎⚓
-   ❀ termux-setup-storage
-   ❀ apt update -y && yes | apt upgrade && pkg install -y bash wget mpv && wget -O - https://raw.githubusercontent.com/diegojadibot/SakuraBotLite-MD/master/sakura.sh | bash
+   let txt = `🍓 𝙄𝙣𝙨𝙩𝙖𝙡𝙖𝙘𝙞𝙤𝙣 𝘼𝙪𝙩𝙤𝙢𝙖𝙩𝙞𝙫𝙖 (𝚃𝙴𝚁𝙼𝚄𝚇)
+   
+➪ termux-setup-storage
 
-🌱 𝐼𝑛𝑠𝑡𝑎𝑙𝑎𝑐𝑖𝑜𝑛 𝑀𝑎𝑛𝑢𝑎𝑙 𝑇𝑒𝑟𝑚𝑢𝑥 ⚓
-    ❀ cd && termux-setup-storage
-    ❀ apt-get update -y && apt-get upgrade -y
-    ❀ pkg install -y git nodejs ffmpeg imagemagick && pkg install yarn 
-    ❀ git clone https://github.com/diegojadibot/SakuraBotLite-MD.git && cd SakuraBotLite-MD
-    ❀ yarn install
-    ❀ npm install
-    ❀ npm update
-    ❀ npm start
+➪ apt update -y && yes | apt upgrade && pkg install -y bash wget mpv && wget -O - https://raw.githubusercontent.com/diegojadibot/SakuraBotLite-MD/master/sakura.sh | bash
 
-⚓𝑆𝑒 𝐴𝑝𝑎𝑔𝑜 𝐸𝑙 𝐵𝑜𝑡? 𝐶𝑜𝑚𝑎𝑛𝑑𝑜 𝑃𝑎𝑟𝑎 𝐸𝑛𝑐𝑒𝑛𝑑𝑒𝑟𝑙𝑜🌱
-   ❀ cd
-   ❀ cd SakuraBotLite-MD
-   ❀ npm start
+🦋 𝙄𝙣𝙨𝙩𝙖𝙡𝙘𝙞𝙤𝙣 𝙈𝙖𝙣𝙪𝙖𝙡 (𝚃𝙴𝚁𝙼𝚄𝚇) 🍓
 
-⚓𝑃𝑒𝑑𝑖𝑟 𝑂𝑡𝑟𝑜 𝑄𝑟 𝑉𝑖𝑎 𝑇𝑒𝑟𝑚𝑢𝑥🌱
+➪ cd && termux-setup-storage
+
+➪ apt-get update -y && apt-get upgrade -y
+
+➪ pkg install -y git nodejs ffmpeg imagemagick && pkg install yarn 
+
+➪ git clone https://github.com/diegojadibot/SakuraBotLite-MD.git && cd SakuraBotLite-MD
+
+➪ yarn install
+
+➪ npm install
+
+➪ npm update
+
+➪ npm start
+
+🟢 𝐀𝐜𝐭𝐢𝐯𝐚𝐫 𝐄𝐥 𝐁𝐨𝐭 🟢
+   ➪ cd
+
+   ➪ cd SakuraBotLite-MD
+
+   ➪ npm start
+
+💖 𝙊𝙗𝙩𝙚𝙣𝙚𝙧 𝙊𝙩𝙧𝙤 𝙌𝙧 😻
    ❀ cd SakuraBotLite-MD
    ❀ rm -rf SakuraBotSession
    ❀ npm start
 
-✨ 𝘓𝘰𝘴 𝘊𝘰𝘮𝘢𝘯𝘥𝘰𝘴 𝘚𝘰𝘯 1 𝘗𝘰𝘳 1 𝘝𝘪𝘢 𝘛𝘦𝘳𝘮𝘶𝘹 🌻
+🟢 (𝙲𝙾𝙼𝙰𝙽𝙳𝙾𝚂 1 𝙿𝙾𝚁 1) 🦋
     
-    🌱 𝑻𝒊𝒆𝒏𝒆𝒔 𝑫𝒖𝒅𝒂 𝑺𝒐𝒃𝒓𝒆 𝑬𝒍 𝑩𝒐𝒕 𝑪𝒐𝒏𝒕𝒂𝒄𝒕𝒂 𝑨𝒍 𝑪𝒓𝒆𝒂𝒅𝒐𝒓 𝑪𝒐𝒏 𝑬𝒍 𝑺𝒊𝒈𝒖𝒊𝒆𝒏𝒕𝒆 𝑪𝒐𝒎𝒂𝒏𝒅𝒐: #𝑐𝑟𝑒𝑎𝑑𝑜𝑟 ⚓  `;
+    🍓 𝑆𝑖 𝑇𝑖𝑒𝑛𝑒𝑠 𝐷𝑢𝑑𝑎 𝑆𝑜𝑏𝑟𝑒 𝑀𝑖 𝐶𝑟𝑒𝑎𝑑𝑜𝑟 𝑂 𝐷𝑒𝑙 𝐵𝑜𝑡 𝐶𝑜𝑛𝑡𝑎𝑐𝑡𝑎𝑙𝑜 𝐸𝑠𝑐𝑟𝑖𝑏𝑒: #𝑐𝑟𝑒𝑎𝑑𝑜𝑟 ⚓  `;
    await conn.sendMessage(m.chat, {text: txt.trim(), mentions: [...txt.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), contextInfo: {forwardingScore: 9999999, isForwarded: true, mentionedJid: [...txt.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "renderLargerThumbnail": true, "title": global.titulowm2, "containsAutoReply": true, "mediaType": 1, "thumbnail": imagen6, "mediaUrl": global.md, "sourceUrl": global.md}}}, {quoted: m});
 };
 handler.help = ['scbot'];
