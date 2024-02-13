70c0acfb77fff2c31356495a70dd4ed2e791b694
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-02-12 23:12:51 -0500

    Actualizar convertidor-tourl.js

--- plugins/convertidor-tourl.js
+++ plugins/convertidor-tourl.js
@@ -5,7 +5,7 @@ const handler = async (m) => {
   const mime = (q.msg || q).mimetype || '';
   if (!mime) throw '*⚓ 𝐻𝑒𝑦 𝑅𝑒𝑠𝑝𝑜𝑛𝑑𝑒 𝐴 𝑈𝑛𝑎 𝐼𝑚𝑎𝑔𝑒𝑛 𝑂 𝑈𝑛 𝑉𝑖𝑑𝑒𝑜 𝐸𝑙 𝐶𝑢𝑎𝑙 𝑄𝑢𝑖𝑒𝑟𝑎𝑠 𝐶𝑜𝑛𝑣𝑒𝑟𝑡𝑖𝑟𝑙𝑜 𝐸𝑛 𝑈𝑛 𝐸𝑛𝑙𝑎𝑐𝑒🌱.*';
   const media = await q.download();
-  const isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime);
+  const isTele = /image\/(jpg|jpe?g|gif)|video\/mp4/.test(mime);
   const link = await (isTele ? uploadImage : uploadFile)(media);
   m.reply(`*⚓ 𝑆𝑢 𝐸𝑛𝑙𝑎𝑐𝑒 𝐷𝑒𝑙 𝐴𝑟𝑐ℎ𝑖𝑣𝑜:* ${link}`);
 };
