e73152b66e6e2d1369c72cecf6a5a0b519325d6d
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-02-14 18:26:06 -0500

    Actualizar menu-owner.js

--- plugins/menu-owner.js
+++ plugins/menu-owner.js
@@ -2,26 +2,30 @@ import fetch from 'node-fetch';
 const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems}) => {
   if (usedPrefix == 'a' || usedPrefix == 'A') return;
   try {
-    const pp = imagen2;
+    const pp = imagen1;
     // let vn = './media/menu.mp3'
     const img = './Menu2.jpg';
     const d = new Date(new Date + 3600000);
-    const locale = 'es';
+    const locale = 'es-ES';
     const week = d.toLocaleDateString(locale, {weekday: 'long'});
-    const date = d.toLocaleDateString(locale, {day: 'numeric', month: 'long', year: 'numeric'});
+    const date = d.toLocaleDateString(locale, {day: '2-digit', month: '2-digit', year: 'numeric'});
     const _uptime = process.uptime() * 1000;
     const uptime = clockString(_uptime);
     const user = global.db.data.users[m.sender];
     const {money, joincount} = global.db.data.users[m.sender];
     const {exp, limit, level, role} = global.db.data.users[m.sender];
     const rtotalreg = Object.values(global.db.data.users).filter((user) => user.registered == true).length;
+    const rtotal = Object.entries(global.db.data.users).length || '0'
     const more = String.fromCharCode(8206);
     const readMore = more.repeat(850);
-    const url = global.md
     const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
     const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
-    const document = doc[Math.floor(Math.random() * doc.length)];
-    const str = `╭━━━━･❪ *PROPIETARIO* ❫ ･━━━━╮
+
+ await conn.sendMessage(m.chat, { react: { text: '👑', key: m.key } })
+
+
+const document = doc[Math.floor(Math.random() * doc.length)];
+ const str = `╭━━━━･❪ *PROPIETARIO* ❫ ･━━━━╮
 ┃💧 > *<funcion>*
 ┃💧 => *<funcion>*
 ┃💧 $ *<funcion>*
@@ -87,9 +91,8 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
 ┃💧 _!delcmd *<responder a sticker/imagen con comando o texto asignado>*_
 ┃💧 _!saveimage_
 │❢ _!pviewimage_
-╰•°• ✾ •°••°• ✾ •°••°• ✾ •°•°••°• ✾ •°•`.trim();
-    if (m.isGroup) {
-      // await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, { type: 'audioMessage', ptt: true})
+╰•°• ✾ •°••°• ✾ •°••°• ✾ •°•°••°• ✾ •°•`.trim();     if (m.isGroup) { 
+ // await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, { type: 'audioMessage', ptt: true})
       const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
       conn.sendMessage(m.chat, {image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: m});
     } else {
@@ -98,10 +101,11 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
       conn.sendMessage(m.chat, {image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: fkontak2});
     }
   } catch {
-    conn.reply(m.chat, '⚠️ 𝐸𝑟𝑟𝑜𝑟 𝐴𝑙 𝐸𝑛𝑣𝑖𝑎𝑟 𝐸𝑙 𝑀𝑒𝑛𝑢 𝑃𝑜𝑟 𝐹𝑎𝑣𝑜𝑟 𝐼𝑛𝑡𝑒𝑛𝑡𝑒 𝑀𝑎𝑠 𝑇𝑎𝑟𝑑𝑒. 𝑆𝑖 𝑆𝑖𝑔𝑢𝑒 𝐴𝑠𝑖 𝐶𝑜𝑛𝑡𝑎𝑐𝑡𝑎 𝐴𝑙 𝑆𝑡𝑎𝑓𝑓', m);
+    conn.reply(m.chat, '*🙌🏻 Error Al Enviar!.*', m);
   }
 };
-handler.command = /^(wtodo|ownermenu|menuowner)$/i;
+handler.command = /^(ownermenu|menucreador|creadormenu|menuowner)$/i;
+handler.register = true
 handler.exp = 50;
 handler.fail = null;
 export default handler;
