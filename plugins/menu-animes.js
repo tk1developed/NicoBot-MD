1ba7c3a42cafd306be2dcb51f51efc7979d6457f
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-02-14 18:17:19 -0500

    Actualizar menu-animes.js

--- plugins/menu-animes.js
+++ plugins/menu-animes.js
@@ -1,83 +1,90 @@
 import fetch from 'node-fetch';
 const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems}) => {
+  if (usedPrefix == 'a' || usedPrefix == 'A') return;
   try {
     const pp = imagen2;
     // let vn = './media/menu.mp3'
+    const img = './Menu2.jpg';
     const d = new Date(new Date + 3600000);
-    const locale = 'es';
+    const locale = 'es-ES';
     const week = d.toLocaleDateString(locale, {weekday: 'long'});
-    const date = d.toLocaleDateString(locale, {day: 'numeric', month: 'long', year: 'numeric'});
+    const date = d.toLocaleDateString(locale, {day: '2-digit', month: '2-digit', year: 'numeric'});
     const _uptime = process.uptime() * 1000;
     const uptime = clockString(_uptime);
     const user = global.db.data.users[m.sender];
     const {money, joincount} = global.db.data.users[m.sender];
     const {exp, limit, level, role} = global.db.data.users[m.sender];
     const rtotalreg = Object.values(global.db.data.users).filter((user) => user.registered == true).length;
+    const rtotal = Object.entries(global.db.data.users).length || '0'
     const more = String.fromCharCode(8206);
     const readMore = more.repeat(850);
     const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
     const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
-    const document = doc[Math.floor(Math.random() * doc.length)];
-    const str = `╭━━━･❪ *MENU/ANIME* ❫ ･━━━╮
-┣⟣❥🔮  _${usedPrefix}sakurita_
-┣⟣❥🔮  _${usedPrefix}anuel_
-┣⟣❥🔮  _${usedPrefix}karolg_
-┣⟣❥🔮  _${usedPrefix}antiguo_
-┣⟣❥🔮  _${usedPrefix}lolivid_
-┣⟣❥🔮  _${usedPrefix}loli_
-┣⟣❥🔮  _${usedPrefix}ppcouple_
-┣⟣❥🔮  _${usedPrefix}neko_
-┣⟣❥🔮  _${usedPrefix}waifu_
-┣⟣❥🔮  _${usedPrefix}akira_
-┣⟣❥🔮  _${usedPrefix}akiyama_
-┣⟣❥🔮  _${usedPrefix}anna_
-┣⟣❥🔮  _${usedPrefix}asuna_
-┣⟣❥🔮  _${usedPrefix}ayuzawa_
-┣⟣❥🔮  _${usedPrefix}boruto_
-┣⟣❥🔮  _${usedPrefix}chiho_
-┣⟣❥🔮  _${usedPrefix}chitoge_
-┣⟣❥🔮  _${usedPrefix}deidara_
-┣⟣❥🔮  _${usedPrefix}erza_
-┣⟣❥🔮  _${usedPrefix}elaina_
-┣⟣❥🔮  _${usedPrefix}eba_
-┣⟣❥🔮  _${usedPrefix}emilia_
-┣⟣❥🔮  _${usedPrefix}hestia_
-┣⟣❥🔮  _${usedPrefix}hinata_
-┣⟣❥🔮  _${usedPrefix}inori_
-┣⟣❥🔮  _${usedPrefix}isuzu_
-┣⟣❥🔮  _${usedPrefix}itachi_
-┣⟣❥🔮  _${usedPrefix}itori_
-┣⟣❥🔮  _${usedPrefix}kaga_
-┣⟣❥🔮  _${usedPrefix}kagura_
-┣⟣❥🔮  _${usedPrefix}kaori_
-┣⟣❥🔮  _${usedPrefix}keneki_
-┣⟣❥🔮  _${usedPrefix}kotori_
-┣⟣❥🔮  _${usedPrefix}kurumi_
-┣⟣❥🔮  _${usedPrefix}madara_
-┣⟣❥🔮  _${usedPrefix}mikasa_
-┣⟣❥🔮  _${usedPrefix}miku_
-┣⟣❥🔮  _${usedPrefix}minato_
-┣⟣❥🔮  _${usedPrefix}naruto_
-┣⟣❥🔮  _${usedPrefix}nezuko_
-┣⟣❥🔮  _${usedPrefix}sagiri_
-┣⟣❥🔮  _${usedPrefix}sasuke_
-┣⟣❥🔮  _${usedPrefix}sakura_
-┣⟣❥🔮  _${usedPrefix}cosplay_
-┗━━━━━━━━━━━━━━━━┛`.trim();
-    if (m.isGroup) {
-      // await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, { type: 'audioMessage', ptt: true})
+
+ await conn.sendMessage(m.chat, { react: { text: '🍨', key: m.key } })
+
+
+const document = doc[Math.floor(Math.random() * doc.length)];
+ const str = `╭━━━･❪ *MENU/ANIME* ❫ ･━━━╮
+┣⟣❥🔮  _!sakurita_
+┣⟣❥🔮  _!anuel_
+┣⟣❥🔮  _!karolg_
+┣⟣❥🔮  _!antiguo_
+┣⟣❥🔮  _!lolivid_
+┣⟣❥🔮  _!loli_
+┣⟣❥🔮  _!ppcouple_
+┣⟣❥🔮  _!neko_
+┣⟣❥🔮  _!waifu_
+┣⟣❥🔮  _!akira_
+┣⟣❥🔮  _!akiyama_
+┣⟣❥🔮  _!anna_
+┣⟣❥🔮  _!asuna_
+┣⟣❥🔮  _!ayuzawa_
+┣⟣❥🔮  _!boruto_
+┣⟣❥🔮  _!chiho_
+┣⟣❥🔮  _!chitoge_
+┣⟣❥🔮  _!deidara_
+┣⟣❥🔮  _!erza_
+┣⟣❥🔮  _!elaina_
+┣⟣❥🔮  _$!eba_
+┣⟣❥🔮  _!emilia_
+┣⟣❥🔮  _!hestia_
+┣⟣❥🔮  _!hinata_
+┣⟣❥🔮  _!inori_
+┣⟣❥🔮  _!isuzu_
+┣⟣❥🔮  _!itachi_
+┣⟣❥🔮  _!itori_
+┣⟣❥🔮  _!kaga_
+┣⟣❥🔮  _!kagura_
+┣⟣❥🔮  _!kaori_
+┣⟣❥🔮  _!keneki_
+┣⟣❥🔮  _!kotori_
+┣⟣❥🔮  _!kurumi_
+┣⟣❥🔮  _!madara_
+┣⟣❥🔮  _!mikasa_
+┣⟣❥🔮  _!miku_
+┣⟣❥🔮  _!minato_
+┣⟣❥🔮  _!naruto_
+┣⟣❥🔮  _!nezuko_
+┣⟣❥🔮  _!sagiri_
+┣⟣❥🔮  _!sasuke_
+┣⟣❥🔮  _!sakura_
+┣⟣❥🔮  _!cosplay_
+┗━━━━━━━━━━━━━━━━┛`.trim();     if (m.isGroup) { 
+ // await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, { type: 'audioMessage', ptt: true})
       const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
-      conn.sendMessage(m.chat, {image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: fkontak2});
+      conn.sendMessage(m.chat, {image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: m});
     } else {
       // await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, { type: 'audioMessage', ptt: true})
       const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
       conn.sendMessage(m.chat, {image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: fkontak2});
     }
   } catch {
-    conn.reply(m.chat, '*🍨 Error*', m);
+    conn.reply(m.chat, '*[ ℹ️ ] Este menu tiene un error interno, por lo cual no fue posible enviarlo.*', m);
   }
 };
-handler.command = /^(animes|menuanimes)$/i;
+handler.command = /^(menu2|menuaudios)$/i;
+handler.register = true
 handler.exp = 50;
 handler.fail = null;
 export default handler;
