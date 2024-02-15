02ac34a2987cc4497e4c391eb44977944ea2b7b9
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-02-14 22:56:00 -0500

    Actualizar juego-ruleta.js

--- plugins/juego-ruleta.js
+++ plugins/juego-ruleta.js
@@ -1,8 +1,8 @@
 //CÓDIGO CREADO POR elrebelde21 : https://github.com/elrebelde21
 const handler = async (m, {conn, text, isPrems}) => {
-if (!db.data.chats[m.chat].game) throw `🍓 𝙀𝙨𝙩𝙚 𝙟𝙪𝙚𝙜𝙤𝙨 𝙚𝙨𝙩𝙖 𝙙𝙚𝙨𝙖𝙘𝙩𝙞𝙫𝙖𝙙𝙤 𝙥𝙤𝙧 𝙡𝙤𝙨 𝙖𝙙𝙢𝙞𝙣𝙨 𝙙𝙚𝙡 𝙂𝙧𝙪𝙥𝙤 𝙨𝙞 𝙩𝙪 𝙚𝙧𝙚𝙨 𝙖𝙙𝙢𝙞𝙣𝙨 𝙮 𝙦𝙪𝙞𝙚𝙧𝙚 𝙖𝙘𝙩𝙞𝙫𝙖𝙧𝙡𝙤 𝙪𝙨𝙖𝙧: #on juegos` 
+if (!db.data.chats[m.chat].game) throw `🍓 𝖤𝗌𝗍𝖾 𝖩𝗎𝖾𝗀𝗈 𝖤𝗌𝗍𝖺 𝖬𝗈𝖽𝗈 𝖮𝖿𝖿 𝖯𝗈𝗋 𝖫𝗈𝗌 𝖠𝖽𝗆𝗂𝗇𝗂𝗌𝗍𝗋𝖺𝖽𝗈𝗋𝖾𝗌, 𝖲𝗂 𝖤𝗋𝖾𝗌 𝖠𝖽𝗆𝗂𝗇 𝖴𝗌𝖺: #enable juegos` 
 const date = global.db.data.users[m.sender].juegos + 21600000; //21600000 = 6 hs 
-if (new Date - global.db.data.users[m.sender].juegos < 21600000) throw `⏰ 𝙀𝙨𝙥𝙚𝙧𝙖 : ${msToTime(date - new Date())} 𝙥𝙖𝙧𝙖 𝙫𝙤𝙡𝙫𝙚𝙧 𝙖 𝙟𝙪𝙜𝙖𝙧 `
+if (new Date - global.db.data.users[m.sender].juegos < 21600000) throw `⏰ 𝐄𝐬𝐩𝐞𝐫𝐚 : ${msToTime(date - new Date())} 𝐏𝐚𝐫𝐚 𝐒𝐞𝐠𝐮𝐢𝐫 𝐉𝐮𝐠𝐚𝐧𝐝𝐨 `
 let user = global.db.data.users[m.sender]
 const prem = Math.floor(Math.random() * 3600000)
 const exp = Math.floor(Math.random() * 10000)
@@ -11,12 +11,12 @@ const money = Math.floor(Math.random() * 10000)
 let rulet = ['text', 'text2', 'text3', 'text4', 'text5', 'text6']; 
 let ruleta = rulet[Math.floor(Math.random() * 6)]
 global.db.data.users[m.sender].juegos = new Date * 1;
-if (ruleta === 'text') return m.reply(`😺 𝙌𝙐𝙀 𝘽𝙐𝙀𝙉𝘼 𝙎𝙐𝙀𝙍𝙏𝙀 🐞🍀\n*𝙊𝙗𝙩𝙞𝙚𝙣𝙚 :* ${exp} XP`).catch(global.db.data.users[m.sender].exp += exp) 
-if (ruleta === 'text2') return m.reply(`😿 𝙉𝙊𝙊 𝙀𝙎𝙏𝘼𝙎 𝘿𝙀 𝙈𝘼𝙇𝘼 𝙎𝙐𝙀𝙍𝙏𝙀 𝘼𝘾𝘼𝘽𝘼 𝘿𝙀 𝙋𝙀𝙍𝘿𝙀𝙍 : ${exp} XP`).catch(global.db.data.users[m.sender].exp -= exp) 
-if (ruleta === 'text3') return conn.groupParticipantsUpdate(m.chat, [m.sender], 'demote').catch(m.reply(`🚀 𝙀𝙎𝙏𝘼𝙎 𝙍𝙀 𝙈𝘼𝙇𝘼 𝙌𝙐𝙀 𝙌𝙐𝙄𝙏𝙊 𝙀𝙇 𝙋𝙊𝘿𝙀𝙍 𝘼𝙃𝙊𝙍𝘼 𝙔𝘼 𝙉𝙊 𝙀𝙍𝙀𝙎 𝘼𝘿𝙈𝙄𝙉𝙎 𝙅𝙊𝘿𝙀𝙍𝙏𝙀 😹😹😹`)) 
-if (ruleta === 'text4') return conn.groupParticipantsUpdate(m.chat, [m.sender], 'promote').catch(m.reply(`😼 𝙀𝙎𝙏𝘼 𝙍𝙀 𝘽𝙐𝙀𝙉𝘼 💖, 𝘼𝙝𝙤𝙧𝙖 𝙚𝙧𝙚𝙨 𝙪𝙣 𝙖𝙙𝙢𝙞𝙣𝙞𝙨𝙩𝙧𝙖𝙙𝙤𝙧, 𝙚𝙡 𝙦𝙪𝙚 𝙦𝙪𝙞𝙩𝙖𝙧 𝙖𝙙𝙢𝙞𝙣𝙨 𝙚𝙨 𝙛𝙖𝙣 𝙙𝙚 𝙠𝙪𝙣𝙤 🌥`)) 
-if (ruleta === 'text5') return m.reply(`𝙒𝙐𝙐𝙐 𝙎𝙀𝙉̃𝙊𝙍 𝙀𝙎𝙏𝘼 𝘿𝙀 𝙎𝙐𝙀𝙍𝙏𝙀, 𝙑𝘼𝙔𝘼𝙍 𝘼 𝙅𝙐𝙂𝘼𝙍 𝘼𝙇 𝘾𝘼𝙎𝙄𝙉𝙊 🎰\n*𝙂𝘼𝙉𝘼𝙍𝙏𝙀 :* ${diamond} 💎`).catch(global.db.data.users[m.sender].diamond += diamond) 
-if (ruleta === 'text6') return m.reply(`🌹 𝙂𝙐𝘼𝘼 𝙀𝙎𝙏𝘼𝙎 𝙍𝙀 𝘽𝙐𝙀𝙉𝘼 𝙎𝙐𝙀𝙍𝙏𝙀 𝙂𝘼𝙉𝘼𝙍𝙏𝙀 𝙎𝙀𝙍 𝙋𝙍𝙀𝙈𝙄𝙐𝙈 𝙋𝙊𝙍 : 1 𝙃𝙤𝙧𝙖 ⏰`).catch(global.db.data.users[m.sender].premium += prem) 
+if (ruleta === 'text') return m.reply(`😻 𝑄𝑢𝑒 𝐵𝑢𝑒𝑛𝑎 𝑆𝑢𝑒𝑟𝑡𝑒 𝑇𝑖𝑒𝑛𝑒𝑠 🐞🍀\n*𝑹𝒆𝒄𝒍𝒂𝒎𝒂𝒔 :* ${exp} XP`).catch(global.db.data.users[m.sender].exp += exp) 
+if (ruleta === 'text2') return m.reply(`😿 𝑁𝑜𝑜 𝑄𝑢𝑒 𝑀𝑎𝑙𝑎 𝑆𝑢𝑒𝑟𝑡𝑒 𝑂𝑏𝑡𝑖𝑒𝑛𝑒𝑠 : ${exp} XP`).catch(global.db.data.users[m.sender].exp -= exp) 
+if (ruleta === 'text3') return conn.groupParticipantsUpdate(m.chat, [m.sender], 'demote').catch(m.reply(`🚀 𝐸𝑠𝑡𝑎𝑠 𝑅𝑒 𝑀𝑎𝑙𝑎 𝑇𝑒 𝑄𝑢𝑖𝑡𝑜 𝐸𝑙 𝑃𝑜𝑑𝑒𝑟 𝐷𝑒𝑙 𝐴𝑑𝑚𝑖𝑛 😹😹😹`)) 
+if (ruleta === 'text4') return conn.groupParticipantsUpdate(m.chat, [m.sender], 'promote').catch(m.reply(`😼 𝐸𝑟𝑒𝑠 𝐵𝑢𝑒𝑛𝑎 𝑆𝑢𝑒𝑟𝑡𝑒, 𝐴ℎ𝑜𝑟𝑎 𝐸𝑟𝑒𝑠 𝐴𝑑𝑚𝑖𝑛, 𝑆𝑖 𝑄𝑢𝑖𝑡𝑎𝑠 𝐴𝑑𝑚𝑖𝑛 𝐸𝑟𝑒𝑠 𝐹𝑎𝑛𝑠 𝐷𝑒 𝐾𝑢𝑛𝑛𝑜 🌥`)) 
+if (ruleta === 'text5') return m.reply(`𝐻𝑜𝑜 𝑆𝑒𝑛̃𝑜𝑟 𝐸𝑠𝑡𝑎 𝐷𝑒 𝐵𝑢𝑒𝑛𝑎 𝑆𝑢𝑒𝑟𝑡𝑒 𝑉𝑒 𝑌 𝐽𝑢𝑒𝑔𝑎 𝐸𝑛 𝐸𝑙 𝐶𝑎𝑛𝑠𝑖𝑛𝑜 🎰\n*𝑮𝒂𝒏𝒂𝒔𝒕𝒆𝒔 :* ${diamond} 💎`).catch(global.db.data.users[m.sender].diamond += diamond) 
+if (ruleta === 'text6') return m.reply(`🌹 𝑇𝑎𝑠 𝐷𝑒 𝑆𝑢𝑒𝑟𝑡𝑒 𝑂𝑏𝑡𝑖𝑒𝑛𝑒𝑠 𝑆𝑒𝑟 𝑃𝑟𝑒𝑚𝑖𝑢𝑚 𝑃𝑜𝑟 : 1 𝙃𝙤𝙧𝙖 ⏰`).catch(global.db.data.users[m.sender].premium += prem) 
 }
 handler.help = ['game'];
 handler.tags = ['xp'];
