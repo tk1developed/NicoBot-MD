f8fa460e0d1b7ae34d9824355e3c230942fca97f
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-02-16 11:53:14 -0500

    Actualizar owner-broadcast.js

--- plugins/owner-broadcast.js
+++ plugins/owner-broadcast.js
@@ -1,30 +1,60 @@
-const handler = async (m, {conn, isROwner, text}) => {
-  const delay = (time) => new Promise((res) => setTimeout(res, time));
-  const getGroups = await conn.groupFetchAllParticipating();
-  const groups = Object.entries(getGroups).slice(0).map((entry) => entry[1]);
-  const anu = groups.map((v) => v.id);
-  const pesan = m.quoted && m.quoted.text ? m.quoted.text : text;
-  if (!pesan) throw '*😻 𝙋𝙤𝙧 𝙁𝙖𝙫𝙤𝙧 𝙀𝙨𝙘𝙧𝙞𝙗𝙖 𝙐𝙣 𝙏𝙚𝙭𝙩𝙤! 🦋*';
-  // m.reply(`*😻 𝖬𝖾𝗇𝗌𝖺𝗃𝖾 𝖤𝗇𝗏𝗂𝖺𝖽𝗈 𝖠 : ${anu.length} 𝖦𝗋𝗎𝗉𝗈𝗌*\n\n*𝘗𝘶𝘦𝘥𝘦 𝘴𝘦𝘳 𝘲𝘶𝘦 𝘯𝘰 𝘴𝘦 𝘦𝘯𝘷𝘪𝘦 𝘢 𝘵𝘰𝘥𝘰𝘴 𝘭𝘰𝘴 𝘤𝘩𝘢𝘵𝘴 𝘱𝘰𝘳 𝘴𝘱𝘢𝘮 𝘰 𝘱𝘶𝘦𝘥𝘦 𝘮𝘢𝘯𝘥𝘢𝘳 𝘳𝘭 𝘣𝘰𝘵 𝘢𝘭 𝘴𝘰𝘱𝘰𝘳𝘵𝘦 🦋*`)
-  for (const i of anu) {
-    await delay(500);
-    conn.relayMessage(i,
-        {liveLocationMessage: {
-          degreesLatitude: 35.685506276233525,
-          degreesLongitude: 139.75270667105852,
-          accuracyInMeters: 0,
-          degreesClockwiseFromMagneticNorth: 2,
-          caption: '😻 𝖢𝗈𝗆𝗎𝗇𝗂𝖼𝖺𝖽𝗈 🦋\n\n' + pesan + '\n\n🟢 𝘌𝘴𝘵𝘢𝘮𝘰𝘴 𝘈𝘤𝘵𝘪𝘷𝘰𝘴',
-          sequenceNumber: 2,
-          timeOffset: 3,
-          contextInfo: m,
-        }}, {}).catch((_) => _);
+// codigo adaptado por: https://github.com/GataNina-Li & https://github.com/elrebelde21
+
+import {randomBytes} from 'crypto';
+
+const handler = async (m, {conn, command, participants, usedPrefix, text}) => {
+  if (!text) return '*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 𝙼𝙴𝙽𝚂𝙰𝙹𝙴 𝚀𝚄𝙴 𝚀𝚄𝙸𝙴𝚁𝙴 𝚃𝚁𝙰𝙼𝙸𝚃𝙴*';
+  const fkontak = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${conn.user.jid.split('@')[0]}:${conn.user.jid.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
+  const cc4 = text ? m : m.quoted ? await m.getQuotedObj() : false || m;
+  const teks4 = text ? text : cc4.text;
+  const groups2 = Object.keys(await conn.groupFetchAllParticipating());
+  const chats2 = Object.keys(global.db.data.users).filter((user) => user.endsWith('@s.whatsapp.net'));
+  await conn.reply(m.chat, '*[❗𝐈𝐍𝐅𝐎❗] 𝙼𝙴𝙽𝚂𝙰𝙹𝙴 𝙴𝙽𝚅𝙸𝙰𝙳𝙾 𝙰 𝚃𝙾𝙳𝙾𝚂 𝙻𝙾𝚂 𝙲𝙷𝙰𝚃𝚂*\n\n*𝐍𝐎𝐓𝐀: 𝙴𝚂 𝙿𝙾𝚂𝙸𝙱𝙻𝙴 𝚀𝚄𝙴 𝚃𝙴𝙽𝙶𝙰 𝙵𝙰𝙻𝙻𝙾𝚂 𝙴𝚂𝚃𝙴 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 𝚈 𝙽𝙾 𝚂𝙴 𝙴𝙽𝚅𝙸𝙴 𝙰 𝚃𝙾𝙳𝙾𝚂 𝙻𝙾𝚂 𝙲𝙷𝙰𝚃𝚂, 𝙳𝙸𝚂𝙲𝚄𝙻𝙿𝙴 𝙿𝙾𝚁 𝙴𝙻 𝙼𝙾𝙼𝙴𝙽𝚃𝙾*', m);
+  const start2 = new Date().getTime();
+  const usersTag2 = participants.map((u) => conn.decodeJid(u.id));
+  let totalPri2 = 0;
+  for (let i = 0; i < groups2.length; i++) {
+    const group = groups2[i];
+    const delay = i * 4000; // 4 seg
+    setTimeout(async () => {
+      await conn.reply(group, `*╔══❰ 𝐂𝐎𝐌𝐔𝐍𝐈𝐂𝐀𝐃𝐎 ❱══╗*\n\n` + teks4, {mentions: usersTag2}, {quoted: fkontak});
+    }, delay);
   }
-  m.reply(`*😻 𝖬𝖾𝗇𝗌𝖺𝗃𝖾 𝖤𝗇𝗏𝗂𝖺𝖽𝗈 𝖠: ${anu.length} 𝙶𝚁𝚄𝙿𝙾/𝚂*\n\n*𝘗𝘶𝘦𝘥𝘦 𝘴𝘦𝘳 𝘲𝘶𝘦 𝘯𝘰 𝘴𝘦 𝘦𝘯𝘷𝘪𝘦 𝘢 𝘵𝘰𝘥𝘰𝘴 𝘭𝘰𝘴 𝘤𝘩𝘢𝘵𝘴 𝘱𝘰𝘳 𝘴𝘱𝘢𝘮 𝘰 𝘱𝘶𝘦𝘥𝘦 𝘮𝘢𝘯𝘥𝘢𝘳 𝘳𝘭 𝘣𝘰𝘵 𝘢𝘭 𝘴𝘰𝘱𝘰𝘳𝘵𝘦 🦋*`);
+  for (const user of chats2) {
+    await new Promise((resolve) => setTimeout(resolve, 2000)); // 2 segundos
+    await conn.reply(user, `*╔══❰ 𝐂𝐎𝐌𝐔𝐍𝐈𝐂𝐀𝐃𝐎 ❱══╗*\n\n` + teks4, fkontak, null);
+    totalPri2++;
+    if (totalPri2 >= 500000) {
+      break;
+    }
+  }
+  const end2 = new Date().getTime();
+  const totalPrivate2 = chats2.length;
+  const totalGroups2 = groups2.length;
+  const total2 = totalPrivate2 + totalGroups2;
+  let time2 = Math.floor((end2 - start2) / 1000);
+  if (time2 >= 60) {
+    const minutes = Math.floor(time2 / 60);
+    const seconds = time2 % 60;
+    time2 = `${minutes} minutos y ${seconds} segundos`;
+  } else {
+    time2 = `${time2} segundos`;
+  }
+  await m.reply(`𝙼𝙴𝙽𝚂𝙰𝙹𝙴 𝙴𝙽𝚅𝙸𝙰𝙳𝙾 𝙰:
+\`\`\`𝙲𝙷𝙰𝚃 𝙿𝚁𝙸𝚅𝙰𝙳𝙾 >> ${totalPrivate2}\`\`\`
+\`\`\`𝙲𝙷𝙰𝚃 𝙶𝚁𝚄𝙿𝙾𝚂  >>   ${totalGroups2}\`\`\`
+\`\`\`𝙲𝙷𝙰𝚃𝚂 𝚃𝙾𝚃𝙰𝙻𝙴𝚂  >>   ${total2}\`\`\`\n\n*𝚃𝙸𝙴𝙼𝙿𝙾 𝚃𝙾𝚃𝙰𝙻 𝙳𝙴 𝙴𝙽𝚅𝙸𝙾 ${time2}*\n${totalPri2 >= 500000 ? `\n*𝐍𝐎𝐓𝐀: 𝙴𝚂 𝙿𝙾𝚂𝙸𝙱𝙻𝙴 𝚀𝚄𝙴 𝚃𝙴𝙽𝙶𝙰 𝙵𝙰𝙻𝙻𝙾𝚂 𝙴𝚂𝚃𝙴 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 𝚈 𝙽𝙾 𝚂𝙴 𝙴𝙽𝚅𝙸𝙴 𝙰 𝚃𝙾𝙳𝙾𝚂 𝙻𝙾𝚂 𝙲𝙷𝙰𝚃𝚂, 𝙳𝙸𝚂𝙲𝚄𝙻𝙿𝙴 𝙿𝙾𝚁 𝙴𝙻 𝙼𝙾𝙼𝙴𝙽𝚃𝙾*` : ''}`);
 };
-handler.help = ['broadcastgroup', 'bcgc'].map((v) => v + ' <teks>');
+handler.help = ['broadcast', 'bc'].map((v) => v + ' <teks>');
 handler.tags = ['owner'];
-handler.command = /^(bc)$/i;
+handler.command = /^(comunicar|comunicado|broadcastall|bc)$/i;
+
 handler.owner = true;
 
 export default handler;
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+const more = String.fromCharCode(8206);
+const readMore = more.repeat(4001);
+
+const randomID = (length) => randomBytes(Math.ceil(length * .5)).toString('hex').slice(0, length);
