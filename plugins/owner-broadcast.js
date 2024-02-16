c6eb41bae38a8f82bd4b99c63d4e953101a5c993
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-02-16 11:42:19 -0500

    Actualizar owner-broadcast.js

--- plugins/owner-broadcast.js
+++ plugins/owner-broadcast.js
@@ -1,28 +1,30 @@
-import fs from 'fs'
-let handler = async (m, { conn, text } ) => {  
-const delay = time => new Promise(res => setTimeout(res, time))
-let chats = Object.entries(conn.chats).filter(([jid, chat]) => !jid.endsWith('@g.us') && chat.isChats).map(v => v[0])
-if(!text) throw '*🍓 𝑰𝒏𝒈𝒓𝒆𝒔𝒂 𝑬𝒍 𝑻𝒆𝒙𝒕𝒐 𝑸𝒖𝒆 𝑸𝒖𝒊𝒆𝒓𝒆𝒔 𝑻𝒓𝒂𝒏𝒔𝒎𝒊𝒕𝒊𝒓 𝑨 𝑻𝒐𝒅𝒐 𝑪𝒉𝒂𝒕𝒔.*'
-let cc = text ? m : m.quoted ? await m.getQuotedObj() : false || m
-let teks = text ? text : cc.text
-for (let i of chats) {
-await delay(500)
-conn.relayMessage(i, 
-{ liveLocationMessage: {
-  degreesLatitude: 35.685506276233525,
-  degreesLongitude: 139.75270667105852,
-  accuracyInMeters: 0,
-degreesClockwiseFromMagneticNorth: 2,
-caption: '◈ ━━━━━━━ ⸙ ━━━━━━━ ◈\n\n' + teks + '\n\n*🦋 Texto Del Creador Del Bot*',
-sequenceNumber: 2,
-timeOffset: 3,
-contextInfo: m,
-}}, {}).catch(_ => _)
-    }
-m.reply(`*📑 𝑴𝒆𝒏𝒔𝒂𝒋𝒆 𝑬𝒏𝒗𝒊𝒂𝒅𝒐 𝑨 ${chats.length} 𝑪𝒉𝒂𝒕𝒔 𝑷𝒓𝒊𝒗𝒂𝒅𝒐𝒔*\n\n*🔔 𝑷𝒖𝒆𝒅𝒆 𝑷𝒓𝒐𝒃𝒐𝒄𝒂𝒓 𝑺𝒐𝒑𝒐𝒓𝒕𝒆 𝑨𝒔𝒊 𝑸𝒖𝒆 𝑵𝒐 𝑷𝒖𝒆𝒅𝒆 𝑬𝒏𝒗𝒊𝒂𝒓𝒔𝒆 𝑨 𝑻𝒐𝒅𝒐 𝑳𝒐𝒔 𝑪𝒉𝒂𝒕𝒔*`)
-}
-handler.help = ['broadcastchats', 'bcchats'].map(v => v + ' <teks>')
-handler.tags = ['owner']
-handler.command = /^(broadcastchats?|bc(hats?)?)$/i
-handler.rowner = true
-export default handler
+const handler = async (m, {conn, isROwner, text}) => {
+  const delay = (time) => new Promise((res) => setTimeout(res, time));
+  const getGroups = await conn.groupFetchAllParticipating();
+  const groups = Object.entries(getGroups).slice(0).map((entry) => entry[1]);
+  const anu = groups.map((v) => v.id);
+  const pesan = m.quoted && m.quoted.text ? m.quoted.text : text;
+  if (!pesan) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 𝙼𝙴𝙽𝚂𝙰𝙹𝙴 𝚀𝚄𝙴 𝚀𝚄𝙸𝙴𝚁𝙴 𝚃𝚁𝙰𝙼𝙸𝚃𝙴*';
+  // m.reply(`*[❗𝐈𝐍𝐅𝐎❗] 𝙼𝙴𝙽𝚂𝙰𝙹𝙴 𝙴𝙽𝚅𝙸𝙰𝙳𝙾 𝙰 ${anu.length} 𝙶𝚁𝚄𝙿𝙾/𝚂*\n\n*𝐍𝐎𝐓𝐀: 𝙴𝚂 𝙿𝙾𝚂𝙸𝙱𝙻𝙴 𝚀𝚄𝙴 𝚃𝙴𝙽𝙶𝙰 𝙵𝙰𝙻𝙻𝙾𝚂 𝙴𝚂𝚃𝙴 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 𝚈 𝙽𝙾 𝚂𝙴 𝙴𝙽𝚅𝙸𝙴 𝙰 𝚃𝙾𝙳𝙾𝚂 𝙻𝙾𝚂 𝙲𝙷𝙰𝚃𝚂, 𝙳𝙸𝚂𝙲𝚄𝙻𝙿𝙴 𝙿𝙾𝚁 𝙴𝙻 𝙼𝙾𝙼𝙴𝙽𝚃𝙾`)
+  for (const i of anu) {
+    await delay(500);
+    conn.relayMessage(i,
+        {liveLocationMessage: {
+          degreesLatitude: 35.685506276233525,
+          degreesLongitude: 139.75270667105852,
+          accuracyInMeters: 0,
+          degreesClockwiseFromMagneticNorth: 2,
+          caption: '[❗𝐂𝐎𝐌𝐔𝐍𝐈𝐂𝐀𝐃𝐎❗]\n\n' + pesan + '\n\n𝙀𝙎𝙏𝙀 𝙀𝙎 𝙐𝙉 𝘾𝙊𝙈𝙐𝙉𝙄𝘾𝘼𝘿𝙊 𝙊𝙁𝙄𝘾𝙄𝘼𝙇',
+          sequenceNumber: 2,
+          timeOffset: 3,
+          contextInfo: m,
+        }}, {}).catch((_) => _);
+  }
+  m.reply(`*[❗𝐈𝐍𝐅𝐎❗] 𝙼𝙴𝙽𝚂𝙰𝙹𝙴 𝙴𝙽𝚅𝙸𝙰𝙳𝙾 𝙰 ${anu.length} 𝙶𝚁𝚄𝙿𝙾/𝚂*\n\n*𝐍𝐎𝐓𝐀: 𝙴𝚂 𝙿𝙾𝚂𝙸𝙱𝙻𝙴 𝚀𝚄𝙴 𝚃𝙴𝙽𝙶𝙰 𝙵𝙰𝙻𝙻𝙾𝚂 𝙴𝚂𝚃𝙴 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 𝚈 𝙽𝙾 𝚂𝙴 𝙴𝙽𝚅𝙸𝙴 𝙰 𝚃𝙾𝙳𝙾𝚂 𝙻𝙾𝚂 𝙲𝙷𝙰𝚃𝚂, 𝙳𝙸𝚂𝙲𝚄𝙻𝙿𝙴 𝙿𝙾𝚁 𝙴𝙻 𝙼𝙾𝙼𝙴𝙽𝚃𝙾*`);
+};
+handler.help = ['broadcastgroup', 'bcgc'].map((v) => v + ' <teks>');
+handler.tags = ['owner'];
+handler.command = /^(broadcast|bc)(group|grup|gc)$/i;
+handler.owner = true;
+
+export default handler;
