556aa7ac3427e72fc5d31c9c2db62830163b919c
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-02-17 10:14:27 -0500

    Actualizar info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,20 +1,18 @@
-function handler(m) {
+let handler = async (m, { conn, usedPrefix, isOwner }) => {
+let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:Diego ✨️\nFN:Diego ✨️\nORG:Diego ✨️\nTITLE:\nitem1.TEL;waid=573013482814:573013482814\nitem1.X-ABLabel:Diego ✨️\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:Diego ✨️\nEND:VCARD`
+await conn.sendMessage(m.chat, { contacts: { displayName: 'Diego ✨️', contacts: [{ vcard }] }}, {quoted: m})
+}
+handler.help = ['owner']
+handler.tags = ['main']
+handler.command = ['owner', 'creator', 'creador', 'dueño'] 
+
+export default handler
+
+/*function handler(m) {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 const data = global.owner.filter(([id, isCreator]) => id && isCreator) 
 this.sendContact(m.chat, data.map(([id, name]) => [id, name]), fkontak, { contextInfo: { externalAdReply: { showAdAttribution: true }}})
 }
 
 handler.command = ['owner', 'creador']  
-export default handler
-
-/*function handler(m) {
-  const data = global.owner.filter(([id, isCreator]) => id && isCreator)
-  this.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
-
-}
-
-handler.help = ['owner']
-handler.tags = ['main']
-handler.command = ['owner', 'contactanos', 'crestor', 'creadores'] 
-handler.register = true
 export default handler*/
