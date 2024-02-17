085e7cf404440a63cb4db6d417bf7e521690e363
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-02-17 16:13:03 -0500

    Actualizar fun-love.js

--- plugins/fun-love.js
+++ plugins/fun-love.js
@@ -1,7 +1,7 @@
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let name = conn.getName(who)
-const pp = './Menu.png'
+const pp = './Menu2.jpg
 
 let love = `\t\t*➺ 𝐸𝑙 𝑀𝑒𝑑𝑖𝑑𝑜𝑟 𝐷𝑒𝑙 𝐴𝑚𝑜𝑟💘*
 
@@ -11,11 +11,13 @@ let love = `\t\t*➺ 𝐸𝑙 𝑀𝑒𝑑𝑖𝑑𝑜𝑟 𝐷𝑒𝑙 𝐴𝑚
 ┇➺ 🌻𝑫𝒆𝒃𝒆𝒓𝒊𝒂𝒔 𝑷𝒆𝒅𝒊𝒓𝒍𝒆 𝑴𝒂𝒕𝒓𝒊𝒎𝒐𝒏𝒊𝒐?
 ╰•–––––––––☆–––––––––•╯`
 
-conn.sendMessage(m.chat,{image: {url: pp}, caption: love, mentions: [m.sender]}, {quoted: m}) 
-}
+conn.sendMessage(m.chat, {text: love, mentions: [m.sender]}, {quoted: m})
 
-handler.help = ['love  @user']
-handler.tags = ['fun']
-handler.command = /^(love)$/i
+}
+handler.help = ['love']
+handler.tags = ['juegos']
+handler.command = /^(love|amor|poncentaje)$/i
+
 handler.register = true
+
 export default handler
