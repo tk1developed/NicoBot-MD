fd4d6a6eab590b8d63144609d1bc2bcdb3de00f0
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-02-17 18:34:13 -0500

    Actualizar _autobio.js

--- plugins/_autobio.js
+++ plugins/_autobio.js
@@ -1,4 +1,4 @@
-let handler = m => m
+/*let handler = m => m
 handler.all = async function (m) {
 let setting = global.db.data.settings[this.user.jid]
 
@@ -20,4 +20,75 @@ function clockString(ms) {
   let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
   let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
   return [d, ' • ', h, ' » ', m, ' » ', s].map(v => v.toString().padStart(2, 0)).join('') 
-}
+}*/
+
+if (command == 'status' || command == 'velocidad') {
+const { performance } = require('perf_hooks') 
+const osu = require('node-os-utils') 
+const used = process.memoryUsage()
+const cpus = os.cpus().map(cpu => {
+cpu.total = Object.keys(cpu.times).reduce((last, type) => last + cpu.times[type], 0)
+return cpu
+})
+const cpu = cpus.reduce((last, cpu, _, { length }) => {
+last.total += cpu.total
+last.speed += cpu.speed / length
+last.times.user += cpu.times.user
+last.times.nice += cpu.times.nice
+last.times.sys += cpu.times.sys
+last.times.idle += cpu.times.idle
+last.times.irq += cpu.times.irq
+return last
+}, {
+speed: 0,
+total: 0,
+times: {
+user: 0,
+nice: 0,
+sys: 0,
+idle: 0,
+irq: 0
+}})
+let timestamp = speed()
+let latensi = speed() - timestamp
+neww = performance.now()
+oldd = performance.now()
+const totalMemory = Math.round(os.totalmem() / (1024 * 1024 * 1024))
+const freeMemory = Math.round(os.freemem() / (1024 * 1024 * 1024))
+const usedMemory = totalMemory - freeMemory
+const cpuUsage = os.loadavg()[0]                
+let respon = `      \`⧼⧼⧼ 🚀 ＶＥＬＯＣＩＤＡＤ ⧽⧽⧽\`
+> ${latensi.toFixed(4)} Seg
+> ${oldd - neww} _milisegundos_
+                
+\`✅ INFORMACIÓN DEL BOT\`               
+➢ 𝙰𝙲𝚃𝙸𝚅𝙸𝙳𝙰𝙳 : ${runtime(process.uptime())}
+➢ 𝙶𝚁𝚄𝙿𝙾𝚂 𝙱𝙰𝙽𝙴𝙰𝙳𝙾𝚂: ${Object.entries(global.db.data.chats).filter(chat => chat[1].isBanned).length}
+➢ 𝚄𝚂𝚄𝙰𝚁𝙸𝙾𝚂 𝙱𝙰𝙽𝙴𝙰𝙳𝙾𝚂: ${Object.entries(global.db.data.users).filter(user => user[1].banned).length}
+➢ 𝚄𝚂𝚄𝙰𝚁𝙸𝙾𝚂 𝚃𝙾𝚃𝙰𝙻𝙴𝚂: ${Object.keys(global.db.data.users).length}
+
+\`💻 INFO DE SERVIDOR\`
+➢ RAM: ${formatp(os.totalmem() - os.freemem())} / ${formatp(os.totalmem())}
+➢ 𝙿𝙻𝙰𝚃𝙰𝙵𝙾𝚁𝙼𝙰 : ${os.platform()}
+${lenguaje.info.text4} ${os.hostname()}
+${lenguaje.info.text5} ${cpuUsage.toFixed(2)}%
+${lenguaje.info.text6} ${totalMemory} GB
+
+\`❐ Uso de memoria de NodeJS\`
+${Object.keys(used).map((key, _, arr) => `${key.padEnd(Math.max(...arr.map(v=>v.length)),' ')}: ${formatp(used[key])}`).join('\n')}
+
+${cpus[0] ? `\`❐ Uso total de CPU\`
+${cpus[0].model.trim()} (${cpu.speed} MHZ)\n${Object.keys(cpu.times).map(type => `- *${(type + '*').padEnd(6)}: ${(100 * cpu.times[type] / cpu.total).toFixed(2)}%`).join('\n')}
+_Uso de núcleo(s) de CPU (${cpus.length} CPU central)_
+${cpus.map((cpu, i) => `${i + 1}. ${cpu.model.trim()} (${cpu.speed} MHZ)\n${Object.keys(cpu.times).map(type => `- *${(type + '*').padEnd(6)}: ${(100 * cpu.times[type] / cpu.total).toFixed(2)}%`).join('\n')}`).join('\n\n')}` : ''}`.trim()
+conn.sendFile(m.chat, imagen1, 'lp.jpg', respon, m, false, { contextInfo: { externalAdReply: {title: "𝘐𝘔𝘍𝘖𝘙𝘔𝘈𝘊𝘐𝘖𝘕 𝘈𝘊𝘌𝘙𝘊𝘈 𝘋𝘌𝘓 𝘉𝘖𝘛", body: "Click para entrar", sourceUrl: md, thumbnail: imagen3 }}})}}
+
+module.exports = { info }
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+        fs.unwatchFile(file)
+        console.log(chalk.redBright(`Update ${__filename}`))
+        delete require.cache[file]
+        require(file)
+})
