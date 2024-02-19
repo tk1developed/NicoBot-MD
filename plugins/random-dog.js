dcc826b84a2c5c98a27f340a809625ebdeeecd19
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-02-18 21:25:58 -0500

    Actualizar random-dog.js

--- plugins/random-dog.js
+++ plugins/random-dog.js
@@ -3,7 +3,7 @@ const handler = async (m, {conn, text}) => {
   try {
     const res = await fetch('https://api.thedogapi.com/v1/images/search');
     const img = await res.json();
-    const caption = `_➢ ⃟⃞❆𖤍𝙔𝙚𝙅𝙤𝙩𝙖-𝙈𝘿𖤍𖣘 ⃟⃞❆_`.trim();
+    const caption = `_🦮 𝐃 𝐎 𝐆 - 𝐏 𝐄 𝐑 𝐑 𝐎 🐕‍🦺_`.trim();
     conn.sendFile(m.chat, img[0].url, 'dog.jpg', caption, m);
   } catch {
     throw '*Error!*';
