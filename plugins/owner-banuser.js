32f4c9e51da694e142e64778d1f735e63f07fece
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-02-21 21:13:45 -0500

    Actualizar owner-banuser.js

--- plugins/owner-banuser.js
+++ plugins/owner-banuser.js
@@ -1,3 +1,5 @@
+//creditos a gatadios
+
 var handler = async (m, { conn, text, usedPrefix, command}) => {
 
 let user, number, bot, bant, ownerNumber, aa, users, usr, q, mime, img
@@ -25,22 +27,22 @@ user = number + '@s.whatsapp.net'
 }} catch (e) {
 } finally {
 number = user.split('@')[0]
-if(user === conn.user.jid) return conn.reply(m.chat, `😺 *No puede ser baneado con este comando*`, m, { mentions: [user] })   
+if(user === conn.user.jid) return conn.reply(m.chat, `🐱 𝑳𝒐𝒔𝒊𝒆𝒏𝒕𝒐 𝑵𝒐 𝑴𝒆 𝑷𝒖𝒆𝒅𝒐 𝑩𝒂𝒏𝒆𝒂𝒓 𝒀𝒐 𝑴𝒊𝒔𝒎𝒐!`, m, { mentions: [user] })   
 for (let i = 0; i < global.owner.length; i++) {
 ownerNumber = global.owner[i][0];
 if (user.replace(/@s\.whatsapp\.net$/, '') === ownerNumber) {
 aa = ownerNumber + '@s.whatsapp.net'
-await conn.reply(m.chat, `🚀 *No puedo banear al propietario @${ownerNumber} de ${wm}*`, m, { mentions: [aa] })
+await conn.reply(m.chat, `🦋 *𝑁𝑜 𝑆𝑒 𝑃𝑢𝑒𝑑𝑒 𝐵𝑎𝑛𝑒𝑎𝑟 𝐸𝑙 𝑃𝑟𝑜𝑝𝑖𝑒𝑡𝑎𝑟𝑖𝑜 @${ownerNumber} de ${wm}*`, m, { mentions: [aa] })
 return
 }}
 users = global.db.data.users
-if (users[user].banned === true) conn.reply(m.chat, `🚩 *No es necesario volver a banear a @${number}*`, m, { mentions: [user] }) 
+if (users[user].banned === true) conn.reply(m.chat, `🐱𝙾𝙲𝚄𝚁𝚁𝙸𝙾 𝚄𝙽 𝙵𝙰𝙻𝙻𝙾 𝙰𝙻 𝙱𝙰𝙽𝙴𝙰𝚁 𝙰: @${number}*`, m, { mentions: [user] }) 
 users[user].banned = true
 usr = m.sender.split('@')[0]     
-await conn.reply(m.chat, '✅ *Usuario baneado con éxito*', m, { mentions: [user] })   
+await conn.reply(m.chat, '🦋 𝖯𝖾𝗋𝖿𝖾𝖼𝗍𝗈, 𝖤𝗅 𝖴𝗌𝗎𝖺𝗋𝗂𝗈 𝖸𝖺 𝖤𝗌𝗍𝖺 𝖡𝖺𝗇𝖾𝖺𝖽𝗈, 𝖭𝗈 𝖯𝗈𝖽𝗋𝖺 𝖴𝗌𝖺𝗋 𝙎𝙖𝙠𝙪𝙧𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿🍀', m, { mentions: [user] })   
 
 }} catch (e) {
-await conn.reply(m.chat, '🚩 *Ocurrió un fallo*', m, fake, )
+await conn.reply(m.chat, '*Ocurrió un fallo*', m, fake, )
 console.log(e) 
 }
 
@@ -52,3 +54,17 @@ handler.command = /^banuser$/i
 handler.rowner = true
 
 export default handler
+
+/*const handler = async (m, {conn, participants, usedPrefix, command}) => {
+  const BANtext = `🍓 𝐏𝐨𝐫 𝐅𝐚𝐯𝐨𝐫 𝐄𝐭𝐢𝐪𝐮𝐞𝐭𝐚 𝐀 𝐔𝐧 𝐔𝐬𝐚𝐫𝐢𝐨 𝐏𝐚𝐫𝐚 𝐁𝐚𝐧𝐞𝐚𝐫𝐥𝐨 𝐃𝐞𝐥 𝐁𝐨𝐭!`;
+  if (!m.mentionedJid[0] && !m.quoted) return m.reply(BANtext, m.chat, {mentions: conn.parseMention(BANtext)});
+  let who;
+  if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender;
+  else who = m.chat;
+  const users = global.db.data.users;
+  users[who].banned = true;
+  m.reply('*🦋 𝖯𝖾𝗋𝖿𝖾𝖼𝗍𝗈, 𝖤𝗅 𝖴𝗌𝗎𝖺𝗋𝗂𝗈 𝖸𝖺 𝖤𝗌𝗍𝖺 𝖡𝖺𝗇𝖾𝖺𝖽𝗈, 𝖭𝗈 𝖯𝗈𝖽𝗋𝖺 𝖴𝗌𝖺𝗋 𝙎𝙖𝙠𝙪𝙧𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿🍀*');
+};
+handler.command = /^banuser$/i;
+handler.rowner = true;
+export default handler;*/
