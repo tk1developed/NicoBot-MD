3d373d5ba8330c200de4341da3a5fb7c59d23335
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-02-21 19:40:58 -0500

    Actualizar herramientas-fakeReply.js

--- plugins/herramientas-fakeReply.js
+++ plugins/herramientas-fakeReply.js
@@ -19,7 +19,6 @@ const handler = async (m, {conn, text, usedPrefix, command}) => {
 };
 handler.help = ['fake <text> @user <text2>'];
 handler.tags = ['tools'];
-handler.admin = true;
 handler.command = /^(fitnah|fakereply|fake)$/;
 
 export default handler;
