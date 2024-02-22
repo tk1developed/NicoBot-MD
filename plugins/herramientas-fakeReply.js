7faed3c5912d2db672bdb5fe5ea22f16ce30f7b3
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-02-21 19:42:10 -0500

    Actualizar herramientas-fakeReply.js

--- plugins/herramientas-fakeReply.js
+++ plugins/herramientas-fakeReply.js
@@ -1,12 +1,12 @@
 
 const handler = async (m, {conn, text, usedPrefix, command}) => {
-  if (!text) return m.reply(`*[❗𝐈𝐍𝐅𝐎❗] 𝚄𝚂𝙾 𝙳𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾*\n\n*${usedPrefix + command}* hola @${m.sender.split`@`[0]} a`, null, {mentions: [m.sender]});
+  if (!text) return m.reply(`*🐱 𝖤𝗋𝗋𝗈𝗋 𝖠𝗅 𝖴𝗌𝗈 𝖣𝖾𝗅 𝖢𝗈𝗆𝖺𝗇𝖽𝗈, 𝖴𝗌𝗈 𝖢𝗈𝗋𝗋𝖾𝖼𝗍𝗈:*\n\n*${usedPrefix + command}* hola @${m.sender.split`@`[0]} a`, null, {mentions: [m.sender]});
   const cm = copy(m);
   let who;
   if (text.includes('@0')) who = '0@s.whatsapp.net';
   else if (m.isGroup) who = cm.participant = m.mentionedJid[0];
   else who = m.chat;
-  if (!who) return m.reply(`*[❗𝐈𝐍𝐅𝐎❗] 𝚄𝚂𝙾 𝙳𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾*\n\n*${usedPrefix + command}* hola @${m.sender.split`@`[0]} a`, null, {mentions: [m.sender]});
+  if (!who) return m.reply(`*🐱 𝖤𝗋𝗋𝗈𝗋 𝖠𝗅 𝖴𝗌𝗈 𝖣𝖾𝗅 𝖢𝗈𝗆𝖺𝗇𝖽𝗈, 𝖴𝗌𝗈 𝖢𝗈𝗋𝗋𝖾𝖼𝗍𝗈:*\n\n*${usedPrefix + command}* hola @${m.sender.split`@`[0]} a`, null, {mentions: [m.sender]});
   cm.key.fromMe = false;
   cm.message[m.mtype] = copy(m.msg);
   const sp = '@' + who.split`@`[0];
