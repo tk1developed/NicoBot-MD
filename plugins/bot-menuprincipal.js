52f68831c1d35989df697e7b7735a42dee43979b
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-02-23 18:55:40 -0500

    Actualizar bot-menuprincipal.js

--- plugins/bot-menuprincipal.js
+++ plugins/bot-menuprincipal.js
@@ -42,6 +42,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
 
 ╭━━━━･❪ *INFO BOT* ❫ ･━━━━╮
 ┃🌻 _!cuentas_
+┃🌻 _!saludo_
 ┃🌻 _!velocidad_
 ┃🌻 _!instalarbot_
 ┃🌻 _!botreglas_
