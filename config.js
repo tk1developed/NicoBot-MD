2336491d3da7465bad6197af90049ea5abdd4010
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-02-23 17:43:01 -0500

    Actualizar config.js

--- config.js
+++ config.js
@@ -67,9 +67,9 @@ global.channel = 'https://whatsapp.com/channel/0029VaHdt8oJENxtNExL8z2n' //Canal
 //Canal de Yt
 global.yt = 'https://www.youtube.com/@SakuraBotLite' //Canal De Youtube
 
-//Saludos Buen Dia!
+//Saludo User, Buen Dia
 var ase = new Date(); var hour = ase.getHours(); switch(hour){ case 0: hour = 'Linda Mañana 🌞'; break; case 1: hour = 'Linda Mañana 🌞'; break; case 2: hour = 'Linda Mañana 🌞'; break; case 3: hour = 'Linda Mañana 🌞'; break; case 4: hour = 'linda mañana 🌞'; break; case 5: hour = 'Linda Mañana 🌞'; break; case 6: hour = 'Linda Mañana 🌞'; break; case 7: hour = 'Linda Mañana 🌞'; break; case 8: hour = 'Linda Mañana 🌞'; break; case 9: hour = 'Linda Mañana 🌞'; break; case 10: hour = 'Lindo Dia 🥀'; break; case 11: hour = 'Lindo Dia 🥀'; break; case 12: hour = 'Lindo Dia 🥀'; break; case 13: hour = 'Lindo Dia 🥀'; break; case 14: hour = 'Linda Tarde 🌤'; break; case 15: hour = 'Linda Tarde 🌤'; break; case 16: hour = 'Linda Tarde 🌤'; break; case 17: hour = 'Linda Tarde 🌤'; break; case 18: hour = 'Linda Noche 🌙'; break; case 19: hour = 'Linda Noche 🌙'; break; case 20: hour = 'Linda Noche 🌙'; break; case 21: hour = 'Linda Noche 🌙'; break; case 22: hour = 'Linda Noche 🌙'; break; case 23: hour = 'Linda Noche 🌙'; break;}
-  global.saludo = "🥀" + hour;
+ global.saludo = "!" + hour;
 
 global.mods = [];
 
