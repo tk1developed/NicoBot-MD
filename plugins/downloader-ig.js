c3cf7d3489e17db4d738ebec3be704239bbf8fa3
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-02-23 18:17:12 -0500

    Actualizar downloader-ig.js

--- plugins/downloader-ig.js
+++ plugins/downloader-ig.js
@@ -6,7 +6,7 @@ import {instagramdl} from '@bochilteam/scraper';
 import instagramDl from '@sasmeee/igdl';
 import {fileTypeFromBuffer} from 'file-type';
 const handler = async (m, {conn, args, command, usedPrefix}) => {
-  if (!args[0]) throw `*🍓 𝘗𝘰𝘳 𝘍𝘢𝘷𝘰𝘳 𝘐𝘯𝘨𝘳𝘦𝘴𝘢 𝘜𝘯 𝘌𝘯𝘭𝘢𝘤𝘦 𝘋𝘦 𝘐𝘯𝘴𝘵𝘢𝘨𝘳𝘢𝘮, 𝘌𝘫𝘦𝘮𝘱𝘭𝘰: !𝗂𝗀* https://www.instagram.com/reel/CuqAzGRAbZa/?igshid=MzRlODBiNWFlZA==`;
+  if (!args[0]) throw `_*< DESCARGAS - INSTAGRAM />*_\n\n*🥀 Ingrese un enlace de Instagram.*\n\n*🌞 Ejemplo:* _${usedPrefix + command} https://www.instagram.com/reel/Cc0NuYBg8CR/?utm_source=ig_web_copy_link_`;
   m.reply(global.wait);
   try {
 const img = await instagramDl(args[0]);
@@ -23,7 +23,7 @@ for (let i = 0; i < img.length; i++) {
     const datTa = await instagram.download(args[0]);
     for (const urRRl of datTa) {
       const shortUrRRl = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text();
-      const tXXxt = `🔗 *Url:* ${shortUrRRl}`.trim();
+      const tXXxt = `_*< DESCARGAS - INSTAGRAM />*_\n\n*URL:* _${shortUrRRl}_`.trim();
       conn.sendFile(m.chat, urRRl.url, 'error.mp4', tXXxt, m);
       await new Promise((resolve) => setTimeout(resolve, 10000));
     }
@@ -31,13 +31,13 @@ for (let i = 0; i < img.length; i++) {
       try {
         const resultss = await instagramGetUrl(args[0]).url_list[0];
         const shortUrl2 = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text();
-        const txt2 = `🔗 *Url:* ${shortUrl2}`.trim();
+        const txt2 = `_*< DESCARGAS - INSTAGRAM />*_\n\n*URL:* _${shortUrl2}_`.trim();
         await conn.sendFile(m.chat, resultss, 'error.mp4', txt2, m);
       } catch {
         try {
           const resultssss = await instagramdl(args[0]);
           const shortUrl3 = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text();
-          const txt4 = `🔗 *Url:* ${shortUrl3}`.trim();
+          const txt4 = `_*< DESCARGAS - INSTAGRAM />*_\n\n*URL:* _${shortUrl3}_`.trim();
           for (const {url} of resultssss) await conn.sendFile(m.chat, url, 'error.mp4', txt4, m);
         } catch {
           try {
@@ -45,10 +45,10 @@ for (let i = 0; i < img.length; i++) {
             const json = await human.json();
             const videoig = json.result;
             const shortUrl1 = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text();
-            const txt1 = `🔗 *Url:* ${shortUrl1}`.trim();
+            const txt1 = `_*< DESCARGAS - INSTAGRAM />*_\n\n▢ *URL:* _${shortUrl1}_`.trim();
             await conn.sendFile(m.chat, videoig, 'error.mp4', txt1, m);
           } catch {
-            throw `🦋 𝙁𝙪𝙚 𝙐𝙣 𝙀𝙧𝙧𝙤𝙧, 𝙉𝙤 𝙎𝙚 𝙋𝙪𝙙𝙤 𝙀𝙣𝙫𝙞𝙖𝙧 𝙎𝙪 𝘼𝙧𝙘𝙝𝙞𝙫𝙤, 𝙄𝙣𝙩𝙚𝙣𝙩𝙚 𝙈𝙖𝙨 𝙏𝙖𝙧𝙙𝙚`;
+            throw `_*< DESCARGAS - INSTAGRAM />*_\n\n*📍 Un Pequeño Error...*`;
           }
         }
       }
@@ -56,7 +56,6 @@ for (let i = 0; i < img.length; i++) {
   }
 };
 handler.command = /^(instagramdl|instagram|igdl|ig|instagramdl2|instagram2|igdl2|ig2|instagramdl3|instagram3|igdl3|ig3)$/i;
-handler.register = true
 export default handler;
 
 const getBuffer = async (url, options) => {
