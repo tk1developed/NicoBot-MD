265225464ef76c542b05708c78f3f9fefed64c4b
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-02-23 18:48:35 -0500

    Actualizar velocidad.js

--- plugins/velocidad.js
+++ plugins/velocidad.js
@@ -10,7 +10,7 @@ let handler = async (m, { conn }) => {
           let child = stdout.toString("utf-8");
           let ssd = child.replace(/Memory:/, "Ram:");
     
-          m.reply(`⚡️ ${saludo} *_Mi Velocidad_*  ${latensi.toFixed(4)} *ms*`);
+          m.reply(`⚡️ *_Mi Velocidad_*  ${latensi.toFixed(4)} *ms*`);
             });
 }
 handler.help = ['ping']
