c3a420082306789b2981a1b23650e1a211c6f26d
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-02-23 18:49:50 -0500

    Actualizar plugins

---
+++ plugins/bot-saludo.js
@@ -0,0 +1,20 @@
+//comando de https://github.com/diegojadibot/SakuraBotLite-MD
+
+import speed from 'performance-now'
+import { spawn, exec, execSync } from 'child_process'
+
+let handler = async (m, { conn }) => {
+         let timestamp = speed();
+         let latensi = speed() - timestamp;
+         exec(`neofetch --stdout`, (error, stdout, stderr) => {
+          let child = stdout.toString("utf-8");
+          let ssd = child.replace(/Memory:/, "Ram:");
+
+          m.reply(`${saludo}`);
+            });
+}
+handler.help = ['infobot']
+handler.tags = ['main']
+handler.command = ['saludo']
+handler.register = true
+export default handler
