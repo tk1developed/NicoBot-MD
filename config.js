c5453f40bb7063deee34ce1d7ecaa15d1913da9f
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-02-24 16:01:56 -0500

    Actualizar config.js

--- config.js
+++ config.js
@@ -28,6 +28,13 @@ global.contactos = [
 ["5492266466080", '𝙲𝙾𝙻𝙰𝙱𝙾𝚁𝙰𝙳𝙾𝚁 🌿', 1],
 ["5217294888993", '𝙳𝙴𝚂𝙰𝚁𝚁𝙾𝙻𝙻𝙰𝙳𝙾𝚁 🥀', 1]]
 
+//es una prueba
+var img = 'https://telegra.ph/file/d837e687c894ff494b8d0.jpg'; var img1 = 'https://telegra.ph/file/0cdc7553a40170768bb62.jpg'; var img2 = 'https://telegra.ph/file/f65120cc170ec0bd17954.jpg'; var img3 = 'https://telegra.ph/file/cee466f95093295d0e813.jpg'; var img4 = 'https://telegra.ph/file/ee93c98e87acf5d9209df.jpg'; var img5 = 'https://telegra.ph/file/27f4c77f58c19efefb4f6.jpg'
+var img6 = 'https://telegra.ph/file/e69365c893e373d9b9b2a.jpg'; var img7 = 'https://telegra.ph/file/74c4b176f5cdc4961ee3f.jpg'; var img8 = 'https://telegra.ph/file/57205e403964eff285ee6.jpg'; var img9 = 'https://telegra.ph/file/44356ed9419c4e87ed9f3.jpg'; var img10 = 'https://telegra.ph/file/27f4c77f58c19efefb4f6.jpg'
+
+//prueba2
+global.fake = { contextInfo:{ forwardingScore: 0, externalAdReply: {title: saludo + ` ${m.name}`, body: dev, sourceUrl: enlaces, thumbnailUrl: fotos }}}
+}
 
 global.suittag = ['573013482814'];
 global.prems = ['573218138672'];
