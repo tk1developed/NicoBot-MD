0d1b4a097c0d6dca5fe58419df8a7e6ff24464bc
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-02-23 21:52:12 -0500

    Actualizar bot-.js

--- plugins/bot-.js
+++ plugins/bot-.js
@@ -1,15 +1,4 @@
-/*let handler = async (m, { conn }) => {
-let totalf = Object.values(global.plugins).filter(
-    (v) => v.help && v.tags
-  ).length;
-conn.reply(m.chat, `• Total de Funciones: ${totalf}`,m)
-}
-
-handler.help = ['totalfunciones']
-handler.tags = ['info']
-handler.command = ['totalfunciones']
-handler.register = true
-export default handler*/
+//Total funciones
 
 let handler = async (m, { conn }) => {
 let totalf = Object.values(global.plugins).filter(
