b749ade7afb1722900f848c28ed420aca434571d
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-02-24 15:59:51 -0500

    Actualizar owner-restart.js

--- plugins/owner-restart.js
+++ plugins/owner-restart.js
@@ -1,15 +1,14 @@
 import { spawn } from 'child_process'
 let handler = async (m, { conn, isROwner, text }) => {
-
-if (!process.send) throw 'Dont: node main.js\nDo: node index.js'
-if (conn.user.jid == conn.user.jid) {
-await conn.reply(m.chat, '☁️ *Reiniciando*')
-process.send('reset')
-} else throw 'eh'
-
+    if (!process.send) throw 'Dont: node main.js\nDo: node index.js'
+    if (conn.user.jid == conn.user.jid) {
+    await m.reply('😼 𝖤𝗌𝗉𝖾𝗋𝖺 𝖴𝗇𝗈𝗌 𝖬𝗂𝗇𝗎𝗍𝗈𝗌, 𝖤𝗅 𝖡𝗈𝗍 𝖲𝖾 𝖤𝗌𝗍𝖺́ 𝖱𝖾𝗂𝗇𝗂𝖼𝗂𝖺𝗇𝖽𝗈🙀...')
+    process.send('reset')
+  } else throw 'eh'
 }
+
 handler.help = ['restart']
-handler.tags = ['own']
+handler.tags = ['owner']
 handler.command = ['restart','reiniciar'] 
 
 handler.rowner = true
