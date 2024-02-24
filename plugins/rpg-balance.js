c50f0a1f1a3e19f3a5cdb4d9f4e5da043fd8ae0a
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-02-23 21:36:46 -0500

    Actualizar rpg-balance.js

--- plugins/rpg-balance.js
+++ plugins/rpg-balance.js
@@ -3,18 +3,18 @@ const handler = async (m, {usedPrefix}) => {
   if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender;
   else who = m.sender;
   const name = conn.getName(who);
-  m.reply(`╭━━━━━━ • ✿ • ━━━━━━
-├✨ᬽ៸ *𝐂𝐥𝐢𝐞𝐧𝐭𝐞:* ${name}
-├🌻ᬽ៸ *𝐃𝐢𝐚𝐦𝐚𝐧𝐭𝐞𝐬*: ${global.db.data.users[who].diamond} 💎
-├🌼ᬽ  *𝐄𝐱𝐩:* ${global.db.data.users[who].exp}
-◈ ━━━━━━━ ⸙ ━━━━━━━ ◈
+  m.reply(`╭✿❯────「✿」────❮✿
+┋➺ 🥀 *Usuario:* ${name}
+┋➺ 📍 *Diamantes*: ${global.db.data.users[who].diamond} 💎
+┋➺ ⚡️ *Experiencia:* ${global.db.data.users[who].exp}
+╰✶⊶⊷⊶⊷❍⊶⊷⊶⊷✶
 
-╭━━━━━━ • ✿ • ━━━━━━
-├❥ᰰຼ *𝑷𝒖𝒆𝒅𝒆𝒏 𝑪𝒐𝒎𝒑𝒓𝒂𝒓 𝑴𝒂𝒔 💎*
-├❥ᰰຼ *𝑼𝒔𝒂 𝑬𝒔𝒕𝒐𝒔 𝑪𝒐𝒎𝒂𝒏𝒅𝒐𝒔 𝑷𝒂𝒓𝒂 𝑪𝒐𝒎𝒑𝒓𝒂𝒓 𝑴𝒂𝒔 𝑫𝒊𝒂𝒎𝒂𝒏𝒕𝒆𝒔*
-├❥ᰰຼ ✰ *${usedPrefix}buy <cantidad>*
-├❥ᰰຼ ✰ *${usedPrefix}buyall*
-◈ ━━━━━━━ ⸙ ━━━━━━━ ◈`)
+╭✿❯────「✿」────❮✿
+┃ *Puedes Comprar Mas 💎*
+┃ *Usa Los Siguentes Comandos*
+┃ *${usedPrefix}buy <cantidad>*
+┃ *${usedPrefix}buyall*
+╰✶⊶⊷⊶⊷❍⊶⊷⊶⊷✶`)
 };
 handler.help = ['bal'];
 handler.tags = ['xp'];
