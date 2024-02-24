ec68844e4f9c25c6677b8996ca9f66263edbf180
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-02-23 19:34:25 -0500

    Actualizar owner-update.js

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -1,19 +1,42 @@
-import {execSync} from "child_process"
-let handler = async (m, {conn, text}) => {
-    await conn.sendMessage(m.chat, { react: { text: '🏷', key: m.key } })
-  await m.reply(`🐱 𝐄 𝐒 𝐏 𝐄 𝐑 𝐀 🚀`)
-  try {
-    if (global.conn.user.jid == conn.user.jid) {
-      let stdout = execSync("git pull" + (m.fromMe && text ? " " + text : ""))
-      await await await conn.reply(m.chat, stdout.toString(), m)
-    }
-  } catch {
-    var update = execSync("git remote set-url origin https://github.com/diegojadibot/SakuraBotLite-MD.git && git pull")
-    await await await m.reply(update.toString())
-  }
-};
-handler.help = ["update"]
-handler.tags = ["owner"]
-handler.command = /^update|actualizar$/i
+import { execSync } from 'child_process'
+
+var handler = async (m, { conn, text }) => {
+
+try {
+const stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''));
+let messager = stdout.toString()
+if (messager.includes('Already up to date.')) messager = '✅ *No hay actualizaciones pendientes*'
+if (messager.includes('Updating')) messager = '✅ *Actualización finalizada exitosamente*\n\n' + stdout.toString()
+conn.reply(m.chat, messager, m, fake, )
+} catch {      
+try {    
+const status = execSync('git status --porcelain')
+if (status.length > 0) {
+const conflictedFiles = status.toString().split('\n').filter(line => line.trim() !== '').map(line => {
+if (line.includes('.npm/') || line.includes('.cache/') || line.includes('tmp/') || line.includes('sessions/') || line.includes('npm-debug.log')) {
+return null
+}
+return '*→ ' + line.slice(3) + '*'}).filter(Boolean)
+if (conflictedFiles.length > 0) {
+const errorMessage = `🚩 *Se han hecho cambios locales en archivos del bot que entran en conflicto con las actualizaciones del repositorio. Para actualizar, reinstala el bot o realiza las actualizaciones manualmente*\n\nArchivos en conflicto:\n\n${conflictedFiles.join('\n')}`
+await conn.reply(m.chat, errorMessage, m, fake, )
+}
+}
+} catch (error) {
+console.error(error)
+let errorMessage2 = '🚩 *Ocurrió un fallo. Por favor, inténtalo de nuevo más tarde*'
+if (error.message) {
+errorMessage2 += '\n*- Mensaje de error:* ' + error.message;
+}
+await conn.reply(m.chat, errorMessage2, m, fake, )
+}
+}
+
+}
+handler.help = ['update', 'actualizar']
+handler.tags = ['owner']
+handler.command = /^(update|actualizar|gitpull)$/i
+
 handler.rowner = true
+
 export default handler
