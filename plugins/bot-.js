b52e567addd6145410df54f863bdc091fee02506
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-02-23 21:50:03 -0500

    Actualizar bot-.js

--- plugins/bot-.js
+++ plugins/bot-.js
@@ -18,8 +18,8 @@ let totalf = Object.values(global.plugins).filter(
 conn.reply(m.chat, `🦋 𝘛𝘰𝘵𝘢𝘭 𝘍𝘶𝘯𝘤𝘪𝘰𝘯𝘦𝘴: ${totalf}`,m)
 }
 
-handler.help = ['totalfunciones']
+handler.help = ['totalf']
 handler.tags = ['info']
-handler.command = ['totalfunciones']
+handler.command = ['totalf']
 handler.register = true
 export default handler
