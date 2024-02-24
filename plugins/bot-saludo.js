faf0b2738751bc103adc18a1b71873b78c6372ea
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-02-24 12:57:51 -0500

    Actualizar bot-saludo.js

--- plugins/bot-saludo.js
+++ plugins/bot-saludo.js
@@ -1,6 +1,6 @@
 //comando de https://github.com/diegojadibot/SakuraBotLite-MD
 
-/*import speed from 'performance-now'
+import speed from 'performance-now'
 import { spawn, exec, execSync } from 'child_process'
 
 let handler = async (m, { conn }) => {
@@ -17,21 +17,4 @@ handler.help = ['infobot']
 handler.tags = ['main']
 handler.command = ['saludo']
 handler.register = true
-export default handler*/
-
-let handler = async(m, { conn, text, usedPrefix, command }) => {
-let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-let name = conn.getName(who)
-//const pp = './Menu.png'
-
-let love = `\t\t*➺ ${name} ${saludo}`
-
-conn.sendMessage(m.chat,{image: {url: pp}, caption: love, mentions: [m.sender]}, {quoted: m}) 
-}
-
-handler.help = ['love  @user']
-handler.tags = ['fun']
-handler.command = /^(saludo)$/i
-handler.register = true
 export default handler
-
