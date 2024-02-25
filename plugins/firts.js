194559027663e2fd91d4ab1bd5968cc7ee32b1b0
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-02-25 15:40:27 -0500

    Actualizar firts.js

--- plugins/firts.js
+++ plugins/firts.js
@@ -7,12 +7,12 @@ if (m.chat.endsWith('broadcast') || m.fromMe || m.isGroup) return
 let user = global.db.data.users[m.sender]
 
 if (new Date() - user.pc < 21600000) return
-await m.reply(`🌟 *Hola ${username}, Bienvenido A 𝙎𝙖𝙠𝙪𝙧𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿* 🥷🏻
-📲 _Si Deseas Escribe .menu Para Ver Mis Comandos_ 🌤
+await m.reply(`🥀 *Hola ${saludo} ${username}, Bienvenido A 𝙎𝙖𝙠𝙪𝙧𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿* 🥷🏻
+🚀 _Si Deseas Escribe !menu Para Ver Mis Comandos_ 🌤
 
-📌 *Cualquier Duda O Sugerencia Puedes Contactar A Mi Creador:* 🎉
-📩 +57 301 3482814 📭
+📌 *Si Tienes Algun Problema Puedes Contactar A Mi Creador:* 📍
+📩 +57 301 3482814 ⚡️
 
-🙏❣️ Si desea apoyar el proyecto Sakura Bot  Lite Puedes Recomendar Mi Bot 🌻🪁`) 
+📍 Quieres Apoyar El Proyecto SakuraBotLite, Puedes Compartir Mi Bot Y Dejando Una Estrellita 🌟 El Repositorio Oficial Del Bot. 🥀`) 
 user.pc = new Date * 1
 }
