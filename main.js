1ae9af1db234e80912418bf9f3f75b1ab7647b7c
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-02-26 10:21:00 -0500

    Actualizar main.js

--- main.js
+++ main.js
@@ -313,9 +313,9 @@ if (opcion == '1' || methodCodeQR) {
     console.log(chalk.yellow('[ 🦋 ] Escanea el código QR.'));
  }}
    if (connection == 'open') {
-console.log(chalk.yellowBright('\n╭━─━━─━━─━─≪  🦋  ≫─━─━━─━━─━╮\n│\n│SakuraBotLite-MD Conectado🐱.\n│\n╰━─━━━─━━─━─≪ 🟢 ≫─━─━━─━━━─━╯\n'))
+console.log(chalk.yellowBright('\n╭━─━━─━━─━─≪  🦋  ≫─━─━━─━━─━╮\n│\n│SakuraBotLite-MD Conectado🚀.\n│\n╰━─━━━─━━─━─≪ 🟢 ≫─━─━━─━━━─━╯\n'))
 if (conn.user.connect) {
-conn.fakeReply('573013482814@s.whatsapp.net', '🐱 !𝖧𝖾𝗒 𝖢𝗋𝖾𝖺𝖽𝗈𝗋 𝖬𝖾 𝖤 𝖢𝗈𝗇𝖾𝖼𝗍𝖺𝖽𝗈 𝖢𝗈𝗆𝗈 𝖴𝗇 𝖭𝗎𝖾𝗏𝗈 𝖡𝗈𝗍! 🐈', '0@s.whatsapp.net', '🦋 𝚂𝙾𝚈 𝚃𝚄 𝙱𝙾𝚃 𝙾𝙵𝙲 🐱', '0@s.whatsapp.net')
+conn.fakeReply('573013482814@s.whatsapp.net', '✨ ¡Hey Creador Me E Conectado Como Un Nuevo Bot! 🚀', '0@s.whatsapp.net', '🦋 𝚃𝚄 𝙱𝙾𝚃 𝙾𝙵𝙸𝙲𝙸𝙰𝙻.', '0@s.whatsapp.net')
 conn.user.connect = true;
  await conn.groupAcceptInvite('CKqHvmFQDL1Kdj6TFHahUn');
    }
