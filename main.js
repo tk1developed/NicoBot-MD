868a0b9dc987836d27480a96cce007ed26d3882c
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-02-26 10:22:10 -0500

    Actualizar main.js

--- main.js
+++ main.js
@@ -315,7 +315,7 @@ if (opcion == '1' || methodCodeQR) {
    if (connection == 'open') {
 console.log(chalk.yellowBright('\n╭━─━━─━━─━─≪  🦋  ≫─━─━━─━━─━╮\n│\n│SakuraBotLite-MD Conectado🚀.\n│\n╰━─━━━─━━─━─≪ 🟢 ≫─━─━━─━━━─━╯\n'))
 if (conn.user.connect) {
-conn.fakeReply('573013482814@s.whatsapp.net', '✨ ¡Hey Creador Me E Conectado Como Un Nuevo Bot! 🚀', '0@s.whatsapp.net', '🦋 𝚃𝚄 𝙱𝙾𝚃 𝙾𝙵𝙸𝙲𝙸𝙰𝙻.', '0@s.whatsapp.net')
+conn.fakeReply('573013482814@s.whatsapp.net', '✨ ¡Hey Creador Me E Conectado Como Un Nuevo Bot! 🚀', '0@s.whatsapp.net', '🦋 SᴀᴋᴜʀᴀBᴏᴛLɪᴛᴇ-MD', '0@s.whatsapp.net')
 conn.user.connect = true;
  await conn.groupAcceptInvite('CKqHvmFQDL1Kdj6TFHahUn');
    }
