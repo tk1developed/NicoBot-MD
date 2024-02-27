72382924ed64ffa94bb71bd2472f2f96894bfb62
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-02-27 10:28:43 -0500

    Actualizar _autobio.js

--- plugins/_autobio.js
+++ plugins/_autobio.js
@@ -1,4 +1,4 @@
-let handler = m => m
+/*let handler = m => m
 handler.all = async function (m) {
 let setting = global.db.data.settings[this.user.jid]
 
@@ -8,7 +8,7 @@ if (process.send) { process.send('uptime')
 _muptime = await new Promise(resolve => { process.once('message', resolve) 
 setTimeout(resolve, 2000) }) * 1000}
 let uptime = clockString(_uptime)
-let bio = `${global.packname}`
+let bio = `${global.packname} 🟢Mɪ Tɪᴇᴍᴘᴏ Aᴄᴛɪᴠᴀ Es: ${uptime} 𝖶𝗁𝖺𝗍𝗌𝖠𝗉𝗉 - 𝖡𝗈𝗍 😻`
 await this.updateProfileStatus(bio).catch(_ => _)
 setting.status = new Date() * 1
 } 
@@ -20,4 +20,4 @@ function clockString(ms) {
   let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
   let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
   return [d, ' • ', h, ' » ', m, ' » ', s].map(v => v.toString().padStart(2, 0)).join('') 
-}
+}*/
