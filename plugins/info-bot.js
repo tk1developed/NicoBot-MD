7c9e553abe86c4ab097f4dc837b8a0e2026c43c2
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-02-26 19:04:02 -0500

    Actualizar info-bot.js

--- plugins/info-bot.js
+++ plugins/info-bot.js
@@ -9,7 +9,7 @@ handler.all = async function(m) {
   // const estiloaudio = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: {"audioMessage": { "mimetype":"audio/ogg; codecs=opus", "seconds": "99569", "ptt": "true"}}}
   if (/^bot$/i.test(m.text) && !chat.isBanned) {
     conn.sendPresenceUpdate('recording', m.chat);
-    await m.reply(`🐱 𝖤𝗌𝗍𝗈𝗒 𝖠𝗊𝗎𝗂 𝖯𝖺𝗋𝖺 𝖠𝗒𝗎𝖽𝖺𝗋𝗍𝖾`);
+    await m.reply(`🧸 𝖤𝗌𝗍𝗈𝗒 𝖠𝗊𝗎𝗂 𝖯𝖺𝗋𝖺 𝖠𝗒𝗎𝖽𝖺𝗋𝗍𝖾`);
     m.conn.sendMessage(m.chat, {audio: {url: vn}, fileName: 'error.mp3', mimetype: 'audio/mpeg', ptt: true}, {quoted: m});
     // conn.sendFile(m.chat, vn, 'bot.mp3', null, m, true, { type: 'audioMessage', seconds: '4556', ptt: true, sendEphemeral: true, quoted: m })
   }
