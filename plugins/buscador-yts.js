090a913f3c3030ea936f1d4b26e12c37e2000a43
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-02-27 10:20:13 -0500

    Actualizar buscador-yts.js

--- plugins/buscador-yts.js
+++ plugins/buscador-yts.js
@@ -2,16 +2,16 @@ import yts from 'yt-search';
 import fs from 'fs';
 
 const handler = async (m, {conn, text}) => {
-  if (!text) throw '⚠️ *_Que quieres que busque en YouTube?_*';
+  if (!text) throw '🧸 *_Que quieres que busque en YouTube?_*';
   const results = await yts(text);
   const tes = results.all;
   const teks = results.all.map((v) => {
     switch (v.type) {
       case 'video': return `
 ° *_${v.title}_*
-↳ 🫐 *_Link :_* ${v.url}
-↳ 🕒 *_Duración :_* ${v.timestamp}
-↳ 📥 *_Subido :_* ${v.ago}
+↳ 🧸 *_Enlace :_* ${v.url}
+↳ 📍 *_Duración :_* ${v.timestamp}
+↳ 🧩 *_Subido :_* ${v.ago}
 ↳ 👁 *_Vistas :_* ${v.views}`;
     }
   }).filter((v) => v).join('\n\n◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦\n\n');
