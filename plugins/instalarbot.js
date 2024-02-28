de652ccb367b0ac2755fff8b0d659b5f3d9b6036
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-02-28 18:58:06 -0500

    Actualizar instalarbot.js

--- plugins/instalarbot.js
+++ plugins/instalarbot.js
@@ -65,9 +65,7 @@ const document = doc[Math.floor(Math.random() * doc.length)];
 
 ➪ npm start
 
-🟢 (Comandos 1 Por 1) 📍
-    
-    🧸 *Si Tienes Duda Sobre Mi Propietario Utiliza El Comando: !creador* 📍`.trim();     if (m.isGroup) { 
+🟢 (Comandos 1 Por 1) 📍`.trim();     if (m.isGroup) { 
  // await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, { type: 'audioMessage', ptt: true})
       const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
       conn.sendMessage(m.chat, {image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: m});
