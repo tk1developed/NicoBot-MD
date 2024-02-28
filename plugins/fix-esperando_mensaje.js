1168b7dec94d551e7be2ecab5734fa577aee42a1
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-02-28 18:51:47 -0500

    Actualizar fix-esperando_mensaje.js

--- plugins/fix-esperando_mensaje.js
+++ plugins/fix-esperando_mensaje.js
@@ -5,10 +5,10 @@ import path from 'path';
 
 const handler = async (m, { conn, usedPrefix }) => {
   if (global.conn.user.jid !== conn.user.jid) {
-    return conn.sendMessage(m.chat, {text: '*🌵 Utiliza este comando directamente en el número principal del Bot*'}, {quoted: m});
+    return conn.sendMessage(m.chat, {text: '*🥀 Utiliza este comando directamente en el número principal del Bot*'}, {quoted: m});
   }
   const chatId = m.isGroup ? [m.chat, m.sender] : [m.sender];
-  const sessionPath = './SakuraBotSession/';
+  const sessionPath = './sessions/';
   try {
     const files = await fs.readdir(sessionPath);
     let filesDeleted = 0;
@@ -24,13 +24,13 @@ const handler = async (m, { conn, usedPrefix }) => {
     if (filesDeleted === 0) {
       await conn.sendMessage(m.chat, {text: '*🦋 No se encontró ningún archivo que incluya la ID del chat*'}, {quoted: m});
     } else {
-      await conn.sendMessage(m.chat, {text: `*✨️ Se eliminaron ${filesDeleted} archivos de sesión*`}, {quoted: m});
+      await conn.sendMessage(m.chat, {text: `*🧸 Se eliminaron ${filesDeleted} archivos de sesión*`}, {quoted: m});
     }
   } catch (err) {
     console.error('Error al leer la carpeta o los archivos de sesión:', err);
     await conn.sendMessage(m.chat, {text: '*🚀 Ocurrió un error al eliminar los archivos de sesión*'}, {quoted: m});
   }
-  await conn.sendMessage(m.chat, {text: `*💖 ¡Hola! Ahora me ves?*\n\n*🍓 Si el Bot no le responde a sus comandos por favor haga un pequeño spam*\n\n*—◉ Ejemplo:*\n${usedPrefix}s\n${usedPrefix}s\n${usedPrefix}s`}, {quoted: m});
+  await conn.sendMessage(m.chat, {text: `*📍 ¡Hola! Ahora me ves?*\n\n*🧩 Si el Bot no le responde a sus comandos por favor haga un pequeño spam*\n\n*Ejemplo:*\n${usedPrefix}wm\n${usedPrefix}wm\n${usedPrefix}wm`}, {quoted: m});
 };
 handler.help = ['fixmsgespera'];
 handler.tags = ['fix'];
