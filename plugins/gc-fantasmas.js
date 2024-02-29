a81c58fd8f7eaf67aa34103e954d45a0fcd9e9c1
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-02-29 10:15:48 -0500

    Actualizar gc-fantasmas.js

--- plugins/gc-fantasmas.js
+++ plugins/gc-fantasmas.js
@@ -22,7 +22,7 @@ const handler = async (m, {conn, text, participants}) => {
     }
   }
   if (total == 0) return conn.reply(m.chat, `*📍 Este Grupo No Tiene Fantasmas*`, m);
-  m.reply(`*🧩 Versión De Inactivos*\n\n*Grupo:* ${await conn.getName(m.chat)}\n*Miembros Del Grupo:* ${sum}\n\n*👻 Lista De Fantasmas 👻*\n${sider.map((v) => '  👉🏻 @' + v.replace(/@.+/, '')).join('\n')}\n\n*Nota : 𝙴𝚂𝚃𝙾 𝙿𝚄𝙴𝙳𝙴 𝙽𝙾 𝚂𝙴𝚁 𝟷𝟶𝟶% 𝙰𝙲𝙴𝚁𝚃𝙰𝙳𝙾, 𝙴𝙻 𝙱𝙾𝚃 𝙸𝙽𝙸𝙲𝙸𝙰 𝙴𝙻 𝙲𝙾𝙽𝚃𝙴𝙾 𝙳𝙴 𝙼𝙴𝙽𝚂𝙰𝙹𝙴𝚂 𝙰𝙿𝙰𝚁𝚃𝙸𝚁 𝙳𝙴 𝚀𝚄𝙴 𝚂𝙴 𝙰𝙲𝚃𝙸𝚅𝙾 𝙴𝙽 𝙴𝚂𝚃𝙴 𝙽𝚄𝙼𝙴𝚁𝙾*`, null, {mentions: sider});
+  m.reply(`*🧩 Revisión De Inactivos*\n\n*Grupo:* ${await conn.getName(m.chat)}\n*Miembros Del Grupo:* ${sum}\n\n*👻 Lista De Fantasmas 👻*\n${sider.map((v) => '  👉🏻 @' + v.replace(/@.+/, '')).join('\n')}\n\n*Nota : 𝙴𝚂𝚃𝙾 𝙿𝚄𝙴𝙳𝙴 𝙽𝙾 𝚂𝙴𝚁 𝟷𝟶𝟶% 𝙰𝙲𝙴𝚁𝚃𝙰𝙳𝙾, 𝙴𝙻 𝙱𝙾𝚃 𝙸𝙽𝙸𝙲𝙸𝙰 𝙴𝙻 𝙲𝙾𝙽𝚃𝙴𝙾 𝙳𝙴 𝙼𝙴𝙽𝚂𝙰𝙹𝙴𝚂 𝙰𝙿𝙰𝚁𝚃𝙸𝚁 𝙳𝙴 𝚀𝚄𝙴 𝚂𝙴 𝙰𝙲𝚃𝙸𝚅𝙾 𝙴𝙽 𝙴𝚂𝚃𝙴 𝙽𝚄𝙼𝙴𝚁𝙾*`, null, {mentions: sider});
 };
 handler.command = /^(verfantasmas|fantasmas|sider)$/i;
 handler.admin = true;
