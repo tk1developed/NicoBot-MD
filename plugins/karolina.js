f72235eca14e537d5dd05a10f267b05203f7cad1
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-02-29 19:41:32 -0500

    Actualizar karolina.js

--- plugins/karolina.js
+++ plugins/karolina.js
@@ -13,7 +13,7 @@ let expp = Math.floor(Math.random() * 5000)
   global.db.data.users[m.sender].joincount += tok
   global.db.data.users[m.sender].exp += expp
 
-let texto = `*𝐾𝐴𝑅𝑂𝐿𝐺 𝐿𝐴 𝐵𝐼𝐶𝐻𝑂𝑇𝐴🌻✨*`
+let texto = `*KarolG La Bichota!!🌻✨*`
 
 const fkontak = {
         "key": {
