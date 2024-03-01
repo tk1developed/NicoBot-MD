cc2340c6b8a1e32715417944a47614d74e8491b4
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-02-29 22:14:12 -0500

    Actualizar herramienta-bard.js

--- plugins/herramienta-bard.js
+++ plugins/herramienta-bard.js
@@ -6,7 +6,7 @@ if (!text) throw `🧸 _*Ingrese Un Texto*_`
 
 try {
 
-await m.reply(wait)
+conn.sendPresenceUpdate('composing', m.chat)
 var apii = await fetch(`https://aemt.me/bard?text=${text}`)
 var res = await apii.json()
 await m.reply(res.result)
