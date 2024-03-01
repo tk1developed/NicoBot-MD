4fbe5e8bae243cdb8bf415e74dbc5253385c41c2
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-03-01 11:03:06 -0500

    Actualizar owner-unbanuser.js

--- plugins/owner-unbanuser.js
+++ plugins/owner-unbanuser.js
@@ -1,15 +1,38 @@
 const handler = async (m, {conn, text}) => {
-  if (!text) throw '🧸 *Etiqueta Al Usuario Baneado*';
+  if (!text) throw '🧸 *Etiqueta A Un Usuario*';
   let who;
   if (m.isGroup) who = m.mentionedJid[0];
   else who = m.chat;
-  if (!who) throw '🧸 *Etiqueta Al Usuario Baneado*';
+  if (!who) throw '🧸 *Etiqueta A Un Usuario*';
   const users = global.db.data.users;
   users[who].banned = false;
-  conn.reply(m.chat, `🧸 *Perfecto El Usuario Ya Me Puede Usar*`, m);
+  conn.reply(m.chat, `✅️ *Se Desbaneo Al Usuario ${text} Con Éxito*`, m);
 };
 handler.help = ['unbanuser'];
 handler.tags = ['owner'];
 handler.command = /^unbanuser$/i;
 handler.rowner = true;
 export default handler;
+
+/*let handler = async (m, {conn, text}) => {
+
+let user = conn.user.jid.split`@`[0] + '@s.whatsapp.net'
+let bant = `📍 *Etiquete a una persona*`
+if (!text) return conn.reply(m.chat, bant,)
+let who
+if (m.isGroup) who = m.mentionedJid[0]
+else who = m.chat
+if (!who) return conn.reply(m.chat, bant, m, { mentions: [text] })
+let users = global.db.data.users
+
+users[who].banned = false
+ m.reply(`✅ *Se desbaneo al usuario ${text} con éxito*`, m.chat, { mentions: conn.parseMention(text)}) 
+
+}
+handler.help = ['unbanuser']
+handler.tags = ['own']
+handler.command = /^unbanuser$/i
+
+handler.rowner = true
+
+export default handler*/
