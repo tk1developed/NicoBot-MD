0f218a75ffd6201e15d7bab3e733b9e1c07be62a
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-03-01 20:54:44 -0500

    Actualizar _antiprivado.js

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -1,4 +1,4 @@
-export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner}) {
+/*export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner}) {
   if (m.isBaileys && m.fromMe) return !0;
   if (m.isGroup) return !1;
   if (!m.message) return !0;
@@ -10,19 +10,4 @@ export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner}) 
     await this.updateBlockStatus(m.chat, 'block');
   }
   return !1;
-}
-
-
-/*export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner}) {
-  if (m.isBaileys && m.fromMe) return !0;
-  if (m.isGroup) return !1;
-  if (!m.message) return !0;
-  if (m.text.includes('PIEDRA') || m.text.includes('PAPEL') || m.text.includes('TIJERA') || m.text.includes('serbot') || m.text.includes('jadibot')) return !0;
-  const chat = global.db.data.chats[m.chat];
-  const bot = global.db.data.settings[this.user.jid] || {};
-  if (bot.antiPrivate && !isOwner && !isROwner) {
-    await m.reply(`🦋𝑯𝒐𝒍𝒂 @${m.sender.split`@`[0]}, 🍓 𝐋𝐨 𝐒𝐢𝐞𝐧𝐭𝐨 𝐍𝐨 𝐄𝐬𝐭𝐚 𝐏𝐞𝐫𝐦𝐢𝐭𝐢𝐝𝐨 𝐄𝐬𝐜𝐫𝐢𝐛𝐢𝐫𝐦𝐞 𝐀𝐥 𝐏𝐫𝐢𝐯𝐚𝐝𝐨 𝐏𝐨𝐫 𝐋𝐨 𝐂𝐮𝐚𝐥 𝐒𝐞𝐫𝐚𝐬 𝐁𝐥𝐨𝐪𝐮𝐞𝐚𝐝𝐨, 𝐏𝐮𝐞𝐝𝐞𝐬 𝐔𝐧𝐢𝐫𝐭𝐞 𝐀𝐥 𝐆𝐫𝐮𝐩𝐨 𝐎𝐟𝐢𝐜𝐢𝐚𝐥 𝐃𝐞𝐥 𝐁𝐨𝐭: ${gp1}`, false, {mentions: [m.sender]});
-    await this.updateBlockStatus(m.chat, 'block');
-  }
-  return !1;*/
-}
+}*_
