2ae2aa134c938691969d234e9902a0986fc2ff0e
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-03-02 15:41:37 -0500

    Actualizar owner-banchat.js

--- plugins/owner-banchat.js
+++ plugins/owner-banchat.js
@@ -1,6 +1,6 @@
 const handler = async (m) => {
   global.db.data.chats[m.chat].isBanned = true;
-  m.reply('*[🌼]𝗖𝗵𝗮𝘁 𝗕𝗮𝗻𝗲𝗮𝗱𝗼 𝗖𝗼𝗻 𝗘𝘅𝗶𝘁𝗼,𝗡𝗼 𝗣𝗼𝗱𝗿𝗮𝗻 𝗨𝘀𝗮𝗿 𝙎𝙖𝙠𝙪𝙧𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿☘️*');
+  m.reply('*🧸 Chat Baneado Con Exito Ya No Podran Usar ${sk} ☘️*');
 };
 handler.help = ['banchat'];
 handler.tags = ['owner'];
