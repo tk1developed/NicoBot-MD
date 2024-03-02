58256220e096b26691434c38c9308a7727412225
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-03-02 15:42:28 -0500

    Actualizar owner-banchat.js

--- plugins/owner-banchat.js
+++ plugins/owner-banchat.js
@@ -1,6 +1,6 @@
 const handler = async (m) => {
   global.db.data.chats[m.chat].isBanned = true;
-  m.reply('*🧸 Chat Baneado Con Exito Ya No Podran Usar ${sk} ☘️*');
+  m.reply('✅️ *Chat Baneado Con Exito Ya No Podran Usar SakuraBotLite - MD 🧸*');
 };
 handler.help = ['banchat'];
 handler.tags = ['owner'];
