c4c745437a683d15eadb96a77c9e7d76daafada0
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-03-01 21:15:07 -0500

    Actualizar _antiprivado.js

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -1,4 +1,4 @@
-/*export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner}) {
+export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner}) {
   if (m.isBaileys && m.fromMe) return !0;
   if (m.isGroup) return !1;
   if (!m.message) return !0;
@@ -6,8 +6,8 @@
   const chat = global.db.data.chats[m.chat];
   const bot = global.db.data.settings[this.user.jid] || {};
   if (bot.antiPrivate && !isOwner && !isROwner) {
-    await m.reply(`*👋 Hola @${m.sender.split`@`[0]}, Lo Siento No Esta Permitido Escribirme Al Privado Por Lo Cual Seras Bloqueado*\n\n*Puedes Unirte Al Grupo Oficial Del Bot* 👇\n\n${gp1}`, false, {mentions: [m.sender]});
+    await m.reply(`*[❗] ℋ𝓸𝓵𝓪 @${m.sender.split`@`[0]}, 𝖊𝖘𝖙𝖆 𝖕𝖗𝖔𝖍𝖎𝖇𝖎𝖉𝖔 𝖍𝖆𝖇𝖑𝖆𝖗𝖑𝖊 𝖆𝖑 𝖕𝖗𝖎𝖛𝖆𝖉𝖔 𝖉𝖊𝖑 𝖇𝖔𝖙, 𝖒𝖊 𝖛𝖊𝖔 𝖔𝖇𝖑𝖎𝖌𝖆𝖉𝖔 𝖆 𝖇𝖑𝖔𝖖𝖚𝖊𝖆𝖗𝖑𝖔.*`, false, {mentions: [m.sender]});
     await this.updateBlockStatus(m.chat, 'block');
   }
   return !1;
-}*/
+}
