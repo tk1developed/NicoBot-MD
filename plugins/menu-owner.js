92e5a3e003aaa597caec4edffbf839ff718bb567
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-03-02 21:32:58 -0500

    💖 𝙎𝙖𝙠𝙪𝙧𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 💖

--- plugins/menu-owner.js
+++ plugins/menu-owner.js
@@ -100,7 +100,7 @@ const document = doc[Math.floor(Math.random() * doc.length)];
       conn.sendMessage(m.chat, {image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: fkontak2});
     }
   } catch {
-    conn.reply(m.chat, '*🙌🏻 Error Al Enviar!.*', m);
+    conn.reply(m.chat, '📍 *Ocurrió Un Error*', m);
   }
 };
 handler.command = /^(ownermenu|menucreador|creadormenu|menuowner)$/i;
