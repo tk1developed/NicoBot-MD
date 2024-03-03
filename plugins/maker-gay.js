911483f349cab44018f5d6368315faa1ffd730fd
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-03-02 21:32:20 -0500

    💖 𝙎𝙖𝙠𝙪𝙧𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 💖

--- plugins/maker-gay.js
+++ plugins/maker-gay.js
@@ -3,7 +3,7 @@ const handler = async (m, {conn}) => {
   const who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
   await conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/gay', {
     avatar: await conn.profilePictureUrl(who, 'image').catch((_) => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
-  }), 'error.png', '🏳️‍🌈 _Quien Quiere Violar A Este Gay_ 🏳️‍🌈', m);
+  }), 'error.png', '_🏳️‍🌈 Quien Quiere Violar A Este Gay 🏳️‍🌈_', m);
   await conn.sendMessage(m.chat, {audio: {url: vn}, fileName: `error.mp3`, mimetype: 'audio/mpeg', ptt: true}, {quoted: m});
 };
 handler.help = ['gay'];
