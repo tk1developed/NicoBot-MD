3e320ecdc37de1f4b1871c83491ec0826e3cc508
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-03-02 19:40:11 -0500

    Actualizar mini-cursos.js

--- plugins/mini-cursos.js
+++ plugins/mini-cursos.js
@@ -10,7 +10,7 @@ let handler = async (m, { conn }) => {
           let child = stdout.toString("utf-8");
           let ssd = child.replace(/Memory:/, "Ram:");
 
-          m.reply(`_[ MINI CURSO ]_\n\n*Hola user quieres aprender de los idiomas de programación? escribe al numero: +54 9 2266 46-6080 por favor espere, puede demorar algunos minutos para responder!⏰️*\n\n*Tambien puedes contactar a: +52 729 488 8993*\n\n_Mini programación de bots_ 📍`);
+          m.reply(`_*[ MINI CURSO ]*_\n\n*Hola user quieres aprender de los idiomas de programación? entra al numero: +54 9 2266 46-6080 hay encontrarás idiomas basicos!⏰️*\n\n*Tambien puedes contactar a: +52 729 488 8993*\n\n_Difruta del mini curso_ 📍`);
             });
 }
 handler.help = ['info']
