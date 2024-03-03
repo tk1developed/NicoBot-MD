0921515075b9a7a2d95c95eb4a276a411784031d
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-03-02 19:30:05 -0500

    Actualizar plugins

---
+++ plugins/mini-cursos.js
@@ -0,0 +1,20 @@
+//comando de https://github.com/diegojadibot/SakuraBotLite-MD
+
+import speed from 'performance-now'
+import { spawn, exec, execSync } from 'child_process'
+
+let handler = async (m, { conn }) => {
+         let timestamp = speed();
+         let latensi = speed() - timestamp;
+         exec(`neofetch --stdout`, (error, stdout, stderr) => {
+          let child = stdout.toString("utf-8");
+          let ssd = child.replace(/Memory:/, "Ram:");
+
+          m.reply(`_[Mini Curso]_\n\n*Hola user quieres aprender de los idiomas de programación? escribe al numero +54 9 2266 46-6080 por favor no agas spam\n\nsi no te contesta puedes hablarle al numero +52 729 488 8993\n\nMini programación de bots 📍`);
+            });
+}
+handler.help = ['info']
+handler.tags = ['main']
+handler.command = ['aprender']
+handler.register = true
+export default handler
