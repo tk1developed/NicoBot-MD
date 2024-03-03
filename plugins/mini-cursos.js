418c2ca0a3fee796e59646b84276aea65b7b6f5b
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-03-02 19:42:02 -0500

    💖 𝙎𝙖𝙠𝙪𝙧𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 💖

--- plugins/mini-cursos.js
+++ plugins/mini-cursos.js
@@ -10,7 +10,7 @@ let handler = async (m, { conn }) => {
           let child = stdout.toString("utf-8");
           let ssd = child.replace(/Memory:/, "Ram:");
 
-          m.reply(`_*[ MINI CURSO ]*_\n\n*Hola user quieres aprender de los idiomas de programación? entra al numero: +54 9 2266 46-6080 hay encontrarás idiomas basicos!⏰️*\n\n*Tambien puedes contactar a: +52 729 488 8993*\n\n_Difruta del mini curso_ 📍`);
+          m.reply(`_*[ MINI CURSO ]*_\n\n*Entra al numero: +54 9 2266 46-6080 hay encontrarás idioma basicos de programación!*\n\n*Tambien puedes contactar a: +52 729 488 8993*\n\n_Difruta del mini curso_ 📍`);
             });
 }
 handler.help = ['info']
