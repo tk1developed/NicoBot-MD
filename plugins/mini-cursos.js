add9191f25e1f4da2fb4474d1fea9a05830a2796
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-03-02 21:33:39 -0500

    💖 𝙎𝙖𝙠𝙪𝙧𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 💖

--- plugins/mini-cursos.js
+++ plugins/mini-cursos.js
@@ -1,4 +1,4 @@
-//comando de https://github.com/diegojadibot/SakuraBotLite-MD
+//https://github.com/diegojadibot/SakuraBotLite-MD
 
 import speed from 'performance-now'
 import { spawn, exec, execSync } from 'child_process'
