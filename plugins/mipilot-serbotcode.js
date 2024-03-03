729268f452b0c7348013facf60cb7e696db6145c
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-03-02 21:36:22 -0500

    💖 𝙎𝙖𝙠𝙪𝙧𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 💖

--- plugins/mipilot-serbotcode.js
+++ plugins/mipilot-serbotcode.js
@@ -6,7 +6,7 @@ async function handler(m, {usedPrefix}) {
         await m.reply(`El token te permite iniciar sesion en otros bots, recomendamos no compartirlo con nadie.\n\n*Tu token es:*`)
         await m.reply(token)
     } else {
-        await m.reply(`*𝐍𝐨 𝐓𝐢𝐞𝐧𝐞𝐬 𝐍𝐢𝐧𝐠𝐮𝐧 𝐓𝐨𝐤𝐞𝐧 𝐀𝐜𝐭𝐢𝐯𝐨 𝐔𝐬𝐚: ${usedPrefix}jadibot 𝐏𝐚𝐫𝐚 𝐂𝐫𝐞𝐚𝐫 𝐔𝐧 𝐓𝐨𝐤𝐞𝐧 🍓.*`)
+        await m.reply(`*No Tienes Ningun Token, Utiliza: ${usedPrefix}botclone Para Crear Un Token 📍.*`)
     }
   }
   handler.command = handler.help = ['token', 'gettoken', 'serbottoken'];
