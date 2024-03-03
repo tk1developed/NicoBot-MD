60d9bb66065f7fcbe5fc4a9ba55ab0dee77e8c31
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-03-02 21:31:13 -0500

    💖 𝙎𝙖𝙠𝙪𝙧𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 💖

--- plugins/karolina.js
+++ plugins/karolina.js
@@ -13,7 +13,7 @@ let expp = Math.floor(Math.random() * 5000)
   global.db.data.users[m.sender].joincount += tok
   global.db.data.users[m.sender].exp += expp
 
-let texto = `*KarolG La Bichota!!🌻✨*`
+let texto = `*KarolG La Bichota!!😻💖*`
 
 const fkontak = {
         "key": {
