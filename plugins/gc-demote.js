99f325f95a378bb104e16e1ebb89569be19d6fc1
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-03-02 21:28:14 -0500

    Actualizar gc-demote.js

--- plugins/gc-demote.js
+++ plugins/gc-demote.js
@@ -8,7 +8,7 @@ const handler = async (m, {conn, usedPrefix, text}) => {
   }
 
   if (!text && !m.quoted) return conn.reply(m.chat, `*📍 USO APROPIADO*\n\n*┯┷*\n*┠≽ ${usedPrefix}quitaradmin @tag*\n*┠≽ ${usedPrefix}quitaradmin -> responder a un mensaje*\n*┷┯*`, m);
-  if (number.length > 13 || (number.length < 11 && number.length > 0)) return conn.reply(m.chat, `*[ ⚠️ ] El número ingresado es incorrecto, por favor ingrese el número correcto*`, m);
+  if (number.length > 13 || (number.length < 11 && number.length > 0)) return conn.reply(m.chat, `🧸 *Numero Que Ingresastes Es Incorrecto*`, m);
 
   try {
     if (text) {
@@ -21,7 +21,7 @@ const handler = async (m, {conn, usedPrefix, text}) => {
   } catch (e) {
   } finally {
     conn.groupParticipantsUpdate(m.chat, [user], 'demote');
-    conn.reply(m.chat, `*📍 Ordenes Recibidas*`, m);
+    conn.reply(m.chat, `✅️ *Ordenes Recibidas*`, m);
   }
 };
 handler.help = ['*593xxx*', '*@usuario*', '*responder chat*'].map((v) => 'demote ' + v);
