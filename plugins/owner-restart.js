7e1e36215cd6fb89a22fc3cda8915eafeacc0f6c
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-03-03 21:48:47 -0500

    💖 𝙎𝙖𝙠𝙪𝙧𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 💖

--- plugins/owner-restart.js
+++ plugins/owner-restart.js
@@ -2,7 +2,7 @@ import { spawn } from 'child_process'
 let handler = async (m, { conn, isROwner, text }) => {
     if (!process.send) throw 'Dont: node main.js\nDo: node index.js'
     if (conn.user.jid == conn.user.jid) {
-    await m.reply('😼 𝖤𝗌𝗉𝖾𝗋𝖺 𝖴𝗇𝗈𝗌 𝖬𝗂𝗇𝗎𝗍𝗈𝗌, 𝖤𝗅 𝖡𝗈𝗍 𝖲𝖾 𝖤𝗌𝗍𝖺́ 𝖱𝖾𝗂𝗇𝗂𝖼𝗂𝖺𝗇𝖽𝗈🙀...')
+    await m.reply('🔁 *Reiniciando* ...')
     process.send('reset')
   } else throw 'eh'
 }
@@ -12,5 +12,4 @@ handler.tags = ['owner']
 handler.command = ['restart','reiniciar'] 
 
 handler.rowner = true
-
 export default handler
