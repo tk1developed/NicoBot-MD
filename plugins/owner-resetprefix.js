56651759a72f311f240bc94bef74c0410ded07b0
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-03-03 22:00:32 -0500

    💖 𝙎𝙖𝙠𝙪𝙧𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 💖

--- plugins/owner-resetprefix.js
+++ plugins/owner-resetprefix.js
@@ -1,6 +1,6 @@
 const handler = async (m, {conn}) => {
   global.prefix = new RegExp('^[' + (opts['prefix'] || '‎xzXZ/i!#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&.\\-').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']');
-  await m.reply(`🦋 𝑃𝑟𝑒𝑓𝑖𝑗𝑜 𝑅𝑒𝑠𝑡𝑎𝑏𝑙𝑒𝑐𝑖𝑑𝑜 𝐶𝑜𝑛 𝐸𝑥𝑖𝑡𝑜 🍓`);
+  await m.reply(`✅️ *Prefijo Restablecido Con Éxito!*`);
   // conn.fakeReply(m.chat, '🦋 𝑃𝑟𝑒𝑓𝑖𝑗𝑜 𝑅𝑒𝑠𝑡𝑎𝑏𝑙𝑒𝑐𝑖𝑑𝑜 𝐶𝑜𝑚 𝐸𝑥𝑖𝑡𝑜 🍓', '0@s.whatsapp.net', 'Reset Prefix')
 };
 handler.help = ['resetprefix'];
