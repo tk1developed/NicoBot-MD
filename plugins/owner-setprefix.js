782d7b7ad4dfd40f2a6ed49584ad1595c5eb608c
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-03-03 21:50:38 -0500

    💖 𝙎𝙖𝙠𝙪𝙧𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 💖

--- plugins/owner-setprefix.js
+++ plugins/owner-setprefix.js
@@ -1,7 +1,7 @@
 const handler = async (m, {conn, text, usedPrefix, command}) => {
-  if (!text) throw `*🦋 𝑁𝑜 𝑆𝑒 𝐸𝑛𝑐𝑜𝑛𝑡𝑟𝑜 𝑁𝑖𝑛𝑔𝑢𝑛 𝑃𝑟𝑒𝑓𝑖𝑗𝑜 𝑃𝑜𝑟 𝐹𝑎𝑣𝑜𝑟 𝐸𝑠𝑐𝑟𝑖𝑏𝑎 𝐸𝑙 𝑃𝑟𝑒𝑓𝑖𝑗𝑜. 𝐄𝐣𝐞𝐦𝐩𝐥𝐨:* ${usedPrefix + command} /`;
+  if (!text) throw `*🍭 No Se Encontró Ningun Prefijo, Por Favor Escribs Un Prefijo. Ejemplo:* ${usedPrefix + command} /`;
   global.prefix = new RegExp('^[' + (text || global.opts['prefix'] || '‎xzXZ/i!#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&.\\-').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']');
-  await m.reply(`*🦋 𝑷𝒓𝒆𝒇𝒊𝒋𝒐 𝑨𝒄𝒕𝒖𝒂𝒍𝒊𝒛𝒂𝒅𝒐, 𝑷𝒓𝒆𝒇𝒊𝒋𝒐: ${text}*`);
+  await m.reply(`*✅️ Prefijo Actualizado Con Éxito, Prefijo Actual: ${text}*`);
 };
 handler.help = ['setprefix'].map((v) => v + ' [prefix]');
 handler.tags = ['owner'];
