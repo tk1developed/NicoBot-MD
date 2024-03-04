9fc4bd9ecfdf5396b9c50580c07e7271356a9fe4
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-03-03 21:47:30 -0500

    💖 𝙎𝙖𝙠𝙪𝙧𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 💖

--- plugins/owner-sernamebot.js
+++ plugins/owner-sernamebot.js
@@ -1,8 +1,8 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-  if (!text) throw `🦋 𝑸𝒖𝒆 𝑵𝒐𝒎𝒃𝒓𝒆 𝑴𝒆 𝑸𝒖𝒊𝒆𝒓𝒆𝒔 𝑷𝒐𝒏𝒆𝒓? 🍓`
+  if (!text) throw `🧸 *Que Nombre Deseas Ponerme?* 🍭`
   try {
     await conn.updateProfileName(text)
-    m.reply('🦋 𝐿𝑖𝑠𝑡𝑜 𝑃𝑟𝑜𝑝𝑖𝑒𝑡𝑎𝑟𝑖𝑜 𝐸𝑙 𝑁𝑜𝑚𝑏𝑟𝑒 𝐷𝑒𝑙 𝑁𝑢𝑚𝑒𝑟𝑜 𝐷𝑒𝑙 𝐵𝑜𝑡 𝑆𝑒 𝐴𝑐𝑡𝑢𝑎𝑙𝑖𝑧𝑜 𝐶𝑜𝑛 𝐸𝑥𝑖𝑡𝑜 🍓')
+    m.reply('✅️ *Nombre Cambiado Con Éxito*')
   } catch (e) {
     console.log(e)
     throw `𝙴𝚁𝚁𝙾𝚁 𝙰𝙻 𝙲𝙰𝙼𝙱𝙸𝙰𝚁 𝙴𝙻 𝙽𝙰𝙼𝙴`
