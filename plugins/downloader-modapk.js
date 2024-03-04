de3b5879206367d9e5077fb157342bc1667f9e07
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-03-04 10:01:34 -0500

    💖 𝙎𝙖𝙠𝙪𝙧𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 💖

--- plugins/downloader-modapk.js
+++ plugins/downloader-modapk.js
@@ -4,10 +4,10 @@ const handler = async (m, {conn, usedPrefix: prefix, command, text}) => {
   try {    
     const searchA = await search(text);
     const data5 = await download(searchA[0].id);
-    let response = `🦋 *𝘋𝘦𝘴𝘤𝘢𝘳𝘨𝘢𝘥𝘰𝘳 𝘋𝘦 𝘈𝘱𝘵𝘰𝘪𝘥𝘦* 📲\n\n🐱 *𝖭𝗈𝗆𝖻𝗋𝖾:* ${data5.name}\n🚩 *𝖯𝖺𝖼𝗄𝖺𝗀𝖾:* ${data5.package}\n🎌 *𝖴𝗅𝗍𝗂𝗆𝖺 𝖠𝖼𝗍𝗎𝖺𝗅𝗂𝗓𝖺𝖼𝗂𝗈𝗇:* ${data5.lastup}\n💝 *𝖳𝖺𝗆𝖺𝗇̃𝗈:* ${data5.size}`
+    let response = `🍭 _Descargador De Aptoide_ 📲\n\n🐱 *Nombre:* ${data5.name}\n🚩 *Package:* ${data5.package}\n🎌 *Ultima Actualización:* ${data5.lastup}\n💝 *Tamaño:* ${data5.size}`
     await conn.sendMessage(m.chat, {image: {url: data5.icon}, caption: response}, {quoted: m});
  if (data5.size.includes('GB') || data5.size.replace(' MB', '') > 999) {
-      return await conn.sendMessage(m.chat, {text: '*🚩 𝖫𝖺 𝖠𝗉𝗉 𝖤𝗌 𝖬𝗎𝗒 𝖯𝖾𝗌𝖺𝖽𝖺 𝖭𝗈 𝖲𝖾 𝖯𝗎𝖽𝗈 𝖤𝗇𝗏𝗂𝖺𝗋 𝖤𝗅 𝖠𝗋𝖼𝗁𝗂𝗏𝗈 🎌.*'}, {quoted: m});
+      return await conn.sendMessage(m.chat, {text: '*🚩 La App Es Muy Pesada No Se Pudo Enviar El Archivo 🎌.*'}, {quoted: m});
     }
     await conn.sendMessage(m.chat, {document: {url: data5.dllink}, mimetype: 'application/vnd.android.package-archive', fileName: data5.name + '.apk', caption: null}, {quoted: m});
   } catch {
