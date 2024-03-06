93af52deaf3e5a84193b946171ce4fe82a503e79
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-03-05 20:12:59 -0500

    💖 𝙎𝙖𝙠𝙪𝙧𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 💖

--- plugins/owner-reporte.js
+++ plugins/owner-reporte.js
@@ -1,5 +1,5 @@
 const handler = async (m, {conn, text, usedPrefix, command}) => {
-  if (!text) throw `🌼 Iɴɢʀᴇsᴀ Uɴ Rᴇᴘᴏʀᴛᴇ\n\n*Ejemplo:*\n*${usedPrefix + command}* 𝐸𝑙 𝑆𝑖𝑔𝑢𝑖𝑒𝑛𝑡𝑒 𝐶𝑜𝑚𝑎𝑛𝑑𝑜 ${usedPrefix}owner 𝐷𝑖𝑜 𝐸𝑟𝑟𝑜𝑟 𝐴𝑙 𝑈𝑠𝑎𝑟𝑙𝑜`;
+  if (!text) throw `*📖 Ingresa Algun Reporte*\n\n*Ejemplo:*\n*${usedPrefix + command}* El Siguiente Comando ${usedPrefix}menu Tiene Un Error`;
   if (text.length < 10) throw `🌼 𝐑𝐞𝐩𝐨𝐫𝐭𝐞 𝐦𝐢𝐧𝐢𝐦𝐨 𝐝𝐞 10 𝐜𝐚𝐫𝐚𝐜𝐭𝐞𝐫𝐞𝐬`;
   if (text.length > 1000) throw `🌼 𝐑𝐞𝐩𝐨𝐫𝐭𝐞 𝐦𝐚𝐱𝐢𝐦𝐨 𝐝𝐞 1000 𝐜𝐚𝐫𝐚𝐜𝐭𝐞𝐫𝐞𝐬`;
   const teks = `*╭━━[ 𝑹𝑬𝑷𝑶𝑹𝑻𝑬 ]━━━⬣*\n*┃*\n*┃* ➦☬⃝✨𝐶𝑙𝑖𝑒𝑛𝑡𝑒: \n┃ ❁ Wa.me/${m.sender.split`@`[0]}\n*┃*\n*┃* ➦☬⃝✨𝑆𝑚𝑠:\n*┃* ❁ ${text}\n*┃*\n*╰❖────『 𝑆𝑜𝑝𝑜𝑟𝑡𝑒 』────❖*`;
