093ac210d690caa5e2af1aeaca068efbd2829366
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-03-05 21:32:55 -0500

    💖 𝙎𝙖𝙠𝙪𝙧𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 💖

--- plugins/owner-add_del_owner.js
+++ plugins/owner-add_del_owner.js
@@ -1,19 +1,19 @@
 const handler = async (m, { conn, text, args, usedPrefix, command }) => {
-  const why = `*[❗] Uso incorrecto del comando.*\n\n*—◉ Ejemplo de uso válido:*\n*◉ ${usedPrefix + command} @${m.sender.split('@')[0]}*\n*◉ ${usedPrefix + command} ${m.sender.split('@')[0]}*\n*◉ ${usedPrefix + command} <mensaje citado>*`;
+  const why = `*📖 Uso incorrecto del comando.*\n\n*—◉ Ejemplo de uso válido:*\n*◉ ${usedPrefix + command} @${m.sender.split('@')[0]}*\n*◉ ${usedPrefix + command} ${m.sender.split('@')[0]}*\n*◉ ${usedPrefix + command} <mensaje citado>*`;
   const who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : false;
   if (!who) return conn.reply(m.chat, why, m, {mentions: [m.sender]});
   switch (command) {
     case 'addowner':
       const nuevoNumero = who;
       global.owner.push([nuevoNumero]);
-      await conn.reply(m.chat, '*🦁Listo Amo Ya Está En La Lista De Owner El Usuario.*', m);
+      await conn.reply(m.chat, '*✅️ El Numero Se Añadió Con Éxito A La Lista De Owner*', m);
       break;
     case 'delowner':
       const numeroAEliminar = who;
       const index = global.owner.findIndex(owner => owner[0] === numeroAEliminar);
       if (index !== -1) {
         global.owner.splice(index, 1);
-        await conn.reply(m.chat, '*⚡️Eliminado El Numero de la lista de owner correctamente.*', m);
+        await conn.reply(m.chat, '*✅️ Eliminado El Numero de la lista de owner correctamente.*', m);
       } else {
         await conn.reply(m.chat, '*⏳️El Numero No Está En La Lista De Owners.*', m);
       }
