65931406b634a42b4b68e671c4d7432c15eff996
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-03-05 20:09:02 -0500

    💖 𝙎𝙖𝙠𝙪𝙧𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 💖

--- plugins/owner-reporte.js
+++ plugins/owner-reporte.js
@@ -1,12 +1,12 @@
 const handler = async (m, {conn, text, usedPrefix, command}) => {
-  if (!text) throw `🌼 Iɴɢʀᴇsᴀ Uɴ Rᴇᴘᴏʀᴛᴇ\n\n𝐄𝐣𝐞𝐦𝐩𝐥𝐨:\n*${usedPrefix + command}* 𝐸𝑙 𝑆𝑖𝑔𝑢𝑖𝑒𝑛𝑡𝑒 𝐶𝑜𝑚𝑎𝑛𝑑𝑜 ${usedPrefix}owner 𝐷𝑖𝑜 𝐸𝑟𝑟𝑜𝑟 𝐴𝑙 𝑈𝑠𝑎𝑟𝑙𝑜`;
+  if (!text) throw `🌼 Iɴɢʀᴇsᴀ Uɴ Rᴇᴘᴏʀᴛᴇ\n\n*Ejemplo:*\n*${usedPrefix + command}* 𝐸𝑙 𝑆𝑖𝑔𝑢𝑖𝑒𝑛𝑡𝑒 𝐶𝑜𝑚𝑎𝑛𝑑𝑜 ${usedPrefix}owner 𝐷𝑖𝑜 𝐸𝑟𝑟𝑜𝑟 𝐴𝑙 𝑈𝑠𝑎𝑟𝑙𝑜`;
   if (text.length < 10) throw `🌼 𝐑𝐞𝐩𝐨𝐫𝐭𝐞 𝐦𝐢𝐧𝐢𝐦𝐨 𝐝𝐞 10 𝐜𝐚𝐫𝐚𝐜𝐭𝐞𝐫𝐞𝐬`;
   if (text.length > 1000) throw `🌼 𝐑𝐞𝐩𝐨𝐫𝐭𝐞 𝐦𝐚𝐱𝐢𝐦𝐨 𝐝𝐞 1000 𝐜𝐚𝐫𝐚𝐜𝐭𝐞𝐫𝐞𝐬`;
   const teks = `*╭━━[ 𝑹𝑬𝑷𝑶𝑹𝑻𝑬 ]━━━⬣*\n*┃*\n*┃* ➦☬⃝✨𝐶𝑙𝑖𝑒𝑛𝑡𝑒: \n┃ ❁ Wa.me/${m.sender.split`@`[0]}\n*┃*\n*┃* ➦☬⃝✨𝑆𝑚𝑠:\n*┃* ❁ ${text}\n*┃*\n*╰❖────『 𝑆𝑜𝑝𝑜𝑟𝑡𝑒 』────❖*`;
   conn.reply('573013482814@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, null, {contextInfo: {mentionedJid: [m.sender]}});
   conn.reply('51935499065@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, null, {contextInfo: {mentionedjid: [m.sender]}});
   conn.reply('5491168758497@s.whatsapp.net',m.quoted ? teks + m.quoted.text : teks, null, {contextInfo: {mentionedjid: [m.sender]}});
-  m.reply(`[ 🌻 ] ☆𝑬𝒙𝒊𝒕𝒐 𝑬𝒍 𝑺𝒎𝒔 𝑺𝒆 𝑬𝒏𝒗𝒊𝒂𝒓𝒂 𝑨 𝑳𝒐𝒔 𝑪𝒓𝒆𝒂𝒅𝒐𝒓𝒆𝒔 𝑫𝒆𝒍 𝑩𝒐𝒕, 𝕾𝖆𝖐𝖚𝖗𝖆-𝕭𝖔𝖙-𝕷𝖎𝖙𝖊-𝕸𝕯☆`);
+  m.reply(`📖 ☆Éxito El Reporte Se Envió A Los Propietarios Del Bot`);
 };
 handler.help = ['reporte', 'request'].map((v) => v + ' <teks>');
 handler.tags = ['info'];
