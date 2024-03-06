a886e952174146b558bca6a22838ec7b5b91ac54
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-03-06 11:14:54 -0500

    💖 𝙎𝙖𝙠𝙪𝙧𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 💖

--- plugins/convertidor-tourl.js
+++ plugins/convertidor-tourl.js
@@ -3,13 +3,53 @@ import uploadImage from '../lib/uploadImage.js';
 const handler = async (m) => {
   const q = m.quoted ? m.quoted : m;
   const mime = (q.msg || q).mimetype || '';
-  if (!mime) throw '*⚓ 𝐻𝑒𝑦 𝑅𝑒𝑠𝑝𝑜𝑛𝑑𝑒 𝐴 𝑈𝑛𝑎 𝐼𝑚𝑎𝑔𝑒𝑛 𝑂 𝑈𝑛 𝑉𝑖𝑑𝑒𝑜 𝐸𝑙 𝐶𝑢𝑎𝑙 𝑄𝑢𝑖𝑒𝑟𝑎𝑠 𝐶𝑜𝑛𝑣𝑒𝑟𝑡𝑖𝑟𝑙𝑜 𝐸𝑛 𝑈𝑛 𝐸𝑛𝑙𝑎𝑐𝑒🌱.*';
+  if (!mime) throw '*⚠️ Responda A Una Imagen.*';
   const media = await q.download();
   const isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime);
   const link = await (isTele ? uploadImage : uploadFile)(media);
-  m.reply(`*⚓ 𝑆𝑢 𝐸𝑛𝑙𝑎𝑐𝑒 𝐷𝑒𝑙 𝐴𝑟𝑐ℎ𝑖𝑣𝑜:* ${link}`);
+  m.reply(`*📖 Enlace:* ${link}`);
 };
 handler.help = ['tourl <reply image>'];
 handler.tags = ['sticker'];
 handler.command = /^(upload|tourl)$/i;
 export default handler;
+
+
+/*Créditos a https://github.com/AzamiJs
+
+import uploadFile from '../lib/uploadFile.js'
+import uploadImage from '../lib/uploadImage.js'
+import fetch from 'node-fetch'
+
+var handler = async (m) => {
+
+let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+let pp = await conn.profilePictureUrl(who, 'image').catch(_ => './src/avatar_contact.png')
+let name = await conn.getName(who)
+let q = m.quoted ? m.quoted : m
+let mime = (q.msg || q).mimetype || ''
+if (!mime) throw '*⚠️ RESPONDA A UNA IMAGEN*'
+m.react(done)
+let media = await q.download()
+let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime)
+let link = await (isTele ? uploadImage : uploadFile)(media)
+let info = ` *🗂️ ENLACE:*\n${link}\n
+*⚖️ TAMAÑO:*\n${media.length} bytes\n
+*🚀 EXPIRACION:*\n ${isTele ? '✅ NO EXPIRA' : '⚠️ DESCONOCIDO'}\n
+*🔰 ACORTADO:*\n${await shortUrl(link)}`
+
+conn.reply(m.chat, info, m, { contextInfo: { externalAdReply :{ mediaUrl: ig, mediaType: 2, title: wm, body: azami, thumbnail: await(await fetch(link)).buffer(), sourceUrl: link}}})
+
+}
+handler.help = ['tourl']
+handler.tags = ['transformador']
+handler.command = /^(tourl|upload)$/i
+
+handler.limit = true
+
+export default handler
+
+async function shortUrl(url) {
+let res = await fetch(`https://tinyurl.com/api-create.php?url=${url}`)
+return await res.text()
+}*/
