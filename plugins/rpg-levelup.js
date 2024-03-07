1a8dbcce5fdc7d711035c60f1f5a6b5851915191
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-03-07 06:37:36 -0500

    💖 𝙎𝙖𝙠𝙪𝙧𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 💖

--- plugins/rpg-levelup.js
+++ plugins/rpg-levelup.js
@@ -6,32 +6,32 @@ const handler = async (m, {conn}) => {
   const user = global.db.data.users[m.sender];
   if (!canLevelUp(user.level, user.exp, global.multiplier)) {
     const {min, xp, max} = xpRange(user.level, global.multiplier);
-    throw `◆━━━━━━ ✿ 𝐿𝑒𝑣𝑒𝑙𝑢𝑝🧸
-┋➺ 🧸𝐍𝐨𝐦𝐛𝐫𝐞: 
+    throw `◆━━━━━━ ✿  *Levelup* 🧸
+┋➺ *🧸Nombre:* 
 ┋➺ *${name}*
 ┋─── ❖ ── ✦ ── ❖ ───
-┋➺ 📍𝐍𝐢𝐯𝐞𝐥: 
+┋➺ *📍Nivel:* 
 ┋➺ *${user.level}*
 ┋─── ❖ ── ✦ ── ❖ ───
-┋➺ 🧩𝐄𝐱𝐩𝐞𝐫𝐭𝐨: 
+┋➺ *🧩Experto:*
 ┋➺ *${user.exp - min}/${xp}*
 ◆━━━━━━◆❃◆━━━━━━◆
-𝑇𝑒 𝐻𝑎𝑐𝑒 𝐹𝑎𝑙𝑡𝑎 *${max - user.exp}* 𝐷𝑒 𝐸𝑥𝑝𝑒𝑟𝑡𝑜 𝑃𝑎𝑟𝑎 𝑆𝑢𝑏𝑖𝑟 𝑈𝑛 𝑁𝑢𝑒𝑣𝑜 𝐿𝑒𝑣𝑒𝑙𝑢𝑝🧸📍
+Te Hace Falta *${max - user.exp}* De Experto  Para Subir Un Nuevo Nivel 🧸📍
 `.trim();
   }
   const before = user.level * 1;
   while (canLevelUp(user.level, user.exp, global.multiplier)) user.level++;
   if (before !== user.level) {
     const teks = `🎊 Bien hecho ${conn.getName(m.sender)}    Nivel:`;
-    const str = `★━━━━━━•𝑳𝒆𝒗𝒆𝒍𝒖𝒑🧸
-┋➺ 🧸𝐀𝐧𝐭𝐞𝐫𝐢𝐨𝐫 𝐃𝐞𝐥 𝐍𝐢𝐯𝐞𝐥: 
+    const str = `★━━━━━━• *Levelup🧸*
+┋➺ *🧸Anterior Del Nivel:* 
 ┋➺ *${before}*
 ┋─── ❖ ── ✦ ── ❖ ───
-┋➺ 📍𝐍𝐢𝐯𝐞𝐥 𝐀𝐜𝐭𝐮𝐚𝐥:
+┋➺ *📍Nivel Actual:*
 ┋➺ *${user.level}*
 ★━━━━━━✩━━━━━━★
 
-𝑀𝑖𝑒𝑛𝑠𝑡𝑟𝑎 𝑀𝑎𝑠 𝑈𝑠𝑒𝑠 𝕾𝖆𝖐𝖚𝖗𝖆-𝕭𝖔𝖙-𝕷𝖎𝖙𝖊-𝕸𝕯 𝑇𝑢 𝐿𝑒𝑣𝑒𝑙𝑢𝑝 𝑆𝑢𝑏𝑖𝑟𝑎 𝑀𝑎𝑠 𝑅𝑎𝑝𝑖𝑑𝑜🥀📍
+Mienstra Mas Uses *SakuraBotLite-MD* Tu Nivel Subirá Mas Rapido🥀📍
 `.trim();
     try {
       const img = await levelup(teks, user.level);
