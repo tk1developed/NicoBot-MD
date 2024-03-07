af2c228cce11091829ae0005b2ed9194fecc1265
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-03-07 08:29:47 -0500

    💖 𝙎𝙖𝙠𝙪𝙧𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 💖

--- plugins/info-bot.js
+++ plugins/info-bot.js
@@ -9,25 +9,10 @@ handler.all = async function(m) {
   // const estiloaudio = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: {"audioMessage": { "mimetype":"audio/ogg; codecs=opus", "seconds": "99569", "ptt": "true"}}}
   if (/^bot$/i.test(m.text) && !chat.isBanned) {
     conn.sendPresenceUpdate('recording', m.chat);
-    await m.reply(`🧸 *Estoy Aqui Para Ayudarte*`);
+    await m.reply(`*Hola Aqui Estoy, En Que Te Puedo Ayudar? 🍧*`);
     m.conn.sendMessage(m.chat, {audio: {url: vn}, fileName: 'error.mp3', mimetype: 'audio/mpeg', ptt: true}, {quoted: m});
     // conn.sendFile(m.chat, vn, 'bot.mp3', null, m, true, { type: 'audioMessage', seconds: '4556', ptt: true, sendEphemeral: true, quoted: m })
   }
   return !0;
 };
 export default handler;
-
-/*var handler = m => m
-handler.all = async function (m) {
-
-let chat = global.db.data.chats[m.chat]
-
-if (/^bot$/i.test(m.text) && !chat.isBanned) {
-
-conn.sendPresenceUpdate('composing', m.chat)    
-conn.reply(m.chat, '🎌 *Estoy aquí para ayudarte*', m, fake, )}
-
-return !0
-
-}
-export default handler*/
