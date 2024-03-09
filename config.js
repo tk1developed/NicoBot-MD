821710e7ddf84db21c980749b750b31fee176848
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-03-09 11:11:03 -0500

    💖 𝙎𝙖𝙠𝙪𝙧𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 💖

--- config.js
+++ config.js
@@ -11,8 +11,8 @@ import { es } from "./lib/idiomas/total-idiomas.js"
 
 //BETA: Si quiere evitar escribir el número que será bot en la consola, agregué desde aquí entonces:
 //Sólo aplica para opción 2 (ser bot con código de texto de 8 digitos)
-global.botNumber = '' //Ejemplo: +59309090909
-global.confirmCode = ''
+global.botnumber = "" //Ejemplo: +59309090909
+global.confirmCode = ""
 
 //No quites los numeros de los propietarios
 global.owner = [
