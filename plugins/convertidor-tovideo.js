5f02afe6346a6523027b9c7c96a1d5fa342f10ba
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-03-09 09:30:17 -0500

    💖 𝙎𝙖𝙠𝙪𝙧𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 💖

--- plugins/convertidor-tovideo.js
+++ plugins/convertidor-tovideo.js
@@ -1,34 +1,25 @@
-/*Creditos A AzamiJs*/
-
-import { webp2mp4 } from '../lib/webp2mp4.js'
-import { ffmpeg } from '../lib/converter.js'
-
-var handler = async (m, { conn, usedPrefix, command }) => {
-
-if (!m.quoted) throw `*⚠️ RESPONDE CON ${usedPrefix + command} AL AUDIO QUE DESEEA CONVERTIR EN VIDEO*`
-let mime = m.quoted.mimetype || ''
-if (!/webp|audio/.test(mime)) throw `*⚠️ RESPONDE CON ${usedPrefix + command} AL AUDIO QUE DESEEA CONVERTIR EN VIDEO*`
-let media = await m.quoted.download()
-let out = Buffer.alloc(0)
-if (/webp/.test(mime)) {
-out = await webp2mp4(media)
-} else if (/audio/.test(mime)) {
-out = await ffmpeg(media, [
-'-filter_complex', 'color',
-'-pix_fmt', 'yuv420p',
-'-crf', '51',
-'-c:a', 'copy',
-'-shortest'
-], 'mp3', 'mp4')
-
-}
-await conn.sendFile(m.chat, out, 'error.mp4', '*🧃 AQUI ESTA TU VIDEO*', fkontak, m, 0, { thumbnail: out })
-
-}
-handler.help = ['tovideo']
-handler.tags = ['transformador']
-handler.command = ['tovideo']
-
-handler.limit = true
-
-export default handler
+import {webp2mp4} from '../lib/webp2mp4.js';
+import {ffmpeg} from '../lib/converter.js';
+const handler = async (m, {conn, usedPrefix, command}) => {
+  if (!m.quoted) throw `*🍧 Responda A Un Sticker Que Desee Convertir En  Video Con El Comando ${usedPrefix + command}*`;
+  const mime = m.quoted.mimetype || '';
+  if (!/webp/.test(mime)) throw `*🍧 Responda A Un Sticker Que Desee Convertir En  Video Con El Comando ${usedPrefix + command}*`;
+  const media = await m.quoted.download();
+  let out = Buffer.alloc(0);
+  if (/webp/.test(mime)) {
+    out = await webp2mp4(media);
+  } else if (/audio/.test(mime)) {
+    out = await ffmpeg(media, [
+      '-filter_complex', 'color',
+      '-pix_fmt', 'yuv420p',
+      '-crf', '51',
+      '-c:a', 'copy',
+      '-shortest',
+    ], 'mp3', 'mp4');
+  }
+  await conn.sendFile(m.chat, out, 'error.mp4', '*DONE*', m, 0, {thumbnail: out});
+};
+handler.help = ['tovideo'];
+handler.tags = ['sticker'];
+handler.command = ['tovideo', 'tomp4', 'mp4', 'togif'];
+export default handler;
