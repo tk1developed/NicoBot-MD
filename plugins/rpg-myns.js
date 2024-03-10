3f7757debbcf0a8236ebe143b90912ea7031f5ce
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-03-09 20:09:18 -0500

    💖 𝙎𝙖𝙠𝙪𝙧𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 💖

--- plugins/rpg-myns.js
+++ plugins/rpg-myns.js
@@ -1,11 +1,1 @@
-import { createHash } from 'crypto'
-let handler = async function (m, { conn, text, usedPrefix }) {
-let sn = createHash('md5').update(m.sender).digest('hex')
-
-conn.fakeReply(m.chat, sn, '0@s.whatsapp.net', '👇 *ESTE ES SU NUMERO DE SERIE* 👇', 'status@broadcast')
-}
-handler.help = ['myns']
-handler.tags = ['rg']
-handler.command = /^(myns|ceksn)$/i
-handler.register = true
-export default handler
+//💖 𝙎𝙖𝙠𝙪𝙧𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 💖
