43eb61c1b8783a5296b4733f0a917401902a803d
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-03-09 20:03:38 -0500

    💖 𝙎𝙖𝙠𝙪𝙧𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 💖

--- plugins/convertidor-togifaud.js
+++ plugins/convertidor-togifaud.js
@@ -1,4 +1,4 @@
-const handler = async (m, {conn, usedPrefix, command}) => {
+/*const handler = async (m, {conn, usedPrefix, command}) => {
   if (!m.quoted) throw `*🍧 Responda A Un Video Que Desee Convertir En Gif Con Audio*`;
   const q = m.quoted || m;
   const mime = (q.msg || q).mimetype || '';
@@ -8,4 +8,4 @@ const handler = async (m, {conn, usedPrefix, command}) => {
   conn.sendMessage(m.chat, {video: media, gifPlayback: true, caption: '*📖 La Musica Se Reproduce Cuando Abras El Archivo*'}, {quoted: m});
 };
 handler.command = ['togifaud'];
-export default handler;
+export default handler;*/
