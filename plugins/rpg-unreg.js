182812e69bf31c9fc051426856b9137d3cf9fab6
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-03-09 20:10:20 -0500

    💖 𝙎𝙖𝙠𝙪𝙧𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 💖

--- plugins/rpg-unreg.js
+++ plugins/rpg-unreg.js
@@ -1,4 +1,4 @@
-import {createHash} from 'crypto';
+/*import {createHash} from 'crypto';
 const handler = async function(m, {args}) {
   if (!args[0]) throw '*⚠️ Ingrese Su Número De Série, Si No Sabe Cual Es Use #myns*';
   const user = global.db.data.users[m.sender];
@@ -11,5 +11,5 @@ handler.help = ['', 'ister'].map((v) => 'unreg' + v + ' <numero de serie>');
 handler.tags = ['xp'];
 handler.command = /^unreg(ister)?$/i;
 handler.register = true;
-export default handler;
+export default handler;*/
 
