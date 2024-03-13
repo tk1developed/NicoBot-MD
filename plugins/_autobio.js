abde92181971736f66e8302b69de3d831365768e
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-03-12 21:38:43 -0500

    💖 𝙎𝙖𝙠𝙪𝙧𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 💖

--- plugins/_autobio.js
+++ plugins/_autobio.js
@@ -8,7 +8,7 @@ if (process.send) { process.send('uptime')
 _muptime = await new Promise(resolve => { process.once('message', resolve) 
 setTimeout(resolve, 2000) }) * 1000}
 let uptime = clockString(_uptime)
-let bio = `${global.packname} 🟢 Mɪ Tɪᴇᴍᴘᴏ Aᴄᴛɪᴠᴀ Es: ${uptime} 𝖶𝗁𝖺𝗍𝗌𝖠𝗉𝗉 - 𝖡𝗈𝗍 🌹`
+let bio =`⌥${global.packname}彡 ✧ Tiempo Activó : ${uptime} ⌛ ✧ Ⓣ︎Ⓑ︎`
 await this.updateProfileStatus(bio).catch(_ => _)
 setting.status = new Date() * 1
 } 
@@ -19,5 +19,5 @@ function clockString(ms) {
   let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24
   let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
   let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-  return [d, ' • ', h, ' » ', m, ' » ', s].map(v => v.toString().padStart(2, 0)).join('') 
-}
+    return [d, ' • ', h, ' » ', m, ' » ', s].map(v => v.toString().padStart(2, 0)).join('') 
+} 
