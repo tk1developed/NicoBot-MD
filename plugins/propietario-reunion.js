2688c534e0f4fa6672203fb12f6474fe5db246da
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-03-15 22:21:33 -0500

    🌸◌*̥₊ SᴀᴋᴜʀᴀBᴏᴛLɪᴛᴇ ◌❐🌹༉

--- plugins/propietario-reunion.js
+++ plugins/propietario-reunion.js
@@ -1,13 +1,13 @@
 let handler = async(m, { conn, command, text }) => {
 
   let users = m.sender.split`@`[0]
-  if ( users == 573107133321 || users == 573107133321) try {
+  if ( users == 573012482694 || users == 573012482694) try {
   if (!text) return m.reply(`🚀 ¡𝗖𝘂𝗮𝗹 𝗲𝘀 𝗲𝗹 𝗠𝗼𝘁𝗶𝘃𝗼 𝗽𝗮𝗿𝗮 𝗹𝗮 𝗿𝗲𝘂𝗻𝗶𝗼𝗻 𝗱𝗲𝗹 𝘀𝘁𝗮𝗳𝗳!`)
     if (text.length < 10) return m.reply(`¡𝗠𝗼𝘁𝗶𝘃𝗼 𝗺𝘂𝘆 𝗰𝗼𝗿𝘁𝗼 , 𝗺𝗶𝗻𝗶𝗺𝗼 10 𝗰𝗮𝗿𝗮𝗰𝘁𝗲𝗿𝗲𝘀!`)
 
     let texto = `💖 @${m.sender.split`@`[0]} 𝐀 𝐒𝐨𝐥𝐢𝐜𝐢𝐭𝐚𝐝𝐨 𝐋𝐚 𝐏𝐫𝐞𝐬𝐞𝐧𝐜𝐢𝐚 𝐃𝐞 𝐋𝐨𝐬 𝐎𝐰𝐧𝐞𝐫 𝐏𝐨𝐫 𝐌𝐨𝐭𝐢𝐯𝐨: 
 ¡${text}
-𝑺𝒕𝒂𝒇𝒇 𝑺𝒂𝒌𝒖𝒓𝒂𝑳𝒊𝒕𝒆 - 𝑫𝒊𝒆𝒈𝒐 🌻`
+Staff SakuraBot - Diego 🌹`
 m.reply('🚀 Sᴇ Lᴇ A Eɴᴠɪᴀᴅᴏ Lᴏs Mᴇɴsᴀᴊᴇs A Lᴏs Oᴡɴᴇʀ Dᴇ Sᴀᴋᴜʀᴀ-Bᴏᴛ-Lɪᴛᴇ-MD')
 for (let [jid] of global.owner.filter(([number, _, isDeveloper]) => isDeveloper && number)) {
                         let data = (await conn.onWhatsApp(jid))[0] || {}
