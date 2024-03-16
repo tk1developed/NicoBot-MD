654826825f78ee10043e1eb7f54bda1fbd5ca04c
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-03-16 16:35:50 -0500

    🌸◌*̥₊ SᴀᴋᴜʀᴀBᴏᴛLɪᴛᴇ ◌❐🌹༉

--- plugins/owner-reporte.js
+++ plugins/owner-reporte.js
@@ -3,7 +3,7 @@ const handler = async (m, {conn, text, usedPrefix, command}) => {
   if (text.length < 10) throw `📖 Reporte Minimo De 10 Caracteres`;
   if (text.length > 1000) throw `📖 Reporte Maxímo De 1000 Caracteres`;
   const teks = `*╭━━[ 𝑹𝑬𝑷𝑶𝑹𝑻𝑬 ]━━━⬣*\n*┃*\n*┃* ➦☬⃝🌸 *Cliente:* \n┃ ❁ Wa.me/${m.sender.split`@`[0]}\n*┃*\n*┃* ➦☬⃝🌸 *Mensaje:*\n*┃* ❁ ${text}\n*┃*\n*╰❖────『 𝑆𝑜𝑝𝑜𝑟𝑡𝑒 』────❖*`;
-  conn.reply('573012482694@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, null, {contextInfo: {mentionedJid: [m.sender]}});
+  conn.reply('573218138672@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, null, {contextInfo: {mentionedJid: [m.sender]}});
 
   m.reply(`📖 *El Reporte Se Envió A Mi Creador, Tendrá Una Respuesta Más Tarde*`);
 };
