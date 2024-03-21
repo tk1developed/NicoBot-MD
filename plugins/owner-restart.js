5b2ccbb7acf07979c6fb07242ccc54aea10776d4
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-03-21 18:19:12 -0500

    🌸◌*̥₊ SᴀᴋᴜʀᴀBᴏᴛLɪᴛᴇ ◌❐🌹༉

--- plugins/owner-restart.js
+++ plugins/owner-restart.js
@@ -2,7 +2,7 @@ import { spawn } from 'child_process'
 let handler = async (m, { conn, isROwner, text }) => {
     if (!process.send) throw 'Dont: node main.js\nDo: node index.js'
     if (conn.user.jid == conn.user.jid) {
-    await m.reply('🍭 *Reiniciando* ...')
+    await m.reply('╰⊱💚⊱ *ÉXITO* ⊱💚⊱╮\n\n```🌸 REINICIANDO 🌸```\n*⌛ ESPERE UN MOMENTO POR FAVOR...*')
     process.send('reset')
   } else throw 'eh'
 }
