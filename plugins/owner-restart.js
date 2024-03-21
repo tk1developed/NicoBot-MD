0a35b1002083e4d55c752b5c1385fb4b7ca63ae3
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-03-21 11:13:53 -0500

    🌸◌*̥₊ SᴀᴋᴜʀᴀBᴏᴛLɪᴛᴇ ◌❐🌹༉

--- plugins/owner-restart.js
+++ plugins/owner-restart.js
@@ -4,8 +4,8 @@ var handler = async (m, { conn, isROwner, text }) => {
 
 if (!process.send) throw 'Dont: node main.js\nDo: node index.js'
 if (conn.user.jid == conn.user.jid) {
-await conn.reply(m.chat, '☁️ *Reiniciando*' )
-process.send('reiniciar')
+await conn.reply(m.chat, '☁️ *Reiniciando*')
+process.send('reset')
 } else throw 'eh'
 
 }
