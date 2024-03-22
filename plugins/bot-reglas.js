389eda256807da74a97f8ce4488780afc8c9dc13
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-03-22 09:22:28 -0500

    🌸◌*̥₊ SᴀᴋᴜʀᴀBᴏᴛLɪᴛᴇ ◌❐🌹༉

--- plugins/bot-reglas.js
+++ plugins/bot-reglas.js
@@ -1,4 +1,4 @@
-var handler = async (m, { conn, usedPrefix, command}) => {
+/*var handler = async (m, { conn, usedPrefix, command}) => {
 
 let Reglas = `🎌 *Respeta las reglas de SakuraBot*\n
 ⬡ No llamar
@@ -28,4 +28,4 @@ function clockString(ms) {
 let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
 let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
 let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')}
+return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')}*/
