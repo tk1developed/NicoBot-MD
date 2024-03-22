9259aaa30ff8e19ef1ed9922b96602bced748e3c
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-03-21 22:25:02 -0500

    🌸◌*̥₊ SᴀᴋᴜʀᴀBᴏᴛLɪᴛᴇ ◌❐🌹༉

--- plugins/downloader-facebook.js
+++ plugins/downloader-facebook.js
@@ -1,21 +1,90 @@
-let handler = async (m, { conn, command, args }) => {
-        if (!args[0]) return m.reply('Link?')
-        if (!isUrl(args[0])) return m.reply('*📌 Por favor use un Link*')
-        if (!args[0].match(www.facebook.com/)) return m.reply('*✳️Su link es inválido*\n\n📌 Por favor, use un link de Facebook\n')
-        let name = await conn.getName(m.sender)
-        let mcarga = m.reply(MultiNK.Proces(name))
-        await mcarga
-try {
-        let resfb = await fetchJson(`https://latam-api.vercel.app/api/facebookdl?apikey=${MyApiKey}&q=${args[0]}`)
-        conn.sendMessage(m.chat, { video: { url: resfb.video }, fileName: `fbsd.mp4`, mimetype: 'video/mp4' }, { quoted: m }).catch(e => {m.reply(MultiNK.Error1())})
-} catch (e) {
-m.reply(MultiNK.Error0())
-}
-}
+import fg from 'api-dylux';
+import fetch from 'node-fetch';
+import {savefrom, facebookdl, facebookdlv2} from '@bochilteam/scraper';
+import fbDownloader from 'fb-downloader-scrapper';
+import {facebook} from '@xct007/frieren-scraper';
+import axios from 'axios';
+const handler = async (m, {conn, args, command, usedPrefix}) => {
+  if (!args[0]) throw `⚠️ _Ingrese Un Enlace De Facebook_\n\n*Ejemplo:*\n*${usedPrefix + command}* https://fb.watch/fOTpgn6UFQ/`;
+  if (!args[0].match(/www.facebook.com|fb.watch/g)) throw `⚠️ _Ingrese Un Enlace De Facebook_\n\n*Ejemplo:*\n*${usedPrefix + command}* https://fb.watch/fOTpgn6UFQ/`;
+  try {
+    await m.reply(`*🥀 ᴅᴇsᴄᴀʀɢᴀɴᴅᴏ sᴜ ᴠɪᴅᴇᴏ, ᴀɢᴜᴀʀᴅᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ ᴘᴏʀ ғᴀᴠᴏʀ, ᴇsᴛᴇ ᴘʀᴏᴄᴇsᴏ ᴘᴜᴇᴅᴇ ᴅᴜʀᴀʀ ᴇɴᴛʀᴇ 2 ʏ 10 ᴍɪɴᴜᴛᴏs ᴅᴇᴘᴇɴᴅɪᴇɴᴅᴏ ᴅᴇ ʟᴀ ᴅᴜʀᴀᴄɪᴏɴ ᴅᴇʟ ᴠɪᴅᴇᴏ...*`);
+    const d2ata = await facebook.v1(args[0]);
+    let r2es = '';
+    if (d2ata.urls && d2ata.urls.length > 0) {
+      r2es = `${d2ata.urls[0]?.hd || d2ata.urls[1]?.sd || ''}`;
+    }
+    conn.sendFile(m.chat, r2es, 'error.mp4', `*𝖠𝗊𝗎𝗂 𝖤𝗌𝗍𝖺 𝖲𝗎 𝖵𝗂𝖽𝖾𝗈*`, m);
+  } catch (err1) {
+    try {
+      const req = await igeh(args[0]);
+      conn.sendMessage(m.chat, {video: {url: req.url_list}}, m);
+    } catch (err1_2) {
+      try {
+        const Rres = await fetch(`https://api.lolhuman.xyz/api/facebook?apikey=${lolkeysapi}&url=${args[0]}`);
+        const Jjson = await Rres.json();
+        let VIDEO = Jjson.result[0];
+        if (VIDEO == '' || !VIDEO || VIDEO == null) VIDEO = Jjson.result[1];
+        conn.sendFile(m.chat, VIDEO, 'error.mp4', `*Aqui Su Video De Facebook 🚀*`, m);
+      } catch (err2) {
+        try {
+          const ress = await fg.fbdl(args[0]);
+          const urll = await ress.data[0].url;
+          await conn.sendFile(m.chat, urll, 'error.mp4', '*Aqui Su Video De Facebook 🚀*', m);
+        } catch (err3) {
+          try {
+            const res = await fbDownloader(args[0]);
+            for (const result of res.download) {
+              const ur = result.url;
+              await conn.sendFile(m.chat, ur, 'error.mp4', '*Aqui Su Video De Facebook 🚀*', m);
+            }
+          } catch (err4) {
+            try {
+              const res3 = await fetch(`https://latam-api.vercel.app/api/facebookdl?apikey=nekosmic&q=${args[0]}`);
+              const json = await res3.json();
+              const url3 = await json.video;
+              await conn.sendFile(m.chat, url3, 'error.mp4', '*Aqui Su Video De Facebook 🚀*', m);
+            } catch (err5) {
+              try {
+                const {result} = await facebookdl(args[0]).catch(async (_) => await facebookdlv2(args[0])).catch(async (_) => await savefrom(args[0]));
+                for (const {url, isVideo} of result.reverse()) await conn.sendFile(m.chat, url, `facebook.${!isVideo ? 'bin' : 'mp4'}`, '*Aqui Su Video De Facebook 🚀*', m);
+              } catch (err6) {
+                throw `*📍 Error, Intentelo Mas Tarde*`;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+};
+handler.command = /^(facebook|fb|facebookdl|fbdl|facebook2|fb2|facebookdl2|fbdl2|facebook3|fb3|facebookdl3|fbdl3|facebook4|fb4|facebookdl4|fbdl4|facebook5|fb5|facebookdl5|fbdl5)$/i;
+handler.register = true
+export default handler;
 
-handler.help = ['fbdl <link>']
-handler.tags = ['servicio']
-handler.command = /^(fb)$/i
-handler.limit = true
-
-export default handler
+async function igeh(url_media) {
+  return new Promise(async (resolve, reject)=>{
+    const BASE_URL = 'https://instasupersave.com/';
+    try {
+      const resp = await axios(BASE_URL);
+      const cookie = resp.headers['set-cookie']; // get cookie from request
+      const session = cookie[0].split(';')[0].replace('XSRF-TOKEN=', '').replace('%3D', '');
+      const config = {method: 'post', url: `${BASE_URL}api/convert`, headers: {'origin': 'https://instasupersave.com', 'referer': 'https://instasupersave.com/pt/', 'sec-fetch-dest': 'empty', 'sec-fetch-mode': 'cors', 'sec-fetch-site': 'same-origin', 'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.52', 'x-xsrf-token': session, 'Content-Type': 'application/json', 'Cookie': `XSRF-TOKEN=${session}; instasupersave_session=${session}`}, data: {url: url_media}};
+      axios(config).then(function(response) {
+        const ig = [];
+        if (Array.isArray(response.data)) {
+          response.data.forEach((post) => {
+            ig.push(post.sd === undefined ? post.thumb : post.sd.url);
+          });
+        } else {
+          ig.push(response.data.url[0].url);
+        }
+        resolve({results_number: ig.length, url_list: ig});
+      }).catch(function(error) {
+        reject(error.message);
+      });
+    } catch (e) {
+      reject(e.message);
+    }
+  });
+}
