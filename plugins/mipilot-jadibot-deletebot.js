9f7613e834dd31d278d58366b2a21a719166b6b0
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-03-21 21:50:14 -0500

    🌸◌*̥₊ SᴀᴋᴜʀᴀBᴏᴛLɪᴛᴇ ◌❐🌹༉

--- plugins/mipilot-jadibot-deletebot.js
+++ plugins/mipilot-jadibot-deletebot.js
@@ -14,7 +14,7 @@ function _0x7d77(_0x3884d3, _0x297cf0) {
     _0x7d7709=_0x7d7709-0x1b0; const _0x2ead54=_0x5346a4[_0x7d7709]; return _0x2ead54;
   }, _0x7d77(_0x3884d3, _0x297cf0);
 } const _0xe8344c=_0x7d77; function _0x5346() {
-  const _0x2ebb89=['error', 'help', '24jWtTwj', 'command', '╰⊱💚⊱ *ÉXITO* ⊱💚⊱╮\x20\n\n*TE\x20VOY\x20A\x20ESTRAÑAR\x20𝙎𝙖𝙠𝙪𝙧𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 🌸🌹\x20CHAOO!!\x20🥹*', 'fromMe', 'split', '6858gzqVbM', 'La\x20carpeta\x20o\x20archivo\x20de\x20sesion\x20no\x20existen\x20', 'jid', '50TGXqLs', '*⚡️\x20Use\x20este\x20comando\x20directamente\x20en\x20el\x20numero\x20del\x20Bot\x20principal*', '443584xjTIxm', 'deletebot', '79052UoJUol', '3857359pKoOWl', 'sendMessage', './jadibts/', '28btHpPP', '190emRVsT', '42Qwxenc', '1893708Sdldqv', 'jadibot', 'conn', '1133253ZlNWaF', 'chat', 'mentionedJid', '4686144IHEtEb', 'user']; _0x5346=function() {
+  const _0x2ebb89=['error', 'help', '24jWtTwj', 'command', '╰⊱💚⊱ *ÉXITO* ⊱💚⊱╮\x20\n\n*TE\x20VOY\x20A\x20ESTRAÑAR\x20𝙎𝙖𝙠𝙪𝙧𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿🌹\x20CHAOO!!\x20🥹*', 'fromMe', 'split', '6858gzqVbM', 'La\x20carpeta\x20o\x20archivo\x20de\x20sesion\x20no\x20existen\x20', 'jid', '50TGXqLs', '*⚡️\x20Use\x20este\x20comando\x20directamente\x20en\x20el\x20numero\x20del\x20Bot\x20principal*', '443584xjTIxm', 'deletebot', '79052UoJUol', '3857359pKoOWl', 'sendMessage', './jadibts/', '28btHpPP', '190emRVsT', '42Qwxenc', '1893708Sdldqv', 'jadibot', 'conn', '1133253ZlNWaF', 'chat', 'mentionedJid', '4686144IHEtEb', 'user']; _0x5346=function() {
     return _0x2ebb89;
   }; return _0x5346();
 }(function(_0x384d26, _0x49a7ad) {
@@ -27,7 +27,7 @@ function _0x7d77(_0x3884d3, _0x297cf0) {
   }
 }(_0x5346, 0x792d7)); import {readdirSync, statSync, unlinkSync, existsSync, readFileSync, watch, rmSync, promises as _0x3872c8} from 'fs'; import _0x2ef42a, {join} from 'path'; const handler=async (_0x25b602, {conn: _0x191f72}, _0x136d85)=>{
   const _0x58c72f=_0x7d77; const _0x29ea36=_0x191f72; const _0x109b85=_0x25b602[_0x58c72f(0x1b3)]&&_0x25b602[_0x58c72f(0x1b3)][0x0]?_0x25b602[_0x58c72f(0x1b3)][0x0]:_0x25b602[_0x58c72f(0x1bb)]?_0x191f72[_0x58c72f(0x1b5)]['jid']:_0x25b602['sender']; const _0x2ff9d9=''+_0x109b85[_0x58c72f(0x1bc)]`@`[0x0]; if (global[_0x58c72f(0x1b0)][_0x58c72f(0x1b5)][_0x58c72f(0x1bf)]!==_0x191f72['user'][_0x58c72f(0x1bf)]) return _0x191f72[_0x58c72f(0x1c6)](_0x25b602[_0x58c72f(0x1b2)], {'text': _0x58c72f(0x1c1)}, {'quoted': _0x25b602}); else await _0x191f72['sendMessage'](_0x25b602['chat'], {'text': _0x58c72f(0x1ba)}, {'quoted': _0x25b602}); try {
-    _0x3872c8['rmdir'](_0x58c72f(0x1c7)+_0x2ff9d9, {'recursive': !![], 'force': !![]}), await _0x191f72[_0x58c72f(0x1c6)](_0x25b602[_0x58c72f(0x1b2)], {'text': '╰⊱💚⊱ *ÉXITO* ⊱💚⊱╮\x20\x20*HA\x20CERRADO\x20SESIÓN\x20Y\x20BORRADO\x20TODO\x20RASTRO*'}, {'quoted': _0x25b602});
+    _0x3872c8['rmdir'](_0x58c72f(0x1c7)+_0x2ff9d9, {'recursive': !![], 'force': !![]}), await _0x191f72[_0x58c72f(0x1c6)](_0x25b602[_0x58c72f(0x1b2)], {'text': '╰⊱💚⊱ *ÉXITO* ⊱💚⊱╮\x20\n\n*HA\x20CERRADO\x20SESIÓN\x20Y\x20BORRADO\x20TODO\x20RASTRO*'}, {'quoted': _0x25b602});
   } catch (_0xe7bf34) {
     console[_0x58c72f(0x1b6)](_0x58c72f(0x1be), _0xe7bf34);
   }
