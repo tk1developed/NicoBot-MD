5cc920ae3fbd01ec80d88f881e998232617f11dd
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-03-22 08:35:33 -0500

    🌸◌*̥₊ SᴀᴋᴜʀᴀBᴏᴛLɪᴛᴇ ◌❐🌹༉

--- plugins/owner-restart.js
+++ plugins/owner-restart.js
@@ -2,7 +2,7 @@ import { spawn } from 'child_process'
 let handler = async (m, { conn, isROwner, text }) => {
     if (!process.send) throw 'Dont: node main.js\nDo: node index.js'
     if (conn.user.jid == conn.user.jid) {
-    await m.reply('╰⊱🌩⊱ *CORRECTO* ⊱🌩⊱╮\n\n```🚀 REINICIANDO 🚀```\n*⌛ AGUARDE UN MOMENTO POR FAVOR...*')
+    await m.reply('╰⊱🌩⊱ *CORRECTO* ⊱🌩⊱╮\n\n```🚀 REINICIANDO 🚀```\n*⌛ AGUARDE UN MOMENTO POR FAVOR...*', m, fake, )
     process.send('reset')
   } else throw 'eh'
 }
