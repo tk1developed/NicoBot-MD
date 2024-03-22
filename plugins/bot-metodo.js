f94148169652123b378fdb06ba77e95818ba26fa
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-03-22 08:38:48 -0500

    🌸◌*̥₊ SᴀᴋᴜʀᴀBᴏᴛLɪᴛᴇ ◌❐🌹༉

--- plugins/bot-metodo.js
+++ plugins/bot-metodo.js
@@ -10,7 +10,7 @@ let handler = async (m, { conn }) => {
           let child = stdout.toString("utf-8");
           let ssd = child.replace(/Memory:/, "Ram:");
 
-          m.reply(`${saludo}\n\n*🧸 El Bot Tiene 2 Metodos Para Vincularse A Tu WhatsApp.*\n\nSon:\n\n- Texto De 8 Digitos.\n- Codigo Qr.\n\n*📍 Espero Y Instales El Bot Y Uses El Metodo Que Puedas.*`);
+          m.reply(`${saludo}\n\n*🧸 El Bot Tiene 2 Metodos Para Vincularse A Tu WhatsApp.*\n\nSon:\n\n- Texto De 8 Digitos.\n- Codigo Qr.\n\n*📍 Espero Y Instales El Bot Y Uses El Metodo Que Puedas.*` );
             });
 }
 handler.help = ['info']
