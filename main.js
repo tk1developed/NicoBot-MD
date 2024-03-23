7b2ab12927084be01195dd378d82bcbbe2849bc6
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-03-22 20:11:40 -0500

    🌸◌*̥₊ SᴀᴋᴜʀᴀBᴏᴛLɪᴛᴇ ◌❐🌹༉

--- main.js
+++ main.js
@@ -130,18 +130,18 @@ if (!methodCodeQR && !methodCode && !fs.existsSync(`./${authFile}/creds.json`)) 
 do {
 let lineM = '━━━━━━━━━━━━━━━━━━━━'
 opcion = await question(`╭${lineM}╮  
-┃ ${chalk.blueBright('╭━━━━━━━━━━━━━━━━━━━')}
-┃ ${chalk.blueBright('┃')} ${chalk.blue.bgBlue.bold.cyan('MÉTODO DE VINCULACIÓN')}
-┃ ${chalk.blueBright('╰━━━━━━━━━━━━━━━━━━━')}   
-┃ ${chalk.blueBright('╭━━━━━━━━━━━━━━━━━━━ ')}     
-┃ ${chalk.blueBright('┃')} ${chalk.green.bgMagenta.bold.yellow('¿CÓMO DESEA CONECTARSE?')}
-┃ ${chalk.blueBright('┃')} ${chalk.bold.redBright('»  Opción 1:')} ${chalk.greenBright('Código QR.')}
-┃ ${chalk.blueBright('┃')} ${chalk.bold.redBright('»  Opción 2:')} ${chalk.greenBright('Código de 8 digitos.')}
-┃ ${chalk.blueBright('╰━━━━━━━━━━━━━━━━━━━')}
-┃ ${chalk.blueBright('╭━━━━━━━━━━━━━━━━━━━ ')}     
-┃ ${chalk.blueBright('┃')} ${chalk.italic.magenta('Escriba sólo el número de')}
-┃ ${chalk.blueBright('┃')} ${chalk.italic.magenta('la opción para conectarse.')}
-┃ ${chalk.blueBright('╰━━━━━━━━━━━━━━━━━━━ ')} 
+┃ ${chalk.greenBright('╭━━━━━━━━━━━━━━━━━━━')}
+┃ ${chalk.greenBright('┃')} ${chalk.blue.bgBlue.bold.cyan('MÉTODO DE VINCULACIÓN')}
+┃ ${chalk.greenBright('╰━━━━━━━━━━━━━━━━━━━')}   
+┃ ${chalk.greenBright('╭━━━━━━━━━━━━━━━━━━━ ')}     
+┃ ${chalk.greenBright('┃')} ${chalk.blue.bgMagenta.bold.yellow('¿CÓMO DESEA CONECTARSE?')}
+┃ ${chalk.greenBright('┃')} ${chalk.bold.redBright('»  Opción 1:')} ${chalk.yellowBright('Código QR.')}
+┃ ${chalk.greenBright('┃')} ${chalk.bold.redBright('»  Opción 2:')} ${chalk.yellowBright('Código de 8 digitos.')}
+┃ ${chalk.greenBright('╰━━━━━━━━━━━━━━━━━━━')}
+┃ ${chalk.greenBright('╭━━━━━━━━━━━━━━━━━━━ ')}     
+┃ ${chalk.greenBright('┃')} ${chalk.italic.magenta('Escriba sólo el número de')}
+┃ ${chalk.greenBright('┃')} ${chalk.italic.magenta('la opción para conectarse.')}
+┃ ${chalk.greenBright('╰━━━━━━━━━━━━━━━━━━━ ')} 
 ╰${lineM}╯\n${chalk.bold.magentaBright('---> ')}`)
 //if (fs.existsSync(`./${authFile}/creds.json`)) {
 //console.log(chalk.bold.redBright(`PRIMERO BORRE EL ARCHIVO ${chalk.bold.greenBright("creds.json")} QUE SE ENCUENTRA EN LA CARPETA ${chalk.bold.greenBright(authFile)} Y REINICIE.`))
