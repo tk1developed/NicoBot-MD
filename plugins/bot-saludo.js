008efab21b7adc149dea9c5a25a2b6f910298765
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-03-24 17:54:33 -0500

    🌸◌*̥₊ SᴀᴋᴜʀᴀBᴏᴛLɪᴛᴇ ◌❐🌹༉

--- plugins/bot-saludo.js
+++ plugins/bot-saludo.js
@@ -10,7 +10,7 @@ let handler = async (m, { conn }) => {
           let child = stdout.toString("utf-8");
           let ssd = child.replace(/Memory:/, "Ram:");
 
-          m.reply(`_${saludo}_ *_Usuario_*`);
+          m.reply(`_${saludo}_ *_Usuario_*`, m, fake, );
             });
 }
 handler.help = ['info']
