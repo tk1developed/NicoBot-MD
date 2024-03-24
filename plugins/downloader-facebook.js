c10f7846f8d25aa8940671cd086d8a020c1066ef
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-03-24 18:08:37 -0500

    🌸◌*̥₊ SᴀᴋᴜʀᴀBᴏᴛLɪᴛᴇ ◌❐🌹༉

--- plugins/downloader-facebook.js
+++ plugins/downloader-facebook.js
@@ -1,14 +1,14 @@
 import fetch from 'node-fetch';
 
 const handler = async (m, { conn, args }) => {
-    if (!args[0]) throw `Por favor, ingrese un enlace de Facebook.`;
+    if (!args[0]) throw `⚠️ _Ingrese Un Enlace De Facebook_\n\n*Ejemplo:*\n*!fb* https://fb.watch/fOTpgn6UFQ/`;
 
     try {
         const apiUrl = `https://api.lolhuman.xyz/api/facebook?apikey=Gatadios&url=${encodeURIComponent(args[0])}`;
         const response = await fetch(apiUrl);
 
         if (response.ok) {
-            m.reply('Descargando el video, por favor espera...');
+            m.reply('*Descargando el video, por favor espera...*');
 
             const data = await response.json();
             const videoUrl = data.result[0];
@@ -20,7 +20,7 @@ const handler = async (m, { conn, args }) => {
 
             conn.sendFile(m.chat, fileBuffer, fileName, "", m);
 
-            m.reply('Video de Facebook descargado correctamente.');
+            m.reply('*Video de Facebook descargado correctamente.*');
         } else {
             throw `error
 
