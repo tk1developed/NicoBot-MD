7fa7be24f1805dc2df19baa345b6805580f8e26d
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-03-24 18:04:15 -0500

    🌸◌*̥₊ SᴀᴋᴜʀᴀBᴏᴛLɪᴛᴇ ◌❐🌹༉

--- plugins/downloader-facebook.js
+++ plugins/downloader-facebook.js
@@ -1,16 +1,41 @@
-import { facebookdl, facebookdlv2 } from '@bochilteam/scraper'
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-if (!args[0]) throw `⚠️ _Ingrese Un Enlace De Facebook_\n\n*Ejemplo:*\n*${usedPrefix + command}* https://fb.watch/fOTpgn6UFQ/`
-try {
-const { result } = await facebookdl(args[0]).catch(async _ => await facebookdlv2(args[0]))
-for (const { url, isVideo } of result.reverse()) await conn.sendFile(m.chat, url, `facebook.${!isVideo ? 'bin' : 'mp4'}`, `*Aqui Su Video De Facebook 🚀*`, m)
-} catch (e) {
-await m.reply('⚠️ _Ingrese Un Enlace De Facebook_\n\n*Ejemplo:*\n*${usedPrefix + command}* https://fb.watch/fOTpgn6UFQ/')
-console.log(e)
-}}
-handler.help = ['facebbok'].map(v => v + ' <url>')
-handler.tags = ['downloader']
-handler.command = /^((facebook|fb)(downloder|dl)?)$/i
+import fetch from 'node-fetch';
 
-handler.register = true
-export default handler
+const handler = async (m, { conn, args }) => {
+    if (!args[0]) throw `Por favor, ingrese un enlace de Facebook.`;
+
+    try {
+        const apiUrl = `https://api.lolhuman.xyz/api/facebook?apikey=Gatadios&url=${encodeURIComponent(args[0])}`;
+        const response = await fetch(apiUrl);
+
+        if (response.ok) {
+            m.reply('Descargando el video, por favor espera...');
+
+            const data = await response.json();
+            const videoUrl = data.result[0];
+
+            const fileName = "fb.mp4";
+
+            const videoResponse = await fetch(videoUrl);
+            const fileBuffer = await videoResponse.buffer();
+
+            conn.sendFile(m.chat, fileBuffer, fileName, "", m);
+
+            m.reply('Video de Facebook descargado correctamente.');
+        } else {
+            throw `error
+
+No se pudo obtener el contenido de Facebook.`;
+        }
+    } catch (error) {
+        console.error(error);
+        throw `error
+
+Ocurrió un error al descargar el video de Facebook: ${error.message}`;
+    }
+};
+
+handler.help = ['fb'];
+handler.tags = ['dl'];
+handler.command = ['fb', 'face'];
+
+export default handler;
