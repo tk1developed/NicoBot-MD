737599f653e4f15887cbbb5c9ff0a94abf58f51b
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-03-24 14:52:40 -0500

    🌸◌*̥₊ SᴀᴋᴜʀᴀBᴏᴛLɪᴛᴇ ◌❐🌹༉

--- plugins/autoresponder.js
+++ plugins/autoresponder.js
@@ -5,8 +5,9 @@ handler.all = async function (m, {conn}) {
 let chat = global.db.data.chats[m.chat]
 
 if (m.mentionedJid.includes(this.user.jid) && m.isGroup && !chat.isBanned) {
-await this.sendMessage(m.chat, {text: `🧸 𝙌𝙐𝙀 𝙔𝙊 𝙌𝙐𝙀?`}, {quoted: m, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100})}
-
+let stiker = await sticker(imagen1, false, global.packname, global.author)  
+this.sendFile(m.chat, stiker, 'sticker.webp', null, m, false, { 
+contextInfo: { externalAdReply: { title: '𝙎𝙖𝙠𝙪𝙧𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 💖🌻', body: 'Dɪᴇɢᴏ Oғɪᴄɪᴀʟ', sourceUrl: `https://github.com/diegojadibot/SakuraBotLite-MD`, thumbnail: imagen2}}})}
 
 return !0 }
 export default handler
