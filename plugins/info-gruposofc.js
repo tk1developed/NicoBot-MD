a337159f15528d1b006763b9ca60fd6a3d253d32
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-03-25 17:03:28 -0500

    🌸◌*̥₊ SᴀᴋᴜʀᴀBᴏᴛLɪᴛᴇ ◌❐🌹༉

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -2,20 +2,32 @@ let media = './Menu7.png'
 let handler = async (m, { conn, command }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
     await conn.sendMessage(m.chat, { react: { text: '⚡️', key: m.key } })
-let str = `*📍 GRUPO OFICIAL*
+let str = `*📍 GRUPOS OFICIALES*
 
     *_╭━━━⊜ SakuraBotLite-MD_*
   *_┃🧸❏ ${gp1}_*
 *_╰━━━━━━━━━━━━━━━━⊜_*
 
- *🥀 GRUPOS EN COLABORACIÓN*
-
-    *_╭━━━⊜ Curiosity y SakuraBot_*
+    *_╭━━━⊜ SB UPDATE_*
   *_┃🧸❏ ${gp2}_*
 *_╰━━━━━━━━━━━━━━━━⊜_*
 
+    *_╭━━━⊜ Test Bot SakuraBot_*
+  *_┃🧸❏ ${gp3}_*
+*_╰━━━━━━━━━━━━━━━━⊜_*
+
+    *_╭━━━⊜ SakuraBot Test_*
+  *_┃🧸❏ ${gp4}_*
+*_╰━━━━━━━━━━━━━━━━⊜_*
+
+ *🥀 GRUPOS EN COLABORACIÓN*
+
+    *_╭━━━⊜ Curiosity y SakuraBot_*
+  *_┃🧸❏ ${gp5}_*
+*_╰━━━━━━━━━━━━━━━━⊜_*
+
     *_╭━━━⊜ NovaBot & SakuraBot_*
-  *_┃📍❏ ${gp3}_*
+  *_┃📍❏ ${gp6}_*
 *_╰━━━━━━━━━━━━━━━━⊜_*
 
   *🥀 CANAL OFICIAL*
