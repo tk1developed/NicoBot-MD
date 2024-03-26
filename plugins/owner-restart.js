41532a326e7148eab7edf0d48ea49a2dd824d81f
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-03-26 15:22:34 -0500

    🌸◌*̥₊ SᴀᴋᴜʀᴀBᴏᴛLɪᴛᴇ ◌❐🌹༉

--- plugins/owner-restart.js
+++ plugins/owner-restart.js
@@ -1,15 +1,18 @@
 import { spawn } from 'child_process'
-let handler = async (m, { conn, isROwner, text }) => {
-    if (!process.send) throw 'Dont: node main.js\nDo: node index.js'
-    if (conn.user.jid == conn.user.jid) {
-    await m.reply('╰⊱🌩⊱ *CORRECTO* ⊱🌩⊱╮\n\n```🚀 REINICIANDO 🚀```\n*⌛ AGUARDE UN MOMENTO POR FAVOR...*')
-    process.send('reset')
-  } else throw 'eh'
-}
 
+var handler = async (m, { conn, isROwner, text }) => {
+
+if (!process.send) throw 'Dont: node main.js\nDo: node index.js'
+if (conn.user.jid == conn.user.jid) {
+await conn.reply(m.chat, '╰⊱🌩⊱ *CORRECTO* ⊱🌩⊱╮\n\n```🚀 REINICIANDO 🚀```\n*⌛ AGUARDE UN MOMENTO POR FAVOR...*', m, fake, )
+process.send('reset')
+} else throw 'eh'
+
+}
 handler.help = ['restart']
-handler.tags = ['owner']
+handler.tags = ['own']
 handler.command = ['restart','reiniciar'] 
 
 handler.rowner = true
+
 export default handler
