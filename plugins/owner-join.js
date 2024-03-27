bfc6bc24e3fb25212e2eacc171eb6d2ffdd8f56e
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-03-26 19:49:22 -0500

    🌸◌*̥₊ SᴀᴋᴜʀᴀBᴏᴛLɪᴛᴇ ◌❐🌹༉

--- plugins/owner-join.js
+++ plugins/owner-join.js
@@ -5,7 +5,7 @@ const handler = async (m, {conn, text, isMods, isOwner, isPrems}) => {
      enviando = true 
   try {
     const link = text //(m.quoted ? m.quoted.text ? m.quoted.text : text : text) || text;
-    if (!link || !link.match(linkRegex)) throw '*🌩 Te Falta El Link - Enlace De Un Grupo De WhatsApp.*\n\n*📇 Ejemplo:*\n!unete https://chat.whatsapp.com/Eaa9JFA53ps7WHMv2VHbO9';
+    if (!link || !link.match(linkRegex)) throw '*📇 Ejemplo:*\n!unete https://chat.whatsapp.com/Eaa9JFA53ps7WHMv2VHbO9';
     const [_, code] = link.match(linkRegex) || [];
     if ( isPrems || isMods || isOwner || m.fromMe) {
       const res = await conn.groupAcceptInvite(code);
@@ -20,7 +20,7 @@ const handler = async (m, {conn, text, isMods, isOwner, isPrems}) => {
     }
   } catch {
     enviando = false 
-    throw '*🌩 Te Falta El Link - Enlace De Un Grupo De WhatsApp.*\n\n*📇 Ejemplo:*\n!unete https://chat.whatsapp.com/Eaa9JFA53ps7WHMv2VHbO9';
+    throw '*📇 Ejemplo:*\n!unete https://chat.whatsapp.com/Eaa9JFA53ps7WHMv2VHbO9';
   }
 };
 handler.help = ['join [chat.whatsapp.com]'];
