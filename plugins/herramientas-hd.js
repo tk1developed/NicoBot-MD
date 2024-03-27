439a7d81e219c3a695773e9b803866a77e78907a
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-03-26 19:08:38 -0500

    🌸◌*̥₊ SᴀᴋᴜʀᴀBᴏᴛLɪᴛᴇ ◌❐🌹༉

--- plugins/herramientas-hd.js
+++ plugins/herramientas-hd.js
@@ -1,84 +1,46 @@
-import FormData from 'form-data'
-import Jimp from 'jimp'
+import FormData from "form-data";
+import Jimp from "jimp";
+const handler = async (m, {conn, usedPrefix, command}) => {
+ try {    
+  let q = m.quoted ? m.quoted : m;
+  let mime = (q.msg || q).mimetype || q.mediaType || "";
+  if (!mime) throw `*🍭 Envie Una Imagen Y Luego Responda Con El Comando !hd*`;
+  if (!/image\/(jpe?g|png)/.test(mime)) throw `*🚩 El Formato Del Archivo (${mime}) No Es Compartible, Envia O Responde A Una Foto*`;
+  m.reply("⏰️ Espere Un Momento");
+  let img = await q.download?.();
+  let pr = await remini(img, "enhance");
+  conn.sendMessage(m.chat, {image: pr}, {quoted: m});
+ } catch {
+  throw "*⚡️ Ocurrió Un Error*";
+ }
+};
+handler.help = ["remini", "hd", "enhance"];
+handler.tags = ["ai", "tools"];
+handler.command = ["remini", "hd", "enhance"];
+export default handler;
 
-var handler = async (m, { conn, usedPrefix, command }) => {
-
-conn.hdr = conn.hdr ? conn.hdr : {}
-if (m.sender in conn.hdr)
-return conn.reply(m.chat, `*📇 Todavía hay una solicitud en proceso, sea paciente*`, m, fake, )
-
-let q = m.quoted ? m.quoted : m
-let mime = (q.msg || q).mimetype || q.mediaType || ""
-if (!mime)
-
-return conn.reply(m.chat, `*🔮 Etiquete a una foto*`, m, fake, )
-if (!/image\/(jpe?g|png|mp4)/.test(mime))
-
-return conn.reply(m.chat, `*🌦 Mímica ${mime} no soportada*`, m, fake, )
-else conn.hdr[m.sender] = true
-
-conn.reply(m.chat, '⏰ Espere un momento', m, fake, )
-let img = await q.download?.()
-
-let error
-try {
-const This = await processing(img, "enlace")
-conn.sendFile(m.chat, This, '', '🌩 *Toma tu foto*\n\n' + cred.toString('utf-8'), m)
-} catch (er) {
-error = true
-} finally {
-if (error) {
-return conn.reply(m.chat, `*🌹 Ocurrió un fallo*`, m, fake, )
+async function remini(imageData, operation) {
+  return new Promise(async (resolve, reject) => {
+    const availableOperations = ["enhance", "recolor", "dehaze"];
+    if (availableOperations.includes(operation)) {
+      operation = operation;
+    } else {
+      operation = availableOperations[0];
+    }
+    const baseUrl = "https://inferenceengine.vyro.ai/" + operation + ".vyro";
+    const formData = new FormData();
+    formData.append("image", Buffer.from(imageData), {filename: "enhance_image_body.jpg", contentType: "image/jpeg"});
+    formData.append("model_version", 1, {"Content-Transfer-Encoding": "binary", contentType: "multipart/form-data; charset=utf-8"});
+    formData.submit({url: baseUrl, host: "inferenceengine.vyro.ai", path: "/" + operation, protocol: "https:", headers: {"User-Agent": "okhttp/4.9.3", Connection: "Keep-Alive", "Accept-Encoding": "gzip"}},
+      function (err, res) {
+        if (err) reject(err);
+        const chunks = [];
+        res.on("data", function (chunk) {chunks.push(chunk)});
+        res.on("end", function () {resolve(Buffer.concat(chunks))});
+        res.on("error", function (err) {
+        reject(err);
+        });
+      },
+    );
+  });
 }
-delete conn.hdr[m.sender]
-}}
-
-handler.help = ['hd']
-handler.tags = ['ai']
-handler.command = /^(hd)$/i
-
-handler.register = true
-handler.limit = true
-
-export default handler
-
-async function processing(urlPath, method) {
-return new Promise(async (resolve, reject) => {
-let Methods = ['enhance', 'recolor', 'dehaze']
-Methods.includes(method) ? (method = method) : (method = Methods[0])
-let buffer,
-Form = new FormData(),
-scheme = 'https' + '://' + 'inferenceengine' + '.vyro' + '.ai/' + method
-Form.append('model_version', 1, {
-'Content-Transfer-Encoding': 'binary',
-contentType: 'multipart/form-data; charset=uttf-8',
-}) 
-Form.append('image', Buffer.from(urlPath), {
-filename: 'enhance_image_body.jpg',
-contentType: 'image/jpeg',
-})
-Form.submit(
-{
-url: scheme,
-host: 'inferenceengine' + '.vyro' + '.ai',
-path: '/' + method,
-protocol: 'https:',
-headers: {
-'User-Agent': 'okhttp/4.9.3',
-Connection: 'Keep-Alive',
-'Accept-Encoding': 'gzip',
-},
-}, 
-function (err, res) {
-if (err) reject()
-let data = []
-res
-.on('data', function (chunk, resp) {
-data.push(chunk)
-})
-.on('end', () => {
-resolve(Buffer.concat(data))
-})
-res.on('error', (e) => {
-reject()
-})})})}
