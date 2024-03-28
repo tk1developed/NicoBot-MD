55a87c3a2fe4979804f1bf9d87c1a758f1d9583a
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-03-28 16:17:13 -0500

    🌸◌*̥₊ SᴀᴋᴜʀᴀBᴏᴛLɪᴛᴇ ◌❐🌹༉

--- plugins/rpg-verificar.js
+++ plugins/rpg-verificar.js
@@ -1,38 +1,40 @@
-import {createHash} from 'crypto';
-const Reg = /\|?(.*)([.|] *?)([0-9]*)$/i;
-const handler = async function(m, {conn, text, usedPrefix, command}) {
-  const user = global.db.data.users[m.sender];
-  const name2 = conn.getName(m.sender);
-  const pp = await conn.profilePictureUrl(m.chat, 'image').catch((_) => global.imagen1);
-  if (user.registered === true) throw `👊😆Hᴇʏ Yᴀ Esᴛᴀs Rᴇɢɪsᴛʀᴀᴅᴏ, Qᴜɪᴇʀᴇs Rᴇɢɪsᴛʀᴀᴛᴇ Dᴇ Nᴜᴇᴠᴏ?\n\n 📌Usᴇ Esᴛᴇ Cᴏᴍᴀɴᴅᴏ Pᴀʀᴀ Eʟɪᴍɪɴᴀʀ Sᴜ Rᴇɢɪsᴛʀᴏ\n*${usedPrefix}unreg* <Número de serie>`;
+import { createHash } from 'crypto'
+let Reg = /\|?(.*)([.|] *?)([0-9]*)$/i
+let handler = async function (m, { conn, text, usedPrefix, command }) {
+  let user = global.db.data.users[m.sender]
+  let name2 = conn.getName(m.sender)
+  let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? this.user.jid : m.sender
+  let pp = await this.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/1861aab98389b13db8588.jpg')
+  if (user.registered === true) throw `👊😆Hᴇʏ Yᴀ Esᴛᴀs Rᴇɢɪsᴛʀᴀᴅᴏ, Qᴜɪᴇʀᴇs Rᴇɢɪsᴛʀᴀᴛᴇ Dᴇ Nᴜᴇᴠᴏ?\n\n 📌Usᴇ Esᴛᴇ Cᴏᴍᴀɴᴅᴏ Pᴀʀᴀ Eʟɪᴍɪɴᴀʀ Sᴜ Rᴇɢɪsᴛʀᴏ\n*${usedPrefix}unreg* <Número de serie>`
   if (!Reg.test(text)) throw `*⚠️ Mal Uso Del Comando*\n\n*🥀 Uso Del Comando:* 
 *${usedPrefix + command} nombre.edad*\n\n*📍Ejemplo:*
- ${usedPrefix + command} *${name2}.18*`;
-  let [_, name, splitter, age] = text.match(Reg);
-  if (!name) throw '*🧸 Debes Poner El Nombre*';
-  if (!age) throw '*📍 La Edad Es Necesaria Para El Registro*';
-  if (name.length >= 30) throw '📍 *Tu Nombre Es Muy Largo*';
-  age = parseInt(age);
-  if (age > 60) throw '*🧩 Woo, El Abuelo Quiere Jugar Al Bot*';
-  if (age < 5) throw '*🧩 Un Bebé Usa WhatsApp?*';
-  user.name = name.trim();
-  user.age = age;
-  user.regTime = + new Date;
-  user.registered = true;
-  const sn = createHash('md5').update(m.sender).digest('hex');
-  const caption = `╭━━━ •🧸⸽⃕𝘛𝘶 𝘙𝘦𝘨𝘪𝘴𝘵𝘳𝘰🍁⃨፝⃕✰• ━━━
+ ${usedPrefix + command} *${name2}.18*`
+  let [_, name, splitter, age] = text.match(Reg)
+  if (!name) throw '*📝 El nombre no puede estar vacío*'
+  if (!age) throw '*📝 La edad no puede estar vacía*'
+  if (name.length >= 30) throw '*⚠️ El nombre es demasiado largo*' 
+  age = parseInt(age)
+  if (age > 100) throw '*👴🏻 Wow el abuelo quiere jugar al bot*'
+  if (age < 5) throw '*👀 hay un bebé jsjsj*'
+  user.name = name.trim()
+  user.age = age
+  user.regTime = + new Date
+  user.registered = true
+let sn = createHash('md5').update(m.sender).digest('hex').slice(0, 6)        
+m.react('📩') 
+let regbot = `╭━━━ •🧸⸽⃕𝘛𝘶 𝘙𝘦𝘨𝘪𝘴𝘵𝘳𝘰🍁⃨፝⃕✰• ━━━
 ┃ ${saludo}: ${name}
 ┃ *🧩Edad:* ${age} años
 ┃ *📍Numero De Serie:* 
 ┃ ${sn}
-◈ ━━━━━━━ ⸙ ━━━━━━━ ◈`;
-  // let author = global.author
-  await conn.sendFile(m.chat, pp, 'mystic.jpg', caption);
-  // conn.sendButton(m.chat, caption, `¡𝚃𝚄 𝙽𝚄𝙼𝙴𝚁𝙾 𝙳𝙴 𝚂𝙴𝚁𝙸𝙴 𝚃𝙴 𝚂𝙴𝚁𝚅𝙸𝚁𝙰 𝙿𝙾𝚁 𝚂𝙸 𝙳𝙴𝚂𝙴𝙰𝚂 𝙱𝙾𝚁𝚁𝙰𝚁 𝚃𝚄 𝚁𝙴𝙶𝙸𝚂𝚃𝚁𝙾 𝙴𝙽 𝙴𝙻 𝙱𝙾𝚃!\n${author}`, [['¡¡𝙰𝙷𝙾𝚁𝙰 𝚂𝙾𝚈 𝚄𝙽 𝚅𝙴𝚁𝙸𝙵𝙸𝙲𝙰𝙳𝙾/𝙰!!', '/profile']], m)
-  global.db.data.users[m.sender].money += 10000;
-  global.db.data.users[m.sender].exp += 10000;
-};
-handler.help = ['verificar'];
-handler.tags = ['xp'];
-handler.command = /^(verify|register|verificar|reg|registrar)$/i;
-export default handler;
+◈ ━━━━━━━ ⸙ ━━━━━━━ ◈`
+await m.reply(regbot)
+// await conn.sendUrl(m.chat, regbot, m, { externalAdReply: { mediaType: 1, renderLargerThumbnail: true, thumbnail: pp, thumbnailUrl: pp, title: 'Registrado 📩', }})
+
+}
+handler.help = ['reg'].map(v => v + ' <nombre.edad>')
+handler.tags = ['rg']
+
+handler.command = ['verify', 'verificar', 'reg', 'register', 'registrar'] 
+
+export default handler
