010922fdef57cbab32c5d82eb4a73ca0bad5f0c5
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-03-30 18:46:55 -0500

    🌸◌*̥₊ SᴀᴋᴜʀᴀBᴏᴛLɪᴛᴇ ◌❐🌹༉

--- plugins/owner-saveplugin.js
+++ plugins/owner-saveplugin.js
@@ -1,7 +1,7 @@
 import fs from 'fs'
 let handler = async (m, { text, usedPrefix, command }) => {
     if (!text) return conn.reply(m.chat, `Que nombre le pongo al plugin?`)
-    if (!m.quoted.text) return conn.reply(m.chat, `Responder al mensaje!`)
+    if (!m.quoted.text) return conn.reply(m.chat, `*Y EL TEXTO?*`)
     let path = `plugins/${text}.js`
     await fs.writeFileSync(path, m.quoted.text)
     await conn.reply(m.chat, `Guardado en ${path}`)
