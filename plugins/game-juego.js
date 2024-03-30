c10b6641d3dc3ae3b2dd00e975978220ee163c62
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-03-30 08:33:39 -0500

    🌸◌*̥₊ SᴀᴋᴜʀᴀBᴏᴛLɪᴛᴇ ◌❐🌹༉

---
+++ plugins/game-juego.js
@@ -0,0 +1,18 @@
+let handler = async (m, { conn, usedPrefix, text }) => {
+  let { key } = await conn.sendMessage(m.chat, { text: "Tas caliente! Ahora te hare una paja" }, { quoted: m });
+  const array = [
+    "8==👊==D", "8===👊=D", "8=👊===D", "8=👊===D", "8==👊==D", "8===👊=D", "8====👊D", "8==👊=D", "8==👊==D", "8=👊===D", "8👊====D", "8=👊===D","8==👊==D", "8===👊=D", "8====👊D","8==👊==D", "8===👊=D", "8=👊===D", "8=👊===D", "8==👊==D", "8===👊=D", "8====👊D😄"
+  ];
+
+  for (let item of array) {
+    await conn.sendMessage(m.chat, { text: `${item}`, edit: key }, { quoted: m });
+    await new Promise(resolve => setTimeout(resolve, 20)); // Delay 5 seconds
+  }
+  return conn.sendMessage(m.chat, { text: `Oh, se corrió en menos de 1 hora!`.trim() , edit: key, mentions: [m.sender] }, { quoted: m });
+};
+
+handler.help = ['pajeame'];
+handler.tags = ['fun'];
+handler.command = /^pajeame|paja$/i;
+
+export default handler;
