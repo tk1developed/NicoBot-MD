9511994c0fe38f5099298a1167bece5cb263e9ac
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-03-31 08:57:43 -0500

    🌸◌*̥₊ SᴀᴋᴜʀᴀBᴏᴛLɪᴛᴇ ◌❐🌹༉

--- main.js
+++ main.js
@@ -152,7 +152,7 @@ console.log('[ ❗ ] Por favor, seleccione solo 1 o 2.\n')
 }
 
 console.info = () => {}
-console.warn = () => {}
+//console.warn = () => {}
 const connectionOptions = {
 logger: pino({ level: 'silent' }),
 printQRInTerminal: opcion == '1' ? true : methodCodeQR ? true : false,
