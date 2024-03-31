0157d0796c1fa90a40c15ca4f54465995b24e8e2
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-03-31 09:06:13 -0500

    🌸◌*̥₊ SᴀᴋᴜʀᴀBᴏᴛLɪᴛᴇ ◌❐🌹༉

--- plugins/fun-love.js
+++ plugins/fun-love.js
@@ -1,21 +1,28 @@
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let name = conn.getName(who)
-//const pp = './Menu.png'
+const pp = './storage/logos/Menu2.jpg'
 
-let love = `\t\t*➺ 𝐸𝑙 𝑀𝑒𝑑𝑖𝑑𝑜𝑟 𝐷𝑒𝑙 𝐴𝑚𝑜𝑟💘*
+let love = `🚩 𝗨𝗡 𝗣𝗢𝗥𝗖𝗘𝗡𝗧𝗔𝗝𝗘 𝗗𝗘 𝗔𝗠𝗢𝗥
 
-╭• ──────── ✾ ──────── •╮
-┇➺ 𝑬𝒍 𝑨𝒎𝒐𝒓 𝑫𝒆 ${name} 𝑷𝒐𝒓 𝑻𝒊 𝑬𝒔 𝑫𝒆 *${Math.floor(Math.random() * 100)}%* 𝑫𝒆 𝑼𝒏 *100%*
-┇
-┇➺ 🌻𝑫𝒆𝒃𝒆𝒓𝒊𝒂𝒔 𝑷𝒆𝒅𝒊𝒓𝒍𝒆 𝑴𝒂𝒕𝒓𝒊𝒎𝒐𝒏𝒊𝒐?
-╰•–––––––––☆–––––––––•╯`
+╭◈ ━━━━━━━ ⸙ ━━━━━━━ ◈╮
+┋➸ 💖𝖤𝗅 𝖠𝗆𝗈𝗋 𝖣𝖾: 
+┋➸ ${name}
+┋➸ 🚩𝖲𝗂𝖾𝗇𝗍𝖾 𝖯𝗈𝗋 𝖳𝗂 𝖤𝗌 𝖴𝗇: 
+┋➸ *${Math.floor(Math.random() * 100)}%* 𝖣𝖾 𝖴𝗇 *100%*
+╰◈ ━━━━━━━ ⸙ ━━━━━━━ ◈╯
 
-conn.sendMessage(m.chat,{image: {url: pp}, caption: love, mentions: [m.sender]}, {quoted: m}) 
+🎌 𝐇𝐚𝐲 𝐀𝐦𝐨𝐫,𝐃𝐞𝐛𝐞𝐬 𝐏𝐞𝐝𝐢𝐫𝐥𝐞 𝐌𝐚𝐭𝐫𝐢𝐦𝐨𝐧𝐢𝐨?`
+
+conn.sendMessage(m.chat, {text: love, mentions: [m.sender]}, {quoted: m})
+
+//conn.reply(m.chat, `*🚩 Ocurrió un fallo*`)
+
 }
+handler.help = ['love']
+handler.tags = ['juegos']
+handler.command = /^(love|amor|Love)$/i
 
-handler.help = ['love  @user']
-handler.tags = ['fun']
-handler.command = /^(love)$/i
 handler.register = true
+
 export default handler
