def5420c6b6ad2dd5f3fde31c7f29906ddadc3c5
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-03-31 10:45:55 -0500

    🌸◌*̥₊ SᴀᴋᴜʀᴀBᴏᴛLɪᴛᴇ ◌❐🌹༉

---
+++ plugins/bot-menu.js
@@ -0,0 +1,36 @@
+let handler = async(m, { conn, text, usedPrefix, command }) => {
+let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+let name = conn.getName(who)
+const pp = './storage/logos/Menu2.jpg'
+
+let menu = `╭┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈⪩
+┊┏━━━━━━━━━━━━━━•
+┊┃ ┏━━━━━━━━━━━━━━•
+┊┃ ┃ \`🌸 BIENVENIDO\`
+┊┃ ┃${name}
+┊┃ ┗━━━━━━━━━━━━━━•
+┊┃ ┏━━━━━━━━━━━━━━•
+┊┃ ┃ *LISTA MENUS*
+┊┃ ┃━━━━━━━━━━━━━━•
+┊┃ ┃#allmenu
+┊┃ ┃#menujuegos
+┊┃ ┃#menuowner
+┊┃ ┃#menuanimes
+┊┃ ┃#menu2
+┊┃ ┃#hornymenu 
+┊┃ ┗━━━━━━━━━━━━━━•
+┊┗━━━━━━━━━━━━━━•
+╰┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈⪩ `
+
+conn.sendMessage(m.chat, {text: menu, mentions: [m.sender]}, {quoted: m})
+
+//conn.reply(m.chat, `*🚩 Ocurrió un fallo*`)
+
+}
+handler.help = ['menu']
+handler.tags = ['help']
+handler.command = /^(menu|comandos)$/i
+
+handler.register = true
+
+export default handler
