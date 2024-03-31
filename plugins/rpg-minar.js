88ae234bbd8cc81b04c7fdff7dcea80fed9142eb
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-03-31 18:22:04 -0500

    🌸◌*̥₊ SᴀᴋᴜʀᴀBᴏᴛLɪᴛᴇ ◌❐🌹༉

--- plugins/rpg-minar.js
+++ plugins/rpg-minar.js
@@ -51,7 +51,7 @@ texto += `+${recompensas[reward]} ${global.rpgshop.emoticon(reward)}\n`}
 ['𝙈𝙞𝙣𝙖𝙧 𝙂𝙖𝙩𝙖𝘾𝙤𝙞𝙣𝙨 🐈', `.minar2`],
 ['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘️', `.menu`]
 ], m,)*/
-await conn.reply(m.chat, `*${minar} ${xp} XP*`, fkontak, m)
+await conn.reply(m.chat, `*${minar} ${xp} XP*`)
 user.lastmiming = new Date * 1  
 }
 handler.help = ['minar']
