dfbb18bb426a1275adb977ca2ab22f9c86532f7a
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-04-05 18:42:43 -0500

    🌸◌*̥₊ SᴀᴋᴜʀᴀBᴏᴛLɪᴛᴇ ◌❐🌹༉

--- plugins/Ofuscar.js
+++ plugins/Ofuscar.js
@@ -1,4 +1,4 @@
-import JavaScriptObfuscator from 'javascript-obfuscator'
+/*import JavaScriptObfuscator from 'javascript-obfuscator'
 
 let handler = async(m, { conn, text }) => {
 if (!text) return m.reply(`*Y EL TEXTO?*`) 
@@ -9,4 +9,4 @@ let obfuscatedCode = await obfuscateCode(text);
 conn.sendMessage(m.chat, {text: obfuscatedCode}, {quoted: m});
 }
 handler.command = /^(ofuscar|ofuscador)$/i
-export default handler
+export default handler*/
