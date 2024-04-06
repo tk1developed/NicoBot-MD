1538884e3ff1e20aae13688ad797314be2731d15
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-04-05 21:53:56 -0500

    🌸◌*̥₊ SᴀᴋᴜʀᴀBᴏᴛLɪᴛᴇ ◌❐🌹༉

--- plugins/fun-odio.js
+++ plugins/fun-odio.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, command, text }) => {
 let odio = `────────────────\n𝗖𝗮𝗹𝗰𝘂𝗹𝗮𝗱𝗼𝗿𝗮 𝗗𝗲 𝗢𝗱𝗶𝗼 😡
-El Odio Entre ${text} Es De: ${Math.floor(Math.random() * 100)}%\n────────────────`.trim()
+El Odio De ${text} Por Ti Es Un: ${Math.floor(Math.random() * 100)}%\n────────────────`.trim()
 m.reply(odio, null, { mentions: conn.parseMention(odio) })}
 handler.command = /^(odio)$/i
 export default handler
