19d2a8a370aae8edeb4dffac3f6d91568e301924
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-04-05 19:32:34 -0500

    🌸◌*̥₊ SᴀᴋᴜʀᴀBᴏᴛLɪᴛᴇ ◌❐🌹༉

--- plugins/info-covid.js
+++ plugins/info-covid.js
@@ -1,17 +1,17 @@
 import fetch from 'node-fetch';
 const handler = async (m, {text, usedPrefix, command}) => {
-  if (!text) throw `*[❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝚄𝙽 𝙿𝙰𝙸𝚂, 𝙴𝙹𝙴𝙼𝙿𝙻𝙾 ${usedPrefix + command} Mexico*`;
+  if (!text) throw `*INGRESE UN NOMBRE DE ALGUN PAIS!*`;
   const res = await fetch(global.API('https://covid19.mathdro.id', '/api/countries/'+ (text)));
   if (!res.ok) throw await res.text();
   const json = await res.json();
   if (!json.confirmed) throw 'País?';
   if (json.confirmed) {
     m.reply(`
-🌏 País : ${text}
-✅Confirmado : ${json.confirmed.value}
-📉curado : ${json.recovered.value}
-☠️Muertes : ${json.deaths.value}
-💌Info Actualizada : ${json.lastUpdate}
+🚩 País : ${text}
+🔮 Confirmado : ${json.confirmed.value}
+🌸 Curado : ${json.recovered.value}
+☠️ Muertes : ${json.deaths.value}
+💚 Info Actualizada : ${json.lastUpdate}
 `.trim());
   } else throw json;
 };
