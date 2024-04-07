5931d15ee8234910b76edb8b3e4c3a0b1a63b45c
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-04-07 11:20:36 -0500

    Actualizar info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,4 +1,4 @@
-/*let handler = async (m, { conn, usedPrefix, isOwner }) => {
+let handler = async (m, { conn, usedPrefix, isOwner }) => {
 let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:👑 𝗖𝗿𝗲𝗮𝗱𝗼𝗿 ⚡️\nFN:👑 𝗖𝗿𝗲𝗮𝗱𝗼𝗿 ⚡️\nORG:👑 𝗖𝗿𝗲𝗮𝗱𝗼𝗿 ⚡️\nTITLE:\nitem1.TEL;waid=573012482597:573012482597\nitem1.X-ABLabel:👑 𝗖𝗿𝗲𝗮𝗱𝗼𝗿 ⚡️\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:👑 𝗖𝗿𝗲𝗮𝗱𝗼𝗿 ⚡️\nEND:VCARD`
 await conn.sendMessage(m.chat, { contacts: { displayName: '👑 𝗖𝗿𝗲𝗮𝗱𝗼𝗿 ⚡️', contacts: [{ vcard }] }}, {quoted: m})
 }
@@ -7,7 +7,7 @@ handler.tags = ['main']
 handler.command = ['owner', 'creador', 'propietario', 'dueño'] 
 
 handler.register = true
-export default handler*/
+export default handler
 
 /*function handler(m) {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
