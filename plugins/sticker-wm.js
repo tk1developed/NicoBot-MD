82e174fa75a284b9f4d4f251af028def978a2f4a
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-04-10 08:59:21 -0500

    🌸◌*̥₊ SᴀᴋᴜʀᴀBᴏᴛLɪᴛᴇ ◌❐🌹༉

--- plugins/sticker-wm.js
+++ plugins/sticker-wm.js
@@ -1,14 +1,14 @@
 import {addExif} from '../lib/sticker.js';
 const handler = async (m, {conn, text}) => {
-  if (!m.quoted) throw '*📖 Responda A Un Sticker*';
+  if (!m.quoted) throw '🧿 『𝐄𝐑𝐑𝐎𝐑』 🧿\n\n𝗘𝗡𝗩𝗜𝗘 𝗨𝗡 𝗦𝗧𝗜𝗖𝗞𝗘𝗥 𝗤𝗨𝗘 𝗗𝗘𝗦𝗘𝗔 𝗖𝗔𝗠𝗕𝗜𝗔𝗥𝗟𝗘 𝗘𝗟 𝗡𝗢𝗠𝗕𝗥𝗘 𝗬 𝗟𝗨𝗘𝗚𝗢 𝗥𝗘𝗦𝗣𝗢𝗡𝗗𝗘 𝗖𝗢𝗡 𝗘𝗟 𝗖𝗢𝗠𝗔𝗡𝗗𝗢 #𝗐𝗆';
   let stiker = false;
   try {
     let [packname, ...author] = text.split('|');
     author = (author || []).join('|');
     const mime = m.quoted.mimetype || '';
-    if (!/webp/.test(mime)) throw '*📖 Responda A Un Sticker*';
+    if (!/webp/.test(mime)) throw '🧿 『𝐄𝐑𝐑𝐎𝐑』 🧿\n\n𝗘𝗡𝗩𝗜𝗘 𝗨𝗡 𝗦𝗧𝗜𝗖𝗞𝗘𝗥 𝗤𝗨𝗘 𝗗𝗘𝗦𝗘𝗔 𝗖𝗔𝗠𝗕𝗜𝗔𝗥𝗟𝗘 𝗘𝗟 𝗡𝗢𝗠𝗕𝗥𝗘 𝗬 𝗟𝗨𝗘𝗚𝗢 𝗥𝗘𝗦𝗣𝗢𝗡𝗗𝗘 𝗖𝗢𝗡 𝗘𝗟 𝗖𝗢𝗠𝗔𝗡𝗗𝗢 #𝗐𝗆';
     const img = await m.quoted.download();
-    if (!img) throw '*📖 Responda A Un Sticker*';
+    if (!img) throw '🧿 『𝐄𝐑𝐑𝐎𝐑』 🧿\n\n𝗘𝗡𝗩𝗜𝗘 𝗨𝗡 𝗦𝗧𝗜𝗖𝗞𝗘𝗥 𝗤𝗨𝗘 𝗗𝗘𝗦𝗘𝗔 𝗖𝗔𝗠𝗕𝗜𝗔𝗥𝗟𝗘 𝗘𝗟 𝗡𝗢𝗠𝗕𝗥𝗘 𝗬 𝗟𝗨𝗘𝗚𝗢 𝗥𝗘𝗦𝗣𝗢𝗡𝗗𝗘 𝗖𝗢𝗡 𝗘𝗟 𝗖𝗢𝗠𝗔𝗡𝗗𝗢 #𝗐𝗆';
     stiker = await addExif(img, packname || global.packname, author || global.author);
   } catch (e) {
     console.error(e);
