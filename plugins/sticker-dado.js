3856c39266813bb87c4247e2f53b71447ec2b8a3
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-04-10 19:10:57 -0500

    🌸◌*̥₊ SᴀᴋᴜʀᴀBᴏᴛLɪᴛᴇ ◌❐🌹༉

--- plugins/sticker-dado.js
+++ plugins/sticker-dado.js
@@ -11,7 +11,7 @@ let handler = async (m, { conn }) => {
 }
 handler.help = ['dado']
 handler.tags = ['game']
-handler.command = /^(dado|dados)$/i
+handler.command = /^(dado)$/i
 handler.command = new RegExp
 export default handler
 
