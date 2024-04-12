e91d1c4e280fba9ffbdaa3511671bec011767f28
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-04-12 10:22:58 -0500

    Actualizar downloader-play.js

--- plugins/downloader-play.js
+++ plugins/downloader-play.js
@@ -1,11 +1,11 @@
 import fetch from 'node-fetch';
 import axios from 'axios';
-import yts from 'yt-search';
+//import yts from 'yt-search';
 import {youtubedl, youtubedlv2} from '@bochilteam/scraper';
 import ytdl from 'ytdl-core';
 //import {bestFormat, getUrlDl} from '../lib/y2dl.js';
 //import YTDL from "../lib/ytdll.js";
-import fs from "fs";
+//import fs from "fs";
 let limit1 = 100;
 let limit2 = 400;
 let limit_a1 = 50;
