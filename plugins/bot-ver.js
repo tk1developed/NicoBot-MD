bca2c19a337583453cc36383d7d1baddadea5bca
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-04-11 22:30:34 -0500

    🌸◌*̥₊ SᴀᴋᴜʀᴀBᴏᴛLɪᴛᴇ ◌❐🌹༉

--- plugins/bot-ver.js
+++ plugins/bot-ver.js
@@ -1,6 +1,6 @@
 var handler = async (m, { conn }) => {
 
-if (!/viewOnce/.test(m.quoted?.mtype)) throw '*[ 🧿 ] 𝙍𝙚𝙨𝙥𝙤𝙣𝙙𝙖 𝙖 𝙪𝙣 𝙫𝙞𝙙𝙚𝙤 𝙤 𝙛𝙤𝙩𝙤 𝙦𝙪𝙚 𝙚𝙨𝙩𝙖́ 𝙥𝙖𝙧𝙖 𝙫𝙚𝙧 𝙨𝙤𝙡𝙤 𝙪𝙣𝙖 𝙫𝙚𝙯*'
+if (!/viewOnce/.test(m.quoted?.mtype)) throw '╰⊱🧿⊱ 𝗜𝗡𝗙𝗢𝗥𝗠𝗔𝗖𝗜𝗢́𝗡 ⊱🧿⊱╮\n\n𝙍𝙚𝙨𝙥𝙤𝙣𝙙𝙖 𝙖 𝙪𝙣 𝙫𝙞𝙙𝙚𝙤 𝙤 𝙛𝙤𝙩𝙤 𝙦𝙪𝙚 𝙚𝙨𝙩𝙖́ 𝙥𝙖𝙧𝙖 𝙫𝙚𝙧 𝙨𝙤𝙡𝙤 𝙪𝙣𝙖 𝙫𝙚𝙯'
 let mtype = Object.keys(m.quoted.message)[0]
 let buffer = await m.quoted.download()
 let caption = m.quoted.message[mtype].caption || ''
