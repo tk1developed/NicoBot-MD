38658a28d5f45d441cead5c927edc81b10b65f00
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-04-14 23:02:22 -0500

    🌸◌*̥₊ SᴀᴋᴜʀᴀBᴏᴛLɪᴛᴇ ◌❐🌹༉

--- plugins/downloader-spotify.js
+++ plugins/downloader-spotify.js
@@ -1,6 +1,6 @@
 /* Creditos de los tags a @darlyn1234 y diseño a @ALBERTO9883 */
 import fetch from 'node-fetch';
-import Spotify from 'sanzy-spotifydl';
+import Spotify from 'spotifydl-x';
 import fs from 'fs';
 import NodeID3 from 'node-id3';
 import axios from 'axios';
@@ -28,7 +28,7 @@ const handler = async (m, { conn, text }) => {
     const img = await (await fetch(`${spty.data.cover_url}`)).buffer()  
     const letra_s = await find_lyrics(spty.data.name ? spty.data.name : '');
     let letra;
-    letra = `${letra_s ? letra_s + '\n\n🤴🏻 Descarga por diegojadibot & SakuraBotLite-MD 🤖' : '🤴🏻 Descarga por diegojadibot & SakuraBotLite-MD 🤖'}`  
+    letra = `${letra_s ? letra_s + '\n\n🤴🏻 Descarga por BrunoSobrino & TheMystic-Bot-MD 🤖' : '🤴🏻 Descarga por BrunoSobrino & TheMystic-Bot-MD 🤖'}`  
     const tags = {
       title: spty.data.name || '-',
       artist: artist,
