ce195f86ab30702e94d1f149c5af8da43ec75a78
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-04-17 01:59:31 -0500

    🌸◌*̥₊ SᴀᴋᴜʀᴀBᴏᴛLɪᴛᴇ ◌❐🌹༉

--- plugins/rpg-verificar.js
+++ plugins/rpg-verificar.js
@@ -1,41 +1,51 @@
+import db from '../lib/database.js'
 import { createHash } from 'crypto'
+import fs from 'fs'
+import fetch from 'node-fetch'
+
 let Reg = /\|?(.*)([.|] *?)([0-9]*)$/i
 let handler = async function (m, { conn, text, usedPrefix, command }) {
   let user = global.db.data.users[m.sender]
   let name2 = conn.getName(m.sender)
-  let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? this.user.jid : m.sender
-  let pp = await this.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/1861aab98389b13db8588.jpg')
-  if (user.registered === true) throw `👊😆Hᴇʏ Yᴀ Esᴛᴀs Rᴇɢɪsᴛʀᴀᴅᴏ, Qᴜɪᴇʀᴇs Rᴇɢɪsᴛʀᴀᴛᴇ Dᴇ Nᴜᴇᴠᴏ?\n\n 📌Usᴇ Esᴛᴇ Cᴏᴍᴀɴᴅᴏ Pᴀʀᴀ Eʟɪᴍɪɴᴀʀ Sᴜ Rᴇɢɪsᴛʀᴏ\n*${usedPrefix}unreg* <Número de serie>`
-  if (!Reg.test(text)) throw `*⚠️ Mal Uso Del Comando*\n\n*🥀 Uso Del Comando:* 
-*${usedPrefix + command} nombre.edad*\n\n*📍Ejemplo:*
- ${usedPrefix + command} *${name2}.18*`
+  if (user.registered === true) throw `*Ya estás registrado*\n\n*¿Quiere volver a registrarse?*\n\nUse este comando para eliminar su registro \n*${usedPrefix}unreg* <Número de serie>`
+  if (!Reg.test(text)) throw `*Formato incorrecto*\n\nUso del comamdo: *${usedPrefix + command} nombre.edad*\nEjemplo : *${usedPrefix + command} ${name2}.16*`
   let [_, name, splitter, age] = text.match(Reg)
-  if (!name) throw '*📝 El nombre no puede estar vacío*'
-  if (!age) throw '*📝 La edad no puede estar vacía*'
-  if (name.length >= 30) throw '*⚠️ El nombre es demasiado largo*' 
+  if (!name) throw 'El nombre no puede estar vacío'
+  if (!age) throw 'La edad no puede estar vacía'
+  if (name.length >= 100) throw 'El nombre es demasiado largo' 
   age = parseInt(age)
-  if (age > 100) throw '*👴🏻 Wow el abuelo quiere jugar al bot*'
-  if (age < 5) throw '*👀 hay un bebé jsjsj*'
+  if (age > 100) throw '👴🏻 Wow el abuelo quiere jugar al bot'
+  if (age < 5) throw '🚼  hay un abuelo bebé jsjsj '
   user.name = name.trim()
   user.age = age
   user.regTime = + new Date
   user.registered = true
-let sn = createHash('md5').update(m.sender).digest('hex').slice(0, 6)        
-//m.react('📩') 
-await conn.sendMessage(m.chat, { react: { text: '📩', key: m.key } })
-let regbot = `╭━ • 📇 𝖱𝖾𝗀𝗂𝗌𝗍𝗋𝖺𝖽𝗈 📇 • ━╮
-┃ *🌩Nombre*: ${name}
-┃ *⚡️Edad:* ${age} años
-┃ *🔮Numero De Serie:* 
-┃ ${sn}
-╰━━━━━━━ ⸙ ━━━━━━━╯`
-await m.reply(regbot)
-// await conn.sendUrl(m.chat, regbot, m, { externalAdReply: { mediaType: 1, renderLargerThumbnail: true, thumbnail: pp, thumbnailUrl: pp, title: 'Registrado 📩', }})
+  let sn = createHash('md5').update(m.sender).digest('hex')
+  let img = await (await fetch('https://tinyurl.com/225ul2sg')).buffer()  
+  let str = `*–  R E G I S T R O  -  U S E R  –*
 
+✧ *Nombre:* ${name}
+✧ *Edad* : ${age} años
+✧ *Numero de serie* :
+${sn}`
+conn.sendMessage(m.chat, {
+text: str,
+contextInfo: { 
+forwardingScore: 9999, 
+isForwarded: true, 
+externalAdReply: {
+title: botname,
+body: textbot,
+thumbnailUrl: img,
+thumbnail: img,
+sourceUrl: global.canal,
+mediaType: 1,
+renderLargerThumbnail: true
+}}}, { quoted: m})
 }
-handler.help = ['reg'].map(v => v + ' <nombre.edad>')
+handler.help = ['reg'].map(v => v + ' *<nombre.edad>*')
 handler.tags = ['rg']
 
-handler.command = ['verify', 'verificar', 'reg', 'register', 'registrar'] 
+handler.command = ['verify', 'reg', 'register', 'registrar'] 
 
 export default handler
