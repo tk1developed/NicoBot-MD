de1722c41b241ab6a0e412e02ce31e832d84f2ad
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-04-18 23:19:59 -0500

    🌩◌*̥₊ Mᴜʟᴛɪᴠᴇʀsᴇ ◌❐🪐༉

--- plugins/bot-menuprincipal.js
+++ plugins/bot-menuprincipal.js
@@ -182,7 +182,7 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
 
 
-   let pp1 = 'https://telegra.ph/file/04de42dedb041e9ccb708.mp4'
+   let pp1 = 'https://telegra.ph/file/e7b45f68142362e0f9700.mp4'
     let pp2 = 'https://telegra.ph/file/cab0bf344ba83d79c1a47.mp4'
 
     //let img = await (await fetch('https://tinyurl.com/2y223gks')).buffer()  
