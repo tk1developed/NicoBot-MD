9a576ada59a451108709410c62722be8eccf8d1a
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-04-18 22:44:19 -0500

    🌩◌*̥₊ Mᴜʟᴛɪᴠᴇʀsᴇ ◌❐🪐༉

--- plugins/fix-esperando_mensaje.js
+++ plugins/fix-esperando_mensaje.js
@@ -8,7 +8,7 @@ const handler = async (m, { conn, usedPrefix }) => {
     return conn.sendMessage(m.chat, {text: '*🥀 Utiliza este comando directamente en el número principal del Bot*'}, {quoted: m});
   }
   const chatId = m.isGroup ? [m.chat, m.sender] : [m.sender];
-  const sessionPath = './SakuraBotSession/';
+  const sessionPath = './MultiverseSession/';
   try {
     const files = await fs.readdir(sessionPath);
     let filesDeleted = 0;
