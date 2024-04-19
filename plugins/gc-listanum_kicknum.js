d0927baffe82dfc527c8366ebfe2fa7f9787aa8e
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-04-18 22:48:58 -0500

    🌩◌*̥₊ Mᴜʟᴛɪᴠᴇʀsᴇ ◌❐🪐༉

--- plugins/gc-listanum_kicknum.js
+++ plugins/gc-listanum_kicknum.js
@@ -1,5 +1,5 @@
 /*              Codigo Creado Por Bruno Sobrino
-      (https://github.com/diegojadibot/SakuraBotLite-MD)
+      (github.com/diegojadibot/Multiverse-MD)
 */
 
 const handler = async (m, {conn, args, groupMetadata, participants, usedPrefix, command, isBotAdmin, isSuperAdmin}) => {
