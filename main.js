822b21bfd98e7001b131e7f1d0f13486eb6a35be
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-04-19 15:39:49 -0500

    🌩◌*̥₊ Sᴀᴋᴜʀᴀ ◌❐🪐༉

--- main.js
+++ main.js
@@ -262,7 +262,7 @@ function clearTmp() {
 
 function purgeSession() {
 let prekey = []
-let directorio = readdirSync("./MultiverseSession")
+let directorio = readdirSync("./SakuraSession")
 let filesFolderPreKeys = directorio.filter(file => {
 return file.startsWith('pre-key-') /*|| file.startsWith('session-') || file.startsWith('sender-') || file.startsWith('app-') */
 })
