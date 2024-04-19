67175179b26e5c84497d69e01566e4953675f930
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-04-18 21:39:52 -0500

    🌩◌*̥₊ Mᴜʟᴛɪᴠᴇʀsᴇ ◌❐🪐༉

--- config.js
+++ config.js
@@ -90,7 +90,7 @@ global.mes = d.toLocaleDateString('es', {month: 'long'});
 global.año = d.toLocaleDateString('es', {year: 'numeric'});
 global.tiempo = d.toLocaleString('en-US', {hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true});
 //* ****************************
-global.wm2 = `${dia} ${fecha}\nSakuritaBot`;
+global.wm2 = `${dia} ${fecha}\nMultiverse`;
 global.gt = 'MultiverseBot';
 global.mysticbot = 'MultiverseBot';
 global.mysticbot = 'https://github.com/diegojadibot/Multiverse-MD';
