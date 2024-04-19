5dd951865bb35332678c284f14902bd843112355
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-04-19 00:11:07 -0500

    🌩◌*̥₊ Mᴜʟᴛɪᴠᴇʀsᴇ ◌❐🪐༉

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -1,10 +1,18 @@
-let handler = async (m, { conn, usedPrefix }) => {
-let git = '*乂  B O T  -  S C R I P T*\nhttps://github.com/diegojadibot/Multiverse-MD'
-await conn.sendMessage(m.chat, git, m, { externalAdReply: { mediaType: 1, renderLargerThumbnail: true, thumbnail: imagen2, thumbnailUrl: imagen1, title: '\t\t\t\t\t\t᭡͡ᩬ🍧✩̣̣̣̣̣ͯ𝑴𝒖𝒍𝒕𝒊𝒗𝒆𝒓𝒔𝒆𝑩𝒐𝒕-𝑴𝑫᭡͡ᩬ🍧✩̣̣̣̣̣ͯ' }})
-
-}
-handler.tags =['info'] 
-handler.help = ['script'] 
-handler.command = ['sc', 'script', 'codigo', 'git', 'github'] 
-handler.register = true 
-export default handler
+import moment from 'moment-timezone';
+import fetch from 'node-fetch';
+const handler = async (m, { conn, args }) => {
+   const res = await fetch('https://api.github.com/repos/diegojadibot/SakuraBot-MD');
+   const json = await res.json();
+   let txt = `           *乂  B O T S C R I P T  乂*\n\n`;
+      txt += `◦  *Nombre:* ${json?.name || '❥ᰰຼ ⃟ᬽ៸𝘚𝘢𝘬𝘶𝘳𝘢𝘉𝘰𝘵-𝘔𝘋༒★»'}\n`;
+      txt += `◦  *Visitantes:* ${json?.watchers_count || '-'}\n`;
+      txt += `◦  *Tamaño:* ${(json?.size / 1024).toFixed(2) || '-'} MB\n`;
+      txt += `◦  *Actualización:* ${moment(json?.updated_at).format('DD/MM/YY - HH:mm:ss') || '-'}\n`;
+      txt += `◦  *Url:* ${json?.html_url || 'https://github.com/diegojadibot/SakuraBot-MD'}\n\n`;
+      txt += `         ${json?.forks_count || '-'} Forks · ${json?.stargazers_count || '-'} Stars · ${json?.open_issues_count || '-'} Issues`;
+   await conn.sendMessage(m.chat, {text: txt.trim(), mentions: [...txt.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), contextInfo: {forwardingScore: 9999999, isForwarded: true, mentionedJid: [...txt.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "renderLargerThumbnail": true, "title": global.titulowm2, "containsAutoReply": true, "mediaType": 1, "thumbnail": imagen6, "mediaUrl": `https://atom.bio/sakurabot`, "sourceUrl": `https://atom.bio/sakurabot`}}}, {quoted: m});
+};
+handler.help = ['scbot'];
+handler.tags = ['info'];
+handler.command = /^(sc|scbot|scrip|script)$/i;
+export default handler;
