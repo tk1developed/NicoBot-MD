f306f6a41f5708cac6e59a79cc699f10dd8738b6
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-04-18 21:20:21 -0500

    🌩◌*̥₊ Mᴜʟᴛɪᴠᴇʀsᴇ ◌❐🪐༉

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log('🧿 Iniciando...')
+console.log('🚀 Iniciando...')
 import { join, dirname } from 'path'
 import { createRequire } from 'module'
 import { fileURLToPath } from 'url'
@@ -15,7 +15,7 @@ const { name, author } = require(join(__dirname, './package.json')) // https://w
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-say('Hoshino-MD', {
+say('Multiverse-MD', {
 font: 'block',
 align: 'center',
 colors: ['cyan', 'blue']
