9ee21bc627f067e8a976b75af09c1c7fdcd5e4e5
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-04-19 00:06:26 -0500

    🌩◌*̥₊ Mᴜʟᴛɪᴠᴇʀsᴇ ◌❐🪐༉

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -4,7 +4,7 @@ import moment from 'moment-timezone'
 var handler = async (m, { conn, usedPrefix }) => {
 
 let res = await fetch('https://api.github.com/repos/diegojadibot/Multiverse-MD')
-let json = await res.json()
+//let json = await res.json()
 
 let git = `*乂  B O T  -  S C R I P T*\n\n◦ *Nombre* : ${json.name}\n◦ *Visitantes* : ${json.watchers_count}\n◦ *Peso* : ${(json.size / 1024).toFixed(2)} MB\n◦ *Actualizado* : ${moment(json.updated_at).format('DD/MM/YY - HH:mm:ss')}\n◦ *Url* : ${json.html_url}\n\n           ${json.forks_count} Forks · ${json.stargazers_count} Stars · ${json.open_issues_count} Issues\n\n${global.wm}`
 
