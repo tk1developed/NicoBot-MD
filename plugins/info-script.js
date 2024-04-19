e0e58ee49f15a7fa1931da04ebeba8eedbb08f42
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-04-19 09:22:08 -0500

    🌩◌*̥₊ Mᴜʟᴛɪᴠᴇʀsᴇ ◌❐🪐༉

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -1,16 +1,16 @@
 import moment from 'moment-timezone';
 import fetch from 'node-fetch';
 const handler = async (m, { conn, args }) => {
-   const res = await fetch('https://api.github.com/repos/diegojadibot/Multiverse-MD');
+   const res = await fetch('https://api.github.com/repos/diegojadibot/SakuraBotLite-MD');
    const json = await res.json();
    let txt = `           *乂  B O T S C R I P T  乂*\n\n`;
       txt += `◦  *Nombre:* ${json?.name || 'MultiverseBot-MD'}\n`;
       txt += `◦  *Visitantes:* ${json?.watchers_count || '-'}\n`;
       txt += `◦  *Tamaño:* ${(json?.size / 1024).toFixed(2) || '-'} MB\n`;
       txt += `◦  *Actualización:* ${moment(json?.updated_at).format('DD/MM/YY - HH:mm:ss') || '-'}\n`;
-      txt += `◦  *Url:* ${json?.html_url || 'https://github.com/diegojadibot/Multiverse-MD'}\n\n`;
+      txt += `◦  *Url:* ${json?.html_url || 'https://github.com/diegojadibot/SakuraBotLite-MD'}\n\n`;
       txt += `         ${json?.forks_count || '-'} Forks · ${json?.stargazers_count || '-'} Stars · ${json?.open_issues_count || '-'} Issues`;
-   await conn.sendMessage(m.chat, {text: txt.trim(), mentions: [...txt.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), contextInfo: {forwardingScore: 9999999, isForwarded: true, mentionedJid: [...txt.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "renderLargerThumbnail": true, "title": global.titulowm2, "containsAutoReply": true, "mediaType": 1, "thumbnail": imagen6, "mediaUrl": `https://github.com/diegojadibot/Multiverse-MD`, "sourceUrl": `https://github.com/diegojadibot/Multiverse-MD`}}}, {quoted: m});
+   await conn.sendMessage(m.chat, {text: txt.trim(), mentions: [...txt.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), contextInfo: {forwardingScore: 9999999, isForwarded: true, mentionedJid: [...txt.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "renderLargerThumbnail": true, "title": global.titulowm2, "containsAutoReply": true, "mediaType": 1, "thumbnail": imagen6, "mediaUrl": `https://github.com/diegojadibot/SakuraBotLite-MD`, "sourceUrl": `https://github.com/diegojadibot/SakuraBotLite-MD`}}}, {quoted: m});
 };
 handler.help = ['scbot'];
 handler.tags = ['info'];
