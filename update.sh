512d472cd2c690a9ccfacca9070c0d46b26358bb
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-SK-177@users.noreply.github.com>
2024-04-19 20:40:42 -0500

    🌩◌*̥₊ Sᴀᴋᴜʀᴀ ◌❐🪐༉

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
   # * Al bifurcar este repositorio, dejen los créditos a todos los creadores de cada código por favor. ♥
 #!/data/data/com.termux/files/usr/bin/bash 
 BOT_DIR="SakuraBotLite-MD"  
-BOT_REPO="https://github.com/diegojadibot/$BOT_DIR"
+BOT_REPO="https://github.com/Diego-SK-177/$BOT_DIR"
 DB_FILE="database.json"
 
 GREEN='\033[32m'
