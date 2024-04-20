a9422f15507ee02f5692e7ea108bf7d7c779acf2
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-SK-177@users.noreply.github.com>
2024-04-19 20:54:32 -0500

    🌩◌*̥₊ Sᴀᴋᴜʀᴀ ◌❐🪐༉

--- plugins/owner-actualizacion.js
+++ plugins/owner-actualizacion.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 let previousCommitSHA = '';
 let previousUpdatedAt = '';
 let previousCommitUser = ''; 
-const owner = 'diegojadibot';
+const owner = 'Diego-SK-177';
 const repo = 'SakuraBotLite-MD';
 const handler = async (m, {conn, text, usedPrefix, command}) => {
  conn.sendMessage(m.chat, {text: `*✅️ Comando activado con éxito, te notificaremos cuando se actualize el repositorio.*`}, {quoted: m});  
