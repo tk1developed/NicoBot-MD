234eab2e133c815aaabc28f779f32615e72e1664
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-04-21 15:58:42 -0500

    🐢◌*̥₊ Yᴏᴛsᴜʙᴀ ◌🌩༉

--- plugins/gc-kick.js
+++ plugins/gc-kick.js
@@ -1,11 +1,11 @@
 const handler = async (m, {conn, participants, command, usedPrefix}) => {
   if (!global.db.data.settings[conn.user.jid].restrict) throw '*¡Este Comando Esta Desabilitado Por El Propietario Del Bot!*';
-  const kicktext = `*🍧 Etiqueta A Una Persona O Responda A Un Mensaje Para Eliminarlo Del Grupo.*\n\n*📖 Ejemplo:*\n*${usedPrefix + command} @${global.suittag}*`;
+  const kicktext = `*📢 Etiqueta A Una Persona O Responda A Un Mensaje Para Eliminarlo Del Grupo.*\n\n*🏜 Ejemplo:*\n*${usedPrefix + command} @${global.suittag}*`;
   if (!m.mentionedJid[0] && !m.quoted) return m.reply(kicktext, m.chat, {mentions: conn.parseMention(kicktext)});
-  if (m.message.extendedTextMessage === undefined || m.message.extendedTextMessage === null) return m.reply('*🥀 Etiqueta A Una Persona O Responda Al Mensaje.*');
+  if (m.message.extendedTextMessage === undefined || m.message.extendedTextMessage === null) return m.reply('*📢 Etiqueta A Una Persona O Responda Al Mensaje.*');
   if (m.message.extendedTextMessage.contextInfo.participant !== null && m.message.extendedTextMessage.contextInfo.participant != undefined && m.message.extendedTextMessage.contextInfo.participant !== '') {
     const mentioned = m.message.extendedTextMessage.contextInfo.mentionedJid[0] ? m.message.extendedTextMessage.contextInfo.mentionedJid[0] : m.message.extendedTextMessage.contextInfo.participant;
-    if (conn.user.jid.includes(mentioned)) return m.reply('*🍧 No Puedo Eliminarme Con Ese Comando.*');
+    if (conn.user.jid.includes(mentioned)) return m.reply('*⚠️ No Puedo Eliminarme Con Ese Comando.*');
     const responseb = await conn.groupParticipantsUpdate(m.chat, [mentioned], 'remove');
     const exitoso1 = `*@${mentioned.split('@')[0]} ғᴜᴇ ᴇʟɪᴍɪɴᴀᴅᴏ ᴇxɪᴛᴏsᴀᴍᴇɴᴛᴇ ᴅᴇʟ ɢʀᴜᴘᴏ*`;
     const error1 = `*@${mentioned.split('@')[0]} ᴇs ᴇʟ ᴄʀᴇᴀᴅᴏʀ ᴅᴇʟ ɢʀᴜᴘᴏ, ɴᴏ ᴘᴜᴇᴅᴏ ᴇʟɪᴍɪɴᴀʀ ᴀʟ ᴄʀᴇᴀᴅᴏʀ ᴅᴇʟ ɢʀᴜᴘᴏ*`;
@@ -13,7 +13,7 @@ const handler = async (m, {conn, participants, command, usedPrefix}) => {
     if (responseb[0].status === '200') m.reply(exitoso1, m.chat, {mentions: conn.parseMention(exitoso1)});
     else if (responseb[0].status === '406') m.reply(error1, m.chat, {mentions: conn.parseMention(error1)});
     else if (responseb[0].status === '404') m.reply(error2, m.chat, {mentions: conn.parseMention(error2)});
-    else conn.sendMessage(m.chat, {text: `✨ *Api Caida*`, mentions: [m.sender], contextInfo: {forwardingScore: 999, isForwarded: true}}, {quoted: m});
+    else conn.sendMessage(m.chat, {text: `🛑 *Api Caida*`, mentions: [m.sender], contextInfo: {forwardingScore: 999, isForwarded: true}}, {quoted: m});
   } else if (m.message.extendedTextMessage.contextInfo.mentionedJid != null && m.message.extendedTextMessage.contextInfo.mentionedJid != undefined) {
     return;
   }
