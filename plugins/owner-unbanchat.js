b1dd386e09ce374b21c5260cd9f7aebebc5833ef
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-04-21 15:27:47 -0500

    🐢◌*̥₊ Yᴏᴛsᴜʙᴀ ◌🌩༉

--- plugins/owner-unbanchat.js
+++ plugins/owner-unbanchat.js
@@ -1,6 +1,6 @@
 const handler = async (m) => {
   global.db.data.chats[m.chat].isBanned = false;
-  m.reply('✅️ *Chat Desbaneado Con Exito, Ya Pueden Usar SakuraBotLite-MD*');
+  m.reply('*Bot Desactivado*');
 };
 handler.help = ['unbanchat'];
 handler.tags = ['owner'];
