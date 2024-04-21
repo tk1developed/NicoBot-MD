1bbb3351b0cc595fef364f69ed0a45aa8029c145
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-04-21 15:37:53 -0500

    🐢◌*̥₊ Yᴏᴛsᴜʙᴀ ◌🌩༉

--- plugins/owner-unbanchat.js
+++ plugins/owner-unbanchat.js
@@ -1,6 +1,6 @@
 const handler = async (m) => {
   global.db.data.chats[m.chat].isBanned = false;
-  m.reply('*Bot Desactivado*');
+  m.reply('*Bot Encendido*');
 };
 handler.help = ['unbanchat'];
 handler.tags = ['owner'];
