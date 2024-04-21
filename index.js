bf15a879b04c99315c92e5dd45473b1ba605fc81
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-04-21 14:44:45 -0500

    🍁◌*̥₊ Yᴏᴛsᴜʙᴀ Bʏ: Dɪᴇɢᴏ ◌❐🪐༉

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const { name, author } = require(join(__dirname, './package.json')) // https://w
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-say('Kana\nArima\nMD', {
+say('Yotsuba\nNakano\nMD', {
 font: 'block',
 align: 'center',
 colors: ['cyan', 'blue']
