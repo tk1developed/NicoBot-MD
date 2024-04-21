cbf617de865722fc5a24073e3418faba532ff3e1
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-04-21 18:26:30 -0500

    🐢◌*̥₊ Yᴏᴛsᴜʙᴀ ◌🌩༉

--- plugins/downloader-yta.2.js
+++ plugins/downloader-yta.2.js
@@ -1,8 +1,8 @@
 import {youtubedl, youtubedlv2} from '@bochilteam/scraper';
 import fetch from 'node-fetch';
 const handler = async (m, {conn, args}) => {
-  if (!args[0]) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝚂𝙴𝚁𝚃𝙴 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 𝙼𝙰𝚂 𝙴𝙻 𝙴𝙽𝙻𝙰𝙲𝙴 / 𝙻𝙸𝙽𝙺 𝙳𝙴 𝚄𝙽 𝚅𝙸𝙳𝙴𝙾 𝙳𝙴 𝚈𝙾𝚄𝚃𝚄𝙱𝙴*';
-  await m.reply(`*_⏳𝘈𝘶𝘥𝘪𝘰 𝘗𝘳𝘰𝘤𝘦𝘴𝘢𝘯𝘥𝘰...⏳_*\n\n*◉ 𝓼Sɪ Eʟ Aᴜᴅɪᴏ Nᴏ Sᴇ Lᴇ Eɴᴠɪᴀ Pʀᴜᴇʙᴇ Lᴏs Sɪɢᴜɪᴇɴᴛᴇs Cᴏᴍᴀɴᴅᴏs #playdoc ᴏ #play.2 ᴏ #ytmp4doc ◉*`);
+  if (!args[0]) throw '*Y EL LINK?*';
+  await m.reply(`*_⏳ Audio procesando...⏳_*\n\n*𝓼Sɪ Eʟ Aᴜᴅɪᴏ Nᴏ Sᴇ Lᴇ Eɴᴠɪᴀ Pʀᴜᴇʙᴇ Lᴏs Sɪɢᴜɪᴇɴᴛᴇs Cᴏᴍᴀɴᴅᴏs #playdoc ᴏ #play.2 ᴏ #ytmp4doc ◉*`);
   try {
     const q = '128kbps';
     const v = args[0];
@@ -10,7 +10,7 @@ const handler = async (m, {conn, args}) => {
     const dl_url = await yt.audio[q].download();
     const ttl = await yt.title;
     const size = await yt.audio[q].fileSizeH;
-    const cap = `*◉—⌈📥 𝐘𝐎𝐔𝐓𝐔𝐁𝐄 𝐃𝐋 📥⌋—◉*\n❏ *𝚃𝙸𝚃𝚄𝙻𝙾:* ${ttl}\n❏ *𝙿𝙴𝚂𝙾:* ${size}\n  ❥ᰰຼ ⃟ᬽ៸𝙎𝙖𝙠𝙪𝙧𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿༒★»`.trim();
+    const cap = `*◉—⌈📥 𝐘𝐎𝐔𝐓𝐔𝐁𝐄 𝐃𝐋 📥⌋—◉*\n❏ *𝚃𝙸𝚃𝚄𝙻𝙾:* ${ttl}\n❏ *𝙿𝙴𝚂𝙾:* ${size}\n✰ 𝗬𝗼𝘁𝘀𝘂𝗯𝗮 𝗡𝗮𝗸𝗮𝗻𝗼 ★`.trim();
     await conn.sendMessage(m.chat, {document: {url: dl_url}, caption: cap, mimetype: 'audio/mpeg', fileName: `${ttl}.mp3`}, {quoted: m});
   } catch {
     try {
@@ -19,10 +19,10 @@ const handler = async (m, {conn, args}) => {
       const n = lolh.result.title || 'error';
       const n2 = lolh.result.link;
       const n3 = lolh.result.size;
-      const cap2 = `*◉—⌈📥 𝐘𝐎𝐔𝐓𝐔𝐁𝐄 𝐃𝐋 📥⌋—◉*\n❏ *𝚃𝙸𝚃𝚄𝙻𝙾:* ${n}\n❏ *𝙿𝙴𝚂𝙾:* ${n3}\n  ❥ᰰຼ ⃟ᬽ៸𝙎𝙖𝙠𝙪𝙧𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿༒★» `.trim();
+      const cap2 = `*◉—⌈📥 𝐘𝐎𝐔𝐓𝐔𝐁𝐄 𝐃𝐋 📥⌋—◉*\n❏ *𝚃𝙸𝚃𝚄𝙻𝙾:* ${n}\n❏ *𝙿𝙴𝚂𝙾:* ${n3}\n✰ 𝗬𝗼𝘁𝘀𝘂𝗯𝗮 𝗡𝗮𝗸𝗮𝗻𝗼 ★`.trim();
       await conn.sendMessage(m.chat, {document: {url: n2}, caption: cap2, mimetype: 'audio/mpeg', fileName: `${n}.mp3`}, {quoted: m});
     } catch {
-      await conn.reply(m.chat, '*[❗] 𝙴𝚁𝚁𝙾𝚁 𝙽𝙾 𝙵𝚄𝙴 𝙿𝙾𝚂𝙸𝙱𝙻𝙴 𝙳𝙴𝚂𝙲𝙰𝚁𝙶𝙰𝚁 𝙴𝙻 𝙰𝚄𝙳𝙸𝙾*', m);
+      await conn.reply(m.chat, '*⚠️ Ocurrió un error*', m);
     }
   }
 };
