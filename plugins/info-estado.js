0f63fe56e0e66d0b03ae1b9c9ef766058276c224
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-04-21 18:11:33 -0500

    🐢◌*̥₊ Yᴏᴛsᴜʙᴀ ◌🌩༉

--- plugins/info-estado.js
+++ plugins/info-estado.js
@@ -33,5 +33,5 @@ function clockString(ms) {
   const h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24;
   const m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60;
   const s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60;
-  return [`\n┃ *💥 ` + d, ' Día(s)* ', `\n┃ *💫 ` + h, ' Hora(s)* ', `\n┃ *💠 ` + m, ' Minuto(s)* ', `\n┃ *♦ ` + s, ' Segundo(s)* '].map((v) => v.toString().padStart(2, 0)).join('');
+  return [`\n┃⍣ *💥 ` + d, ' Día(s)* ', `\n┃⍣ *💫 ` + h, ' Hora(s)* ', `\n┃⍣ *💠 ` + m, ' Minuto(s)* ', `\n┃⍣ *♦ ` + s, ' Segundo(s)* '].map((v) => v.toString().padStart(2, 0)).join('');
 }
