d67c8647ec5f9cbdebb68ea5f2e657e1608d36e1
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-04-20 22:14:58 -0500

    🍁◌*̥₊ Aʀɪᴍᴀ Bʏ: Dɪᴇɢᴏ ◌❐🪐༉

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
 # Código desarrollado por @gata_dios 
   # * Al bifurcar este repositorio, dejen los créditos a todos los creadores de cada código por favor. ♥
 #!/data/data/com.termux/files/usr/bin/bash 
-BOT_DIR="SakuraBotLite-MD"  
-BOT_REPO="https://github.com/Diego-SK-177/$BOT_DIR"
+BOT_DIR="Kana-Arima-MD"  
+BOT_REPO="https://github.com/Diego-YL-177/$BOT_DIR"
 DB_FILE="database.json"
 
 GREEN='\033[32m'
