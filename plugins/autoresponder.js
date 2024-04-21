727f3d88b70cbc64d19595c65e2ce79477116111
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-04-21 15:13:46 -0500

    🐢◌*̥₊ Yᴏᴛsᴜʙᴀ ◌🌩༉

--- plugins/autoresponder.js
+++ plugins/autoresponder.js
@@ -7,7 +7,7 @@ let chat = global.db.data.chats[m.chat]
 if (m.mentionedJid.includes(this.user.jid) && m.isGroup && !chat.isBanned) {
 let stiker = await sticker(imagen8, false, global.packname, global.author)  
 this.sendFile(m.chat, stiker, 'sticker.webp', null, m, false, { 
-contextInfo: { externalAdReply: { title: 'ꪹ͜𓂃͡𝑺𝒂𝒌𝒖𝒓𝒂𝑩𝒐𝒕𝑳𝒊𝒕𝒆-𝑴𝑫𓏲੭', body: '𝖡𝗒: 𝖣𝗂𝖾𝗀𝗈 - 𝖶𝖺𝗈𝗌', sourceUrl: `https://github.com/Diego-SK-177/SakuraBotLite-MD`, thumbnail: imagen6}}})}
+contextInfo: { externalAdReply: { title: 'ꪹ͜𓂃͡𝐘𝐨𝐭𝐬𝐮𝐛𝐚 𝐍𝐚𝐤𝐚𝐧𝐨𓏲੭', body: '𝐒𝐭𝐚𝐫𝐥𝐢𝐠𝐡𝐭𝐬 𝐓𝐞𝐚𝐦', sourceUrl: `https://github.com/Diego-SK-177/SakuraBotLite-MD`, thumbnail: imagen6}}})}
 
 return !0 }
 export default handler
