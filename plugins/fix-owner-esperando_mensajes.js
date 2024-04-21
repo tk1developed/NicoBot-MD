a4e2f331fcabfe63b05c872603f7c046b5e53e19
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-04-21 16:00:18 -0500

    🐢◌*̥₊ Yᴏᴛsᴜʙᴀ ◌🌩༉

--- plugins/fix-owner-esperando_mensajes.js
+++ plugins/fix-owner-esperando_mensajes.js
@@ -5,13 +5,13 @@ import path from 'path';
 
 const handler = async (m, { conn, usedPrefix }) => {
   if (global.conn.user.jid !== conn.user.jid) {
-    return conn.sendMessage(m.chat, {text: '*🧸 Tienes Que Utilizarlo En El Numero Del Bot Oficial.*'}, {quoted: m});
+    return conn.sendMessage(m.chat, {text: '*📢 Tienes Que Utilizarlo En El Numero Del Bot Oficial.*'}, {quoted: m});
   }
-  await conn.sendMessage(m.chat, {text: '*🧩 Iɴɪᴄɪᴀɴᴅᴏ Eʟ Pʀᴏᴄᴇsᴏ Dᴇ Eʟɪᴍɪɴᴀᴄɪᴏ́ɴ Dᴇ Tᴏᴅᴏs Lᴏs Aʀᴄʜɪᴠᴏs Dᴇ Sᴇssɪᴏɴ, Exᴇᴄᴛᴏ Eʟ Aʀᴄʜɪᴠᴏ creds.json...*'}, {quoted: m});
-  const sessionPath = './SakuraSession/';
+  await conn.sendMessage(m.chat, {text: '*⚠️ Iɴɪᴄɪᴀɴᴅᴏ Eʟ Pʀᴏᴄᴇsᴏ Dᴇ Eʟɪᴍɪɴᴀᴄɪᴏ́ɴ Dᴇ Tᴏᴅᴏs Lᴏs Aʀᴄʜɪᴠᴏs Dᴇ Sᴇssɪᴏɴ, Exᴇᴄᴛᴏ Eʟ Aʀᴄʜɪᴠᴏ creds.json...*'}, {quoted: m});
+  const sessionPath = './NakanoSession/';
   try {
     if (!existsSync(sessionPath)) {
-      return await conn.sendMessage(m.chat, {text: '*📍 La Carpeta SakuraSession No Existe O Está Vacia.*'}, {quoted: m});
+      return await conn.sendMessage(m.chat, {text: '*📢 La Carpeta NakanoSession No Existe O Está Vacia.*'}, {quoted: m});
     }
     const files = await fs.readdir(sessionPath);
     let filesDeleted = 0;
@@ -22,18 +22,18 @@ const handler = async (m, { conn, usedPrefix }) => {
       }
     }
     if (filesDeleted === 0) {
-      await conn.sendMessage(m.chat, {text: '*📍 No Se Encontro El Archivo Para Eliminar La Carpeta SakuraSession.*'}, {quoted: m});
+      await conn.sendMessage(m.chat, {text: '*⚠️ No Se Encontro El Archivo Para Eliminar La Carpeta NakanoSession.*'}, {quoted: m});
     } else {
-      await conn.sendMessage(m.chat, {text: `*🧸 Se Eliminaron ${filesDeleted} Archivos De Sesion, Excepto El Archivo creds.json.*`}, {quoted: m});
+      await conn.sendMessage(m.chat, {text: `*🏜 Se Eliminaron ${filesDeleted} Archivos De Sesion, Excepto El Archivo creds.json.*`}, {quoted: m});
     }
   } catch (err) {
     console.error('Error al leer la carpeta o los archivos de sesión:', err);
-    await conn.sendMessage(m.chat, {text: '*🚀 Ocurrió Un Error Al Eliminar Los Archivos De Sesion.*'}, {quoted: m});
+    await conn.sendMessage(m.chat, {text: '*📢 Ocurrió Un Error Al Eliminar Los Archivos De Sesion.*'}, {quoted: m});
   }
-  await conn.sendMessage(m.chat, {text: `*🧸 ¡Hola! Ahora me ves?*\n\n*🧩 Si el Bot no le responde a sus comandos por favor haga un pequeño spam*\n\n*📍 Ejemplo:*\n${usedPrefix}s\n${usedPrefix}s\n${usedPrefix}s`}, {quoted: m});
+  await conn.sendMessage(m.chat, {text: `*🚩 ¡Hola! Ahora me ves?*\n\n*📢 Si el Bot no le responde a sus comandos por favor haga un pequeño spam*\n\n*📍 Ejemplo:*\n${usedPrefix}s\n${usedPrefix}s\n${usedPrefix}s`}, {quoted: m});
 };
 handler.help = ['del_reg_in_session_owner'];
 handler.tags = ['owner'];
-handler.command = /^(del_reg_in_session_owner|dsowner|delsakura)$/i;
+handler.command = /^(del_reg_in_session_owner|dsowner|delyotsuba)$/i;
 handler.rowner = true
 export default handler;
