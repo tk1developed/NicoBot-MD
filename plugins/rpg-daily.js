0bf45476af6d5ab28c557108763c5543c98123ee
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-04-23 11:33:16 -0500

    🐢◌*̥₊ Yᴏᴛsᴜʙᴀ ◌🌩༉

--- plugins/rpg-daily.js
+++ plugins/rpg-daily.js
@@ -1,79 +1,47 @@
-import fetch from 'node-fetch';
-const handler = async (m, {isPrems, conn}) => {
-  const fkontak = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net',
-  };
-  const mystic = './Menu2.jpg';
-  const dos = [md];
-  const user = global.db.data.users[m.sender];
-  const premium = user.premium;
+const free = 25
+const prem = 15
 
-  const exp = `${pickRandom([500, 600, 700, 800, 900, 999, 1000, 1300, 1500, 1800])}` * 1;
-  const exppremium = `${pickRandom([1000, 1500, 1800, 2100, 2500, 2900, 3300, 3600, 4000, 4500])}` * 1;
+var handler = async (m, {conn, isPrems }) => {
 
-  const money = `${pickRandom([300, 500, 700, 900, 500, 800, 900, 1100, 1350, 1500])}` * 1;
-  const moneypremium = `${pickRandom([800, 1300, 1600, 1900, 2200, 2500, 2700, 3000, 3300, 3500])}` * 1;
+let coin = `${pickRandom([5, 6, 7, 9, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 99, 100, 110, 120, 130, 600, 1000, 1500])}` * 1
+let exp = `${pickRandom([500, 600, 700, 800, 900, 999, 1000, 1300, 1500, 1800])}` * 1
+let exppremium = `${pickRandom([1000, 1500, 1800, 2100, 2500, 2900, 3300, 3600, 4000, 4500])}` * 1
+let d = Math.floor(Math.random() * 30)
+global.db.data.users[m.sender].diamond += d
+global.db.data.users[m.sender].money += d
+let time = global.db.data.users[m.sender].lastclaim + 86400000 //12 Horas
+if (new Date - global.db.data.users[m.sender].lastclaim < 7200000) return conn.reply(m.chat, `🕚 *Vuelve en ${msToTime(time - new Date())}*`, m, fake, )
+global.db.data.users[m.sender].exp += exppremium ? prem : exp
+conn.reply(m.chat, `🎁 *Recompensa Diaria*
 
-  const potion = `${pickRandom([1, 2, 3, 4, 5])}` * 1;
-  const potionpremium = `${pickRandom([2, 4, 6, 9, 12])}` * 1;
+Recursos:
+✨ Xp : *+${isPrems ? exppremium : exp}*
+💎 Diamantes : *+${d}*
+🪙 YotsuCoins : *+${coin}*`, m, )
 
-  const tiketcoin = `${pickRandom([1, 0, 0, 2, 0])}` * 1;
-  const tiketcoinpremium = `${pickRandom([2, 1, 1, 3, 4])}` * 1;
+global.db.data.users[m.sender].lastclaim = new Date * 1
 
-  const eleksirb = `${pickRandom([1, 1, 1, 3, 1, 2, 2, 1, 5, 8])}` * 1;
-  const eleksirbpremium = `${pickRandom([3, 3, 5, 3, 8, 3, 4, 4, 10, 7])}` * 1;
+}
+handler.help = ['daily', 'claim']
+handler.tags = ['rg']
+handler.command = ['daily', 'claim']
 
-  const umpan = `${pickRandom([10, 20, 30, 40, 50, 60, 70, 80, 90, 100])}` * 1;
-  const umpanpremium = `${pickRandom([30, 60, 90, 120, 150, 180, 210, 240, 270, 300])}` * 1;
-
-  // let gata = Math.floor(Math.random() * 2000)
-
-  const recompensas = {
-    exp: premium ? exppremium : exp,
-    money: premium ? moneypremium : money,
-    potion: premium ? potionpremium : potion,
-    tiketcoin: premium ? tiketcoinpremium : tiketcoin,
-    eleksirb: premium ? eleksirbpremium : eleksirb,
-    umpan: premium ? umpanpremium : umpan,
-  };
-
-  const time = user.lastclaim + 7200000; // 2 Horas 7200000
-  if (new Date - user.lastclaim < 7200000) return await conn.reply(m.chat, `𝙔𝘼 𝙍𝙀𝘾𝙇𝘼𝙈𝘼𝙎𝙏𝙀 𝙏𝙐 𝙍𝙀𝙂𝘼𝙇𝙊 🎁\n𝙑𝙐𝙀𝙇𝙑𝙀 𝙀𝙉 *${msToTime(time - new Date())}* 𝙋𝘼𝙍𝘼 𝙑𝙊𝙇𝙑𝙀𝙍 𝘼 𝙍𝙀𝘾𝙇𝘼𝙈𝘼𝙍`, fkontak, m);
-  // await conn.sendButton(m.chat, `𝙔𝘼 𝙍𝙀𝘾𝙇𝘼𝙈𝘼𝙎𝙏𝙀 𝙏𝙐 𝙍𝙀𝙂𝘼𝙇𝙊 🎁\n𝙑𝙐𝙀𝙇𝙑𝙀 𝙀𝙉 *${msToTime(time - new Date())}* 𝙋𝘼𝙍𝘼 𝙑𝙊𝙇𝙑𝙀𝙍 𝘼 𝙍𝙀𝘾𝙇𝘼𝙈𝘼𝙍`, wm, null, [['𝗠 𝗘 𝗡 𝗨 ☘️', '/menu']], fkontak, m)
-  let texto = '';
-  for (const reward of Object.keys(recompensas)) {
-    if (!(reward in user)) continue;
-    user[reward] += recompensas[reward];
-    texto += `*+${recompensas[reward]}* ${global.rpgshop.emoticon(reward)}\n➺ `;
-  }
-  const text = `╭◈ ━━━━━━━ ⸙ ━━━━━━━ ◈
-➺ ✨ 𝙊𝘽𝙏𝙄𝙀𝙉𝙀𝙎 𝙐𝙉 𝙍𝙀𝙂𝘼𝙇𝙊!!
-➺ *${premium ? '🎟️ Recompensa Premium' : '🆓 Recompensa Gratis'}*
-➺ ${texto}
-╰━━━━━ • ஜ • ❈ • ஜ • ━━━━━\n\n🎟️ 𝗣 𝗥 𝗘 𝗠 𝗜 𝗨 𝗠 ⇢ ${premium ? '✅' : '❌'}\n${wm}`;
-  const img = './Menu5.png';
-  await conn.sendFile(m.chat, img, 'mystic.jpg', text, fkontak);
-  // await conn.sendButton(m.chat, text, texto + `\n\n🎟️ 𝗣 𝗥 𝗘 𝗠 𝗜 𝗨 𝗠 ⇢ ${premium ? '✅' : '❌'}\n${wm}`, img, [['⛰️ 𝘼𝘽𝙍𝙄𝙍 𝘾𝙊𝙁𝙍𝙀 | 𝘾𝙊𝙁𝙁𝙀𝙍 ⛰️', '/cofre'], ['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘️', '/menu']], m, dos.getRandom())
-  user.lastclaim = new Date * 1;
-};
-handler.help = ['daily'];
-handler.tags = ['xp'];
-handler.command = ['daily', 'reclamar', 'reclamo', 'regalo', 'claim'];
 handler.register = true
-export default handler;
+
+export default handler
 
 function pickRandom(list) {
-  return list[Math.floor(Math.random() * list.length)];
-}
+return list[Math.floor(Math.random() * list.length)]}
 
 function msToTime(duration) {
-  const milliseconds = parseInt((duration % 1000) / 100);
-  let seconds = Math.floor((duration / 1000) % 60);
-  let minutes = Math.floor((duration / (1000 * 60)) % 60);
-  let hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
+var milliseconds = parseInt((duration % 1000) / 100),
+seconds = Math.floor((duration / 1000) % 60),
+minutes = Math.floor((duration / (1000 * 60)) % 60),
+hours = Math.floor((duration / (1000 * 60 * 60)) % 24)
 
-  hours = (hours < 10) ? '0' + hours : hours;
-  minutes = (minutes < 10) ? '0' + minutes : minutes;
-  seconds = (seconds < 10) ? '0' + seconds : seconds;
+hours = (hours < 10) ? '0' + hours : hours
+minutes = (minutes < 10) ? '0' + minutes : minutes
+seconds = (seconds < 10) ? '0' + seconds : seconds
 
-  return hours + ' Horas ' + minutes + ' Minutos';
+return hours + ' Horas ' + minutes + ' Minutos'
 }
