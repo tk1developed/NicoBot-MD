81dddf9ab36cb74c24b9cfcc2d4b2aaefa5cff3f
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-04-25 07:56:44 -0500

    🐢◌*̥₊ Yᴏᴛsᴜʙᴀ ◌🌩༉

--- plugins/buscador-yts.js
+++ plugins/buscador-yts.js
@@ -2,20 +2,20 @@ import yts from 'yt-search';
 import fs from 'fs';
 
 const handler = async (m, {conn, text}) => {
-  if (!text) throw '🧸 *_Que quieres que busque en YouTube?_*';
+  if (!text) throw '🛑 *Que Deseas Que Busque En Youtube?*';
   const results = await yts(text);
   const tes = results.all;
   const teks = results.all.map((v) => {
     switch (v.type) {
       case 'video': return `
-° *_${v.title}_*
-↳ 🧸 *_Enlace :_* ${v.url}
-↳ 📍 *_Duración :_* ${v.timestamp}
-↳ 🧩 *_Subido :_* ${v.ago}
-↳ 👁 *_Vistas :_* ${v.views}`;
-    }
-  }).filter((v) => v).join('\n\n◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦\n\n');
-  conn.sendFile(m.chat, tes[0], 'yts.jpeg', teks, m);
+ 🚩 Titulo :_ ${v.title}
+ 🔗 Enlace :_ ${v.url}
+ 🕒 Duración :_ ${v.timestamp}
+ 📥 Subido :_ ${v.ago}
+ 👁 Vista :_ ${v.views}`;
+    }                            
+  }).filter((v) => v).join('\n\nꙬꙬꙬꙬꙬꙬꙬꙬꙬꙬ\n\n');
+  conn.sendFile(m.chat, tes[0].thumbnail, 'yts.jpeg', teks, m);
 };
 handler.help = ['ytsearch *<texto>*'];
 handler.tags = ['search'];
