4c09b4ea3c29f988cf764d1431097cc1543b01c9
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-04-25 22:22:24 -0500

    ✰ 𝗬𝗼𝘁𝘀𝘂𝗯𝗮 𝗡𝗮𝗸𝗮𝗻𝗼 ★

--- plugins/bot-menuprincipal.js
+++ plugins/bot-menuprincipal.js
@@ -190,24 +190,19 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
     let pp3 = 'https://telegra.ph/file/52c004a4d1506b02165b1.mp4'
     let pp4 = 'https://telegra.ph/file/1634b24b028638c4067ae.mp4'
 
-    //let img = await (await fetch('https://tinyurl.com/2y223gks')).buffer()  
 
-await conn.reply(m.chat, '*ꪹ͜𓂃͡𝗖𝗮𝗿𝗴𝗮𝗻𝗱𝗼 𝗘𝗹 𝗠𝗲𝗻𝘂 𝗗𝗲𝗹 𝗕𝗼𝘁...𓏲੭*', m, { contextInfo:{ forwardingScore: 2022, isForwarded: true, externalAdReply: {title: '✰ 𝐘𝐨𝐭𝐬𝐮𝐛𝐚 𝐍𝐚𝐤𝐚𝐧𝐨 - 𝐌𝐃 ✰', body: '𝐒𝐭𝐚𝐫𝐥𝐢𝐠𝐡𝐭𝐬 𝐓𝐞𝐚𝐦', sourceUrl: global.channel, thumbnail: imagen6 }}})
+await conn.reply(m.chat, '*ꪹ͜𓂃͡𝗖𝗮𝗿𝗴𝗮𝗻𝗱𝗼 𝗘𝗹 𝗠𝗲𝗻𝘂 𝗗𝗲𝗹 𝗕𝗼𝘁...𓏲੭*', m, { contextInfo:{ forwardingScore: 2022, isForwarded: true, externalAdReply: {title: '✰ 𝐘𝐨𝐭𝐬𝐮𝐛𝐚 𝐍𝐚𝐤𝐚𝐧𝐨 - 𝐌𝐃 ✰', body: team, sourceUrl: global.channel, thumbnail: imagen6 }}})
 
 
 await conn.sendMessage(m.chat, { react: { text: '💫', key: m.key } })
 
-//await conn.sendFile(m.chat, wm, text.trim(), img, img, channel, m)
-
 
     conn.sendMessage(m.chat, { video: { url: [pp1, pp2, pp3, pp4].getRandom() }, gifPlayback: true, caption: text.trim(), mentions: [m.sender] }, { quoted: m })
 
-   //let img = await (await fetch('https://tinyurl.com/242jfywv')).buffer() 
-
-//conn.sendFile(m.chat, img, 'out.png', text.trim(), m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': true, externalAdReply:{ showAdAttribution: false, title: gcname, body: `h`, mediaType: 2, sourceUrl: channel, thumbnail: miniurl}, mentions: [m.sender]}}, { quoted: m })
+   
 
   } catch (e) {
-    conn.reply(m.chat, '❎ Lo sentimos, el menú tiene un error.', m)
+    conn.reply(m.chat, '🛑 *Ocurrió un error*', m)
     throw e
   }
 }
