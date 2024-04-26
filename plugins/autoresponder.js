66fa8ded6991e4ef25de5ccded7dab048d0725bb
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-04-25 22:16:40 -0500

    ✰ 𝗬𝗼𝘁𝘀𝘂𝗯𝗮 𝗡𝗮𝗸𝗮𝗻𝗼 ★

--- plugins/autoresponder.js
+++ plugins/autoresponder.js
@@ -7,7 +7,7 @@ let chat = global.db.data.chats[m.chat]
 if (m.mentionedJid.includes(this.user.jid) && m.isGroup && !chat.isBanned) {
 let stiker = await sticker(imagen8, false, global.packname, global.author)  
 this.sendFile(m.chat, stiker, 'sticker.webp', null, m, false, { 
-contextInfo: { externalAdReply: { title: 'ꪹ͜𓂃͡𝐘𝐨𝐭𝐬𝐮𝐛𝐚 𝐍𝐚𝐤𝐚𝐧𝐨𓏲੭', body: '𝐒𝐭𝐚𝐫𝐥𝐢𝐠𝐡𝐭𝐬 𝐓𝐞𝐚𝐦', sourceUrl: `https://github.com/Diego-YL-177/Yotsuba-Nakano-MD`, thumbnail: imagen6}}})}
+contextInfo: { externalAdReply: { title: 'ꪹ͜𓂃͡𝐘𝐨𝐭𝐬𝐮𝐛𝐚 𝐍𝐚𝐤𝐚𝐧𝐨𓏲੭', body: 'Powered By: Diego.Ofc', sourceUrl: `https://github.com/Diego-YL-177/Yotsuba-Nakano-MD`, thumbnail: imagen6}}})}
 
 return !0 }
 export default handler
