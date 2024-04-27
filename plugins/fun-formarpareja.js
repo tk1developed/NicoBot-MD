1b34877f6c765b957bc36db2981449139a76b230
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-04-27 07:20:30 -0500

    ✰ 𝗬𝗼𝘁𝘀𝘂𝗯𝗮 𝗡𝗮𝗸𝗮𝗻𝗼 ★

--- plugins/fun-formarpareja.js
+++ plugins/fun-formarpareja.js
@@ -5,7 +5,7 @@ function handler(m, {groupMetadata}) {
   let b;
   do b = ps.getRandom();
   while (b === a);
-  m.reply(`*${toM(a)}, 𝙳𝙴𝙱𝙴𝚁𝙸𝙰𝚂 𝙲𝙰𝚂𝙰𝚁𝚃𝙴 💍 𝙲𝙾𝙽 ${toM(b)}, 𝙷𝙰𝙲𝙴𝙽 𝚄𝙽𝙰 𝙱𝚄𝙴𝙽𝙰 𝙿𝙰𝚁𝙴𝙹𝙰 💓*`, null, {
+  m.reply(`*${toM(a)}, Deberias Casarte 💍 Con ${toM(b)}, Hacen Una Bonita Pareja 💓*`, null, {
     mentions: [a, b],
   });
 }
