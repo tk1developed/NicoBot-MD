8b712093b7b621b88f37aa1b07cb2a7a739850f1
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-04-27 07:31:32 -0500

    Actualizar gc-setwelcome.js

--- plugins/gc-setwelcome.js
+++ plugins/gc-setwelcome.js
@@ -2,7 +2,7 @@ const handler = async (m, {conn, text, isROwner, isOwner}) => {
   if (text) {
     global.db.data.chats[m.chat].sWelcome = text;
     m.reply('☄️ *Mensaje de bienvenida configurada con éxito*');
-  } else throw `*Ingrese el mensaje para configurar la. bienvenida, Ejemplo:*\n*- @user (mención)*\n*- @group (nombre de grupo)*\n*- @desc (description de grupo)*`;
+  } else throw `☄️ *Ingrese el mensaje para configurar la. bienvenida, Ejemplo:*\n*- @user (mención)*\n*- @group (nombre de grupo)*\n*- @desc (description de grupo)*`;
 };
 handler.help = ['setwelcome <text>'];
 handler.tags = ['group'];
