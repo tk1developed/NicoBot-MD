ad0e95d97b50f99df03664485feb58f92fdfa8b9
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-04-27 07:42:25 -0500

    Actualizar convertidor-togifaud.js

--- plugins/convertidor-togifaud.js
+++ plugins/convertidor-togifaud.js
@@ -1,11 +1,11 @@
 const handler = async (m, {conn, usedPrefix, command}) => {
-  if (!m.quoted) throw `*🍧 Responda A Un Video Que Desee Convertir En Gif Con Audio*`;
+  if (!m.quoted) throw `*☁️ Responda A Un Video Que Desee Convertir En Gif Con Audio*`;
   const q = m.quoted || m;
   const mime = (q.msg || q).mimetype || '';
-  if (!/(mp4)/.test(mime)) throw `*🍨 El Tipo De Archivo ${mime} No Es Correcto, Responda A Un Video Que Desee Convertir En Gif Con Audio*`;
+  if (!/(mp4)/.test(mime)) throw `*☄️ El Tipo De Archivo ${mime} No Es Correcto, Responda A Un Video Que Desee Convertir En Gif Con Audio*`;
   m.reply(global.wait);
   const media = await q.download();
-  conn.sendMessage(m.chat, {video: media, gifPlayback: true, caption: '*📖 La Musica Se Reproduce Cuando Abras El Archivo*'}, {quoted: m});
+  conn.sendMessage(m.chat, {video: media, gifPlayback: true, caption: '*☁️ La Musica Se Reproduce Cuando Abras El Archivo*'}, {quoted: m});
 };
 handler.command = ['togifaud'];
 export default handler;
