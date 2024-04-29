3416498c02d5048342437c4c94bb58d2285ba5a2
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-04-29 08:42:46 -0500

    Actualizar gc-kick.js

--- plugins/gc-kick.js
+++ plugins/gc-kick.js
@@ -25,7 +25,7 @@ var handler = async (m, { conn, participants, usedPrefix, command }) => {
 };
 
 handler.help = ['kick'];
-handler.tags = ['grupo'];
+handler.tags = ['group'];
 handler.command = /^(kick|echar|hechar|sacar|ban)$/i;
 handler.admin = true;
 handler.group = true;
