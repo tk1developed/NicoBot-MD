8e6a942c4a0e5e4f39e599803d956657f1d1801c
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-04-29 21:10:20 -0500

    Actualizar downloader-modapk.js

--- plugins/downloader-modapk.js
+++ plugins/downloader-modapk.js
@@ -8,7 +8,7 @@ try {
 
 let searchA = await search(text)
 let data5 = await download(searchA[0].id)
-let response = `☄️ _Descargador De Aptoide_ 📲\n\n🪐 *Nombre:* ${data5.name}\n🚩 *Package:* ${data5.package}\n🎌 *Ultima Actualización:* ${data5.lastup}\n☁️ *Tamaño:* ${data5.size}`
+let response = `☄️ Descargador De Aptoide 📲\n\n🪐 *Nombre:* ${data5.name}\n🚩 *Package:* ${data5.package}\n🎌 *Actualización:* ${data5.lastup}\n☁️ *Tamaño:* ${data5.size}`
 await conn.sendMessage(m.chat, { text: response, contextInfo: { externalAdReply: { title: data5.name, body: wm, sourceUrl: md, thumbnailUrl: data5.icon, mediaType: 1, showAdAttribution: true, renderLargerThumbnail: true }}} , { quoted: m })   
 
  if (data5.size.includes('GB') || data5.size.replace(' MB', '') > 999) {
