cd841eab31e1e8d216631c22fe608479a48f9c94
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-01 18:10:34 -0500

    Actualizar info-TyC.js

--- plugins/info-TyC.js
+++ plugins/info-TyC.js
@@ -1,7 +1,7 @@
 const handler = async (m, {conn}) => {
   m.reply(global.terminos);
 };
-handler.customPrefix = /términos y condiciones y privacidad|terminosycondicionesyprivacidad|terminosycondiciones|terminos y condiciones y privacidad|terminos y condiciones|terminos y condiciones|terminos de uso|Terminos de uso|Terminó se uso|términos de uso|Términos de uso|Términos y condiciones|tyc/i;
+handler.customPrefix = /términos y condiciones y privacidad|terminosycondicionesyprivacidad|terminosycondiciones|terminos y condiciones y privacidad|terminos y condiciones|terminos y condiciones|terminos de uso|Terminos de uso|Terminó se uso|términos de uso|Términos de uso|Términos y condiciones/i;
 handler.command = new RegExp;
 export default handler;
 
