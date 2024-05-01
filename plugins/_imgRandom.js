8b4731429f57ce4c5abb82ceea46864269ac7e04
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-01 18:32:40 -0500

    Actualizar _imgRandom.js

--- plugins/_imgRandom.js
+++ plugins/_imgRandom.js
@@ -1,3 +1,8 @@
-var img = 'https://telegra.ph/file/9a9a4851b638c90ebb214.jpg'; var img1 = 'https://telegra.ph/file/c248943164f6a3d28088c.jpg'; var img2 = 'https://telegra.ph/file/f14102fec3b4104e41e98.jpg'; var img3 = 'https://telegra.ph/file/5646b7540b22e8a00615e.jpg'; var img4 = 'https://telegra.ph/file/e6c11fc60a74a77789f56.jpg'; var img5 = 'https://telegra.ph/file/19af932324e24ceb896cd.jpg'
+let img = 'https://telegra.ph/file/9a9a4851b638c90ebb214.jpg'; 
+let img1 = 'https://telegra.ph/file/c248943164f6a3d28088c.jpg'; 
+let img2 = 'https://telegra.ph/file/f14102fec3b4104e41e98.jpg'; 
+let img3 = 'https://telegra.ph/file/5646b7540b22e8a00615e.jpg'; 
+let img4 = 'https://telegra.ph/file/e6c11fc60a74a77789f56.jpg'; 
+let img5 = 'https://telegra.ph/file/19af932324e24ceb896cd.jpg'
 
 global.ImgAll = [img, img1, img2, img3, img4, img5]
