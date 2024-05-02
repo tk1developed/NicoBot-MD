dbcc839b46182e71f43f8a7a6882d491c29868d1
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-01 23:03:00 -0500

    Actualizar downloader-facebook.js

--- plugins/downloader-facebook.js
+++ plugins/downloader-facebook.js
@@ -1,21 +1,89 @@
-let handler = async (m, { conn, command, args }) => {
-        if (!args[0]) return m.reply('Link?')
-        if (!isUrl(args[0])) return m.reply('*📌 Por favor use un Link*')
-        if (!args[0].match(/www.facebook.com|fb.watch/g)) return m.reply('*✳️Su link es inválido*\n\n📌 Por favor, use un link de Facebook\n')
-        let name = await conn.getName(m.sender)
-        let mcarga = m.reply(MultiNK.Proces(name))
-        await mcarga
-try {
-        let resfb = await fetchJson(`https://latam-api.vercel.app/api/facebookdl?apikey=${MyApiKey}&q=${args[0]}`)
-        conn.sendMessage(m.chat, { video: { url: resfb.video }, fileName: `fbsd.mp4`, mimetype: 'video/mp4' }, { quoted: m }).catch(e => {m.reply(MultiNK.Error1())})
-} catch (e) {
-m.reply(MultiNK.Error0())
-}
+import fg from 'api-dylux';
+import fetch from 'node-fetch';
+import {savefrom, facebookdl, facebookdlv2} from '@bochilteam/scraper';
+import fbDownloader from 'fb-downloader-scrapper';
+import {facebook} from '@xct007/frieren-scraper';
+import axios from 'axios';
+
+var handler = async (m, { conn, args, command, usedPrefix, text }) => {
+
+let vid
+const isCommand7 = /^(facebook|fb|facebookdl|fbdl)$/i.test(command)
+
+async function reportError(e) {
+await conn.reply(m.chat, `🛑 *Ocurrió un fallo*`, m, )
+console.log(`🛑 Error En: ${usedPrefix + command} ⚠️\n`)
+console.log(e)
 }
 
-handler.help = ['fbdl <link>']
-handler.tags = ['servicio']
-handler.command = /^(fbdl)$/i
-handler.limit = true
+switch (true) {   
+case isCommand7:
+if (!text) return conn.reply(m.chat, `🌳 *Ingrese un enlace de facebook*\n\nEjemplo, !fb https://fb.watch/kAOXy3wf2L/?mibextid=Nif5oz`, m, )
+if (!args[0].match(/www.facebook.com|fb.watch|web.facebook.com|business.facebook.com|video.fb.com/g)) return conn.reply(m.chat, '🌳 *No es un enlace válido*', m, )
+await conn.reply(m.chat, '⏰ Espere un momento', m, )
+//m.react(done)
+let messageType = checkMessageType(args[0])
+let message = ''
+switch (messageType) {
+case 'groups':
+message = 'Aqui Está Su Video ☄️'
+break
+case 'reel':
+message = 'Aqui Está Su Video ☄️'
+break
+case 'stories':
+message = 'Aqui Está Su Video ☄️'
+break
+case 'posts':
+message = 'Aqui Está Su Video ☄️'
+break
+default:
+message = 'Aqui Está Su Video ☄️'
+break
+}
+try {
+let res = await fetch(`https://api.lolhuman.xyz/api/facebook?apikey=BrunoSobrino&url=${args[0]}`)
+let _json = await res.json()
+vid = _json.result[0]
+if (vid == '' || !vid || vid == null) vid = _json.result[1]
+await conn.sendFile(m.chat, vid, 'error.mp4', `*${message}*`, m)
+} catch (error1) {
+try {
+const d2ata = await facebook.v1(args[0])
+let r2es = ''
+if (d2ata.urls && d2ata.urls.length > 0) {
+r2es = `${d2ata.urls[0]?.hd || d2ata.urls[1]?.sd || ''}`
+}
+await conn.sendFile(m.chat, r2es, 'error.mp4', `*${message}*`, m)
+} catch (error2) {
+try {
+var get = await fetch(`https://api.botcahx.live/api/dowloader/fbdown?url=${args[0]}&apikey=QaepQXxR`)
+var js = await get.json()
+await conn.sendFile(m.chat, js.result.HD, 'error.mp4', `*${message}*`, m)
+} catch (e) {
+reportError(e)}
+}}}
+
+}
+handler.help = ['fb']
+handler.tags = ['descargas']
+handler.command = /^(facebook|fb|facebookdl|fbdl)$/i
+
+handler.register = true
+handler.diamond = true
 
 export default handler
+
+function checkMessageType(url) {
+if (url.includes('www.facebook.com')) {
+if (url.includes('/groups/')) {
+return 'groups'
+} else if (url.includes('/reel/')) {
+return 'reel'
+} else if (url.includes('/stories/')) {
+return 'stories'
+} else if (url.includes('/posts/')) {
+return 'posts'
+}}
+return 'default'
+}
