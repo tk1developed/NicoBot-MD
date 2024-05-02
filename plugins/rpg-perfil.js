6c2f872b5887eff0a66480426898d07ccae6348d
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-01 20:33:20 -0500

    Actualizar rpg-perfil.js

--- plugins/rpg-perfil.js
+++ plugins/rpg-perfil.js
@@ -11,7 +11,7 @@ let username = conn.getName(who)
 let name = conn.getName(who)
 let fkon = { key: { fromMe: false, participant: `${m.sender.split`@`[0]}@s.whatsapp.net`, ...(m.chat ? { remoteJid: '16504228206@s.whatsapp.net' } : {}) }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
 
-let str = ` 𝗣𝗘𝗥𝗙𝗜𝗟 𝗨𝗦𝗨𝗔𝗥𝗜𝗢 🪷
+let str = ` 𝗣 𝗘 𝗥 𝗙 𝗜 𝗟 » 𝗨 𝗦 𝗘 𝗥 🪷\n
 🌳 • *Nombre:* ${username}
 ☄️ • *Tag:* @${who.replace(/@.+/, '')}
 📞 • *Numero:* ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}
