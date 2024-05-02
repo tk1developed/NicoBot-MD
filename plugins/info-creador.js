951c3f2bc755ae838fa0df509b4ec195541985f6
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-01 22:41:59 -0500

    Actualizar info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,25 +1,62 @@
-import fetch from 'node-fetch'
+import PhoneNumber from 'awesome-phonenumber'
 
-var handler = async (m, { conn, usedPrefix, text, args, command }) => {
-
+let handler = async (m, { conn, usedPrefix, text, args, command }) => {
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-let pp = await conn.profilePictureUrl(who).catch(_ => '')
-let name = await conn.getName(who)
-let biografia = await conn.fetchStatus('5217294888993' +'@s.whatsapp.net').catch(_ => 'Sin Biografía')
-let biografiaBot = await conn.fetchStatus('5214531287294' +'@s.whatsapp.net').catch(_ => 'Sin Biografía')
+let pp = await conn.profilePictureUrl(who).catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png')
+let biografia = await conn.fetchStatus('573012482597' +'@s.whatsapp.net').catch(_ => 'Sin Biografía')
+let biografiaBot = await conn.fetchStatus('5219512151615' +'@s.whatsapp.net').catch(_ => 'Sin Biografía')
 let bio = biografia.status?.toString() || 'Sin Biografía'
 let biobot = biografiaBot.status?.toString() || 'Sin Biografía'
+let name = await conn.getName(who)
+const nomorown = "573012482597"
 
-m.reply(conn, m.chat, [
-    [`${nomorown}`, `${await conn.getName('5214434703586'+'@s.whatsapp.net')}`, `🍭 Creador`, saludo, 'argumedod44@gmail.com', `🇲🇽 México`, `https://www.youtube.com/@Azami_YT`, bio],
+  await sendContactArray(conn, m.chat, [
+     [`${nomorown}`, `${await conn.getName('5214434703586'+'@s.whatsapp.net')}`, `🍭 Creador`, saludo, 'argumedod44@gmail.com', `🇲🇽 México`, `https://www.youtube.com/@Azami_YT`, bio],
 [`${conn.user.jid.split('@')[0]}`, `${await conn.getName(conn.user.jid)}`, `🍧 CuriosityBot-MD`, `📵 No Hacer Spam`, 'theyotsubanakano@gmail.com', `🇲🇽 México`, `https://github.com/AzamiJs/CuriosityBot-MD`, biobot]
 ], m)
+  //m.reply(`Hola @${m.sender.split(`@`)[0]} este es el contacto de mi creadora, no hagas spam!!`)
+  } 
 
-}
-handler.help = ['owner', 'contacto', 'creador', 'contactos']
-handler.tags = ['info']
-handler.command = /^(owner|contacto|creador|contactos)/i
-
-handler.register = true
-
+handler.help = ["creador","owner"]
+handler.tags = ["owner"]
+handler.command = /^(owner|creador)$/i
 export default handler
+
+async function sendContactArray(conn, jid, data, quoted, options) {
+        if (!Array.isArray(data[0]) && typeof data[0] === 'string') data = [data]
+                let contacts = []
+        for (let [number, name, isi, isi1, isi2, isi3, isi4, isi5] of data) {
+            number = number.replace(/[^0-9]/g, '')
+            let njid = number + '@s.whatsapp.net'
+            let biz = await conn.getBusinessProfile(njid).catch(_ => null) || {}
+            // N:;${name.replace(/\n/g, '\\n').split(' ').reverse().join(';')};;;
+            let vcard = `
+BEGIN:VCARD
+VERSION:3.0
+N:Sy;Bot;;;
+FN:${name.replace(/\n/g, '\\n')}
+item.ORG:${isi}
+item1.TEL;waid=${number}:${PhoneNumber('+' + number).getNumber('international')}
+item1.X-ABLabel:${isi1}
+item2.EMAIL;type=INTERNET:${isi2}
+item2.X-ABLabel:📧 Email
+item3.ADR:;;${isi3};;;;
+item3.X-ABADR:ac
+item3.X-ABLabel:📍 Region
+item4.URL:${isi4}
+item4.X-ABLabel:Website
+item5.X-ABLabel:${isi5}
+END:VCARD`.trim()
+            contacts.push({ vcard, displayName: name })
+        }
+        return await conn.sendMessage(jid, {
+            contacts: {
+                displayName: (contacts.length > 1 ? `2013 kontak` : contacts[0].displayName) || null,
+                contacts,
+            }
+        },
+        {
+            quoted,
+            ...options
+        })
+        }
