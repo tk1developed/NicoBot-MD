176bc29011f74c2e4e70c481bad0f84b6720d47f
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-03 19:48:10 -0500

    Actualizar bot-menuprincipal.js

--- plugins/bot-menuprincipal.js
+++ plugins/bot-menuprincipal.js
@@ -178,10 +178,12 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
 
 
-   let pp1 = 'https://telegra.ph/file/37a95abc27aabd0a17bd7.mp4'
+   /*let pp1 = 'https://telegra.ph/file/37a95abc27aabd0a17bd7.mp4'
     let pp2 = 'https://telegra.ph/file/fc2f8abffe802f496551a.mp4'
     let pp3 = 'https://telegra.ph/file/52c004a4d1506b02165b1.mp4'
-    let pp4 = 'https://telegra.ph/file/1634b24b028638c4067ae.mp4'
+    let pp4 = 'https://telegra.ph/file/1634b24b028638c4067ae.mp4'*/
+
+let imagen = 'https://telegra.ph/file/4c5e630407258a7b08601.jpg'
 
   const pp = await conn.profilePictureUrl(conn.user.jid).catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png');
 
@@ -195,7 +197,7 @@ await conn.sendMessage(m.chat, { react: { text: '💫', key: m.key } })
 
     //conn.sendMessage(m.chat, { video: { url: [pp1, pp2, pp3, pp4].getRandom() }, gifPlayback: true, caption: text.trim(), mentions: [m.sender] }, { quoted: fkontak })
 
-conn.sendMessage(m.chat, {text: text.trim(), mentions: [...text.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), contextInfo: { mentionedJid: [...text.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "renderLargerThumbnail": true, "title": packname, body: team, "containsAutoReply": true, "mediaType": 1, "thumbnail": imagen6, "mediaUrl": global.channel, "sourceUrl": global.channel}}}, {quoted: fkontak});
+conn.sendMessage(m.chat, {text: text.trim(), mentions: [...text.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), contextInfo: { mentionedJid: [...text.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "renderLargerThumbnail": true, "title": packname, body: team, "containsAutoReply": true, "mediaType": 1, "thumbnail": imagen, "mediaUrl": global.channel, "sourceUrl": global.channel}}}, {quoted: fkontak});
 
    
 
