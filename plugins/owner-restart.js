ff1a702c6d3d41c3dd36748c5142db884d3f4a95
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-03 22:47:40 -0500

    Actualizar owner-restart.js

--- plugins/owner-restart.js
+++ plugins/owner-restart.js
@@ -2,7 +2,7 @@ import { spawn } from 'child_process'
 let handler = async (m, { conn, isROwner, text }) => {
     if (!process.send) throw 'Dont: node main.js\nDo: node index.js'
     if (conn.user.jid == conn.user.jid) {
-    const { key } = await conn.sendMessage(m.chat, {text: `🚀🚀`}, {quoted: m, fake, });
+    const { key } = await conn.sendMessage(m.chat, {text: `🚀🚀`}, {quoted: fake});
 await delay(1000 * 1);
 await conn.sendMessage(m.chat, {text: `🚀🚀🚀🚀`, edit: key});
 await delay(1000 * 1);
