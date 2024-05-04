7692f5bb51bcfa6f981382f6ba10c30c3eb4264d
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-04 08:31:51 -0500

    Actualizar owner-update.js

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -7,7 +7,7 @@ const stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''));
 let messager = stdout.toString()
 if (messager.includes('Already up to date.')) messager = '╰⊱🧿⊱ *𝗜𝗡𝗙𝗢𝗥𝗠𝗔𝗖𝗜𝗢́𝗡* ⊱🧿⊱╮\n\n*𝖸𝖠 𝖤𝖲𝖳𝖮𝖸 𝖠𝖢𝖳𝖴𝖠𝖫𝖨𝖹𝖠𝖣𝖠 𝖠 𝖫𝖠 𝖵𝖤𝖱𝖲𝖨𝖮́𝖭 𝖬𝖠𝖲 𝖱𝖤𝖢𝖨𝖤𝖭𝖳𝖤.*'
 if (messager.includes('Updating')) messager = '✅️ *Actualizacion Exitosa.*\n\n' + stdout.toString()
-conn.reply(m.chat, m, fake, messager,)
+conn.reply(m.chat, messager, m, fake,)
 } catch { 
 try {
 const status = execSync('git status --porcelain')
@@ -19,7 +19,7 @@ return null
 return '*→ ' + line.slice(3) + '*'}).filter(Boolean)
 if (conflictedFiles.length > 0) {
 const errorMessage = `🚩 *Se Han Hecho Cambios Locales En Archivos Del Bot Que Entran En Conflicto Con Las Actualizaciones Del Repositorio. Para Actualizar, Reinstala El Bot o Realiza Las Actualizaciones Manualmente*\n\nArchivos En Conflicto:\n\n${conflictedFiles.join('\n')}`
-await conn.reply(m.chat, m, fake, errorMessage,)
+await conn.reply(m.chat, errorMessage, m, fake,)
 }
 }
 } catch (error) {
@@ -28,7 +28,7 @@ let errorMessage2 = '📍 *Ocurrió Un Error.*'
 if (error.message) {
 errorMessage2 += '\n*- Mensaje de error:* ' + error.message;
 }
-await conn.reply(m.chat, m, fake, errorMessage2,)
+await conn.reply(m.chat, errorMessage2, m, fake,)
 }
 }
 
