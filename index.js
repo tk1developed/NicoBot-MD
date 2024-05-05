3f3121caf505a2b555a812859b9b9e33a147dcf7
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-05 13:24:06 -0500

    Actualizar index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const rl = createInterface(process.stdin, process.stdout)
 say('Yotsuba\nNakano\nMD', {
 font: 'block',
 align: 'center',
-colors: ['cyan', 'blue']
+colors: ['magenta', 'green']
 })
 say(`Bot En Desarrollo, By: Diego Oficial`, {
 font: 'console',
