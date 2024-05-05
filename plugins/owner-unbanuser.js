d09410c915964ee49b3e088f33e2617a4693d193
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-04 23:12:45 -0500

    Actualizar owner-unbanuser.js

--- plugins/owner-unbanuser.js
+++ plugins/owner-unbanuser.js
@@ -1,8 +1,24 @@
-var handler = async (m, {conn, text}) => {
+const handler = async (m, {conn, text}) => {
+  if (!text) throw '🧸 *Etiqueta A Un Usuario*';
+  let who;
+  if (m.isGroup) who = m.mentionedJid[0];
+  else who = m.chat;
+  if (!who) throw '🧸 *Etiqueta A Un Usuario*';
+  const users = global.db.data.users;
+  users[who].banned = false;
+  conn.reply(m.chat, `✅️ *Se Desbaneo Al Usuario Con Éxito*`, m);
+};
+handler.help = ['unbanuser'];
+handler.tags = ['owner'];
+handler.command = /^unbanuser$/i;
+handler.rowner = true;
+export default handler;
+
+/*let handler = async (m, {conn, text}) => {
 
 let user = conn.user.jid.split`@`[0] + '@s.whatsapp.net'
-let bant = `🏷 *Etiquete a una persona*`
-if (!text) return conn.reply(m.chat, bant, m, fake, )
+let bant = `📍 *Etiquete a una persona*`
+if (!text) return conn.reply(m.chat, bant,)
 let who
 if (m.isGroup) who = m.mentionedJid[0]
 else who = m.chat
@@ -19,4 +35,4 @@ handler.command = /^unbanuser$/i
 
 handler.rowner = true
 
-export default handler
+export default handler*/
