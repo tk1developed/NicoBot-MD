c4c3896fee3b49d48cc25fe41b7173cbf4ad51d0
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-04 22:13:24 -0500

    Actualizar gc-demote.js

--- plugins/gc-demote.js
+++ plugins/gc-demote.js
@@ -1,34 +1,38 @@
-const handler = async (m, {conn, usedPrefix, text}) => {
-  if (isNaN(text) && !text.match(/@/g)) {
+var handler = async (m, { conn,usedPrefix, command, text }) => {
 
-  } else if (isNaN(text)) {
-    var number = text.split`@`[1];
-  } else if (!isNaN(text)) {
-    var number = text;
-  }
+if (isNaN(text) && !text.match(/@/g)){
 
-  if (!text && !m.quoted) return conn.reply(m.chat, `*📍 USO APROPIADO*\n\n*┯┷*\n*┠≽ ${usedPrefix}quitaradmin @tag*\n*┠≽ ${usedPrefix}quitaradmin -> responder a un mensaje*\n*┷┯*`, m, fake, );
-  if (number.length > 13 || (number.length < 11 && number.length > 0)) return conn.reply(m.chat, `🧸 *Numero Que Ingresastes Es Incorrecto*`, m, fake, );
+} else if (isNaN(text)) {
+var number = text.split`@`[1]
+} else if (!isNaN(text)) {
+var number = text
+}
 
-  try {
-    if (text) {
-      var user = number + '@s.whatsapp.net';
-    } else if (m.quoted.sender) {
-      var user = m.quoted.sender;
-    } else if (m.mentionedJid) {
-      var user = number + '@s.whatsapp.net';
-    }
-  } catch (e) {
-  } finally {
-    conn.groupParticipantsUpdate(m.chat, [user], 'demote');
-    conn.reply(m.chat, `✅️ *Ordenes Recibidas*`, m, fake, );
-  }
-};
-handler.help = ['*593xxx*', '*@usuario*', '*responder chat*'].map((v) => 'demote ' + v);
-handler.tags = ['group'];
-handler.command = /^(demote|quitarpoder|quitaradmin)$/i;
-handler.group = true;
-handler.admin = true;
-handler.botAdmin = true;
-handler.fail = null;
-export default handler;
+if (!text && !m.quoted) return conn.reply(m.chat, `🏷 *Etiqueta al admin. que desea degradar*\n\nEjemplo, !demote @tag\n!demote *responde a un mensaje*`, m, fake, )
+if (number.length > 13 || (number.length < 11 && number.length > 0)) return conn.reply(m.chat, `🌼 *Número erróneo*`, m, fake, )
+
+try {
+if (text) {
+let user = number + '@s.whatsapp.net'
+} else if (m.quoted.sender) {
+let user = m.quoted.sender
+} else if (m.mentionedJid) {
+let user = number + '@s.whatsapp.net'
+} 
+} catch (e) {
+} finally {
+conn.groupParticipantsUpdate(m.chat, [user], 'demote')
+conn.reply(m.chat, `✅ *Se degrado de admin con éxito*`, m, fake, )
+}
+
+}
+handler.help = ['demote']
+handler.tags = ['grupo']
+handler.command = ['demote', 'degradar']
+
+handler.group = true
+handler.admin = true
+handler.botAdmin = true
+handler.fail = null
+
+export default handler
