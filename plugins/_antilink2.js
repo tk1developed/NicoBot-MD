48838e7588666b541e4c638ddb83e6c1a4900f98
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-04 21:02:02 -0500

    Actualizar _antilink2.js

--- plugins/_antilink2.js
+++ plugins/_antilink2.js
@@ -1,32 +1,30 @@
-// manden porno test
-const linkRegex = /https:/i;
-export async function before(m, {conn, isAdmin, isBotAdmin, text}) {
-  if (m.isBaileys && m.fromMe) {
-    return !0;
-  }
-  if (!m.isGroup) return !1;
-  const chat = global.db.data.chats[m.chat];
-  const delet = m.key.participant;
-  const bang = m.key.id;
-  const bot = global.db.data.settings[this.user.jid] || {};
-  const user = `@${m.sender.split`@`[0]}`;
-  const isGroupLink = linkRegex.exec(m.text);
-  if (chat.antiLink2 && isGroupLink && !isAdmin) {
-    if (isBotAdmin) {
-      const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`;
-      const linkThisGroup2 = `https://www.youtube.com/`;
-      const linkThisGroup3 = `https://youtu.be/`;
-      if (m.text.includes(linkThisGroup)) return !0;
-      if (m.text.includes(linkThisGroup2)) return !0;
-      if (m.text.includes(linkThisGroup3)) return !0;
-    }
-    await this.sendMessage(m.chat, {text: `*¡Enlace detectado!, Mandaste un enlace prohibido por lo cual seras eliminado*`, mentions: [m.sender]}, {quoted: m});
-    if (!isBotAdmin) return m.reply('🌹 *No soy admin, no puedo eliminar intrusos*');
-    if (isBotAdmin && bot.restrict) {
-      await conn.sendMessage(m.chat, {delete: {remoteJid: m.chat, fromMe: false, id: bang, participant: delet}});
-      const responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
-      if (responseb[0].status === '404') return;
-    } else if (!bot.restrict) return m.reply('*¡Esta característica esta desactivada!*');
-  }
-  return !0;
+let linkRegex = /https:/i
+
+export async function before(m, { isAdmin, isBotAdmin,text }) {
+
+if (m.isBaileys && m.fromMe)
+return !0
+if (!m.isGroup) return !1
+let chat = global.db.data.chats[m.chat]
+let bot = global.db.data.settings[this.user.jid] || {}
+const isGroupLink = linkRegex.exec(m.text)
+if (chat.antiLink2 && isGroupLink && !isAdmin) {
+if (isBotAdmin) {
+const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
+const linkThisGroup2 = `https://www.youtube.com/`
+const linkThisGroup3 = `https://youtu.be/`
+const linkThisGroup4 = `https://whatsapp.com/channel/`
+if (m.text.includes(linkThisGroup)) return !0
+if (m.text.includes(linkThisGroup2)) return !0
+if (m.text.includes(linkThisGroup3)) return !0
+if (m.text.includes(linkThisGroup4)) return !0
+}
+await conn.reply(m.chat, `😎 *¡Enlace detectado!*\n\n*${await this.getName(m.sender)} rompiste las reglas, seras eliminado*`, m, fake, )
+if (!isBotAdmin) return conn.reply(m.chat, `🏷 *No soy admin, no puedo eliminar intrusos*`, m, fake, )
+if (isBotAdmin && bot.restrict) {
+await conn.groupParticipantsUpdate(m.chat, [m.sender],'remove')
+} else if (!bot.restrict) return conn.reply(m.chat, `*¡Esta característica esta desactivada!*`, m, fake, )
+}
+return !0
+
 }
