1fc338711d3f780047016886d677dcc8975ebf65
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-05 13:41:08 -0500

    Actualizar index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const rl = createInterface(process.stdin, process.stdout)
 say('Yotsuba\nNakano\nMD', {
 font: 'block',
 align: 'center',
-colors: ['system', 'redBright']
+colors: ['whiteBright', 'redBright']
 })
 say(`by: yotsuba nakano, bot en desarrollo`, {
 font: 'console',
