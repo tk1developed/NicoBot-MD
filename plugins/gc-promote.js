8c5b2c2d766ea676eba4e8da31c906a9cc841982
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-04 23:12:21 -0500

    Actualizar gc-promote.js

--- plugins/gc-promote.js
+++ plugins/gc-promote.js
@@ -1,38 +1,34 @@
-var handler = async (m, { conn,usedPrefix, command, text }) => {
+const handler = async (m, {conn, usedPrefix, text}) => {
+  if (isNaN(text) && !text.match(/@/g)) {
 
-if (isNaN(text) && !text.match(/@/g)){
+  } else if (isNaN(text)) {
+    var number = text.split`@`[1];
+  } else if (!isNaN(text)) {
+    var number = text;
+  }
 
-} else if (isNaN(text)) {
-var number = text.split`@`[1]
-} else if (!isNaN(text)) {
-var number = text
-}
+  if (!text && !m.quoted) return conn.reply(m.chat, `*📍 USO APROPIADO*\n\n*┯┷*\n*┠≽ ${usedPrefix}daradmin @tag*\n*┠≽ ${usedPrefix}darpoder -> responder a un mensaje*\n*┷┯*`, m);
+  if (number.length > 13 || (number.length < 11 && number.length > 0)) return conn.reply(m.chat, `*[ ⚠️ ] El número ingresado es incorrecto, por favor ingrese el número correcto*`, m);
 
-if (!text && !m.quoted) return conn.reply(m.chat, `🏷 *Etiqueta al usuario. que desea promover a admin*\n\nEjemplo, !demote @tag\n!demote *responde a un mensaje*`, m, fake, )
-if (number.length > 13 || (number.length < 11 && number.length > 0)) return conn.reply(m.chat, `🌼 *Número erróneo*`, m, fake, )
-
-try {
-if (text) {
-var user = number + '@s.whatsapp.net'
-} else if (m.quoted.sender) {
-var user = m.quoted.sender
-} else if (m.mentionedJid) {
-var user = number + '@s.whatsapp.net'
-} 
-} catch (e) {
-} finally {
-conn.groupParticipantsUpdate(m.chat, [user], 'promote')
-conn.reply(m.chat, `✅ *Se promovió a admin con éxito*`, m, fake, )
-}
-
-}
-handler.help = ['promote']
-handler.tags = ['grupo']
-handler.command = ['promote', 'promover']
-
-handler.group = true
-handler.admin = true
-handler.botAdmin = true
-handler.fail = null
-
-export default handler
+  try {
+    if (text) {
+      var user = number + '@s.whatsapp.net';
+    } else if (m.quoted.sender) {
+      var user = m.quoted.sender;
+    } else if (m.mentionedJid) {
+      var user = number + '@s.whatsapp.net';
+    }
+  } catch (e) {
+  } finally {
+    conn.groupParticipantsUpdate(m.chat, [user], 'promote');
+    conn.reply(m.chat, `✅️ Ordenes Recibidas`, m);
+  }
+};
+handler.help = ['*593xxx*', '*@usuario*', '*responder chat*'].map((v) => 'promote ' + v);
+handler.tags = ['group'];
+handler.command = /^(promote|daradmin|darpoder)$/i;
+handler.group = true;
+handler.admin = true;
+handler.botAdmin = true;
+handler.fail = null;
+export default handler;
