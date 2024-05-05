fbbd80c552e81951d9729da8354106c4435f96ba
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-05 13:44:28 -0500

    Actualizar index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log('☄️ Iniciando Yotsuba...')
+console.log('❤ Iniciando Yotsuba...')
 import { join, dirname } from 'path'
 import { createRequire } from 'module'
 import { fileURLToPath } from 'url'
