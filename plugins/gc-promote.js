de2e0cc23340c3d49dab26188bfa97c82f75a80c
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-04 22:12:24 -0500

    Actualizar gc-promote.js

--- plugins/gc-promote.js
+++ plugins/gc-promote.js
@@ -3,9 +3,9 @@ let handler = async (m, { conn,usedPrefix, command, text }) => {
 if (isNaN(text) && !text.match(/@/g)){
 
 } else if (isNaN(text)) {
-var number = text.split`@`[1]
+let number = text.split`@`[1]
 } else if (!isNaN(text)) {
-var number = text
+let number = text
 }
 
 if (!text && !m.quoted) return conn.reply(m.chat, `🏷 *Etiqueta al usuario. que desea promover a admin*\n\nEjemplo, !demote @tag\n!demote *responde a un mensaje*`, m, fake, )
@@ -13,11 +13,11 @@ if (number.length > 13 || (number.length < 11 && number.length > 0)) return conn
 
 try {
 if (text) {
-var user = number + '@s.whatsapp.net'
+let user = number + '@s.whatsapp.net'
 } else if (m.quoted.sender) {
-var user = m.quoted.sender
+let user = m.quoted.sender
 } else if (m.mentionedJid) {
-var user = number + '@s.whatsapp.net'
+let user = number + '@s.whatsapp.net'
 } 
 } catch (e) {
 } finally {
