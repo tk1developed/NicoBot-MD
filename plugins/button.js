df14e56a370fbdcb7c0c40f7b4787b29691cc96a
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-05 20:57:46 -0500

    Actualizar button.js

--- plugins/button.js
+++ plugins/button.js
@@ -3,11 +3,11 @@ let handler = async (m, { conn, command }) => {
 let user = db.data.users[m.sender]
 let text = `❤️ Waoz Ese Es Mi Propietario. 🌼`
 await conn.sendButton(m.chat, text, `👋🏻 ¡Hola! ` + nombre, foto, [
-['⌨️ Menu Oficial ⌨️', '!allmenu'],
-['🍧 Waoz', '!sc'],
-['❤️ Propietario', '#owner'],
-['🌼 Bot', '!bot Hola']], null, [
-['🇦🇲 Canal', `${channel}`]], m)}
+['🎋 𝗠𝗲𝗻𝘂 𝗖𝗼𝗺𝗽𝗹𝗲𝘁𝗼 💫', '!allmenu'],
+['🌻 𝗦𝗰𝗿𝗶𝗽𝘁 🔥', '!sc'],
+['❤️ 𝗣𝗿𝗼𝗽𝗶𝗲𝘁𝗮𝗿𝗶𝗼 🍄', '#owner'],
+['🌼 𝗕𝗼𝘁 🏝️', '!bot Hola']], null, [
+['🇦🇲 𝗖𝗮𝗻𝗮𝗹 𝗢𝗳𝗶𝗰𝗶𝗮𝗹 🌹', `${channel}`]], m)}
 handler.customPrefix = /Diego|diego/i;
 handler.command = new RegExp;
 export default handler;
