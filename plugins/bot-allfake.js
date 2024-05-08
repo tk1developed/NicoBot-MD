dbc0c0207501a74448d73ccf6e94ea25a26f9d01
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-07 22:33:39 -0500

    Actualizar bot-allfake.js

--- plugins/bot-allfake.js
+++ plugins/bot-allfake.js
@@ -35,7 +35,7 @@ let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? t
 Mensajes Fakes
 */
 
-global.biot = bio.status?.toString() || '*Sin Bio ×*'
+//global.biot = bio.status?.toString() || '*Sin Bio ×*'
 global.bio = await conn.fetchStatus(who).catch(_ => 'undefined')
 
 global.fkontak = { key: { participants:"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
