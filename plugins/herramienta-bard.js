0d82d47b2ebbed48ad844bdf101ba6c5a7c0c6e8
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-10 16:54:02 -0500

    Actualizar herramienta-bard.js

--- plugins/herramienta-bard.js
+++ plugins/herramienta-bard.js
@@ -1,4 +1,26 @@
+import fetch from 'node-fetch'
 var handler = async (m, { text,  usedPrefix, command }) => {
+if (!text) throw `🍃 *Ingres Una Petición*\n- Ejemplo: !bard conoces a yotsuba?`
+try {
+await m.reply('🌺 *C A R G A N D O*\n- 🍃 Cargando Información')
+conn.sendPresenceUpdate('composing', m.chat);
+var apii = await fetch(`https://aemt.me/gemini?text=${text}`)
+var res = await apii.json()
+await m.reply(res.result)
+} catch (e) {
+await conn.reply(m.chat, `*🍃 Ocurrió Un Error Inesperado*`, fkontak, m)
+console.log(`❗`)
+console.log(e)
+}}
+handler.command = ['bard', 'gemini']
+handler.help = ['bard', 'gemini']
+handler.tags = ['herramientas']
+
+handler.premium = false
+
+export default handler
+
+/*var handler = async (m, { text,  usedPrefix, command }) => {
 
 if (!text) throw `🍃 *Ingres Una Petición*\n- Ejemplo: !bard conoces a yotsuba?`
 
@@ -22,4 +44,4 @@ handler.tags = ['ai']
 
 handler.premium = false
 
-export default handler
+export default handler*/
