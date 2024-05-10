68375f7ce34c384197e76af4b240b73814182d1d
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-10 17:14:15 -0500

    Actualizar owner-saveplugin.js

--- plugins/owner-saveplugin.js
+++ plugins/owner-saveplugin.js
@@ -1,5 +1,21 @@
 import fs from 'fs'
 
+let handler = async (m, { text, usedPrefix, command }) => {
+
+
+    if (!text) return conn.reply(m.chat, `☄️ *Ingresé el nombre del plugin*`, m, fake, )
+    if (!m.quoted.text) return conn.reply(m.chat, `🍃 *Ingresa el contenido del plugin*`, m, fake, )
+    let path = `plugins/${text}.js`
+    await fs.writeFileSync(path, m.quoted.text)
+    await conn.reply(m.chat, `🌺 *Guardado en* ${path}`)
+}
+handler.help = ['saveplugin']
+handler.tags = ['owner']
+handler.command = ['salvar', 'saveplugin']
+
+
+/*import fs from 'fs'
+
 var handler = async (m, { text, usedPrefix, command }) => {
 
 if (!text) return conn.reply(m.chat, `☄️ *Ingresé el nombre del plugin*`, m, fake, )
@@ -15,4 +31,4 @@ handler.command = ['salvar', 'saveplugin']
 
 handler.rowner = true
 
-export default handler
+export default handler*/
