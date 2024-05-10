3588f5dfd77dc661a0a7e9ee52b7790cff409031
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-10 17:03:31 -0500

    Actualizar herramientas-chatgpt.js

--- plugins/herramientas-chatgpt.js
+++ plugins/herramientas-chatgpt.js
@@ -1,29 +1,4 @@
-import fetch from 'node-fetch';
-import axios from 'axios';
-import translate from '@vitalets/google-translate-api';
-import {Configuration, OpenAIApi} from 'openai';
-const configuration = new Configuration({organization: global.openai_org_id, apiKey: global.openai_key});
-const openaiii = new OpenAIApi(configuration);
-const handler = async (m, {conn, text, usedPrefix, command}) => {
-if (usedPrefix == 'a' || usedPrefix == 'A') return;
-if (!text) throw `🍃 *Ingrese una petición*\n\nEjemplo, !${command} Codigo en Js para un juego de sopa de letras`, m, fake, )   
-try {
-//await m.reply('🌺 *C A R G A N D O*\n- 🍃 Cargando Información')
-conn.sendPresenceUpdate('composing', m.chat);
-let gpt = await fetch(`https://delirius-api-oficial.vercel.app/api/ia2?text=${text}`)
-let res = await gpt.json()
-await m.reply(res.gpt)
-} catch {
-try {
-let gpt = await fetch(`https://delirius-api-oficial.vercel.app/api/chatgpt?q=${text}`)
-let res = await gpt.json()
-await m.reply(res.data)
-} catch {
-}}}
-handler.command = /^(openai|chatgpt|ia|ai|openai2|chatgpt2|ia2)$/i;
-export default handler;
-
-/*import fetch from 'node-fetch'
+import fetch from 'node-fetch'
 import axios from 'axios'
 import translate from '@vitalets/google-translate-api'
 import {Configuration, OpenAIApi} from 'openai'
@@ -34,7 +9,7 @@ const openaiii = new OpenAIApi(configuration)
 var handler = async (m, {conn, text, usedPrefix, command}) => {
 
 if (usedPrefix == 'a' || usedPrefix == 'A') return
-if (!text) return conn.reply(m.chat, `🍃 *Ingrese una petición*\n\nEjemplo, !${command} Codigo en JS para un juego de cartas`, m, fake, ) 
+if (!text) return conn.reply(m.chat, `🍃 *Ingrese una petición*\n\nEjemplo, !${command} Codigo en JS para un juego de ahorcado`, m, fake, ) 
 
 try {
 
@@ -75,4 +50,4 @@ handler.help = ['openia', 'chatgpt', 'ia']
 handler.tags = ['ai']
 handler.command = /^(openai|chatgpt|ia)$/i
 
-export default handler*/
+export default handler
