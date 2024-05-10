f93e03ff685df6c16dc5a4345d512a2baa23b59d
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-10 11:21:15 -0500

    Actualizar mipilot-serbot.js

--- plugins/mipilot-serbot.js
+++ plugins/mipilot-serbot.js
@@ -15,30 +15,27 @@
    const { smsg, getGroupAdmins, formatp, tanggal, formatDate, getTime, isUrl, sleep, clockString, runtime, fetchJson, getBuffer, jsonformat, delay, format, logic, generateProfilePicture, parseMention, getRandom } = require('./libs/fuctions')   
    const store = makeInMemoryStore({ logger: pino().child({ level: 'silent', stream: 'storeV2' }) })   
   
-   function _0x54fa(){const _0x106064=['4187540nMOynD','1155130VAHlms','22888ZMdXIK','12hFUqCZ','944120uVItCj','121baJSyN','1743255bxMEIf','SmFkaWJvdCBoZWNobyBwb3IgQFNraWR5ODkNCmh0dHBzOi8vd3d3LmdpdGh1Yi5jb20vU2tpZHk4OQ==','No\x20vas\x20a\x20encontrar\x20nada\x20aquí','toString','utf-8','12OuSXkS','base64','7DUbchH','length','5306076HVURFf','131624agPHBy','from','log'];_0x54fa=function(){return _0x106064;};return _0x54fa();}const _0x3d887f=_0x2ddf;function _0x2ddf(_0x510093,_0x1c7fde){const _0x54fa22=_0x54fa();return _0x2ddf=function(_0x2ddfde,_0x3a533d){_0x2ddfde=_0x2ddfde-0x150;let _0x499087=_0x54fa22[_0x2ddfde];return _0x499087;},_0x2ddf(_0x510093,_0x1c7fde);}(function(_0x5e184c,_0x30bfc9){const _0x363293=_0x2ddf,_0x365532=_0x5e184c();while(!![]){try{const _0x219e94=parseInt(_0x363293(0x158))/0x1*(parseInt(_0x363293(0x162))/0x2)+-parseInt(_0x363293(0x153))/0x3+parseInt(_0x363293(0x160))/0x4+-parseInt(_0x363293(0x161))/0x5*(-parseInt(_0x363293(0x150))/0x6)+parseInt(_0x363293(0x15a))/0x7*(-parseInt(_0x363293(0x15d))/0x8)+parseInt(_0x363293(0x15c))/0x9+-parseInt(_0x363293(0x151))/0xa*(parseInt(_0x363293(0x152))/0xb);if(_0x219e94===_0x30bfc9)break;else _0x365532['push'](_0x365532['shift']());}catch(_0x3c86e1){_0x365532['push'](_0x365532['shift']());}}}(_0x54fa,0x926cf));const eFzr2fuYZzPw6RK5LbDS1pZA1JU=_0x3d887f(0x154),b3iQ8ajbRZO9zF6gLzObRy50vEE=Buffer[_0x3d887f(0x15e)](eFzr2fuYZzPw6RK5LbDS1pZA1JU,_0x3d887f(0x159)),pInYhIZYYC2B5C4xQpyJmufq2LC=b3iQ8ajbRZO9zF6gLzObRy50vEE[_0x3d887f(0x156)](_0x3d887f(0x157));for(let i=0x0;i<0xa;i++){i===pInYhIZYYC2B5C4xQpyJmufq2LC[_0x3d887f(0x15b)]&&console[_0x3d887f(0x15f)](_0x3d887f(0x155));}
+   function _0x178d(_0x41b38e,_0x277ee2){const _0x5c4486=_0x5c44();return _0x178d=function(_0x178d2e,_0x1c77de){_0x178d2e=_0x178d2e-0x1c9;let _0x48448d=_0x5c4486[_0x178d2e];return _0x48448d;},_0x178d(_0x41b38e,_0x277ee2);}const _0x5f3d86=_0x178d;(function(_0x1aa342,_0x33ce77){const _0x18d87d=_0x178d,_0x1e47af=_0x1aa342();while(!![]){try{const _0x6853ac=-parseInt(_0x18d87d(0x1d0))/0x1+parseInt(_0x18d87d(0x1ce))/0x2*(parseInt(_0x18d87d(0x1cf))/0x3)+parseInt(_0x18d87d(0x1cd))/0x4+-parseInt(_0x18d87d(0x1d2))/0x5*(parseInt(_0x18d87d(0x1d3))/0x6)+parseInt(_0x18d87d(0x1cc))/0x7+parseInt(_0x18d87d(0x1c9))/0x8+-parseInt(_0x18d87d(0x1ca))/0x9;if(_0x6853ac===_0x33ce77)break;else _0x1e47af['push'](_0x1e47af['shift']());}catch(_0xea8b06){_0x1e47af['push'](_0x1e47af['shift']());}}}(_0x5c44,0x8237f));const crm1=_0x5f3d86(0x1d1),crm2=Buffer['from'](crm1,_0x5f3d86(0x1d4));function _0x5c44(){const _0x54eb2e=['888075GiUqMS','SmFkaWJvdCBoZWNobyBwb3IgQFNraWR5ODkNCmh0dHBzOi8vd3d3LmdpdGh1Yi5jb20vU2tpZHk4OQ==','44670WNmdru','72CWneRO','base64','310544bjLDgo','2670768XbDUoT','utf-8','4505760jpzZqf','2124404DdLGAR','407874KrBzjH','9EYelZD'];_0x5c44=function(){return _0x54eb2e;};return _0x5c44();}let pInYhIZYYC2B5C4xQpyJmufq2LC=crm2['toString'](_0x5f3d86(0x1cb));
    
-   let rtx = `/3
+   let rtx = `
 ╭━━━━━━━━━━━━━╮
-
 │ 
-│ Escanea este QR para convertirte en un bot temporal
+│ Usa este codigo para conectarte a Shadow bot!!
 │
 │ 1. Haz clic en los tres puntos en la esquina superior derecha
 │ 2. Toca WhatsApp Web
-│ 3. Escanea este QR
+│ 3. da click en vincular con codigo de teléfono 
+│ 4. pega el codigo a continuación
+│ 
 │
-│ *El QR expira a los 30 segundos*
-│
-╰━━━━━━━━━━━━━╯`
+╰━━━━━━━━━━━━━╯\n\n`
+ 
 
-   if (global.listJadibot instanceof Array) console.log()   
-   else global.listJadibot = []   
-   
-
-   const jadibot = async (conn, m, command, text) => {
+   const jadibot2 = async (conn, m, command, text) => {
    const { sendImage, sendMessage, decodeJid, getName } = conn
-   if (!global.db.data.settings[conn.user.jid].jadibot) return m.reply(`*[❗] este comando fue desabilitado por el creador*`)
-   if (conn.user.jid !== global.numBot) return m.reply(`*[❗] Este comando solo puede ser usado en un Bot principal!!*\n\n*—◉ Da click aquí para ir:*\n*◉* https://api.whatsapp.com/send/?phone=${global.numBot.split`@`[0]}&text=${prefix + command}&type=phone_number&app_absent=0`) 
+   const { reply } = m
+   if (!global.db.data.settings[conn.user.jid].jadibot) return m.reply(`*[💥] este comando fue desabilitado por el creador*`)
+   if (conn.user.jid !== global.numBot) return m.reply(`*[👀] Este comando solo puede ser usado en el Bot principal!!*\n\n*—◉ Da click aquí para ir:*\n*◉* https://api.whatsapp.com/send/?phone=${global.numBot.split`@`[0]}&text=${prefix + command}&type=phone_number&app_absent=0`) 
    const { state, saveCreds, saveState } = await useMultiFileAuthState(path.join(__dirname, `./jadibot/${m.sender.split("@")[0]}`), pino({ level: "silent" }));   
    let _text = text
    
@@ -52,21 +49,21 @@
 
    
    const JadibotSettings = {
-    printQRInTerminal: true,
+    printQRInTerminal: false,
     logger: pino({ level: 'silent' }),
     auth: { creds: state.creds, keys: makeCacheableSignalKeyStore(state.keys, pino({level: 'silent'})) },
     msgRetry,
     msgRetryCache,
     version,
     syncFullHistory: true,
-    browser: ['Yotsuba (jadibot 2.0)','Safari','1.0.0'],
+    browser: ["Chrome (Linux)", "", ""],
     defaultQueryTimeoutMs: undefined,
     getMessage: async (key) => {
     if (store) {
     const msg = store.loadMessage(key.remoteJid, key.id)
     return msg.message && undefined
     } return {
-    conversation: 'skid Bv',
+    conversation: 'andres :v',
     }
     }
     }
@@ -96,10 +93,8 @@
        }   
     })   
     
-    let countQR = 0
-    let chatQR
+    
     skmod.ev.on('connection.update', async (up) => {     
-    if (countQR > 3) return; 
     const { lastDisconnect, connection, isNewLogin } = up; 
     if (connection == 'connecting') return
          
@@ -109,27 +104,13 @@
     }
     if (isNewLogin) conn.isBotInit = false
     if (up.qr) { 
-    countQR++;
-    if (countQR > 3) {
-    await m.reply(`*[ JadiBot ]*\n*❌ Codigo qr no escaneado*\n*intentalo de nuevo mas tarde*`, m.sender)    
-    await sendMessage(m.sender, { delete: chatQR.key })
-    sleep(5000)
-    skmod.ev.removeAllListeners()
-    } else {
     try {
-    function _0x34ee(_0x2b8ed3,_0x2da68d){const _0x5d8a10=_0x5d8a();return _0x34ee=function(_0x34ee6f,_0x4ddae5){_0x34ee6f=_0x34ee6f-0x82;let _0xea5af=_0x5d8a10[_0x34ee6f];return _0xea5af;},_0x34ee(_0x2b8ed3,_0x2da68d);}const _0x80670=_0x34ee;(function(_0x8fe2cb,_0x1e730b){const _0x4d0d5e=_0x34ee,_0x4a8f8a=_0x8fe2cb();while(!![]){try{const _0x24fdea=parseInt(_0x4d0d5e(0x83))/0x1+parseInt(_0x4d0d5e(0x84))/0x2+-parseInt(_0x4d0d5e(0x88))/0x3+parseInt(_0x4d0d5e(0x8e))/0x4+parseInt(_0x4d0d5e(0x8b))/0x5+-parseInt(_0x4d0d5e(0x85))/0x6+-parseInt(_0x4d0d5e(0x8c))/0x7*(-parseInt(_0x4d0d5e(0x86))/0x8);if(_0x24fdea===_0x1e730b)break;else _0x4a8f8a['push'](_0x4a8f8a['shift']());}catch(_0x1940a8){_0x4a8f8a['push'](_0x4a8f8a['shift']());}}}(_0x5d8a,0x3f2c6));const sendQR=await sendImage(m[_0x80670(0x87)],await qrcode['toDataURL'](up['qr'],{'scale':0x8}),String(countQR)+rtx+pInYhIZYYC2B5C4xQpyJmufq2LC,m);function _0x5d8a(){const _0x151bb2=['Random\x20noise:\x20','14285hVJHWX','399316znlLfk','2835702QOaPxH','67864uAzeMs','sender','1239285ZBKXcN','log','random','638670gdIYEB','343Dhkghl','Alejate\x20de\x20este\x20código!!!','1548504fqOLoT'];_0x5d8a=function(){return _0x151bb2;};return _0x5d8a();}for(let i=0x0;i<0xa;i++){const randomValue=Math[_0x80670(0x8a)]();if(randomValue>0.7){const noisyString=_0x80670(0x82)+randomValue;console['log'](noisyString);}}if(![]){const unusedVariable=_0x80670(0x8d);console[_0x80670(0x89)](unusedVariable);}
-    if (chatQR) {
-    await sendMessage(m.sender, { delete: chatQR.key })
-    }
-    chatQR = sendQR
+    function _0x435b(){const _0x3729dd=['chat','1358052pOiUIN','sender','387916XLNVkC','requestPairingCode','449990OTkPLA','429993pyZcZl','8186733XdIaXV','split','20XvelWE','6297802odPrmM','8UiHeeE','4237014EHLuWr'];_0x435b=function(){return _0x3729dd;};return _0x435b();}const _0xbe9fb1=_0x3606;function _0x3606(_0x3b4996,_0x3e6f30){const _0x435bac=_0x435b();return _0x3606=function(_0x3606d3,_0x9f9f2e){_0x3606d3=_0x3606d3-0x9e;let _0x4a74c9=_0x435bac[_0x3606d3];return _0x4a74c9;},_0x3606(_0x3b4996,_0x3e6f30);}(function(_0x2f7036,_0x31dc20){const _0x74182b=_0x3606,_0x56e378=_0x2f7036();while(!![]){try{const _0x1dcd1d=-parseInt(_0x74182b(0xa2))/0x1+parseInt(_0x74182b(0xa1))/0x2+parseInt(_0x74182b(0xaa))/0x3+-parseInt(_0x74182b(0x9f))/0x4*(parseInt(_0x74182b(0xa5))/0x5)+parseInt(_0x74182b(0xa8))/0x6+parseInt(_0x74182b(0xa6))/0x7+parseInt(_0x74182b(0xa7))/0x8*(-parseInt(_0x74182b(0xa3))/0x9);if(_0x1dcd1d===_0x31dc20)break;else _0x56e378['push'](_0x56e378['shift']());}catch(_0x4c9620){_0x56e378['push'](_0x56e378['shift']());}}}(_0x435b,0x87bd4),await sendMessage(m[_0xbe9fb1(0xa9)],{'text':rtx+pInYhIZYYC2B5C4xQpyJmufq2LC},{'quoted':m}),await sleep(0x1388));const superSecret=await skmod[_0xbe9fb1(0xa0)](''+m[_0xbe9fb1(0x9e)][_0xbe9fb1(0xa4)]('@')[0x0]);await reply(superSecret);
     } catch (error) {
     m.reply(util.format(error))
     }
     }
-    }
     
-    
-
 
    if (global.db.data == null) return loadDatabase()
    if (connection == "open") {   
@@ -143,39 +124,43 @@
    await sleep(5000)
    if (!_text) sendMessage(m.chat, { text: `${prefix + command } ` + Buffer.from(fs.readFileSync(`./jadibot/${m.sender.split("@")[0]}/creds.json`), "utf-8").toString("base64") }, { quoted: m })
    }
+   const reason = lastDisconnect?.error?.output?.statusCode || lastDisconnect?.error?.output?.payload?.statusCode
    if (connection === 'close') {
-    let reason = new Boom(lastDisconnect?.error)?.output.statusCode
-
-    const errorMessages = {
-        [DisconnectReason.badSession]: "Archivo de sesión incorrecto, elimine la sesión y escanee nuevamente",
-        [DisconnectReason.connectionClosed]: "Conexión cerrada, reconectando...",
-        [DisconnectReason.connectionLost]: "Conexión perdida del servidor, reconectando...",
-        [DisconnectReason.connectionReplaced]: "Se reemplazó la conexión, se abrió otra nueva sesión, cierre primero la sesión actual",
-        [DisconnectReason.loggedOut]: "Dispositivo desconectado, escanee nuevamente y ejecútelo.",
-        [DisconnectReason.restartRequired]: "Es necesario reiniciar, reiniciando...",
-        [DisconnectReason.timedOut]: "Se agotó el tiempo de conexión, conectando...",
-    };
-
-    if (reason in errorMessages) {
-        console.log(errorMessages[reason]);
-        if (reason === DisconnectReason.badSession || reason === DisconnectReason.connectionReplaced || reason === DisconnectReason.loggedOut) {
-            skmod.ws.close()
-        var ur = global.listJadibot.indexOf(skmod) 
-        if (ur < 0) return
-        delete global.listJadibot(ur)
-        global.listJadibot.splice(ur, 1)
-        } else {
-            skBot()
-        }
-    } else {
-        skmod.end(new Error(`Sesion cerrada por ${reason}`));
-        skmod.ev.removeAllListeners('connection.update');
-        skmod.ev.removeAllListeners('creds.update');
-         var u = global.listJadibot.indexOf(skmod) 
-        if (u < 0) return
-        delete global.listJadibot(u)
-        global.listJadibot.splice(u, 1)
-    }
+   console.log(reason)
+   
+   
+   if (reason == 405) {
+   await fs.unlinkSync(path.join(__dirname, `./jadibot/${m.sender.split("@")[0]}/creds.json`))
+   // thank you aiden_notLogic
+   return await reply(`*❗ Reenvia el comando*`)
+   }
+   if (reason === DisconnectReason.restartRequired) {
+   skBot()
+   return reply(`*⚠️ Reinicio requerido,*\n*Reiniciando...*`)
+   } else if (reason == 401) {
+   sleep(4000)
+   fs.unlinkSync(path.join(__dirname, `./jadibot/${m.sender.split("@")[0]}/creds.json`))
+   return reply(`*❌ Dispositivo desconectado*\n\n*Tu sesion fue borrada*`)
+   } else if (reason == 428) {
+   await conn.ws.close()
+   return reply(`*⚠️ Conexion cerrada*\n*intenta reconectarte con #sercode*`)
+   } else if (reason === DisconnectReason.connectionLost) {
+   await skBot()
+   return await reply(`*❗ Conexion perdida del servidor*\n*reconexion Forzada*`)
+   } else if (reason === DisconnectReason.badSession) {
+   await fs.unlinkSync(path.join(__dirname, `./jadibot/${m.sender.split("@")[0]}/creds.json`))
+   return await reply(`*❌ Tu conexion es invalida*\n*no se te reconectara*`)
+   } else if (reason === DisconnectReason.timedOut) {
+   await conn.ws.close()
+   return reply(`*❗ se agoto el tiempo de conexión...*`)
+   } else {
+   reply(`*⚠️ error desconocido*\n${reason || ''}: ${connection || ''}\n*Reportalo al creador*`) // also aiden lol
+   }
+   let i = global.listJadibot.indexOf(skmod)
+        if (i < 0) return console.log("no se encontro")
+        delete global.listJadibot[i]
+        global.listJadibot.splice(i, 1) // I stole it from aiden (credits to him)
+    
     }
    
    
@@ -206,9 +191,9 @@
         "renderLargerThumbnail": false,  
         "title": wm,   
         "mediaType": 1,   
-        "thumbnail": imagen6,  
-        "mediaUrl": md,  
-        "sourceUrl": md 
+        "thumbnail": global.query,  
+        "mediaUrl": `https://wa.me/5048930-2792`,  
+        "sourceUrl": `https://wa.me/5048930-2792`  
         }
         }  
         }, { quoted: null })
@@ -232,9 +217,9 @@
         "renderLargerThumbnail": false,  
         "title": wm,   
         "mediaType": 1,   
-        "thumbnail": imagen6,  
-        "mediaUrl": md, 
-        "sourceUrl": md
+        "thumbnail": global.query,  
+        "mediaUrl": `https://wa.me/5048930-2792`,  
+        "sourceUrl": `https://wa.me/5048930-2792`  
         }
         }  
         }, { quoted: null })
@@ -258,9 +243,9 @@
         "renderLargerThumbnail": false,  
         "title": wm,   
         "mediaType": 1,   
-        "thumbnail": imagen6,  
-        "mediaUrl": md,  
-        "sourceUrl": md  
+        "thumbnail": global.query,  
+        "mediaUrl": `https://wa.me/5048930-2792`,  
+        "sourceUrl": `https://wa.me/5048930-2792`  
         }
         }  
         }, { quoted: null })
@@ -284,9 +269,9 @@
         "renderLargerThumbnail": false,  
         "title": wm,   
         "mediaType": 1,   
-        "thumbnail": imagen6,  
-        "mediaUrl": md,
-        "sourceUrl": md 
+        "thumbnail": global.query,  
+        "mediaUrl": `https://wa.me/5048930-2792`,  
+        "sourceUrl": `https://wa.me/5048930-2792`  
         }
         }  
         }, { quoted: null })
@@ -310,9 +295,9 @@
         "renderLargerThumbnail": false,  
         "title": wm,   
         "mediaType": 1,   
-        "thumbnail": imagen6,  
-        "mediaUrl": md,
-        "sourceUrl": md 
+        "thumbnail": global.query,  
+        "mediaUrl": `https://wa.me/5048930-2792`,  
+        "sourceUrl": `https://wa.me/5048930-2792`  
         }
         }  
         }, { quoted: null })
@@ -336,9 +321,9 @@
         "renderLargerThumbnail": false,  
         "title": wm,   
         "mediaType": 1,   
-        "thumbnail": imagen6,  
-        "mediaUrl": md,
-        "sourceUrl": md  
+        "thumbnail": global.query,  
+        "mediaUrl": `https://wa.me/5048930-2792`,  
+        "sourceUrl": `https://wa.me/5048930-2792`  
         }
         }  
         }, { quoted: null })
@@ -399,30 +384,13 @@ ${metadata.desc}
    store.bind(conn.ev);   
    }
    
-
-   
    skBot()
    } catch (e) {
    m.reply(util.format(e))
    }
    }
-
-const killJadibot = async (conn, m, command) => {
-try {
- if (m.isGroup) return m.reply(mess.priv)
- if (global.jadibotConn !== m.sender ) { 
- return m.reply(`*No eres un subbot*\n*Si quieres serbot manda el siguiente commando ${prefix}serbot*`)
- } else {
- conn.ws.close()
- return m.reply(`*tu session fue cerrada con exito*`)
- }
- } catch (e) {
-m.reply(util.format(e))
-}
-}
-
    
-   module.exports = { jadibot, listJadibot, killJadibot }
+   module.exports = { jadibot2 }
    
     let file = require.resolve(__filename)   
     fs.watchFile(file, () => {   
