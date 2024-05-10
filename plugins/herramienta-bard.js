c14140fed13459abd600d18c30e0b98fe5bba30f
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-10 16:44:26 -0500

    Actualizar herramienta-bard.js

--- plugins/herramienta-bard.js
+++ plugins/herramienta-bard.js
@@ -1,25 +1,24 @@
-import fetch from 'node-fetch'
-
 var handler = async (m, { text,  usedPrefix, command }) => {
 
-if (!text) return conn.reply(m.chat, `🏷 *Ingresé una petición*\n\nEjemplo, !bard Conoces Yotsuba-Nakano-MD?`, m, fake, )
+if (!text) throw `*[❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝚄𝙽𝙰 𝙿𝙴𝚃𝙸𝙲𝙸𝙾𝙽 𝙾 𝚄𝙽𝙰 𝙾𝚁𝙳𝙴𝙽 𝙿𝙰𝚁𝙰 𝚄𝚂𝙰𝚁 𝙻𝙰 𝙵𝚄𝙽𝙲𝙸𝙾𝙽 𝙳𝙴 𝙲𝙷𝙰𝚃𝙶𝙿𝚃*\n\n*—◉ 𝙴𝙹𝙴𝙼𝙿𝙻𝙾𝚂 𝙳𝙴 𝙿𝙴𝚃𝙸𝙲𝙸𝙾𝙽𝙴𝚂 𝚈 𝙾𝚁𝙳𝙴𝙽𝙴𝚂*\n*◉ ${usedPrefix + command} Reflexion sobre la serie Merlina 2022 de netflix*\n*◉ ${usedPrefix + command} Codigo en JS para un juego de cartas* `
 
 try {
 
+//await m.reply('*🚀 C A R G A N D O*')
 conn.sendPresenceUpdate('composing', m.chat)
-var apii = await fetch(`https://aemt.me/bard?text=${text}`)
+var apii = await fetch(`https://aemt.me/gemini?text=${text}`)
 var res = await apii.json()
 await m.reply(res.result)
 
 } catch (error) {
 console.error(error)
-return conn.reply(m.chat, `*🏷 Ocurrió un fallo*`, m, fake, )
+throw '*[❗] 𝙴𝚁𝚁𝙾𝚁, 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝚁𝙻𝙾*'
 }
 
 }
 handler.command = ['bard']
 handler.help = ['bard']
-handler.tags = ['tools']
+handler.tags = ['ai']
 
 handler.premium = false
 
