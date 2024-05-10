da85ee3deefef2816a25126f838547517db29c0a
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-10 17:11:27 -0500

    Actualizar owner-saveplugin.js

--- plugins/owner-saveplugin.js
+++ plugins/owner-saveplugin.js
@@ -3,7 +3,7 @@ import fs from 'fs'
 var handler = async (m, { text, usedPrefix, command }) => {
 
 if (!text) return conn.reply(m.chat, `☄️ *Ingresé el nombre del plugin*`, m, fake, )
-if (!m.quoted.text) return conn.reply(m.chat, `🍃 *Ingresa el contenido del plugin*`, m, fake, )
+//if (!m.quoted.text) return conn.reply(m.chat, `🍃 *Ingresa el contenido del plugin*`, m, fake, )
 let path = `plugins/${text}.js`
 await fs.writeFileSync(path, m.quoted.text)
 conn.reply(m.chat, `🌺 *Guardado en* ${path}`, m, fake, )
