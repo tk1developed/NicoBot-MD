ec09042e16ebcafe5d9be07eddc166b1dc69f84c
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-13 10:30:49 -0500

    Actualizar rpg-myns.js

--- plugins/rpg-myns.js
+++ plugins/rpg-myns.js
@@ -1,10 +1,10 @@
-//💖 𝙎𝙖𝙠𝙪𝙧𝙖𝘽𝙤𝙩𝙇𝙞𝙩𝙚-𝙈𝘿 💖
+//🌺 Yotsuba-Nakano-MD
 
 import { createHash } from 'crypto'
 let handler = async function (m, { conn, text, usedPrefix }) {
 let sn = createHash('md5').update(m.sender).digest('hex')
 
-conn.fakeReply(m.chat, sn, '0@s.whatsapp.net', '👇 *ESTE ES SU NUMERO DE SERIE* 👇', 'status@broadcast')
+conn.fakeReply(m.chat, sn, '0@s.whatsapp.net', '乂 N U M E R O - D E - S E R I E 乂', 'status@broadcast')
 }
 handler.help = ['myns']
 handler.tags = ['rg']
