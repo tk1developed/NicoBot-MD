3b1b1ae754013c7fc9d66245bf724fd5d8e41800
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-14 06:25:34 -0500

    Actualizar gc-link.js

--- plugins/gc-link.js
+++ plugins/gc-link.js
@@ -4,9 +4,9 @@ const handler = async (m, {conn, args}) => {
   conn.reply(m.chat, 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group), m, {
     contextInfo: {externalAdReply: {mediaUrl: null, mediaType: 1, description: null,
        title: 'Eɴʟᴀᴄᴇ Dᴇʟ Gʀᴜᴘᴏ',
-      body: '✰ 𝗬𝗼𝘁𝘀𝘂𝗯𝗮 𝗡𝗮𝗸𝗮𝗻𝗼 ★',    
+      body: '✰ 𝗬𝗼𝘀𝗵𝗶𝗸𝗼 𝗕𝗼𝘁 - 𝗠𝗗 ★',    
       previewType: 0, thumbnail: fs.readFileSync('./Menu.png'),
- sourceUrl: `https://github.com/Diego-YL-177/Yotsuba-Nakano-MD`}}});
+ sourceUrl: `https://github.com/Diego-YL-177/YoshikoBot-MD`}}});
 };     
 handler.help = ['linkgroup'];
 handler.tags = ['group'];
