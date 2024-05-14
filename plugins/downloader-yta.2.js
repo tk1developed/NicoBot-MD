13123b8f23dfb5a6cc25066270ca0fa141633ac0
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-14 06:49:26 -0500

    Actualizar downloader-yta.2.js

--- plugins/downloader-yta.2.js
+++ plugins/downloader-yta.2.js
@@ -1,7 +1,7 @@
 import {youtubedl, youtubedlv2} from '@bochilteam/scraper';
 import fetch from 'node-fetch';
 const handler = async (m, {conn, args}) => {
-  if (!args[0]) throw '*Y EL LINK?*';
+  if (!args[0]) throw '🌺 𝐄𝐧𝐯𝐢𝐞 𝐔𝐧 𝐋𝐢𝐧𝐤';
   await m.reply(`*_⏳ Audio procesando...⏳_*\n\n*𝓼Sɪ Eʟ Aᴜᴅɪᴏ Nᴏ Sᴇ Lᴇ Eɴᴠɪᴀ Pʀᴜᴇʙᴇ Lᴏs Sɪɢᴜɪᴇɴᴛᴇs Cᴏᴍᴀɴᴅᴏs #playdoc ᴏ #play.2 ᴏ #ytmp4doc ◉*`);
   try {
     const q = '128kbps';
@@ -10,7 +10,7 @@ const handler = async (m, {conn, args}) => {
     const dl_url = await yt.audio[q].download();
     const ttl = await yt.title;
     const size = await yt.audio[q].fileSizeH;
-    const cap = `*◉—⌈📥 𝐘𝐎𝐔𝐓𝐔𝐁𝐄 𝐃𝐋 📥⌋—◉*\n❏ *𝚃𝙸𝚃𝚄𝙻𝙾:* ${ttl}\n❏ *𝙿𝙴𝚂𝙾:* ${size}\n✰ 𝗬𝗼𝘁𝘀𝘂𝗯𝗮 𝗡𝗮𝗸𝗮𝗻𝗼 ★`.trim();
+    const cap = `*◉—⌈📥 𝐘𝐎𝐔𝐓𝐔𝐁𝐄 𝐃𝐋 📥⌋—◉*\n❏ *𝚃𝙸𝚃𝚄𝙻𝙾:* ${ttl}\n❏ *𝙿𝙴𝚂𝙾:* ${size}\n✰ 𝗬𝗼𝘀𝗵𝗶𝗸𝗼 𝗕𝗼𝘁 - 𝗠𝗗 ★`.trim();
     await conn.sendMessage(m.chat, {document: {url: dl_url}, caption: cap, mimetype: 'audio/mpeg', fileName: `${ttl}.mp3`}, {quoted: m});
   } catch {
     try {
@@ -19,7 +19,7 @@ const handler = async (m, {conn, args}) => {
       const n = lolh.result.title || 'error';
       const n2 = lolh.result.link;
       const n3 = lolh.result.size;
-      const cap2 = `*◉—⌈📥 𝐘𝐎𝐔𝐓𝐔𝐁𝐄 𝐃𝐋 📥⌋—◉*\n❏ *𝚃𝙸𝚃𝚄𝙻𝙾:* ${n}\n❏ *𝙿𝙴𝚂𝙾:* ${n3}\n✰ 𝗬𝗼𝘁𝘀𝘂𝗯𝗮 𝗡𝗮𝗸𝗮𝗻𝗼 ★`.trim();
+      const cap2 = `*◉—⌈📥 𝐘𝐎𝐔𝐓𝐔𝐁𝐄 𝐃𝐋 📥⌋—◉*\n❏ *𝚃𝙸𝚃𝚄𝙻𝙾:* ${n}\n❏ *𝙿𝙴𝚂𝙾:* ${n3}\n✰ 𝗬𝗼𝘀𝗵𝗶𝗸𝗼 𝗕𝗼𝘁 - 𝗠𝗗 ★`.trim();
       await conn.sendMessage(m.chat, {document: {url: n2}, caption: cap2, mimetype: 'audio/mpeg', fileName: `${n}.mp3`}, {quoted: m});
     } catch {
       await conn.reply(m.chat, '*⚠️ Ocurrió un error*', m);
