9e249b413895435eaec3254479109f5fc063bfa7
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-15 10:50:58 -0500

    Actualizar gc-link.js

--- plugins/gc-link.js
+++ plugins/gc-link.js
@@ -1,16 +1,10 @@
-import fs from 'fs';
-const handler = async (m, {conn, args}) => {
-  const group = m.chat;
-  conn.reply(m.chat, 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group), m, {
-    contextInfo: {externalAdReply: {mediaUrl: null, mediaType: 1, description: null,
-       title: 'Eɴʟᴀᴄᴇ Dᴇʟ Gʀᴜᴘᴏ',
-      body: '✰ 𝗬𝗼𝘀𝗵𝗶𝗸𝗼 𝗕𝗼𝘁 - 𝗠𝗗 ★',    
-      previewType: 0, thumbnail: fs.readFileSync('./Menu.png'),
- sourceUrl: `https://github.com/Diego-YL-177/YoshikoBot-MD`}}});
-};     
-handler.help = ['linkgroup'];
-handler.tags = ['group'];
-handler.command = /^link(gro?up)?$/i;
-handler.group = true;
-handler.botAdmin = true;
-export default handler;
+let handler = async (m, { conn, args }) => {
+let group = m.chat
+let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group)
+conn.reply(m.chat, link, m, {detectLink: true})
+//conn.sendMessage(m.chat, { text: link }, { quoted: m, detectLink: true })
+}
+handler.command = /^link(gro?up)?$/i
+handler.group = true
+handler.botAdmin = true
+export default handler
