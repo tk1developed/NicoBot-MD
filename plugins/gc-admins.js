9f17f8db9d8fddaab841a121c8e3f102564964e3
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-20 10:05:34 -0500

    Actualizar gc-admins.js

--- plugins/gc-admins.js
+++ plugins/gc-admins.js
@@ -1,7 +1,7 @@
 const handler = async (m, {conn, participants, groupMetadata, args}) => {
   const pp = await conn.profilePictureUrl(m.chat, 'image').catch((_) => null) || './src/admins.jpg';
   const groupAdmins = participants.filter((p) => p.admin);
-  const listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.id.split('@')[0]}`).join('\n');
+  const listAdmin = groupAdmins.map((v, i) => `┃ ${i + 1}. @${v.id.split('@')[0]}`).join('\n');
   const owner = groupMetadata.owner || groupAdmins.find((p) => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net';
   const pesan = args.join` `;
   const oi = `*Mensaje:* ${pesan}`;
