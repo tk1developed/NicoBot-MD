e2340054cd5c4cb65196c6abd64c6a5cb7a97ead
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-20 21:32:50 -0500

    Actualizar rpg-verificar.js

--- plugins/rpg-verificar.js
+++ plugins/rpg-verificar.js
@@ -6,8 +6,8 @@ let user = global.db.data.users[m.sender]
 let name2 = conn.getName(m.sender)
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? this.user.jid : m.sender
 let pp = await this.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/1861aab98389b13db8588.jpg')
-if (user.registered === true) throw `🏷 𝐄𝐑𝐑𝐎𝐑 🏷 *Ya ᥱs𝗍ᥲ́s registrado*\n\n¿𝗊ᥙіᥱrᥱ ᥎᥆ᥣ᥎ᥱr ᥲ rᥱgіs𝗍rᥲrsᥱ?\n\n✏️ ᥙsᥱ ᥱs𝗍ᥱ ᥴ᥆mᥲᥒძ᥆ para *eliminar su registro*\n*.unreg* <ᥒᥙ́mᥱr᥆ ძᥱ serie>`
-if (!Reg.test(text)) throw `*✏️ Formato incorrecto*\n\n📩 Uso del comamdo: *${usedPrefix + command} nombre.edad*\n💡 Ejemplo : *${usedPrefix + command}* ${name2}.18`
+if (user.registered === true) throw `*⚠️ Ya estás registrado*\n\n¿Quiere volver a registrarse?\n\n💬 Use este comando para *eliminar su registro*\n*${usedPrefix}unreg* <Número de serie>`
+if (!Reg.test(text)) throw `*⚠️ Formato incorrecto*\n\n📝 Uso del comamdo: *${usedPrefix + command} nombre.edad*\n💡 Ejemplo : *${usedPrefix + command}* ${name2}.18`
 let [_, name, splitter, age] = text.match(Reg)
 if (!name) throw '*📝 El nombre no puede estar vacío*'
 if (!age) throw '*📝 La edad no puede estar vacía*'
@@ -19,35 +19,19 @@ user.name = name.trim()
 user.age = age
 user.regTime = + new Date
 user.registered = true
-global.db.data.users[m.sender].money += 600
-global.db.data.users[m.sender].limit += 10
-global.db.data.users[m.sender].exp += 245
-global.db.data.users[m.sender].joincount += 5
-let sn = createHash('md5').update(m.sender).digest('hex').slice(0, 6) 
-       
-await conn.sendMessage(m.chat, { react: { text: '📩', key: m.key } }
-
-let regbot = `╭━〔  𝐑 𝐄 𝐆 𝐈 𝐒 𝐓 𝐑 𝐎 📇  〕⬣
-┃ • 🌺 Nombre: ${name}
-┃ • 🔮 Edad : ${age} años
-╰━━━━━━━━━━━━⬣
-
-╭━〔  𝐑𝐄𝐂𝐎𝐌𝐏𝐄𝐍𝐒𝐀𝐒 ✏️  〕⬣
-┃ • 10 Diamantes 💎
-┃ • 600 YoshiCoins 💰
-┃ • 245 Exp 💸
-┃ • 5 Monedas 🪙
-╰━━━━━━━━━━━━⬣
-
-╭━〔  𝐍𝐔𝐌𝐄𝐑𝐎 𝐃𝐄 𝐒𝐄𝐑𝐈𝐄 ✏️  〕⬣
-┃ ${sn}
-╰━━━━━━━━━━━━⬣`
+let sn = createHash('md5').update(m.sender).digest('hex').slice(0, 6)        
+m.react('📩') 
+let regbot = `🗃️ *R E G I S T R A D O* 🗃️
+🪁 *Nombre:* ${name}
+🎨 *Edad* : ${age} años
+🥏 *Numero de serie*:
+${sn}`
 await m.reply(regbot)
-//await conn.sendUrl(m.chat, regbot, m, { externalAdReply: { mediaType: 1, renderLargerThumbnail: true, thumbnail: pp, thumbnailUrl: pp, title: 'Registrado 📩', }})
+// await conn.sendUrl(m.chat, regbot, m, { externalAdReply: { mediaType: 1, renderLargerThumbnail: true, thumbnail: pp, thumbnailUrl: pp, title: 'Registrado 📩', }})
 
 }
 handler.help = ['reg']
 handler.tags = ['rg']
-handler.command = ['verificar', 'reg', 'verify'] 
+handler.command = ['verify', 'reg'] 
 
 export default handler
