381211b5d0ec0700816a6a23cd1d1ee024db636f
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-21 10:54:21 -0500

    Actualizar herramientas-chatgpt.js

--- plugins/herramientas-chatgpt.js
+++ plugins/herramientas-chatgpt.js
@@ -9,7 +9,7 @@ if (usedPrefix == 'a' || usedPrefix == 'A') return;
 if (!text) return conn.reply(m.chat, `🍃 *Ingrese una petición*\n\n- Ejemplo, !${command} Codigo en Js para un juego de ahorcado`, m, fake, )   
 try {
 await m.reply('🌺 *C A R G A N D O*\n- 🍃 Cargando Información')
-m.react('⌛️') 
+//m.react('⌛️') 
 conn.sendPresenceUpdate('composing', m.chat);
 let gpt = await fetch(`https://delirius-api-oficial.vercel.app/api/ia2?text=${text}`)
 let res = await gpt.json()
@@ -20,7 +20,6 @@ let gpt = await fetch(`https://delirius-api-oficial.vercel.app/api/chatgpt?q=${t
 let res = await gpt.json()
 await m.reply(res.data) 
 } catch {
-m.react('✅️') 
 }}}
 handler.help = ['openia', 'chatgpt', 'ia']
 handler.tags = ['ai']
