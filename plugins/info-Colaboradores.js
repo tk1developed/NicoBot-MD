feed1693809ee5e88500bec76ec28327ba8723e9
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-21 11:24:31 -0500

    Actualizar info-Colaboradores.js

--- plugins/info-Colaboradores.js
+++ plugins/info-Colaboradores.js
@@ -20,10 +20,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
     const readMore = more.repeat(850);
     const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
     const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
-
- await conn.sendMessage(m.chat, { react: { text: '🇨🇴', key: m.key } })
-
-
+m.react('🥏') 
 const document = doc[Math.floor(Math.random() * doc.length)];
  const str = `╭━〔  𝐘𝐎𝐒𝐇𝐈𝐊𝐎 ⛅  〕⬣
 ┃➸ ${packname}
