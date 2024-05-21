47a8eb4bd1c6d91b642cb5027d5a93cc9ee318a1
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-20 21:40:17 -0500

    Actualizar rpg-verificar.js

--- plugins/rpg-verificar.js
+++ plugins/rpg-verificar.js
@@ -24,7 +24,7 @@ global.db.data.users[m.sender].limit += 10
 global.db.data.users[m.sender].exp += 245
 global.db.data.users[m.sender].joincount += 5
 let sn = createHash('md5').update(m.sender).digest('hex').slice(0, 6)        
-await conn.sendMessage(m.chat, { react: { text: '📩', key: m.key } }
+await conn.sendMessage(m.chat, { react: { text: '📩', key: m.key } } 
 let regbot = `╭━〔  𝐑 𝐄 𝐆 𝐈 𝐒 𝐓 𝐑 𝐎 📇  〕⬣
 ┃ • 🌺 Nombre: ${name}
 ┃ • 🔮 Edad : ${age} años
@@ -41,11 +41,11 @@ let regbot = `╭━〔  𝐑 𝐄 𝐆 𝐈 𝐒 𝐓 𝐑 𝐎 📇  〕⬣
 ┃ ${sn}
 ╰━━━━━━━━━━━━⬣`
 await m.reply(regbot)
-//await conn.sendUrl(m.chat, regbot, m, { externalAdReply: { mediaType: 1, renderLargerThumbnail: true, thumbnail: pp, thumbnailUrl: pp, title: 'Registrado 📩', }})
+// await conn.sendUrl(m.chat, regbot, m, { externalAdReply: { mediaType: 1, renderLargerThumbnail: true, thumbnail: pp, thumbnailUrl: pp, title: 'Registrado 📩', }})
 
 }
 handler.help = ['reg']
 handler.tags = ['rg']
-handler.command = ['verificar', 'reg', 'verify'] 
+handler.command = ['verify', 'reg'] 
 
 export default handler
