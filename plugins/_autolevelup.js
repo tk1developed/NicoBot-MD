97fa5634478f46f232f8f09bb0ba62a46c202d48
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-21 20:29:39 -0500

    Actualizar _autolevelup.js

--- plugins/_autolevelup.js
+++ plugins/_autolevelup.js
@@ -1,15 +1,16 @@
-//import db from '../lib/database.js'
-import { canLevelUp } from '../lib/levelling.js'
+import { canLevelUp, xpRange } from '../lib/levelling.js'
+import { levelup } from '../lib/canvas.js'
+export function before(m, { conn }) {
+//if (!db.data.chats[m.chat].autonivel && m.isGroup) throw 
 
-export async function before(m, { conn }) {
-    let user = global.db.data.users[m.sender]
-    if (!user.autolevelup)
-        return !0
-    let before = user.level * 1
-    while (canLevelUp(user.level, user.exp, global.multiplier))
-        user.level++
-    //user.role = global.rpg.role(user.level).name
-    if (before !== user.level) {
+let user = global.db.data.users[m.sender]
+let chat = global.db.data.chats[m.chat]
+if (!chat.autolevelup)
+return !0
+
+let before = user.level * 1
+while (canLevelUp(user.level, user.exp, global.multiplier)) user.level++
+ if (before !== user.level) {
         m.reply(`╭━〔  𝐀𝐔𝐓𝐎 - 𝐍𝐈𝐕𝐄𝐋 🍄  〕⬣        
 ┃֪࣪ *🍂 𝐅𝐞𝐥𝐢𝐜𝐢𝐝𝐚𝐝𝐞𝐬 𝐒𝐮𝐛𝐢𝐬𝐭𝐞 𝐃𝐞 𝐍𝐢𝐯𝐞𝐥 🍁*
 ┃֪࣪ 🌺 *Nɪᴠᴇʟ Aɴᴛɪɢᴜᴏ : ${before}* 
