6d3b441a1960f158f323325f63c68a985900acc5
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-24 11:27:31 -0500

    Actualizar bot-menuprincipal.js

--- plugins/bot-menuprincipal.js
+++ plugins/bot-menuprincipal.js
@@ -47,7 +47,6 @@ const defaultMenu = {
       ⬣〔  𝐈𝐍𝐅𝐎 𝐁𝐎𝐓 🍄  〕⬣
 
 ✰્᭄͜͡ 🅲︎ʀᴇᴀᴅᴏʀ: %author
-✰્᭄͜͡ 🅃ᴀɢ: %taguser
 ✰્᭄͜͡ 🆁︎ᴜɴᴛɪᴍᴇ: %muptime
 ✰્᭄͜͡ 🅄sᴜᴀʀɪᴏs: %totalreg
 ✰્᭄͜͡ 🆅︎ᴇʀᴄɪᴏɴ: 1.0.7
@@ -155,7 +154,7 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
       me: conn.getName(conn.user.jid),
       npmname: _package.name,
       version: _package.version,
-      sbot: (conn.user.jid == global.conn.user.jid ? '' : `✰્᭄͜͡ 🅂ᴏʏ sᴜʙʙᴏᴛ ᴅᴇ:\n✰્᭄͜͡ Wa.me/${global.conn.user.jid.split`@`[0]}\n`), 
+      sbot: (conn.user.jid == global.conn.user.jid ? '' : `✰્᭄͜͡ 🅂ᴏʏ sᴜʙʙᴏᴛ ᴅᴇ: Wa.me/${global.conn.user.jid.split`@`[0]}\n`), 
       npmdesc: _package.description,
       npmmain: _package.main,
       author: _package.author.name,
