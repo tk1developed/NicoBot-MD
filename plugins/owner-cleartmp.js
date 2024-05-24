3d2c95ae74f327432b1f83b3240f9849e6a809a4
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-24 10:30:39 -0500

    Actualizar owner-cleartmp.js

--- plugins/owner-cleartmp.js
+++ plugins/owner-cleartmp.js
@@ -1,26 +1,27 @@
-import {tmpdir} from 'os';
-import path, {join} from 'path';
+import { tmpdir } from 'os'
+import path, { join } from 'path'
 import {
-  readdirSync,
-  statSync,
-  unlinkSync,
-  existsSync,
-  readFileSync,
-  watch,
-} from 'fs';
-const handler = async (m, {conn, usedPrefix: _p, __dirname, args}) => {
-  conn.reply(m.chat, '*[ ✔ ] 𝙰𝚁𝙲𝙷𝙸𝚅𝙾𝚂 𝙳𝙴 𝙻𝙰 𝙲𝙰𝚁𝙿𝙴𝚃𝙰 𝚃𝙼𝙿 𝙴𝙻𝙸𝙼𝙸𝙽𝙰𝙳𝙾𝚂 𝙲𝙾𝙽 𝙴𝚇𝙸𝚃𝙾!!*', m);
+readdirSync,
+statSync,
+unlinkSync,
+existsSync,
+readFileSync,
+watch
+} from 'fs'
+let handler = async (m, { conn, usedPrefix: _p, __dirname, args }) => { 
 
-  const tmp = [tmpdir(), join(__dirname, '../tmp')];
-  const filename = [];
-  tmp.forEach((dirname) => readdirSync(dirname).forEach((file) => filename.push(join(dirname, file))));
-  return filename.map((file) => {
-    const stats = statSync(file);
-    unlinkSync(file);
-  });
-};
-handler.help = ['cleartmp'];
-handler.tags = ['owner'];
-handler.command = /^(cleartmp|cleartemp)$/i;
-handler.rowner = true;
-export default handler;
+conn.reply(m.chat, `🧰 Realizado, ya se ha eliminado los archivos de la carpeta tmp`, m, fake, )
+
+const tmp = [tmpdir(), join(__dirname, '../tmp')]
+const filename = []
+tmp.forEach(dirname => readdirSync(dirname).forEach(file => filename.push(join(dirname, file))))
+return filename.map(file => {
+const stats = statSync(file)
+unlinkSync(file)
+})} //NO USAR ESTE COMANDO EN HEROKU | DO NOT USE THIS COMMAND ON HEROKU
+handler.help = ['cleartmp']
+handler.tags = ['owner']
+handler.command = /^(cleartmp|cleartemp|borrartmp|eliminartmp|borrartemp|borrartemp)$/i
+handler.exp = 500
+handler.rowner = true
+export default handler
