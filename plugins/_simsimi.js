a80e1f6d432d799888be1aaabe700f09ccc00263
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-24 09:58:14 -0500

    Actualizar _simsimi.js

--- plugins/_simsimi.js
+++ plugins/_simsimi.js
@@ -8,6 +8,7 @@ handler.before = async (m) => {
   if (chat.simi) {
     if (/^.*false|disnable|(turn)?off|0/i.test(m.text)) return;
     let textodem = m.text;
+    m.react('🗣️')
     try {
       const ressimi = await simitalk(textodem);
       await m.conn.sendMessage(m.chat, { text: ressimi.resultado.simsimi }, { quoted: m });
