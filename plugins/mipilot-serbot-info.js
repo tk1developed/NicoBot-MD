d2c4865fb735de7e74e8beb15536c6836d146891
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-24 18:01:30 -0500

    Actualizar mipilot-serbot-info.js

--- plugins/mipilot-serbot-info.js
+++ plugins/mipilot-serbot-info.js
@@ -10,8 +10,8 @@ async function handler(m, { conn, usedPrefix }) {
         const userJid = user.user.jid.replace(/[^0-9]/g, "");
         if (!addedNumbers.has(userJid)) {
           addedNumbers.add(userJid);
-          const userName = user.user.name || "𝚈𝙾𝚂𝙷𝙸𝙺𝙾 𝚂𝚄𝙱-𝙱𝙾𝚃 🍂";
-          connectedUsers.add(`Wa.me/${userJid}?text=${usedPrefix}allmenu (${userName})`);
+          const userName = user.user.name || "𝚂𝚄𝙱-𝙱𝙾𝚃";
+          connectedUsers.add(`Wa.me/${userJid}?text=!allmenu (${userName})`);
         }
       });
 
@@ -21,7 +21,7 @@ async function handler(m, { conn, usedPrefix }) {
       //await conn.sendFile(m.chat, imageBuffer.data, 'image.jpg', '🕒 𝙲𝙰𝚁𝙶𝙰𝙽𝙳𝙾 𝙻𝙰 𝙻𝙸𝚂𝚃𝙰 𝙳𝙴 𝙻𝙰𝚂 𝚂𝚄𝙱𝙱𝙾𝚃𝚂 𝙰𝙲𝚃𝙸𝚅𝙰𝚂 ⚙️');
 
       const userList = [...connectedUsers].join(`\n`);
-      await m.reply(`╭─⬣「 *LISTA DE JADIBTS* 」⬣\n┃ 🕒 𝚂𝚄𝙱𝙱𝙾𝚃𝚂 𝙰𝙲𝚃𝙸𝚅𝙰𝚂:\n╰─⬣\n╭─⬣「 🌩 YoshikoBot-MD 」⬣\n┃ ${userList}\n╰─⬣`);
+      await m.reply(`╭─⬣「 *LISTA DE JADIBTS* 」⬣\n┃ 🕒 𝚂𝚄𝙱𝙱𝙾𝚃𝚂 𝙰𝙲𝚃𝙸𝚅𝙰𝚂:\n╰─⬣\n╭─⬣「 ${packname} 」⬣\n┃ ${userList}\n╰─⬣`);
     } else {
       await m.reply("🍄 𝙽𝙾 𝙷𝙰𝚈 𝙽𝙸𝙽𝙶𝚄𝙽𝙰 𝚂𝚄𝙱𝙱𝙾𝚃 𝙰𝙲𝚃𝙸𝚅𝙰, 𝚁𝙴𝙶𝙻𝙴𝚂𝙰 𝙼𝙰𝚂 𝚃𝙰𝚁𝙳𝙴 ✏️");
     }
