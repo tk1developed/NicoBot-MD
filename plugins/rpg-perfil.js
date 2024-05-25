133859c62144ef0ac2391141b3ad7bf944793499
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-25 12:29:44 -0500

    Actualizar rpg-perfil.js

--- plugins/rpg-perfil.js
+++ plugins/rpg-perfil.js
@@ -9,15 +9,7 @@ let handler = async (m, { conn, usedPrefix, command}) => {
   let { min, xp, max } = xpRange(user.level, global.multiplier)
   let username = conn.getName(who)
   let prem = global.prems.includes(who.split`@`[0])
-  let txt = `╭─⬣「 *User Perfil* 」⬣\n`
-     txt += `│  ≡◦ *🪴 Nombre ∙* ${name}\n`
-     txt += `│  ≡◦ *🐢 Edad ∙* ${age} años\n`
-
-     txt += `│  ≡◦ *📞 Numero ∙* ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}\n`
-     txt += `│  ≡◦ *🍬 Dulces ∙* ${limit}\n`
-     txt += `│  ≡◦ *💫 Experiencia ∙* Total ${exp} ( *${user.exp - min}/${xp}* )\n`
-     txt += `│  ≡◦ *👑 Premium ∙* ${prem ? 'Si' : 'No'}\n`
-     txt += `╰─⬣`
+  let txt = `╭─⬣「 *User Perfil* 」⬣`
 conn.sendMessage(m.chat, {
 text: txt,
 contextInfo: { 
