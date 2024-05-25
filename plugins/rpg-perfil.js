2905fc48579575fc00f1eeac7805347d77c046cc
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-25 12:51:01 -0500

    Actualizar rpg-perfil.js

--- plugins/rpg-perfil.js
+++ plugins/rpg-perfil.js
@@ -9,17 +9,16 @@ let handler = async (m, { conn, usedPrefix, command}) => {
   let { min, xp, max } = xpRange(user.level, global.multiplier)
   let username = conn.getName(who)
   let prem = global.prems.includes(who.split`@`[0])
-  let txt = `╭─⬣「 *User Perfil* 」⬣`.trim(),
-contextInfo: {
-externalAdReply: {
-title: wm,
-body: nombre,
-thumbnailUrl: pp, 
-sourceUrl: channel,
-mediaType: 1,
-showAdAttribution: true,
-renderLargerThumbnail: true
-}}}, { quoted: fkontak })    
+  let txt = `╭─⬣「 *User Perfil* 」⬣\n`
+     txt += `│  ≡◦ *🪴 Nombre ∙* ${name}\n`
+     txt += `│  ≡◦ *🐢 Edad ∙* ${age} años\n`
+
+     txt += `│  ≡◦ *📞 Numero ∙* ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}\n`
+     txt += `│  ≡◦ *🍬 Dulces ∙* ${limit}\n`
+     txt += `│  ≡◦ *💫 Experiencia ∙* Total ${exp} ( *${user.exp - min}/${xp}* )\n`
+     txt += `│  ≡◦ *👑 Premium ∙* ${prem ? 'Si' : 'No'}\n`
+     txt += `╰─⬣`
+await conn.sendFile(m.chat, pp, 'thumbnail.jpg', txt, m)
 }
 handler.help = ['perfil', 'perfil @user']
 handler.tags = ['rg']
