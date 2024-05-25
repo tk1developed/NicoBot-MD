86aac3d1a1878dc50e42a7651e9e6aa61b6107aa
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-24 19:17:04 -0500

    Actualizar mipilot-serbot-info.js

--- plugins/mipilot-serbot-info.js
+++ plugins/mipilot-serbot-info.js
@@ -11,7 +11,7 @@ async function handler(m, { conn: stars, usedPrefix }) {
 
   let users = [...uniqueUsers.values()]
 
-  let message = users.map((v, index) => `✧ *${index + 1}.-* @${v.user.jid.replace(/[^0-9]/g, '')}\n✧ *Link:* https://wa.me/${v.user.jid.replace(/[^0-9]/g, '')}\n✧ *Nombre:* ${v.user.name || '𝚂𝚄𝙱-𝙱𝙾𝚃'}`).join('\n\n')
+  let message = users.map((v, index) => `╭─⬣「 ${packname} 」⬣\n│✧ *${index + 1}.-* @${v.user.jid.replace(/[^0-9]/g, '')}\n│✧ *Link:* https://wa.me/${v.user.jid.replace(/[^0-9]/g, '')}\n│✧ *Nombre:* ${v.user.name || '𝚂𝚄𝙱-𝙱𝙾𝚃'}\n╰─⬣`).join('\n\n')
 
   let replyMessage = message.length === 0 ? '' : message
   let totalUsers = users.length
