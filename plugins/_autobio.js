a2a721c79938c2a13d8bbe06b60992114d8d41ac
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-25 16:00:33 -0500

    Actualizar _autobio.js

--- plugins/_autobio.js
+++ plugins/_autobio.js
@@ -8,7 +8,7 @@ if (process.send) { process.send('uptime')
 _muptime = await new Promise(resolve => { process.once('message', resolve) 
 setTimeout(resolve, 2000) }) * 1000}
 let uptime = clockString(_uptime)
-let bio = `𝗬𝗼𝘀𝗵𝗶𝗸𝗼𝗕𝗼𝘁-𝗠𝗗 🔮 ┃ Super WhatsApp Bot Multi Device ┃ Tiempo Activa: ${uptime} ┃ By: OfcDiego 😊` 
+let bio = `𝗬𝗼𝘀𝗵𝗶𝗸𝗼𝗕𝗼𝘁-𝗠𝗗 🔮 ┃ Bot WhatsApp ┃ Tiempo Activa: ${uptime} ┃ By: OfcDiego 😊` 
 await this.updateProfileStatus(bio).catch(_ => _)
 setting.status = new Date() * 1
 } 
