7bec99c8a01bc5ca187a923c3b485fb28b979f59
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-25 10:05:54 -0500

    Actualizar random-itzy.js

--- plugins/random-itzy.js
+++ plugins/random-itzy.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 const handler = async (m, {conn, args, usedPrefix, command}) => {
-  const res = (await axios.get(`https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/master/src/JSON/itzy.json`)).data;
-  const mystic = await res[Math.floor(res.length * Math.random())];
-  conn.sendFile(m.chat, mystic, 'error.jpg', `_${command}_`, m);
+  const res = (await axios.get(`https://raw.githubusercontent.com/Diego-YL-177/YoshikoBot-MD/master/src/JSON/itzy.json`)).data;
+  const yoshiko = await res[Math.floor(res.length * Math.random())];
+  conn.sendFile(m.chat, yoshiko, 'error.jpg', `_${command}_`, m);
 };
 
 handler.help = ['itzy', 'kpopitzy'];
