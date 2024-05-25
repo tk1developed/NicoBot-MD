a4f62e06083eacf1c13c962bbdf1b253243b5484
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-24 20:05:32 -0500

    Actualizar _autobio.js

--- plugins/_autobio.js
+++ plugins/_autobio.js
@@ -1,4 +1,4 @@
-/*let handler = m => m
+let handler = m => m
 handler.all = async function (m) {
 let setting = global.db.data.settings[this.user.jid]
 
@@ -8,7 +8,7 @@ if (process.send) { process.send('uptime')
 _muptime = await new Promise(resolve => { process.once('message', resolve) 
 setTimeout(resolve, 2000) }) * 1000}
 let uptime = clockString(_uptime)
-let bio = `🌻 𝙷𝙾𝙻𝙰, 𝚂𝙾𝚈 𝚄𝙽𝙰 𝙱𝙾𝚃 𝙼𝙸 𝙽𝙾𝙼𝙱𝚁𝙴 𝙴𝚂 𝚈𝙾𝚂𝙷𝙸𝙺𝙾, 𝚃𝙸𝙴𝙼𝙿𝙾 𝙰𝙲𝚃𝙸𝚅𝙰: ${uptime}` 
+let bio = `𝗬𝗼𝘀𝗵𝗶𝗸𝗼𝗕𝗼𝘁-𝗠𝗗 🔮 ┃ Tiempo Activa: ${uptime} ┃ 𝗕𝘆: 𝗢𝗳𝗰𝗗𝗶𝗲𝗴𝗼 😊` 
 await this.updateProfileStatus(bio).catch(_ => _)
 setting.status = new Date() * 1
 } 
@@ -19,5 +19,5 @@ function clockString(ms) {
   let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24
   let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
   let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-  return [d, ' » ', h, ' ・ ', m, ' ・ ', s].map(v => v.toString().padStart(2, 0)).join('') 
-}*/
+  return [d, ':', h, ':', m, ':', s].map(v => v.toString().padStart(2, 0)).join('') 
+}
