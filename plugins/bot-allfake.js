19212fd94f87f5bea79f4dbe819a7b91e3c2af7a
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-26 08:57:10 -0500

    Actualizar bot-allfake.js

--- plugins/bot-allfake.js
+++ plugins/bot-allfake.js
@@ -37,11 +37,11 @@ Mensajes Fakes
 
 global.fkontak = { key: { participants:"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 
-var img = 'https://telegra.ph/file/990a21926fc608ec26dd3.jpg';
-var img1 = 'https://telegra.ph/file/335eba6ffc2f8e2197cc3.jpg';
-var img2 = 'https://telegra.ph/file/c435c48a55edbfe4f390b.jpg';
-var img3 = 'https://telegra.ph/file/2ce374abef0541010a65d.jpg';
-var img4 = 'https://telegra.ph/file/986b010ced2811f300abb.jpg';
+const img = 'https://telegra.ph/file/990a21926fc608ec26dd3.jpg';
+const img1 = 'https://telegra.ph/file/335eba6ffc2f8e2197cc3.jpg';
+const img2 = 'https://telegra.ph/file/c435c48a55edbfe4f390b.jpg';
+const img3 = 'https://telegra.ph/file/2ce374abef0541010a65d.jpg';
+const img4 = 'https://telegra.ph/file/986b010ced2811f300abb.jpg';
 
 global.yoshiImg = [img, img1, img2, img3, img4].getRandom()
 
