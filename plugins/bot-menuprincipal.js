8c5df602e391de2f7799228e153063c195acd578
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-25 21:51:05 -0500

    Actualizar bot-menuprincipal.js

--- plugins/bot-menuprincipal.js
+++ plugins/bot-menuprincipal.js
@@ -170,8 +170,8 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
     }
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
 
-    let pp1 = 'https://telegra.ph/file/a7ea164ca067545aeeec4.mp4'
-    //let pp3 = 'https://telegra.ph/file/78fca2a07333385b8633b.mp4'
+    //let pp1 = 'https://telegra.ph/file/a7ea164ca067545aeeec4.mp4'
+    let pp1 = 'https://telegra.ph/file/78fca2a07333385b8633b.mp4'
 
   //const pp = await conn.profilePictureUrl(conn.user.jid).catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png');
 
