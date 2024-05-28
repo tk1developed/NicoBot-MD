bef080edf9bc10be05d477589a45b71b39980442
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-28 16:16:55 -0500

    Actualizar main.js

--- main.js
+++ main.js
@@ -142,7 +142,7 @@ console.log('☄️ Por favor, seleccione solo 1 o 2.\n')
 }
 
 console.info = () => {}
-//console.warn = () => {}
+console.warn = () => {}
 const connectionOptions = {
 logger: pino({ level: 'silent' }),
 printQRInTerminal: opcion == '1' ? true : methodCodeQR ? true : false,
