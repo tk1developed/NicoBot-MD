141737bc586e5bd1d899c15eb4302c131d9a8e6f
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-27 20:52:03 -0500

    Actualizar config.js

--- config.js
+++ config.js
@@ -32,7 +32,8 @@ global.suittag = ['573012482694']
 global.prems = []
 global.mods = []
 
-global.lenguajeYL = es //idioma de yoshiko
+//gb prueba - uwu
+global.lenguajeGB = es //idioma de yoshiko
 
 //• ↳ ◜𝑴𝑨𝑹𝑪𝑨𝑺 𝑫𝑬 𝑨𝑮𝑼𝑨◞ • 🍁
 global.packname = '𝗬𝗼𝘀𝗵𝗶𝗸𝗼 𝗕𝗼𝘁 - 𝗠𝗗 🌸';
