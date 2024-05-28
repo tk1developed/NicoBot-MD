180d5ed71584400712789c9b4f10f4359c19480a
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-28 08:16:19 -0500

    Actualizar plugins

---
+++ plugins/bot-firstchat.js
@@ -0,0 +1,24 @@
+import moment from 'moment-timezone'
+
+export async function before(m) {
+
+if (m.chat.endsWith('broadcast') || m.fromMe || m.isGroup) return
+
+let user = global.db.data.users[m.sender]
+
+if (new Date() - user.pc < 86400000) return
+await conn.reply(m.chat, `👋 Hola ${nombre}!!
+ *${saludo}*
+
+📅 Fecha: ${botdate}
+⏰ Hora: ${bottime}
+
+🌸 *Nota:* no envíe spam al bot
+🔵 Escriba *.allmenu* para mostrar el menú 
+  
+🌟 ¿Quieres apoyar este proyecto para que siga actualizándose? únete a nuestro grupo: 
+*https://chat.whatsapp.com/Eaa9JFA53ps7WHMv2VHbO9*`, m, fake, )
+
+
+user.pc = new Date * 1
+}
