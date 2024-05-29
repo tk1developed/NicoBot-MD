943b93b4a2b97075f8ca76d28923441d71a26eda
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-29 11:54:53 -0500

    Actualizar info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -1,4 +1,4 @@
-let media = 'https://telegra.ph/file/509e978329182bbcc0729.mp4'
+let media = 'https://telegra.ph/file/0f4ecc755b1e20e12f41e.mp4'
 let handler = async (m, { conn, command }) => {
 let str = `*🧃 GRUPOS OFICIALES*
 
