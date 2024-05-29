d5cc51e30b278c666ad54c19856bacd8c421becb
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-29 07:12:28 -0500

    Actualizar main.js

--- main.js
+++ main.js
@@ -141,8 +141,8 @@ console.log('☄️ Por favor, seleccione solo 1 o 2.\n')
 }} while (opcion !== '1' && opcion !== '2' || fs.existsSync(`./${authFile}/creds.json`))
 }
 
-//console.info = () => {}
-console.warn = () => {}
+console.info = () => {}
+//console.warn = () => {}
 const connectionOptions = {
 logger: pino({ level: 'silent' }),
 printQRInTerminal: opcion == '1' ? true : methodCodeQR ? true : false,
