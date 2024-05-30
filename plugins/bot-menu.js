78e31b42c98aadc8b3ba42468f36557bdcb45895
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-30 09:47:04 -0500

    Actualizar bot-menu.js

--- plugins/bot-menu.js
+++ plugins/bot-menu.js
@@ -38,7 +38,7 @@ let menu = `☄️ !Hola¡ ${user}
 !owner
 *(Para ver el propietario del bot)*`
 
-await conn.reply(m.chat, menu, fkontak, { contextInfo: { externalAdReply: {title: 'ꪶໍٜ߭۫ި᮫ׅ࣪۬߭ׄ𖾏🍨𝚮͜ᴏ፝ʟ⃨ᴀ⃨⃕🍫 ' + user, body: wm, sourceUrl: md, thumbnail: await (await fetch(pp)).buffer() }}})
+await conn.reply(m.chat, menu, fkontak, { contextInfo: { externalAdReply: {title: packname, body: 'ꪶໍٜ߭۫ި᮫ׅ࣪۬߭ׄ𖾏🍨𝚮͜ᴏ፝ʟ⃨ᴀ⃨⃕🍫 ' + user, sourceUrl: md, thumbnail: await (await fetch(pp)).buffer() }}})
 
 } catch {
 conn.reply(m.chat, `🛑 *Ocurrió un fallo*`, m, fake, )
