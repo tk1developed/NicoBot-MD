0c9628eb6dddc15fb5073d6db3a8232f0a10add3
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-06-01 11:49:53 -0500

    Actualizar owner-unbanchat.js

--- plugins/owner-unbanchat.js
+++ plugins/owner-unbanchat.js
@@ -1,9 +1,9 @@
 let handler = async (m, { conn }) => {
-if (!(m.chat in global.db.data.chats)) return m.reply(lenguajeYL['smsAvisoMG']() + '🔵 *¡Este Chat No Está Registrado En Mi Base!*')
+if (!(m.chat in global.db.data.chats)) return m.reply(lenguajeYL['smsAvisoMG']() + '🔵 *¡ESTE CHAT NO ESTÁ REGISTRADO!*')
 let chat = global.db.data.chats[m.chat]
-if (!chat.isBanned) return m.reply(lenguajeYL.smsAvisoMG() + '🍬 *¡La Bot No Está Baneada En Este Chat!*')
+if (!chat.isBanned) return m.reply(lenguajeYL.smsAvisoMG() + '🍬 *¡LA BOT NO ESTÁ BANEADA EN ESTE CHAT!*')
 chat.isBanned = false
-await conn.reply(m.chat, lenguajeYL['smsAvisoEG']() + '🍒 *¡La Bot Ya Responderá A Los Comandos!*', m)
+await conn.reply(m.chat, lenguajeYL['smsAvisoEG']() + '🍒 *¡LA BOT YA FUÉ DESBANEADA EN ESTE CHAT!*', m)
 }
 handler.command = /^unbanchat|desbanearchat|desbanchat$/i
 handler.owner = true 
