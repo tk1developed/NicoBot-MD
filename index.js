4b14caa406e97d0a0bff8331c72779ee264b35c3
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-06-01 13:22:15 -0500

    Actualizar index.js

--- index.js
+++ index.js
@@ -1,50 +1,47 @@
+console.log('❤ Iniciando...')
 import { join, dirname } from 'path'
-import { createRequire } from 'module';
+import { createRequire } from 'module'
 import { fileURLToPath } from 'url'
 import { setupMaster, fork } from 'cluster'
 import { watchFile, unwatchFile } from 'fs'
-import cfonts from 'cfonts';
+import cfonts from 'cfonts'
 import { createInterface } from 'readline'
 import yargs from 'yargs'
-import express from 'express'
-import chalk from 'chalk'
-import path from 'path'
-import os from 'os'
-import { promises as fsPromises } from 'fs'
 
 // https://stackoverflow.com/a/50052194
 const __dirname = dirname(fileURLToPath(import.meta.url))
-const require = createRequire(__dirname) //Incorpora la capacidad de crear el método 'requerir'
-const { name, author } = require(join(__dirname, './package.json')) //https://www.stefanjudis.com/snippets/how-to-import-json-files-in-es-modules-node-js/
+const require = createRequire(__dirname) // Bring in the ability to create the 'require' method
+const { name, author } = require(join(__dirname, './package.json')) // https://www.stefanjudis.com/snippets/how-to-import-json-files-in-es-modules-node-js/
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-//const app = express()
-//const port = process.env.PORT || 8080;
-
-say('Yoshiko\nBot\nMD', {
+say('Yartex\nBot\nMD', {
 font: 'block',
 align: 'center',
-colors: ['system', 'redBright']
+colors: ['magenta', 'cyan']
 })
-say(`la mejor bot de whatsapp, by: yoshiko`, {
+say(`Bot: ${name}\nVersion: 1.0.5\nCreador: GataNina-Li\nEmail: centergatabot@gmail.com`, {
 font: 'console',
 gradient: ['blue', 'magenta']
 })
 
 var isRunning = false
-
-async function start(file) {
+/**
+ * Start a js file
+ * @param {String} file `path/to/file`
+ */
+function start(file) {
 if (isRunning) return
 isRunning = true
-const currentFilePath = new URL(import.meta.url).pathname
 let args = [join(__dirname, file), ...process.argv.slice(2)]
 say([process.argv[0], ...args].join(' '), {
 font: 'console',
 align: 'center',
-gradient: ['red', 'magenta']
+gradient: ['blue', 'magenta']
 })
-setupMaster({exec: args[0], args: args.slice(1),
+setupMaster({
+exec: args[0],
+args: args.slice(1),
 })
 let p = fork()
 p.on('message', data => {
@@ -57,13 +54,12 @@ break
 case 'uptime':
 p.send(process.uptime())
 break
-}})
-
+}
+})
 p.on('exit', (_, code) => {
 isRunning = false
-console.error('⚠️ ERROR ⚠️ >> ', code)
-start('main.js'); //
-
+console.error('⚠️ Ocurrió un error inesperado:', code)
+process.exit();
 if (code === 0) return
 watchFile(args[0], () => {
 unwatchFile(args[0])
