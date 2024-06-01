91aa7fea76796e484748589bcf16f91816761adc
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-06-01 11:52:15 -0500

    Actualizar owner-banchat.js

--- plugins/owner-banchat.js
+++ plugins/owner-banchat.js
@@ -1,7 +1,7 @@
 let handler = async (m) => {
 
 global.db.data.chats[m.chat].isBanned = true
-conn.reply(m.chat, `✅ *La Bot Ha Sido Desactivado En Este Grupo*`, m, fake, )
+conn.reply(m.chat, `✅ *La Bot Ha Sido Desactivada En Este Chat*`, m, fake, )
 
 }
 handler.help = ['banchat']
@@ -9,7 +9,7 @@ handler.tags = ['owner']
 handler.command = /^banchat$/i
 
 handler.botAdmin = true
-handler.admin = true 
+//handler.admin = true 
 handler.group = true
 
 export default handler
