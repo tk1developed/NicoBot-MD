12701b63d8c215a5c99c8be09bd662dcef8b03aa
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-06-01 09:35:38 -0500

    Actualizar gc-delete.js

--- plugins/gc-delete.js
+++ plugins/gc-delete.js
@@ -13,9 +13,9 @@ const handler = async (m, {conn, usedPrefix, command}) => {
 handler.help = ['del', 'delete'];
 handler.tags = ['group'];
 handler.command = /^del(ete)?$/i;
-handler.group = true;
-handler.admin = true;
-handler.botAdmin = true;
+//handler.group = true;
+//handler.admin = true;
+//handler.botAdmin = true;
 export default handler;
 
 /* let handler = function (m) {
