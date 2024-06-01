271d6c6e4c32426475ebba6ecae7838c2ee7c92e
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-06-01 11:53:41 -0500

    Actualizar owner-banchat.js

--- plugins/owner-banchat.js
+++ plugins/owner-banchat.js
@@ -7,5 +7,5 @@ conn.reply(m.chat, `✅ *La Bot Ha Sido Desactivada En Este Chat*`, m, fake, )
 handler.help = ['banchat']
 handler.tags = ['owner']
 handler.command = /^banchat$/i
-
+handler.owner = true 
 export default handler
