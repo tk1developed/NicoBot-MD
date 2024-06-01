730117627e317cae97ed21ba1deac5a4aed31d13
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-06-01 13:18:22 -0500

    Actualizar owner-restart.js

--- plugins/owner-restart.js
+++ plugins/owner-restart.js
@@ -5,7 +5,7 @@ var handler = async (m, { conn, isROwner, text }) => {
 if (!process.send) throw 'Dont: node main.js\nDo: node index.js'
 if (conn.user.jid == conn.user.jid) {
 await conn.reply(m.chat, '🍬 *R E I N I C I A N D O*', m, fake, )
-process.send('reinciar')
+process.send('reset')
 } else throw 'eh'
 
 }
