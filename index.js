40b0ba37bb55924558efdf40ac8e08033a9aae7a
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-06-01 13:12:50 -0500

    Actualizar index.js

--- index.js
+++ index.js
@@ -45,9 +45,6 @@ align: 'center',
 gradient: ['red', 'magenta']
 })
 setupMaster({exec: args[0], args: args.slice(1),
-setupMaster({
-exec: args[0],
-args: args.slice(1),
 })
 let p = fork()
 p.on('message', data => {
@@ -60,8 +57,8 @@ break
 case 'uptime':
 p.send(process.uptime())
 break
-}
-})
+}})
+
 p.on('exit', (_, code) => {
 isRunning = false
 console.error('⚠️ ERROR ⚠️ >> ', code)
