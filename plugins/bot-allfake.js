ea7d691aa86af8974e75a49983f017577dfeeae9
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-06-01 19:20:14 -0500

    Actualizar bot-allfake.js

--- plugins/bot-allfake.js
+++ plugins/bot-allfake.js
@@ -46,7 +46,7 @@ var img6 = fs.readFileSync('./media/menus/MiniYoshi.jpg');
 var img7 = fs.readFileSync('./media/menus/MiniBot.jpg');
 var img8 = fs.readFileSync('./media/menus/Mini.jpg');
 
-global.yoshiImg = [img6].getRandom()
+global.yoshiImg = [img7].getRandom()
 
 global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: packname, orderTitle: 'Bang', thumbnail: yoshiImg, sellerJid: '0@s.whatsapp.net' }}}
 
