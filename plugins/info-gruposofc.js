83f759a6758d9ed42a5b67f27546a66b9c2acd08
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-06-01 23:09:29 -0500

    Actualizar info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -2,23 +2,30 @@ let media = './Menu.png'
 let handler = async (m, { conn, command }) => {
 let str = `*◜🍬 GRUPOS OFICIALES 🍬◞*
 
-_❏ ${gp1}_
+❏ *YoshikoBot-MD 1*
+_${gp1}_
 
-_❏ ${gp2}_
+❏ *YoshikoBot-MD 2*
+_${gp2}_
 
  *◜🌸 COLABORACIÓN 🌸◞*
 
-_❏ ${gp3}_
+❏ *GataBot & YoshikoBot*
+_${gp3}_
 
-_❏ ${gp4}_
+❏ *CuriosityBot & YoshikoBot*
+_${gp4}_
 
-_❏ ${gp5}_
+❏ *NovaBot & Yoshiko*
+_${gp5}_
 
-_❏ ${gp6}_
+❏ *Multi Bots*
+_${gp6}_
 
  *◜🌺 CANAL OFICIAL 🌺◞*
 
-_❏ ${channel}_`
+❏ *YoshikoBot-MD*
+_${channel}_`
 m.react('🔵') 
     await conn.sendFile(m.chat, media, 'yoshiko.mp4', str, fkontak)}
 
