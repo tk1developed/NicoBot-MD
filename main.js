bb11e458b69fba2272bf600c43cf9f7fcf080c87
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-06-02 20:15:32 -0500

    Actualizar main.js

--- main.js
+++ main.js
@@ -315,25 +315,34 @@ if (opcion == '1' || methodCodeQR) {
     await conn.groupAcceptInvite(`${gp2}`)
 console.log(chalk.bold.cyan('\n╭┈ ┈ ┈ ┈ ┈ • 𝗬𝗼𝘀𝗵𝗶𝗸𝗼𝗕𝗼𝘁-𝗠𝗗 🍂 • ┈ ┈ ┈ ┈ ┈ ┈╮\n┊ LA BOT YA ESTÁ CONECTADA AL WHATSAPP 🟢\n╰┈ ┈ ┈ ┈ ┈ ┈ ┈ ┈ ┈ ┈ ┈ ┈ ┈ ┈ ┈ ┈ ┈ ┈ ┈ ┈ ┈╯\n'))}
 let reason = new Boom(lastDisconnect?.error)?.output?.statusCode;
-if (connection === 'close') {
 if (reason == 405) {
 await fs.unlinkSync("./YoshiSession/" + "creds.json")
+console.log(chalk.bold.redBright(`⚠️ Conexión replazada, Por favor espere un momento me voy a reiniciar...\nSi aparecen error vuelve a iniciar con : npm start`)) 
+process.send('reset')}
+if (connection === 'close') {
+if (reason === DisconnectReason.badSession) {
 conn.logger.error(`⚠️ Sesión incorrecta, por favor elimina la carpeta ${global.authFile} y escanea nuevamente.`);
 } else if (reason === DisconnectReason.connectionClosed) {
-conn.logger.warn(`⚠️ Conexión cerrada, reconectando...`)
+conn.logger.warn(`⚠️ Conexión cerrada, reconectando...`);
+await global.reloadHandler(true).catch(console.error);
 } else if (reason === DisconnectReason.connectionLost) {
+conn.logger.warn(`⚠️ Conexión perdida con el servidor, reconectando...`);
+await global.reloadHandler(true).catch(console.error);
 } else if (reason === DisconnectReason.connectionReplaced) {
-conn.logger.error(`⚠️ Conexión reemplazada, se ha abierto otra nueva sesión. Por favor, cierra la sesión actual primero.`)
+conn.logger.error(`⚠️ Conexión reemplazada, se ha abierto otra nueva sesión. Por favor, cierra la sesión actual primero.`);
 } else if (reason === DisconnectReason.loggedOut) {
-conn.logger.error(`⚠️ Conexion cerrada, por favor elimina la carpeta ${global.authFile} y escanea nuevamente.`)
+conn.logger.error(`⚠️ Conexion cerrada, por favor elimina la carpeta ${global.authFile} y escanea nuevamente.`);
 } else if (reason === DisconnectReason.restartRequired) {
-conn.logger.info(`⚠️ Reinicio necesario, reinicie el servidor si presenta algún problema.`)
+conn.logger.info(`⚠️ Reinicio necesario, reinicie el servidor si presenta algún problema.`);
+await global.reloadHandler(true).catch(console.error);
 } else if (reason === DisconnectReason.timedOut) {
-conn.logger.warn(`⚠️ Tiempo de conexión agotado, reconectando...`)
-await global.reloadHandler(true).catch(console.error)
+conn.logger.warn(`⚠️ Tiempo de conexión agotado, reconectando...`);
+await global.reloadHandler(true).catch(console.error);
 } else {
-conn.logger.warn(`⚠️ Razón de desconexión desconocida. ${reason || ''}: ${connection || ''}`)
-}}
+conn.logger.warn(`⚠️ Razón de desconexión desconocida. ${reason || ''}: ${connection || ''}`);
+await global.reloadHandler(true).catch(console.error);
+    }
+}
 
   /*if (connection == 'close') {
     console.log(chalk.yellow(`🚩ㅤConexion cerrada, por favor borre la carpeta ${global.authFile} y reescanee el codigo QR`));
