27852f85d54895c987b72afbdee682118ad3ddfc
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-06-04 09:18:23 -0500

    Actualizar

---
+++ koyeb.js
@@ -0,0 +1,16 @@
+FROM fedora:37
+
+RUN sudo dnf -y update &&\
+sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm &&\
+sudo dnf install -y git ffmpeg ImageMagick nodejs yarnpkg libwebp &&\
+sudo dnf clean all -y
+
+RUN git clone https://github.com/Diego-YL-177/YoshikoBot-MD
+
+WORKDIR /root/YoshikoBot-MD
+
+COPY ./root/YoshikoBot-MD
+
+RUN yarn install
+
+CMD ["node", "index.js"]
