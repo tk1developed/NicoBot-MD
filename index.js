db8f3113270dee1e456fb0952c606e1136a20d62
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-06-04 19:17:21 -0500

    Actualizar index.js

--- index.js
+++ index.js
@@ -1,50 +1,47 @@
+console.log('🚀 Iniciando...')
 import { join, dirname } from 'path'
-import { createRequire } from 'module';
+import { createRequire } from 'module'
 import { fileURLToPath } from 'url'
 import { setupMaster, fork } from 'cluster'
 import { watchFile, unwatchFile } from 'fs'
-import cfonts from 'cfonts';
+import cfonts from 'cfonts'
 import { createInterface } from 'readline'
 import yargs from 'yargs'
-import express from 'express'
-import chalk from 'chalk'
-import path from 'path'
-import os from 'os'
-import { promises as fsPromises } from 'fs'
 
 // https://stackoverflow.com/a/50052194
 const __dirname = dirname(fileURLToPath(import.meta.url))
-const require = createRequire(__dirname) //Incorpora la capacidad de crear el método 'requerir'
-const { name, author } = require(join(__dirname, './package.json')) //https://www.stefanjudis.com/snippets/how-to-import-json-files-in-es-modules-node-js/
+const require = createRequire(__dirname) // Bring in the ability to create the 'require' method
+const { name, author } = require(join(__dirname, './package.json')) // https://www.stefanjudis.com/snippets/how-to-import-json-files-in-es-modules-node-js/
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
-
-//const app = express()
-//const port = process.env.PORT || 8080;
 
 say('Yoshiko\nBot\nMD', {
 font: 'block',
 align: 'center',
 colors: ['system', 'redBright']
 })
-say(`la mejor bot de whatsapp, by: yoshiko`, {
+say(`Bot:${name}\nCreador: Diego-YL-177\nCorreo: theyoshikobot@gmail.com`, {
 font: 'console',
 gradient: ['blue', 'magenta']
 })
 
 var isRunning = false
-
-async function start(file) {
+/**
+ * Start a js file
+ * @param {String} file `path/to/file`
+ */
+function start(file) {
 if (isRunning) return
 isRunning = true
-const currentFilePath = new URL(import.meta.url).pathname
 let args = [join(__dirname, file), ...process.argv.slice(2)]
 say([process.argv[0], ...args].join(' '), {
 font: 'console',
 align: 'center',
-gradient: ['red', 'magenta']
+gradient: ['blue', 'magenta']
 })
-setupMaster({exec: args[0], args: args.slice(1),
+setupMaster({
+exec: args[0],
+args: args.slice(1),
 })
 let p = fork()
 p.on('message', data => {
@@ -57,51 +54,23 @@ break
 case 'uptime':
 p.send(process.uptime())
 break
-}})
-
+}
+})
 p.on('exit', (_, code) => {
 isRunning = false
-console.error('⚠️ ERROR ⚠️ >> ', code)
-start('main.js'); //
-
+console.error('⚠️ Ocurrió un error inesperado:', code)
+process.exit();
 if (code === 0) return
 watchFile(args[0], () => {
 unwatchFile(args[0])
 start(file)
-})})
-
-//const ramInYL = os.totalmem() / (1024 * 1024 * 1024)
-//const freeRamInYL = os.freemem() / (1024 * 1024 * 1024)
-const packageJsonPath = path.join(path.dirname(currentFilePath), './package.json')
-try {
-const packageJsonData = await fsPromises.readFile(packageJsonPath, 'utf-8')
-const packageJsonObj = JSON.parse(packageJsonData)
-const currentTime = new Date().toLocaleString()
-let lineM = '⋯ ⋯ ⋯ ⋯ ⋯ ⋯ ⋯ ⋯ ⋯ ⋯ ⋯ 》'
-console.log(chalk.yellow(`╭${lineM}
-┊${chalk.blueBright('╭┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅')}
-┊${chalk.blueBright('┊')} ${chalk.blue.bold(`🔵 INFORMACIÓN :`)}
-┊${chalk.blueBright('┊')} ${chalk.blueBright('┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅')} 
-┊${chalk.blueBright('┊')}${chalk.cyan(`💚 Nombre: ${packageJsonObj.name}`)}
-┊${chalk.blueBright('┊')}${chalk.cyan(`𓃠 Versión: ${packageJsonObj.version}`)}
-┊${chalk.blueBright('┊')}${chalk.cyan(`🍓 Descripción: ${packageJsonObj.description}`)}
-┊${chalk.blueBright('┊')}${chalk.cyan(`👑 Project Author: Diego-YL-177 (@diego_ofc)`)}
-┊${chalk.blueBright('┊')}${chalk.blueBright('┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅')} 
-┊${chalk.blueBright('┊')}${chalk.yellow(`🍒 Colaboradores:`)}
-┊${chalk.blueBright('┊')}${chalk.yellow(`• elrebelde21 (Mario ofc)`)}
-┊${chalk.blueBright('┊')}${chalk.yellow(`• GataNina-Li (gata_dios)`)}
-┊${chalk.blueBright('┊')}${chalk.yellow(`• AzamiJs (Azami)`)}
-┊${chalk.blueBright('╰┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅')} 
-╰${lineM}`));
-setInterval(() => {}, 1000)
-} catch (err) {
-console.error(chalk.red(`❌ No se pudo leer el archivo package.json: ${err}`))
-}
-
+})
+})
 let opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse())
 if (!opts['test'])
 if (!rl.listenerCount()) rl.on('line', line => {
 p.emit('message', line.trim())
-})}
+})
+}
 
 start('main.js')
