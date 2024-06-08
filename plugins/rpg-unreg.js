4ddfdc0b755ab0c83b20df0ba09f5f9c8543ecbd
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-06-08 11:05:23 -0500

    Actualizar rpg-unreg.js

--- plugins/rpg-unreg.js
+++ plugins/rpg-unreg.js
@@ -1,15 +1,13 @@
-import {createHash} from 'crypto';
-const handler = async function(m, {args}) {
-  if (!args[0]) throw '*⚠️ Ingrese Su Número De Série, Si No Sabe Cual Es Use #myns*';
-  const user = global.db.data.users[m.sender];
-  const sn = createHash('md5').update(m.sender).digest('hex');
-  if (args[0] !== sn) throw '*⚠️ Verifique Que Su Número De Série Sea Correcto*';
-  user.registered = false;
-  m.reply(`✅ *Registro eliminado*`);
-};
-handler.help = ['', 'ister'].map((v) => 'unreg' + v + ' <numero de serie>');
-handler.tags = ['xp'];
-handler.command = /^unreg(ister)?$/i;
-handler.register = true;
-export default handler;
+let handler = async (m, { conn, text }) => {
 
+let user = global.db.data.users[m.sender]
+
+user.registered = false
+m.reply(`*『✅』Ya no estas registrado en NaufraZappBot*`)
+
+}
+handler.help = ['unreg']
+handler.tags = ['xp']
+handler.command = /^unreg(ister)?$/i
+handler.register = true
+export default handler
