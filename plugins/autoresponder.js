b9e853d8792df17e8150e307e5b2d8b8bd6d2375
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-06-08 08:35:39 -0500

    Actualizar autoresponder.js

--- plugins/autoresponder.js
+++ plugins/autoresponder.js
@@ -9,7 +9,7 @@ if (m.mentionedJid.includes(this.user.jid) && m.isGroup && !chat.isBanned) {
 let noetiqueta = 'https://qu.ax/MKCm.webp'
 let or = ['texto', 'sticker']; 
 let media = or[Math.floor(Math.random() * 2)]
-if (media === 'sticker') return await this.sendFile(m.chat, noetiqueta, 'sticker.webp', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: 'Yo que?', mediaType: 2, sourceUrl: redes.getRandom(), thumbnail: yoshiImg }}}, { quoted: m, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100})
+if (media === 'sticker') return await this.sendFile(m.chat, noetiqueta, 'sticker.webp', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: 'Yo que?', mediaType: 2, sourceUrl: redesYishi, thumbnail: yoshiImg }}}, { quoted: m, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100})
 if (media === 'texto') return await this.sendMessage(m.chat, {text: ['*QUE YO QUE?*', 'Que?', 'Hola?'].getRandom()}, {quoted: m, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100})}
 return !0 }
 export default handler
