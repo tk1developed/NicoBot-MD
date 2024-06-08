2cc73b9117e0d9491f569647cb6835b94a41581f
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-06-08 09:05:36 -0500

    Actualizar _autosticker.js

--- plugins/_autosticker.js
+++ plugins/_autosticker.js
@@ -1,38 +1,37 @@
-import {sticker} from '../lib/sticker.js';
+import { sticker } from '../lib/sticker.js'
 
-const handler = (m) => m;
+let handler = m => m
 
-handler.all = async function(m) {
-  const chat = db.data.chats[m.chat];
-  const user = db.data.users[m.sender];
+handler.all = async function (m) {
+let chat = db.data.chats[m.chat]
+let user = db.data.users[m.sender]
 
-  if (chat.autosticker && m.isGroup) {
-    const q = m;
-    let stiker = false;
-    const mime = (q.msg || q).mimetype || q.mediaType || '';
-    if (/webp/g.test(mime)) return;
-    if (/image/g.test(mime)) {
-      const img = await q.download?.();
-      if (!img) return;
-      stiker = await sticker(img, false, packname, author);
-    } else if (/video/g.test(mime)) {
-      if (/video/g.test(mime)) if ((q.msg || q).seconds > 8) return await m.reply(`*🐈 𝑬𝒍 𝑽𝒊𝒅𝒆𝒐 𝑵𝒐 𝑷𝒖𝒆𝒅𝒆 𝑫𝒖𝒓𝒂𝒓 𝑴𝒂𝒔 𝑫𝒆 7 𝑺𝒆𝒈𝒖𝒏𝒅𝒐𝒔*\n\n𝑃𝑎𝑟𝑎 𝐷𝑒𝑠𝑎𝑐𝑡𝑖𝑣𝑎𝑟 𝐸𝑙 𝐴𝑢𝑡𝑜𝑠𝑡𝑖𝑐𝑘𝑒𝑟 𝐸𝑠𝑐𝑟𝑖𝑏𝑎: (#𝚍𝚒𝚜𝚊𝚋𝚕𝚎 𝚊𝚞𝚝𝚘𝚜𝚝𝚒𝚌𝚔𝚎𝚛)`);
-      // await this.sendButton(m.chat, '*🐱 𝑯𝒆𝒚 𝑬𝒍 𝑽𝒊𝒅𝒆𝒐 𝑵𝒐 𝑷𝒖𝒆𝒅𝒆 𝑫𝒖𝒓𝒂𝒓 𝑴𝒂𝒅 𝑫𝒆 7 𝑺𝒆𝒈𝒖𝒏𝒅𝒐𝒔*', wm, [['𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰𝚁 𝙰𝚄𝚃𝙾𝚂𝚃𝙸𝙲𝙺𝙴𝚁', '/disable autosticker']], m)
-      const img = await q.download();
-      if (!img) return;
-      stiker = await sticker(img, false, packname, author);
-    } else if (m.text.split(/\n| /i)[0]) {
-      if (isUrl(m.text)) stiker = await sticker(false, m.text.split(/\n| /i)[0], packname, author);
-      else return;
-    }
-    if (stiker) {
-      await mconn.conn.sendFile(m.chat, stiker, null, {asSticker: true});
-    }
-  }
-  return !0;
-};
-export default handler;
+if (chat.autosticker && m.isGroup) {
+let q = m
+let stiker = false
+let mime = (q.msg || q).mimetype || q.mediaType || ''
+if (/webp/g.test(mime)) return
+if (/image/g.test(mime)) {
+let img = await q.download?.()
+if (!img) return
+stiker = await sticker(img, false, packname, author)
+} else if (/video/g.test(mime)) {
+if (/video/g.test(mime)) if ((q.msg || q).seconds > 8) return await m.reply('᥀·࣭࣪̇˖✨️◗  *El video no debe de durar mas de 7 segundos, intentalo de nuevo.*')
+
+let img = await q.download()
+if (!img) return
+stiker = await sticker(img, false, packname, author)
+} else if (m.text.split(/\n| /i)[0]) {
+if (isUrl(m.text)) stiker = await sticker(false, m.text.split(/\n| /i)[0], packname, author)
+else return
+}
+if (stiker) {
+await conn.sendFile(m.chat, stiker, 'sticker.webp', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: wm, body: '᥀·࣭࣪̇˖🌸◗ 𝗖𝗿𝗲𝗮𝗿 𝗦𝘁𝗶𝗰𝗸𝗲𝗿𝘀 𝗔𝘂𝘁𝗼𝗺𝗮𝘁𝗶𝗰𝗼𝘀!!!',  mediaType: 2, thumbnail: yoshiImg, sourceUrl: redesYoshi }}}, { quoted: m })
+//this.sendFile(m.chat, stiker, null, { asSticker: true })
+}}
+return !0
+}
+export default handler
 
 const isUrl = (text) => {
-  return text.match(new RegExp(/^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)(jpe?g|gif|png|mp4)/, 'gi'));
-};
+return text.match(new RegExp(/^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)(jpe?g|gif|png|mp4)/, 'gi'))}
