c9d26a18cf96620f4cd1061abd2e22d013ba0c82
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-06-09 17:33:44 -0500

    Actualizar rpg-verificar.js

--- plugins/rpg-verificar.js
+++ plugins/rpg-verificar.js
@@ -1,6 +1,66 @@
 import { createHash } from 'crypto'
 let Reg = /\|?(.*)([.|] *?)([0-9]*)$/i
 let handler = async function (m, { conn, text, usedPrefix, command }) {
+let user = global.db.data.users[m.sender]
+let name2 = conn.getName(m.sender)
+if (user.registered === true) throw `*『✦』Ya estas registrado, para volver a registrarte, usa el comando: #myns*`
+if (!Reg.test(text)) throw `*『✦』El comando ingresado es incorrecto, uselo de la siguiente manera:*\n\n#reg *Nombre.edad*\n\n\`\`\`Ejemplo:\`\`\`\n#reg *Diego.18*`
+let [_, name, splitter, age] = text.match(Reg)
+if (!name) throw '*『✦』No puedes registrarte sin nombre, el nombre es obligatorio. Inténtelo de nuevo.*'
+if (!age) throw '*『✦』No puedes registrarte sin la edad, la edad es opcional. Inténtelo de nuevo.*'
+if (name.length >= 30) throw '*『✦』El nombre no debe de tener mas de 30 caracteres.*' 
+age = parseInt(age)
+if (age > 999) throw '*『😏』Viejo/a Sabroso/a*'
+if (age < 5) throw '*『🍼』Ven aquí, te adoptare!!*'
+user.name = name.trim()
+user.age = age
+user.regTime = + new Date
+user.registered = true
+global.db.data.users[m.sender].money += 600
+global.db.data.users[m.sender].diamond += 10
+global.db.data.users[m.sender].exp += 245
+global.db.data.users[m.sender].joincount += 5
+let sn = createHash('md5').update(m.sender).digest('hex').slice(0, 6)        
+m.react('📩') 
+let img = yoshiImg;
+let regbot = `👤 𝗥 𝗘 𝗚 𝗜 𝗦 𝗧 𝗥 𝗢 👤
+•┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄•
+「💭」𝗡𝗼𝗺𝗯𝗿𝗲:
+${name}
+
+「✨️」𝗘𝗱𝗮𝗱:
+${age} años
+•┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄•
+「🎁」𝗥𝗲𝗰𝗼𝗺𝗽𝗲𝗻𝘀𝗮𝘀:
+• 10 Diamantes 💎
+• 5 YoshiCoins 💰
+• 245 Experiencia 💸
+• 12 Tokens 🪙
+•┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄•
+${packname}`
+conn.sendMessage(m.chat, {
+text: regbot,
+contextInfo: {
+externalAdReply: {
+title: '⊱『✅𝆺𝅥 𝗥𝗘𝗚𝗜𝗦𝗧𝗥𝗔𝗗𝗢(𝗔) 𝆹𝅥✅』⊰',
+body: wm, 
+thumbnaiUrl: img, 
+sourceUrl: canalesYoshi,
+mediaType: 1,
+showAdAttribution: true,
+renderLargerThumbnail: true,
+}}}, { quoted: fkontak })
+await m.reply(`${sn}`)        
+}
+handler.help = ['reg']
+handler.tags = ['rg']
+handler.command = ['verify', 'verificar', 'reg', 'register', 'registrar'] 
+
+export default handler
+
+/*import { createHash } from 'crypto'
+let Reg = /\|?(.*)([.|] *?)([0-9]*)$/i
+let handler = async function (m, { conn, text, usedPrefix, command }) {
 let user = db.data.users[m.sender]
 let totalreg = Object.keys(global.db.data.users).length
 let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length
@@ -41,4 +101,4 @@ await m.reply(`${sn}`)}
 handler.help = ['reg']
 handler.tags = ['rg']
 handler.command = /^(verify|verificar|registrar|reg(ister)?)$/i
-export default handler
+export default handler*/
