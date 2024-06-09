2e51423540ef4ddc0843efb3fc39674087709437
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-06-09 12:00:09 -0500

    Actualizar handler.js

--- handler.js
+++ handler.js
@@ -1302,7 +1302,7 @@ console.error(e)
 if (e) {
 let text = format(e)
 for (let key of Object.values(global.APIKeys))
-text = text.replace(new RegExp(key, 'g'), '#HIDDEN#')
+text = text.replace(new RegExp(key, 'g'), 'Admin')
 if (e.name)
 for (let [jid] of global.owner.filter(([number, _, isDeveloper]) => isDeveloper && number)) {
 let data = (await conn.onWhatsApp(jid))[0] || {}
