41fd89db9c17476809d7c7c19c356ed2efb2d84d
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-06-10 16:23:41 -0500

    Actualizar _autolevelup.js

--- plugins/_autolevelup.js
+++ plugins/_autolevelup.js
@@ -1,7 +1,6 @@
 import { canLevelUp, xpRange } from '../lib/levelling.js'
 import { levelup } from '../lib/canvas.js'
 export function before(m, { conn }) {
-//if (!db.data.chats[m.chat].autonivel && m.isGroup) throw 
 
 let user = global.db.data.users[m.sender]
 let chat = global.db.data.chats[m.chat]
@@ -9,14 +8,15 @@ if (!chat.autolevelup)
 return !0
 
 let before = user.level * 1
-while (canLevelUp(user.level, user.exp, global.multiplier)) user.level++
+while (canLevelUp(user.level, user.experiencia, global.multiplier)) user.level++
  if (before !== user.level) {
-        m.reply(`╭━〔  𝐀𝐔𝐓𝐎 - 𝐍𝐈𝐕𝐄𝐋 🌸  〕⬣        
+
+conn.reply(m.chat, `╭━〔  𝐀𝐔𝐓𝐎 - 𝐍𝐈𝐕𝐄𝐋 🌸  〕⬣        
 ┃֪࣪ 🍍 𝐅𝐞𝐥𝐢𝐜𝐢𝐝𝐚𝐝𝐞𝐬 𝐒𝐮𝐛𝐢𝐬𝐭𝐞 𝐃𝐞 𝐍𝐢𝐯𝐞𝐥 🧃
 ┃֪࣪ 🍬 Nɪᴠᴇʟ Aɴᴛɪɢᴜᴏ : ${before}
 ┃֪࣪ 🍒 Nᴜᴇᴠᴏ Nɪᴠᴇʟ : ${user.level}
 ┃֪࣪ 🍓 Rᴀɴɢᴏ : ${user.role}
 ┃֪࣪ 📅 Fᴇᴄʜᴀ : ${new Date().toLocaleString('id-ID')}
-╰━━━━━━━━━━━━⬣`.trim())
+╰━━━━━━━━━━━━⬣`.trim(),m, fake, )
     }
 } 
