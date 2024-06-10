fe0a6d3cd804767b72c4f6f2eba1c871d1706d21
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-06-10 08:23:31 -0500

    Actualizar fun-ruletas.js

--- plugins/fun-ruletas.js
+++ plugins/fun-ruletas.js
@@ -1,13 +1,37 @@
-const handler = async (m, {conn, text, isPrems}) =>  {
-let kick = ['ban1', 'ban2']; 
-let rulekick = kick[Math.floor(Math.random() * 6)]
-if (rulekick === 'ban1') return conn.groupParticipantsUpdate(m.chat, [m.sender], 'addowner').catch(m.reply(`💭 *JAJJAJA WAOZ TE GANAS OWNER ASTA QUE ME REINICIE.* 🎁`)) 
-
-if (rulekick === 'ban2') return conn.groupParticipantsUpdate(m.chat, [m.sender], 'kick').catch(m.reply(`😹 *QUE MALA SUERTE TIENES JAJAJ, ADIOS.* 😹😹😹`)) 
+//CÓDIGO CREADO POR elrebelde21 : https://github.com/elrebelde21
+const handler = async (m, {conn, text, isPrems}) => {
+if (!db.data.chats[m.chat].game) throw `𝙀𝙨𝙩𝙚 𝙟𝙪𝙚𝙜𝙤𝙨 𝙚𝙨𝙩𝙖 𝙙𝙚𝙨𝙖𝙘𝙩𝙞𝙫𝙖𝙙𝙤 𝙥𝙤𝙧 𝙡𝙤𝙨 𝙖𝙙𝙢𝙞𝙣𝙨 𝙙𝙚𝙡 𝙂𝙧𝙪𝙥𝙤 𝙨𝙞 𝙩𝙪 𝙚𝙧𝙚𝙨 𝙖𝙙𝙢𝙞𝙣𝙨 𝙮 𝙦𝙪𝙞𝙚𝙧𝙚 𝙖𝙘𝙩𝙞𝙫𝙖𝙧𝙡𝙤 𝙪𝙨𝙖𝙧: #enable juegos` 
+const date = global.db.data.users[m.sender].juegos + 10800000; //10800000 = 3 hs 
+if (new Date - global.db.data.users[m.sender].juegos < 10800000) throw `『⏰』𝙀𝙨𝙥𝙚𝙧𝙖 : ${msToTime(date - new Date())} 𝙥𝙖𝙧𝙖 𝙫𝙤𝙡𝙫𝙚𝙧 𝙖 𝙟𝙪𝙜𝙖𝙧 `
+if (global.db.data.users[m.sender].exp < 0 || global.db.data.users[m.sender].money < 0 || global.db.data.users[m.sender].limit < 0) return m.reply(`[ ⚠️ ] 𝙉𝙊 𝙏𝙄𝙀𝙉𝙀 𝙎𝙐𝙁𝙄𝘾𝙄𝙀𝙉𝙏𝙀𝙎 𝙍𝙀𝘾𝙐𝙍𝙎𝙊 🐈`)
+let user = global.db.data.users[m.sender]
+const prem = Math.floor(Math.random() * 3600000) 
+const exp = Math.floor(Math.random() * 8500)
+const diamond = Math.floor(Math.random() * 130)
+const money = Math.floor(Math.random() * 8500)
+let rulet = ['ban2']; 
+let ruleta = rulet[Math.floor(Math.random() * 6)]
+global.db.data.users[m.sender].juegos = new Date * 1;
+if (ruleta === 'ban2') return conn.groupParticipantsUpdate(m.chat, [m.sender], 'kick').catch(m.reply(`😹 *QUE MALA SUERTE TIENES JAJAJ, ADIOS.* 😹😹😹`))  
 }
-handler.command = ['rulekick', 'kickrule']
-handler.botAdmin = true
-export default handler
+handler.help = ['suerte', 'ruleta', 'ruletas']
+handler.tags = ['fun']
+handler.command = /^(ruletas|ruleta|suerte)$/i
+handler.fail = null;
+handler.group = true
+handler.register = true
+export default handler;
+
+function msToTime(duration) {
+var milliseconds = parseInt((duration % 1000) / 100),
+seconds = Math.floor((duration / 1000) % 60),
+minutes = Math.floor((duration / (1000 * 60)) % 60),
+hours = Math.floor((duration / (1000 * 60 * 60)) % 24)
+hours = (hours < 10) ? "0" + hours : hours
+minutes = (minutes < 10) ? "0" + minutes : minutes
+seconds = (seconds < 10) ? "0" + seconds : seconds
+return hours + " Hora(s) " + minutes + " Minuto(s)"}
+
 
 function pickRandom(list) {
   return list[Math.floor(list.length * Math.random())];
