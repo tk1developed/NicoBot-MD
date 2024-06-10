25cb25c14fbfdb9e1d6e40a1cbe1f785a0b7eb2a
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-06-10 16:32:26 -0500

    Actualizar _autolevelup.js

--- plugins/_autolevelup.js
+++ plugins/_autolevelup.js
@@ -11,6 +11,6 @@ let before = user.level * 1
 while (canLevelUp(user.level, user.exp, global.multiplier)) user.level++
  if (before !== user.level) {
 
-conn.reply(m.chat, `☘️ *SUBISTE DE NIVEL*\n\n✰ *Nuevo Nivel ⪼* ${user.level}\n◈ *Nivel Anterior ⪼* ${before}\n◈ *Rango ⪼* ${user.rule}\n◈ *Fecha ⪼* ${new Date().toLocaleString('id-ID')}`.trim(),m, fake, )
+conn.reply(m.chat, `☘️ *SUBISTE DE NIVEL*\n\n✰ *Nuevo Nivel ⪼* ${user.level}\n◈ *Nivel Anterior ⪼* ${before}\n◈ *Rango ⪼* ${user.role}\n◈ *Fecha ⪼* ${new Date().toLocaleString('id-ID')}`.trim(),m, fake, )
     }
 } 
