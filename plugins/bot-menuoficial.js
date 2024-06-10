81fc54f3e2fcb3fac5ba442ac06a0254bff94aac
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-06-09 20:05:19 -0500

    Actualizar bot-menuoficial.js

--- plugins/bot-menuoficial.js
+++ plugins/bot-menuoficial.js
@@ -200,7 +200,8 @@ function clockString(ms) {
   let d = isNaN(ms) ? '--' : Math.floor(ms / 86400000)
   let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
   let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
-  return [d, h, m].map(v => v.toString().padStart(2, 0)).join(':')
+  let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+  return [d, h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
 }
 
   var ase = new Date();
