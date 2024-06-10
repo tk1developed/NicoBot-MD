5151eb4b62032bce54a72ab7c5a26e8d65ab71fe
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-06-10 08:34:07 -0500

    Actualizar gc-kick.js

--- plugins/gc-kick.js
+++ plugins/gc-kick.js
@@ -1,6 +1,6 @@
 var handler = async (m, { conn, participants, usedPrefix, command }) => {
     if (!m.mentionedJid[0] && !m.quoted) {
-        return conn.reply(m.chat, '🛑 *Etiqueta o responde al mensaje de la persona que quieres eliminar*', m);
+        return conn.reply(m.chat, '✨️ *Etiqueta o responde al mensaje de la persona que quieres eliminar*', m);
     }
 
     let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender;
@@ -10,15 +10,15 @@ var handler = async (m, { conn, participants, usedPrefix, command }) => {
     const ownerBot = global.owner[0][0] + '@s.whatsapp.net';
 
     if (user === conn.user.jid) {
-        return conn.reply(m.chat, '🛑 No puedo eliminar el bot del grupo', m);
+        return conn.reply(m.chat, '💭 No puedo eliminar el bot del grupo', m);
     }
 
     if (user === ownerGroup) {
-        return conn.reply(m.chat, '🛑 No puedo eliminar al propietario del grupo', m);
+        return conn.reply(m.chat, '💭 No puedo eliminar al propietario del grupo', m);
     }
 
     if (user === ownerBot) {
-        return conn.reply(m.chat, '🛑 No puedo eliminar al propietario del bot', m);
+        return conn.reply(m.chat, '💭 No puedo eliminar al propietario del bot', m);
     }
 
     await conn.groupParticipantsUpdate(m.chat, [user], 'remove');
