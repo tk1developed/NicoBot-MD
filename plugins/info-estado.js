bc3c97f345b25d625b534d40cf9863d3c9d56a9b
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-06-10 19:48:19 -0500

    Actualizar info-estado.js

--- plugins/info-estado.js
+++ plugins/info-estado.js
@@ -1,37 +1,28 @@
 const handler = async (m, {conn}) => {
-  try {
-    const pp = imagen6;
-    const img = await(await fetch('https://images3.alphacoders.com/125/1251707.png')).buffer();
     const _uptime = process.uptime() * 1000;
     const uptime = clockString(_uptime);
     const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
+    const sbot = (conn.user.jid == global.conn.user.jid ? 'Bot Oficial' : 'Sub-Bot');
 m.react('⚙️') 
-    const str = `╭━〔  𝐒𝐓𝐀𝐓𝐔𝐒 ✏️  〕⬣
-┃ *Yoshiko Bot - MD* 🚀
-┃
-┃ *👋🏻 Hola:* ${taguser}
-┃
-┃ ⏳️ *Activa:* ${uptime}
-┃ 🚩 *Bot Uso: 𝘗𝘶𝘣𝘭𝘪𝘤𝘰*
-┃ 🍨 *Owner: Diego*
-╰━━━━━━━━━━━━⬣`.trim();
-    if (m.isGroup) {
-      conn.sendMessage(m.chat, {text: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), contextInfo: {forwardingScore: 9999999, isForwarded: true, mentionedJid: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "renderLargerThumbnail": true, "title": global.titulowm, "containsAutoReply": true, "mediaType": 1, "thumbnail": pp, "mediaUrl": `https://github.com/Diego-YL-177/YoshikoBot-MD`, "sourceUrl": `https://github.com/Diego-YL-177/YoshikoBot-MD`}}}, {quoted: fkontak});
-    } else {
-      const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
-      conn.sendMessage(m.chat, {text: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), contextInfo: {forwardingScore: 9999999, isForwarded: true, mentionedJid: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "renderLargerThumbnail": true, "title": global.titulowm, "containsAutoReply": true, "mediaType": 1, "thumbnail": pp, "mediaUrl": `https://github.com/Diego-YL-177/YoshikoBot-MD`, "sourceUrl": `https://github.com/Diego-YL-177/YoshikoBot-MD`}}}, {quoted: fkontak2});
-    }
-  } catch {
-  }
+    const str = `☘️ *ESTADO DE:* ${global.botname}
+🕒 *Activa:* ${uptime}
+💭 *Tipo:* ${sbot}
+🥷 *Bot Uso:* Publico
+👑 *Owner:* Ofc.Diego`;
+      conn.reply(m.chat, str, m, fake, )
 };
+
 handler.help = ['estado'];
-handler.tags = ['main'];
-handler.command = /^(estado|status|estate|state|stado|stats|runtime|uptime)$/i;
+handler.tags = ['info'];
+handler.command = ['status', 'estado'];
+handler.registrado = true;
+
 export default handler;
+
 function clockString(ms) {
   const d = isNaN(ms) ? '--' : Math.floor(ms / 86400000);
   const h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24;
   const m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60;
   const s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60;
-  return [`\n┃ *💥 ` + d, ' Día(s)* ', `\n┃ *💫 ` + h, ' Hora(s)* ', `\n┃ *💠 ` + m, ' Minuto(s)* ', `\n┃ *♦ ` + s, ' Segundo(s)* '].map((v) => v.toString().padStart(2, 0)).join('');
+  return [`` + d, ' d ', `` + h, ' h ', `` + m, ' m ', `` + s, ' s'].map((v) => v.toString().padStart(2, 0)).join('');
 }
