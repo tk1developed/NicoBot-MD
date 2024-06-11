154ccb071e34cc0490139ed9e223eb4c3de8f2bf
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-06-10 22:26:43 -0500

    Actualizar velocidad.js

--- plugins/velocidad.js
+++ plugins/velocidad.js
@@ -20,7 +20,7 @@ let chats = Object.entries(conn.chats).filter(([id, data]) => id && data.isChats
 let groups = Object.entries(conn.chats).filter(([jid, chat]) => jid.endsWith('@g.us') && chat.isChats && !chat.metadata?.read_only && !chat.metadata?.announce).map(v => v[0])
 
 
-let texto = `☘️ *${global.botname}*
+let texto = `☘️ *${global.yl}*
 🚀 *Velocidad:*
 → ${latensi.toFixed(4)}
 
