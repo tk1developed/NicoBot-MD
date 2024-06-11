0e82a766f0cc27819927872715b6cfd5be982cdc
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-06-10 21:59:18 -0500

    Actualizar _autolevelup.js

--- plugins/_autolevelup.js
+++ plugins/_autolevelup.js
@@ -1,4 +1,4 @@
-/*import { canLevelUp, xpRange } from '../lib/levelling.js'
+import { canLevelUp, xpRange } from '../lib/levelling.js'
 import { levelup } from '../lib/canvas.js'
 export function before(m, { conn }) {
 
@@ -11,6 +11,6 @@ let before = user.level * 1
 while (canLevelUp(user.level, user.exp, global.multiplier)) user.level++
  if (before !== user.level) {
 
-conn.reply(m.chat, `🏆 *SUBISTE DE NIVEL*\n\n💭 *Nuevo Nivel:* ${user.level}\n✨️ *Nivel Anterior:* ${before}\n☘️ *Rango:* ${user.role}\n🗓 *Fecha:* ${new Date().toLocaleString('id-ID')}`.trim(), m, fake, )
+conn.reply(m.chat, `🏆 *SUBISTE DE NIVEL*\n\n💭 *Nuevo Nivel:* ${user.level}\n✨️ *Nivel Anterior:* ${before}\n☘️ *Rango:* ${user.role}\n🗓 *Fecha:* ${new Date().toLocaleString('id-ID')}`.trim(),m, fake, )
     }
-} */
+}
