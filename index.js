758336bf4bbac3dee6e3fb1853d136a48a24c3b6
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-06-10 21:52:51 -0500

    Actualizar index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const { name, author } = require(join(__dirname, './package.json'))
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-say('Yishiko\nBot-MD', {
+say('Yoshiko\nBot-MD', {
 font: 'block',
 align: 'center',
 colors: ['white']
