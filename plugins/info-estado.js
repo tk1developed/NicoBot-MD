5e8ab8249d1fa1a4a5aae20fd4399efdbecb9517
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-06-10 21:39:40 -0500

    Actualizar info-estado.js

--- plugins/info-estado.js
+++ plugins/info-estado.js
@@ -1,4 +1,4 @@
-/*const handler = async (m, {conn}) => {
+const handler = async (m, {conn}) => {
     const _muptime = process.uptime() * 1000
     const muptime = clockString(_muptime)
     const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
@@ -23,4 +23,4 @@ function clockString(ms) {
 let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
 let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
 let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')}*/
+return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')}
