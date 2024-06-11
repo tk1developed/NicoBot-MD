5dd20696caa63f442d568e1c6a9cf117e9c4d197
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-06-11 11:42:03 -0500

    Actualizar owner-reporte.js

--- plugins/owner-reporte.js
+++ plugins/owner-reporte.js
@@ -3,7 +3,10 @@ const handler = async (m, {conn, text, usedPrefix, command}) => {
   if (text.length < 10) throw `🛑 *Reporte Minimo De 10 Caracteres*`;
   if (text.length > 1000) throw `🛑 *Reporte Maxímo De 1000 Caracteres*`;
   const teks = `╭━〔  𝐑𝐄𝐏𝐎𝐑𝐓𝐄 ⚙️  〕⬣\n┃🍄 *Cliente:* \n┃✏️ Wa.me/${m.sender.split`@`[0]}\n┃\n┃📮 *Mensaje:*\n┃📩 ${text}\n╰━━━━━━━━━━━━⬣`;
-  conn.reply('573012482597@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, null, {contextInfo: {mentionedJid: [m.sender]}});
+
+  conn.reply('573012482597@s.whatsapp.net', teks, m, fake, );
+
+conn.reply('120363289173968879@g.us', teks, m, fake, );
 
   m.reply(`✅️ *El Reporte Se Envió A Mi Creador, Tendrá Una Respuesta Más Tarde*`);
 };
