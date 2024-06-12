0d4e5965aae2011f2120ab0a7ccddf2ec4ba2d84
tk1developed <tk1hax@gmail.com>
2024-06-12 20:33:11 -0300

    Actualizar adult-comandos+18.js

--- plugins/adult-comandos+18.js
+++ plugins/adult-comandos+18.js
@@ -4,87 +4,87 @@ const handler = async (m, {command, conn}) => {
   if (!db.data.chats[m.chat].modohorny && m.isGroup) throw '*📛 Comando Desactivado, Si Eres Administrador Use: !enable modohorny*';
 
   if (command == 'nsfwloli') {
-    const res = (await axios.get(`https://raw.githubusercontent.com/Diego-YL-177/YoshikoBot-MD/master/src/JSON/nsfwloli.json`)).data;
+    const res = (await axios.get(`https://raw.githubusercontent.com/tk1developed/NicoBot-MD/master/src/JSON/nsfwloli.json`)).data;
     const haha = await res[Math.floor(res.length * Math.random())];
     conn.sendMessage(m.chat, {image: {url: haha}, caption: `_${command}_`.trim()}, {quoted: m});
   }
 
   if (command == 'nsfwfoot') {
-    const res = (await axios.get(`https://raw.githubusercontent.com/Diego-YL-177/YoshikoBot-MD/master/src/JSON/nsfwfoot.json`)).data;
+    const res = (await axios.get(`https://raw.githubusercontent.com/tk1developed/NicoBot-MD/master/src/JSON/nsfwfoot.json`)).data;
     const haha = await res[Math.floor(res.length * Math.random())];
     conn.sendMessage(m.chat, {image: {url: haha}, caption: `_${command}_`.trim()}, {quoted: m});
   }
 
   if (command == 'nsfwass') {
-    const res = (await axios.get(`https://raw.githubusercontent.com/Diego-YL-177/YoshikoBot-MD/master/src/JSON/nsfwass.json`)).data;
+    const res = (await axios.get(`https://raw.githubusercontent.com/tk1developed/NicoBot-MD/master/src/JSON/nsfwass.json`)).data;
     const haha = await res[Math.floor(res.length * Math.random())];
     conn.sendMessage(m.chat, {image: {url: haha}, caption: `_${command}_`.trim()}, {quoted: m});
   }
 
   if (command == 'nsfwbdsm') {
-    const res = (await axios.get(`https://raw.githubusercontent.com/Diego-YL-177/YoshikoBot-MD/master/src/JSON/nsfwbdsm.json`)).data;
+    const res = (await axios.get(`https://raw.githubusercontent.com/tk1developed/NicoBot-MD/master/src/JSON/nsfwbdsm.json`)).data;
     const haha = await res[Math.floor(res.length * Math.random())];
     conn.sendMessage(m.chat, {image: {url: haha}, caption: `_${command}_`.trim()}, {quoted: m});
   }
 
   if (command == 'nsfwcum') {
-    const res = (await axios.get(`https://raw.githubusercontent.com/Diego-YL-177/YoshikoBot-MD/master/src/JSON/nsfwcum.json`)).data;
+    const res = (await axios.get(`https://raw.githubusercontent.com/tk1developed/NicoBot-MD/master/src/JSON/nsfwcum.json`)).data;
     const haha = await res[Math.floor(res.length * Math.random())];
     conn.sendMessage(m.chat, {image: {url: haha}, caption: `_${command}_`.trim()}, {quoted: m});
   }
 
   if (command == 'nsfwero') {
-    const res = (await axios.get(`https://raw.githubusercontent.com/Diego-YL-177/YoshikoBot-MD/master/src/JSON/nsfwero.json`)).data;
+    const res = (await axios.get(`https://raw.githubusercontent.com/tk1developed/NicoBot-MD/master/src/JSON/nsfwero.json`)).data;
     const haha = await res[Math.floor(res.length * Math.random())];
     conn.sendMessage(m.chat, {image: {url: haha}, caption: `_${command}_`.trim()}, {quoted: m});
   }
 
   if (command == 'nsfwfemdom') {
-    const res = (await axios.get(`https://raw.githubusercontent.com/Diego-YL-177/YoshikoBot-MD/master/src/JSON/nsfwfemdom.json`)).data;
+    const res = (await axios.get(`https://raw.githubusercontent.com/tk1developed/NicoBot-MD/master/src/JSON/nsfwfemdom.json`)).data;
     const haha = await res[Math.floor(res.length * Math.random())];
     conn.sendMessage(m.chat, {image: {url: haha}, caption: `_${command}_`.trim()}, {quoted: m});
   }
 
   if (command == 'nsfwglass') {
-    const res = (await axios.get(`https://raw.githubusercontent.com/Diego-YL-177/YoshikoBot-MD/master/src/JSON/nsfwglass.json`)).data;
+    const res = (await axios.get(`https://raw.githubusercontent.com/tk1developed/NicoBot-MD/master/src/JSON/nsfwglass.json`)).data;
     const haha = await res[Math.floor(res.length * Math.random())];
     conn.sendMessage(m.chat, {image: {url: haha}, caption: `_${command}_`.trim()}, {quoted: m});
   }
 
   if (command == 'hentai') {
-    const res = (await axios.get(`https://raw.githubusercontent.com/Diego-YL-177/YoshikoBot-MD/master/src/JSON/hentai.json`)).data;
+    const res = (await axios.get(`https://raw.githubusercontent.com/tk1developed/NicoBot-MD/master/src/JSON/hentai.json`)).data;
     const haha = await res[Math.floor(res.length * Math.random())];
     conn.sendMessage(m.chat, {image: {url: haha}, caption: `_${command}_`.trim()}, {quoted: m});
   }
 
   if (command == 'nsfworgy') {
-    const res = (await axios.get(`https://raw.githubusercontent.com/Diego-YL-177/YoshikoBot-MD/master/src/JSON/nsfworgy.json`)).data;
+    const res = (await axios.get(`https://raw.githubusercontent.com/tk1developed/NicoBot-MD/master/src/JSON/nsfworgy.json`)).data;
     const haha = await res[Math.floor(res.length * Math.random())];
     conn.sendMessage(m.chat, {image: {url: haha}, caption: `_${command}_`.trim()}, {quoted: m});
   }
 
   if (command == 'tetas') {
-    const resError = (await axios.get(`https://raw.githubusercontent.com/Diego-YL-177/YoshikoBot-MD/master/src/JSON/tetas.json`)).data;
+    const resError = (await axios.get(`https://raw.githubusercontent.com/tk1developed/NicoBot-MD/master/src/JSON/tetas.json`)).data;
     let res = await conn.getFile(`https://api-fgmods.ddns.net/api/nsfw/boobs?apikey=fg-dylux`).data;
     if (res == '' || !res || res == null) res = await resError[Math.floor(resError.length * Math.random())];
     conn.sendMessage(m.chat, {image: {url: res}, caption: `_${command}_`.trim()}, {quoted: m});
   }
 
   if (command == 'booty') {
-    const resError = (await axios.get(`https://raw.githubusercontent.com/Diego-YL-177/YoshikoBot-MD/master/src/JSON/booty.json`)).data;
+    const resError = (await axios.get(`https://raw.githubusercontent.com/tk1developed/NicoBot-MD/master/src/JSON/booty.json`)).data;
     let res = await conn.getFile(`https://api-fgmods.ddns.net/api/nsfw/ass?apikey=fg-dylux`).data;
     if (res == '' || !res || res == null) res = await resError[Math.floor(resError.length * Math.random())];
     conn.sendMessage(m.chat, {image: {url: res}, caption: `_${command}_`.trim()}, {quoted: m});
   }
 
   if (command == 'ecchi') {
-    const res = (await axios.get(`https://raw.githubusercontent.com/Diego-YL-177/YoshikoBot-MD/master/src/JSON/ecchi.json`)).data;
+    const res = (await axios.get(`https://raw.githubusercontent.com/tk1developed/NicoBot-MD/master/src/JSON/ecchi.json`)).data;
     const url = await res[Math.floor(res.length * Math.random())];
     conn.sendMessage(m.chat, {image: {url: url}, caption: `_${command}_`.trim()}, {quoted: m});
   }
 
   if (command == 'furro') {
-    const res = (await axios.get(`https://raw.githubusercontent.com/Diego-YL-177/YoshikoBot-MD/master/src/JSON/furro.json`)).data;
+    const res = (await axios.get(`https://raw.githubusercontent.com/tk1developed/NicoBot-MD/master/src/JSON/furro.json`)).data;
     const url = await res[Math.floor(res.length * Math.random())];
     conn.sendMessage(m.chat, {image: {url: url}, caption: `_${command}_`.trim()}, {quoted: m});
   }
@@ -97,33 +97,33 @@ const handler = async (m, {command, conn}) => {
   }
 
   if (command == 'imagenlesbians') {
-    const resError = (await axios.get(`https://raw.githubusercontent.com/Diego-YL-177/YoshikoBot-MD/master/src/JSON/imagenlesbians.json`)).data;
+    const resError = (await axios.get(`https://raw.githubusercontent.com/tk1developed/NicoBot-MD/master/src/JSON/imagenlesbians.json`)).data;
     let res = await conn.getFile(`https://api-fgmods.ddns.net/api/nsfw/lesbian?apikey=fg-dylux`).data;
     if (res == '' || !res || res == null) res = await resError[Math.floor(resError.length * Math.random())];
     conn.sendMessage(m.chat, {image: {url: res}, caption: `_${command}_`.trim()}, {quoted: m});
   }
 
   if (command == 'panties') {
-    const res = (await axios.get(`https://raw.githubusercontent.com/Diego-YL-177/YoshikoBot-MD/master/src/JSON/panties.json`)).data;
+    const res = (await axios.get(`https://raw.githubusercontent.com/tk1developed/NicoBot-MD/master/src/JSON/panties.json`)).data;
     const url = await res[Math.floor(res.length * Math.random())];
     conn.sendMessage(m.chat, {image: {url: url}, caption: `_${command}_`.trim()}, {quoted: m});
   }
 
   if (command == 'pene') {
-    const resError = (await axios.get(`https://raw.githubusercontent.com/Diego-YL-177/YoshikoBot-MD/master/src/JSON/pene.json`)).data;
+    const resError = (await axios.get(`https://raw.githubusercontent.com/tk1developed/NicoBot-MD/master/src/JSON/pene.json`)).data;
     let res = await conn.getFile(`https://api-fgmods.ddns.net/api/nsfw/penis?apikey=fg-dylux`).data;
     if (res == '' || !res || res == null) res = await resError[Math.floor(resError.length * Math.random())];
     conn.sendMessage(m.chat, {image: {url: res}, caption: `_${command}_`.trim()}, {quoted: m});
   }
 
   if (command == 'porno') {
-    const res = (await axios.get(`https://raw.githubusercontent.com/Diego-YL-177/YoshikoBot-MD/master/src/JSON/porno.json`)).data;
+    const res = (await axios.get(`https://raw.githubusercontent.com/tk1developed/NicoBot-MD/master/src/JSON/porno.json`)).data;
     const url = await res[Math.floor(res.length * Math.random())];
     conn.sendMessage(m.chat, {image: {url: url}, caption: `_${command}_`.trim()}, {quoted: m});
   }
 
   if (command == 'randomxxx') {
-    const rawjsonn = ['https://raw.githubusercontent.com/Diego-YL-177/YoshikoBot-MD/master/src/JSON/tetas.json', 'https://raw.githubusercontent.com/Diego-YL-177/YoshikoBot-MD/master/src/JSON/booty.json', 'https://raw.githubusercontent.com/Diego-YL-177/YoshikoBot-MD/master/src/JSON/imagenlesbians.json', 'https://raw.githubusercontent.com/Diego-YL-177/YoshikoBot-MD/master/src/JSON/panties.json', 'https://raw.githubusercontent.com/Diego-YL-177/YoshikoBot-MD/master/src/JSON/porno.json'];
+    const rawjsonn = ['https://raw.githubusercontent.com/tk1developed/NicoBot-MD/master/src/JSON/tetas.json', 'https://raw.githubusercontent.com/tk1developed/NicoBot-MD/master/src/JSON/booty.json', 'https://raw.githubusercontent.com/tk1developed/NicoBot-MD/master/src/JSON/imagenlesbians.json', 'https://raw.githubusercontent.com/tk1developed/NicoBot-MD/master/src/JSON/panties.json', 'https://raw.githubusercontent.com/tk1developed/NicoBot-MD/master/src/JSON/porno.json'];
     const rawjson = await rawjsonn[Math.floor(rawjsonn.length * Math.random())];
     const res = (await axios.get(rawjson)).data;
     const url = await res[Math.floor(res.length * Math.random())];
@@ -131,7 +131,7 @@ const handler = async (m, {command, conn}) => {
   }
 
   if (command == 'pechos') {
-    const res = (await axios.get(`https://raw.githubusercontent.com/Diego-YL-177/YoshikoBot-MD/master/src/JSON/pechos.json`)).data;
+    const res = (await axios.get(`https://raw.githubusercontent.com/tk1developed/NicoBot-MD/master/src/JSON/pechos.json`)).data;
     const url = await res[Math.floor(res.length * Math.random())];
     conn.sendMessage(m.chat, {image: {url: url}, caption: `_${command}_`.trim()}, {quoted: m});
   }
@@ -151,13 +151,13 @@ const handler = async (m, {command, conn}) => {
   }
 
   if (command == 'yuri') {
-    const res = (await axios.get(`https://raw.githubusercontent.com/Diego-YL-177/YoshikoBot-MD/master/src/JSON/yuri.json`)).data;
+    const res = (await axios.get(`https://raw.githubusercontent.com/tk1developed/NicoBot-MD/master/src/JSON/yuri.json`)).data;
     const url = await res[Math.floor(res.length * Math.random())];
     conn.sendMessage(m.chat, {image: {url: url}, caption: `_${command}_`.trim()}, {quoted: m});
   }
 
   if (command == 'yuri2') {
-    const resError = (await axios.get(`https://raw.githubusercontent.com/Diego-YL-177/YoshikoBot-MD/master/src/JSON/yuri.json`)).data;
+    const resError = (await axios.get(`https://raw.githubusercontent.com/tk1developed/NicoBot-MD/master/src/JSON/yuri.json`)).data;
     const res = await fetch(`https://purrbot.site/api/img/nsfw/yuri/gif`);
     const json = await res.json();
     let url = json.link;
