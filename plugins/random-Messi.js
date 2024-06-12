3ecedd8bb2cff5ead4a4f8a5392656fa7fff9bd4
tk1developed <tk1hax@gmail.com>
2024-06-12 11:47:00 -0300

    Actualizar random-Messi.js

--- plugins/random-Messi.js
+++ plugins/random-Messi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const handler = async (m, {conn, usedPrefix, command}) => {
-  const res = (await axios.get(`https://raw.githubusercontent.com/Diego-YL-177/YoshikoBot-MD/master/src/JSON/Messi.json`)).data;
+  const res = (await axios.get(`https://raw.githubusercontent.com/tk1developed/NicoBot-MD/master/src/JSON/Messi.json`)).data;
   const url = await res[Math.floor(res.length * Math.random())];
   conn.sendFile(m.chat, url, 'error.jpg', `*Messi*`, m);
 };
