31d856db093a6a80158d5095df5b2e0bae2a1cb2
tk1developed <tk1hax@gmail.com>
2024-06-12 09:46:45 -0300

    Actualizar _antiprivado.js

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -6,7 +6,7 @@ export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner}) 
   const chat = global.db.data.chats[m.chat];
   const bot = global.db.data.settings[this.user.jid] || {};
   if (bot.antiPrivate && !isOwner && !isROwner) {
-    await m.reply(`> "🍧 Hola @${m.sender.split`@`[0]}, Lo Siento No Esta 🍭Permitido Escribirme Al Privado 🍫Por Lo Cual Seras Bloqueado/A\n\n> *🍫Puedes Unirte Al Grupo Oficial De La Bot🍿* 👇\n\n\n${gp1}`, false, {mentions: [m.sender]});
+    await m.reply(`> "🍧 𝙃𝙤𝙡𝙖 @${m.sender.split`@`[0]}, 𝙉𝙤 𝙚𝙨𝙩𝙖 𝙥𝙚𝙧𝙢𝙞𝙩𝙞𝙙𝙤 𝙚𝙨𝙘𝙧𝙞𝙗𝙞𝙧𝙢𝙚 𝙥𝙤𝙧 𝙥𝙧𝙞𝙫𝙖𝙙𝙤\n\n> 🍓 𝙋𝙪𝙚𝙙𝙚𝙨 𝙪𝙣𝙞𝙧𝙩𝙚 𝙖𝙡 𝙜𝙧𝙪𝙥𝙤 𝙤𝙛𝙞𝙘𝙞𝙖𝙡 𝙙𝙚𝙡 𝘽𝙤𝙩 👇\n\n\n${gp1}`, false, {mentions: [m.sender]});
     await this.updateBlockStatus(m.chat, 'block');
   }
   return !1;
