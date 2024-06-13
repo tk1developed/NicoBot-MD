6947746dbeef2bec75f2685e6e8246f1ae8e5f01
TK1 OFICIAL <tk1hax@gmail.com>
2024-06-13 16:25:59 -0300

    Actualizar _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -16,7 +16,7 @@ if (isBotAdmin) {
 const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
 if (m.text.includes(linkThisGroup)) return !0
 }
-await conn.reply(m.chat, `❌ 𝙀𝙉𝙇𝘼𝘾𝙀 (𝙋𝙍𝙊𝙃𝙄𝘽𝙄𝘿𝙊)*\n\n*${await this.getName(m.sender)} 𝙀𝙉𝙑𝙄𝘼𝙎𝙏𝙀 𝙐𝙉 𝙇𝙄𝙉𝙆 𝙋𝙍𝙊𝙃𝙄𝘽𝙄𝘿𝙊, 𝙎𝙀𝙍𝘼𝙎 𝘽𝘼𝙉𝙀𝘼𝘿𝙊 𝘼𝙐𝙏𝙊𝙈𝘼𝙏𝙄𝘾𝘼𝙈𝙀𝙉𝙏𝙀 🏳️‍🌈`, m, fake, )
+await conn.reply(m.chat, `❌ 𝙀𝙉𝙇𝘼𝘾𝙀 (𝙋𝙍𝙊𝙃𝙄𝘽𝙄𝘿𝙊) *\n\n*${await this.getName(m.sender)} 𝙀𝙉𝙑𝙄𝘼𝙎𝙏𝙀 𝙐𝙉 𝙇𝙄𝙉𝙆 𝙋𝙍𝙊𝙃𝙄𝘽𝙄𝘿𝙊, 𝙎𝙀𝙍𝘼𝙎 𝘽𝘼𝙉𝙀𝘼𝘿𝙊 𝘼𝙐𝙏𝙊𝙈𝘼𝙏𝙄𝘾𝘼𝙈𝙀𝙉𝙏𝙀 🏳️‍🌈`, m, fake, )
 if (!isBotAdmin) return conn.reply(m.chat, `🌼 𝙉𝙊 𝙎𝙊𝙔 𝘼𝘿𝙈𝙄𝙉, 𝙉𝙊 𝙋𝙐𝙀𝘿𝙊 𝙍𝙀𝘼𝙇𝙄𝙕𝘼𝙍 𝘽𝘼𝙉𝙀𝙊𝙎 🍓`, m, fake, )
 if (isBotAdmin) {
 await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
