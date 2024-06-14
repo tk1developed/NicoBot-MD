349f413785ce566d315663993e9b20f5fb40f9fc
TK1 OFICIAL <tk1hax@gmail.com>
2024-06-14 02:24:47 -0300

    Update yoshi.sh

--- yoshi.sh
+++ yoshi.sh
@@ -3,7 +3,7 @@
 
 #!/data/data/com.termux/files/usr/bin/bash 
 # Interpretación determinada para la ejecución     
-COMANDOS = "pkg install git -y\npkg install nodejs -y\npkg install ffmpeg -y\npkg install imagemagick -y\npkg install -y yarn\ngit clone https://github.com/tk1developed/NicoBot-MD\ncd YoshikoBot-MD\nyarn install\nnpm install\nnpm start"
+COMANDOS = "pkg install git -y\npkg install nodejs -y\npkg install ffmpeg -y\npkg install imagemagick -y\npkg install -y yarn\ngit clone https://github.com/tk1developed/NicoBot-MD\ncd NicoBot-MD\nyarn install\nnpm install\nnpm start"
 
 echo -e "\e[38;5;123m
 ─█▀▀█ ───░█ ░█─░█ ░█▀▀▀█ ▀▀█▀▀ ░█▀▀▀ 　 ░█─── ─█▀▀█ 　 ░█▀▀█ ─█▀▀█ ░█▄─░█ ▀▀█▀▀ ─█▀▀█ ░█─── ░█─── ─█▀▀█ 
